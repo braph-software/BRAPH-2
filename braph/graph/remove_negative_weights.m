9afdb6b94c6e2e9f6352d153af176892d1327280
egolol <egolol87@gmail.com>
2020-02-05 09:49:33 +0100

    Issue 1 & 2 (#3)
    * Issue 1 & 2
    
    * Small adjustment to max function

--- braph/graph/remove_negative_weights.m
+++ braph/graph/remove_negative_weights.m
@@ -1,3 +1,20 @@
 function B = remove_negative_weights(A, rule)
 
-B = A;
+%the rule is going to get pass as a string? int?
+
+    switch (rule)
+        case '0'
+            B = max(A,0); %  change negative values to zeros
+
+        case 'zero'
+            B = max(A,0); %  change negative values to zeros
+            
+        case 'null'
+            A(isnan(A))=0 ; %  change NaN values to zeros
+            B = A;
+            
+        otherwise
+            B = abs(A); %  want the absolute value of every element in the matrix A
+    end
+
+end
