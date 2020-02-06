31b1cd4cbaff4380f59c82233ecf7fec2939f4a1
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2020-02-06 17:24:00 +0100

    Issue 1 & 2 (#3) (#4)
    * Issue 1 & 2
    
    * Small adjustment to max function
    
    Co-authored-by: egolol <egolol87@gmail.com>

---
+++ braph/graph/GraphBD.m
@@ -0,0 +1,31 @@
+classdef GraphBD < Graph
+    methods
+        function g = GraphBD(A, varargin)
+
+            A = removediagonal(A); % removes self-connections by removing diagonal from adjacency matrix
+            A = remove_negative_weights(A, varargin{:});  % removes negative weights
+
+            g = g@Graph(A, varargin{:});
+        end
+    end
+    methods (Static)
+        function bool = is_selfconnected()
+            bool = false;
+        end        
+        function bool = is_nonnegative()
+            bool = true;
+        end        
+        function bool = is_weighted()
+            bool = false;
+        end
+        function bool = is_binary()
+            bool = true;
+        end
+        function bool = is_directed()
+            bool = true;
+        end
+        function bool = is_undirected()
+            bool = false;
+        end        
+    end
+end
