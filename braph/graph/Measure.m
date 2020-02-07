660f33af64d22ca90a02e93da0c5ff528f195373
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-02-07 16:25:28 +0100

    Obj Degree + Measure checks

--- braph/graph/Measure.m
+++ braph/graph/Measure.m
@@ -3,10 +3,15 @@ classdef Measure < handle & matlab.mixin.Copyable
         g  % graph
         value  % graph measure value
                % scalar for global measures
-               % vector for nodal measures
+               % column vector for nodal measures
     end
     methods (Access=protected)
         function m = Measure(g, varargin)
+            
+            if ~m.is_compatible_graph(g)
+                error([class(g) ' is not compatible with ' class(m)])
+            end
+            
             m.g = g;
         end
         function setValue(m, value)
@@ -28,6 +33,11 @@ classdef Measure < handle & matlab.mixin.Copyable
     end
     methods (Abstract, Access=protected)
         calculate(m)  % calculates the value of the measure
+    end
+    methods (Static)
+        function bool = is_nodal()
+            bool = ~Measure.is_global();
+        end
     end
     methods (Static, Abstract)
         is_global()  % whether is global measure
