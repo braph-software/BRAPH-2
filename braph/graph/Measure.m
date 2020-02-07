f2b11afb865ccbe1a389cd121f3d4bae2c1c430c
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-02-07 08:29:05 +0100

    Added dir as argument to subclasses

--- braph/graph/Measure.m
+++ braph/graph/Measure.m
@@ -11,16 +11,16 @@ classdef Measure < handle & matlab.mixin.Copyable
         end
     end
     methods
-        function getGraph(m)
-            return m.g;
+        function g = getGraph(m)
+            g = m.g;
         end
-        function getValue(m)
+        function value = getValue(m)
             
             if isempty(m.value)
                 m.calculate();
             end
             
-           return m.value;
+            value = m.value;
         end
     end
     methods (Abstract, Access=protected)
