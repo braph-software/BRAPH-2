d732ec02558f6c23c4afb34a957c1a8a9932f8e3
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-02-10 19:36:35 +0100

    Update test_remove_diagonal.m

--- braph/graph/test_remove_diagonal.m
+++ braph/graph/test_remove_diagonal.m
@@ -1,15 +1,29 @@
 % test remove_diagonal
-A = [1 1 1; 1 1 1; 1 1 1];
+A = [
+    1 1 1; 
+    1 1 1; 
+    1 1 1
+    ];
 
 %% Test 1: Remove diagonal
 B = remove_diagonal(A);
-assert(isequal(B, [0 1 1; 1 0 1; 1 1 0]), ...
+assert(isequal(B, ...
+    [
+    0 1 1; 
+    1 0 1; 
+    1 1 0
+    ]), ...
     'BRAPH:remove_diagonal:Bug', ...
     'Diagonal not correctly set to 0')
 
 %% Test 2: Remove diagonal and set value
 value = randn();
 B = remove_diagonal(A, value);
-assert(isequal(B, [value 1 1; 1 value 1; 1 1 value]), ...
+assert(isequal(B, ...
+    [
+    value 1 1; 
+    1 value 1; 
+    1 1 value
+    ]), ...
     'BRAPH:remove_diagonal:Bug', ...
     'Diagonal not correctly set to value')
