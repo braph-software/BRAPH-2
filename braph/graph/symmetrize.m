516d7fe86c47e52435fd33ad23321d6e44a53e18
egolol <egolol87@gmail.com>
2020-02-10 18:49:12 +0100

    Issue 5. Comments for functions: remove diagonal, remove negative wei… (#10)
    * Issue 5. Comments for functions: remove diagonal, remove negative weights, symmetrize.
    
    * Update remove_diagonal.m
    
    * Update to comments
    
    Added Authors, see also functions, and inverted order of functions from single argument to two arguments
    
    * Issue 5
    
    New Revision. Changed signature in function arguments. Added separation inside comments
    
    * Update to comments. Remove leading space and added ' to remove negative weights
    
    Co-authored-by: LauraPerezG <47359519+LauraPerezG@users.noreply.github.com>
    Co-authored-by: Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>

--- braph/graph/symmetrize.m
+++ braph/graph/symmetrize.m
@@ -1,6 +1,29 @@
 function B = symmetrize(A, varargin)
-
-    % 'SymmetrizeRule' (input from varargin)
+    % SYMMETRIZE symmetrizes a matrix
+    %
+    % B = SYMETRIZE(A) symmetrizes the matrix A with default RULE 'max'.s
+    %
+    % B = SYMMETRIZE(A, 'SymmetryzeRule', RULE) symmetrizes the matrix A by the
+    % Symmetrize RULE specified by the PropertyName.
+    % Admissible RULE options are:
+    %       RULE    -   'max' (default) | 'sum' | 'add' | 'av' | 'average' |
+    %                   'min' | 'minimum' | 'or' | 'weak'
+    %                   'max' - maximum between inconnection and outconnection (default)
+    %                   'sum' - sum of inconnection and outconnection
+    %                   'add' - sum of inconnection and outconnection
+    %                   'av' - average of inconnection and outconnection
+    %                   'average' - average of inconnection and
+    %                               outconnnection
+    %                   'min' - minimum between inconnection and outconnection
+    %                   'minimum' - minimum between inconnection and outconnection
+    %                   'or' - minimum between inconnection and outconnection
+    %                   'weak' - minimum between inconnection and outconnection
+    % 
+    % See also REMOVE_DIAGONAL, REMOVE_NEGATIVE_WEIGHTS
+    
+    % Author: Emiliano Gomez & Giovanni Volpe
+    % Date: 2020/02/07
+    
     symmetrize_rule = 'max';
     for n = 1:1:length(varargin)-1
         if strcmpi(varargin{n}, 'SymmetrizeRule')
@@ -15,7 +38,7 @@ function B = symmetrize(A, varargin)
             B = (A + transpose(A)) / 2;
         case {'min', 'minimum', 'or', 'weak'}  % minimum rule
             B = min(A, transpose(A));
-        otherwise  % {'max', 'maximum', 'and', 'strong'} % maximum rule
+        otherwise  % {'max', 'maximum', 'and', 'strong'}  % maximum rule
             B = max(A, transpose(A));
     end
 end
