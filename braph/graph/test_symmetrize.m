a5cbc0a25323a96af7698dee1734c8249de9ec95
egolol <egolol87@gmail.com>
2020-02-10 23:36:09 +0100

    Issue 23 (#29)
    * Issue 23
    
    #23 Created a unit test for symmetrize
    
    * Issue 23
    
    #23 Added more test cases, and changed message.

---
+++ braph/graph/test_symmetrize.m
@@ -0,0 +1,105 @@
+% test symmetrize
+A = [
+    1 2 3; 
+    2 3 1; 
+    3 2 1
+    ];
+
+%% Test 1: Symmetrize Max Default
+B = symmetrize(A);
+assert(isequal(B,...
+    [
+    1 2 3;
+    2 3 2;
+    3 2 1;
+    ]),...
+    'BRAPH:symmetrize:Bug',...
+    'Symmetrize max (default) case not working correctly')
+
+%% Test 2: Symmetrize Add
+B = symmetrize(A, 'SymmetrizeRule', 'add');
+assert(isequal(B,...
+    [
+    2 4 6;
+    4 6 3;
+    6 3 2;
+    ]),...
+    'BRAPH:symmetrize:Bug',...
+    'Symmetrize add case not working correctly')
+
+%% Test 3: Symmetrize Sum
+B = symmetrize(A, 'SymmetrizeRule', 'sum');
+assert(isequal(B,...
+    [
+    2 4 6;
+    4 6 3;
+    6 3 2;
+    ]),...
+    'BRAPH:symmetrize:Bug',...
+    'Symmetrize sum case not working correctly')
+
+%% Test 4: Symmetrize Average
+B = symmetrize(A, 'SymmetrizeRule', 'average');
+assert(isequal(B,...
+    [
+    1.000000000000000 2.000000000000000 3.000000000000000;
+    2.000000000000000 3.000000000000000 1.500000000000000;
+    3.000000000000000 1.500000000000000 1.000000000000000;
+    ]),...
+    'BRAPH:symmetrize:Bug',...
+    'Symmetrize average case not working correctly')
+
+%% Test 5: Symmetrize Av
+B = symmetrize(A, 'SymmetrizeRule', 'av');
+assert(isequal(B,...
+    [
+    1.000000000000000 2.000000000000000 3.000000000000000;
+    2.000000000000000 3.000000000000000 1.500000000000000;
+    3.000000000000000 1.500000000000000 1.000000000000000;
+    ]),...
+    'BRAPH:symmetrize:Bug',...
+    'Symmetrize av case not working correctly')
+
+%% Test 6: Symmetrize Minimum
+B = symmetrize(A, 'SymmetrizeRule', 'minimum');
+assert(isequal(B,...
+    [
+    1 2 3;
+    2 3 1;
+    3 1 1;
+    ]),...
+    'BRAPH:symmetrize:Bug',...
+    'Symmetrize minimum case not working correctly')
+
+%% Test 7: Symmetrize Min
+B = symmetrize(A, 'SymmetrizeRule', 'min');
+assert(isequal(B,...
+    [
+    1 2 3;
+    2 3 1;
+    3 1 1;
+    ]),...
+    'BRAPH:symmetrize:Bug',...
+    'Symmetrize min case not working correctly')
+
+%% Test 8: Symmetrize Or
+B = symmetrize(A, 'SymmetrizeRule', 'or');
+assert(isequal(B,...
+    [
+    1 2 3;
+    2 3 1;
+    3 1 1;
+    ]),...
+    'BRAPH:symmetrize:Bug',...
+    'Symmetrize or case not working correctly')
+
+%% Test 9: Symmetrize Weak
+B = symmetrize(A, 'SymmetrizeRule', 'weak');
+assert(isequal(B,...
+    [
+    1 2 3;
+    2 3 1;
+    3 1 1;
+    ]),...
+    'BRAPH:symmetrize:Bug',...
+    'Symmetrize weak case not working correctly')
