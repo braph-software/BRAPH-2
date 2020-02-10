9fc6f586f3448037bcf74bbf5a61e7be72504e4d
egolol <egolol87@gmail.com>
2020-02-10 19:10:35 +0100

    Issue #6 Indegree and Outdegree plus a small change to Degree and Mea… (#11)
    * Issue #6 Indegree and Outdegree plus a small change to Degree and Measure.
    
    * Issue 6
    
    Renamed files and change to indegree
    degree = sum(A, 1); -> degree = sum(A, 1)';
    
    * Issue 6 commit change to is_nodal()
    
    * Issue 6
    
    changed name of variables for clarity.
    
    Co-authored-by: LauraPerezG <47359519+LauraPerezG@users.noreply.github.com>
    Co-authored-by: Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>

---
+++ braph/graph/OutDegree.m
@@ -0,0 +1,32 @@
+classdef OutDegree < Measure
+    methods
+        function m = OutDegree(g, varargin)
+            m = m@Measure(g, varargin);
+        end
+    end
+    methods(Access = protected)
+        function calculate(m)
+            g = m.getGraph();
+            A = g.getA();
+            out_degree = sum(A, 2); % row sum of A
+            m.setValue(out_degree);
+        end
+    end
+    methods (Static)
+        function bool = is_global()
+            bool = false;
+        end
+        function bool = is_nodal()
+            bool = true;
+        end
+        function bool = is_compatible_graph(g)
+            bool = g.is_directed();
+        end
+        function list = compatible_graph_list()
+            list = { ...
+                'GraphBD', ...
+                'GraphWD' ...
+                };
+        end
+    end
+end
