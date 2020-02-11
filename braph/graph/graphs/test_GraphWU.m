67a14f883f5bffdacb5d35ea61ca135fdaaf7ecb
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2020-02-11 20:47:40 +0100

    Update test_GraphWU.m

--- braph/graph/graphs/test_GraphWU.m
+++ braph/graph/graphs/test_GraphWU.m
@@ -1,13 +1,11 @@
 % test GraphWU
-
 A = rand(randi(10));
 
 %% Test 1: GraphWU instantiate well
-
 g = GraphWU(A);
 
-A = remove_diagonal(A);
-A = remove_negative_weights(A);
+A = dediagonalize(A);
+A = semipositivize(A);
 A = symmetrize(A);    
 
 assert(isequal(g.getA(), A), ...
