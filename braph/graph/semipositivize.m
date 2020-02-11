2f3449b298edad4368df30f9b31fb10aab609477
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2020-02-11 20:58:12 +0100

    Update semipositivize.m

--- braph/graph/semipositivize.m
+++ braph/graph/semipositivize.m
@@ -1,11 +1,11 @@
 function B = semipositivize(A, varargin)
 % SEMIPOSITIVIZE removes the negative values of a matrix
 %
-% B = SEMIPOSITIVIZE(A) removes the negative weights of a
-% matrix A using default RULE 'zero'.
+% B = SEMIPOSITIVIZE(A) removes the negative weights of the matrix A 
+% using default RULE 'zero'.
 %
 % B = SEMIPOSITIVIZE(A, 'SemipositivizeRule', RULE) 
-% removes the negative weights of a matrix A using the RULE specified.
+% removes the negative weights of a matrix A using the specified RULE.
 % Admissible RULE options are:
 %       RULE    -   'zero' (default) | 'abs' | 'absolute' | 'modulus' 
 %                   'zero' - convert negative values to zeros.
@@ -21,12 +21,7 @@ function B = semipositivize(A, varargin)
 % Author: Emiliano Gomez & Giovanni Volpe
 % Date: 2020/02/07
 
-semipositivize_rule = 'zero';
-for n = 1:1:length(varargin)-1
-    if strcmpi(varargin{n}, 'SemipositivizeRule')
-        semipositivize_rule = varargin{n+1};
-    end
-end
+semipositivize_rule = get_from_varargin('zero', 'SemipositivizeRule', varargin{:});
 
 switch lower(semipositivize_rule)
     case {'abs', 'absolute', 'modulus'}  % take-absolute-value rule
