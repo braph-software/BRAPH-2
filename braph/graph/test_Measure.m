b630a88183d6ae19a717954f1b2942708c87351c
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2020-02-11 12:22:18 +0100

    Update test_Measure.m (#38)

--- braph/graph/test_Measure.m
+++ braph/graph/test_Measure.m
@@ -8,3 +8,17 @@ for i = 1:1:length(measure_code_list)
         'BRAPH:Measure:NodalOrGlobal', ...
         [measure_code '.is_nodal() == ' measure_code '.is_global()'])
 end
+
+%% Test 2: Check all measures are not abstract
+for i = 1:1:length(measure_code_list)
+    measure_code = measure_code_list{i};
+    
+    graph_list = eval([measure_code '.compatible_graph_list()']);
+    
+    for j = 1:1:length(graph_list)
+        graph_code = graph_list{j};
+        A = rand(randi(10));
+        g = Graph.getGraph(graph_code, A);
+        m = Measure.getMeasure(measure_code, g);
+    end
+end
