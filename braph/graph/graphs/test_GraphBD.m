7a2854df593e7d4a41286160b664c4fc5b3bddab
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2020-02-11 20:45:07 +0100

    Update test_GraphBD.m

--- braph/graph/graphs/test_GraphBD.m
+++ braph/graph/graphs/test_GraphBD.m
@@ -1,13 +1,11 @@
 % test GraphBD
-
 A = rand(randi(10));
 
-%% Test 1: GraphBD instantiate well
-
+%% Test 1: GraphBD instantiates well
 g = GraphBD(A);
 
-A = remove_diagonal(A); 
-A = remove_negative_weights(A); 
+A = dediagonalize(A); 
+A = semipositivize(A); 
 A = binarize(A); 
 
 assert(isequal(g.getA(), A), ...
