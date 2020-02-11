45615f07cbf0f298e2a481f6b5b6d93f8a019da6
egolol <egolol87@gmail.com>
2020-02-11 13:36:04 +0100

    22 eg remove diagonal change (#35)
    * Issue 22 Update remove_diagonal with varargin
    
    #22 Update remove_diagonal with varargin. Update to comments
    
    * Update remove_diagonal.m
    
    small fix to comments

--- braph/graph/remove_diagonal.m
+++ braph/graph/remove_diagonal.m
@@ -1,10 +1,10 @@
-function B = remove_diagonal(A, value)
+function B = remove_diagonal(A, varargin)
 % REMOVE_DIAGONAL removes the diagonal values of a matrix
 %
 % B = REMOVE_DIAGONAL(A) removes the diagonal
 % values of a matrix A and sets them to 0.
 %
-% B = REMOVE_DIAGONAL(A, VALUE) removes the diagonal
+% B = REMOVE_DIAGONAL(A, 'RemoveDiagonalValue', VALUE) removes the diagonal
 % values of a matrix A and sets them to VALUE.
 %
 % See also SYMMETRIZE, REMOVE_NEGATIVE_WEIGHTS, BINARIZE.
@@ -12,10 +12,14 @@ function B = remove_diagonal(A, value)
 % Author: Emiliano Gomez & Giovanni Volpe
 % Date: 2020/02/07
 
-    if nargin < 2
-        value = 0;
+
+    remove_diagonal_value = 0;
+    for n = 1:1:length(varargin)-1
+        if strcmpi(varargin{n}, 'RemoveDiagonalValue')
+            remove_diagonal_value = varargin{n+1};
+        end
     end
 
     B = A;
-    B(1:length(A)+1:numel(A)) = value;
+    B(1:length(A)+1:numel(A)) = remove_diagonal_value;
 end
