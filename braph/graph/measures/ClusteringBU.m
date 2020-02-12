6b4db45e6e4717f6f489bfb9aac7bd0d6efcc367
Mite Mijalkov <mite.mijalkov@gmail.com>
2020-02-12 14:17:58 +0100

    Create ClusteringBU.m
    #59 Clustering BU
    
    Started to create the object but noticed the triangle needs to be created before.

---
+++ braph/graph/measures/ClusteringBU.m
@@ -0,0 +1,18 @@
+classdef ClusteringBU < Measure
+    methods
+       function m = ClusteringBU(g, varargin)
+            m = m@Measure(g, varargin{:});
+        end 
+    end
+    methods (Access=protected)
+       function calculate(m)
+           
+           
+           
+       end     
+    end
+    
+    
+    
+    
+end
