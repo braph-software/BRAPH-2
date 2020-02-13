8fd9c62d58f3af7ae32e87e99456d17fc93c0d7a
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2020-02-13 22:41:18 +0100

    Update test_InStrength.m

--- braph/graph/measures/test_InStrength.m
+++ braph/graph/measures/test_InStrength.m
@@ -12,27 +12,3 @@ in_strength = sum(A, 1)';
 assert(isequal(InStrength.getValue(), in_strength), ...
     'BRAPH:InStrength:Bug', ...
     'InStrength is not beeing calculated correctly for GraphWD')
-
-%% Test 2: InStrength get Class correctly
-g = GraphWD(A);
-InStrength = InStrength(g);
-
-assert(isequal(InStrength.getClass(), 'InStrength'), ...
-    'BRAPH:InStrength:Bug', ...
-    'InStrength is not getting class correctly')
-
-%% Test 3: InStrength get Name correctly
-g = GraphWD(A);
-InStrength = InStrength(g);
-
-assert(isequal(InStrength.getName(), 'In-Strength'), ...
-    'BRAPH:InStrength:Bug', ...
-    'InStrength is not getting class correctly')
-
-%% Test 4: InStrength get Description works
-g = GraphWD(A);
-InStrength = InStrength(g);
-
-assert(ischar(InStrength.getDescription()), ...
-    'BRAPH:InStrength:Bug', ...
-    'InStrength is not getting class correctly')
