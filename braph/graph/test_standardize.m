39f5c36565f460819a1a90ccec78631a8a8485a1
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2020-02-13 20:29:31 +0100

    Update test_standardize.m

--- braph/graph/test_standardize.m
+++ braph/graph/test_standardize.m
@@ -1,18 +1,20 @@
 % test standardize
-%A = randn(10);
 A = randn(randi(10));
 
 %% Test 1: Standardize Range Default
 B = standardize(A);
+
 assert(isequal(B, A-min(A)/(max(A)-min(A))),...
     'BRAPH:standardize:Bug',...
     'Standardize default case (''range between 0 and 1'') not working correctly')
 
 %% Test 2: Standardize One
-B = standardize(A, 'StandardizeRule','one');
+B = standardize(A, 'StandardizeRule', 'one');
+
 C = A;
 C(C<0)=0;
 C(C>1)=1;
+
 assert(isequal(B, C),...
     'BRAPH:standardize:Bug',...
     'Standardize default case (''range between 0 and 1'') not working correctly')
