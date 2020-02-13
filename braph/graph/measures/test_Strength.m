63bba212d5f0ac402799b106518d3e01277041a8
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2020-02-13 22:43:45 +0100

    Update test_Strength.m

--- braph/graph/measures/test_Strength.m
+++ braph/graph/measures/test_Strength.m
@@ -13,27 +13,3 @@ strength_test = sum(A, 1)';
 assert(isequal(Strength.getValue(), strength_test), ...
     'BRAPH:Strength:Bug', ...
     'Strength is not beeing calculated correctly for GraphWU')
-
-%% Test 2: Strength get Class correctly
-g = GraphWU(A);
-Strength = Strength(g);
-
-assert(isequal(Strength.getClass(), 'Strength'), ...
-    'BRAPH:Strength:Bug', ...
-    'Strength is not getting class correctly')
-
-%% Test 3: Strength get Name correctly
-g = GraphWU(A);
-Strength = Strength(g);
-
-assert(isequal(Strength.getName(), 'Strength'), ...
-    'BRAPH:Strength:Bug', ...
-    'Strength is not getting class correctly')
-
-%% Test 4: Strength get Description works
-g = GraphWU(A);
-Strength = Strength(g);
-
-assert(ischar(Strength.getDescription()), ...
-    'BRAPH:Strength:Bug', ...
-    'Strength is not getting class correctly')
