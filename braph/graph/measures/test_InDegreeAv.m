98809a15778a538d8896987410124ed3ca07be47
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2020-02-13 22:40:41 +0100

    Update test_InDegreeAv.m

--- braph/graph/measures/test_InDegreeAv.m
+++ braph/graph/measures/test_InDegreeAv.m
@@ -27,27 +27,3 @@ average_in_degree_test = mean(in_degree_test);
 assert(isequal(average_in_degree.getValue(), average_in_degree_test), ...
     'BRAPH:InDegreeAv:Bug', ...
     'InDegreeAv is not beeing calculated correctly for GraphWD')
-
-%% Test 3: InDegreeAv get Class correctly
-g = GraphWD(A);
-average_in_degree = InDegreeAv(g);
-
-assert(isequal(average_in_degree.getClass(), 'InDegreeAv'), ...
-    'BRAPH:InDegreeAv:Bug', ...
-    'InDegreeAv is not getting class correctly')
-
-%% Test 4: InDegreeAv get Name correctly
-g = GraphWD(A);
-average_in_degree = InDegreeAv(g);
-
-assert(isequal(average_in_degree.getName(), 'Average In Degree'), ...
-    'BRAPH:InDegreeAv:Bug', ...
-    'InDegreeAv is not getting class correctly')
-
-%% Test 5: InDegreeAv get Description works
-g = GraphWD(A);
-average_in_degree = InDegreeAv(g);
-
-assert(ischar(average_in_degree.getDescription()), ...
-    'BRAPH:InDegreeAv:Bug', ...
-    'InDegreeAv is not getting class correctly')
