4f778d1c611fe12a25a941cb0924ec13fe9d105b
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2020-02-13 22:39:54 +0100

    Update test_InDegree.m

--- braph/graph/measures/test_InDegree.m
+++ braph/graph/measures/test_InDegree.m
@@ -25,27 +25,3 @@ in_degree_test = sum(A, 1)';
 assert(isequal(in_degree.getValue(), in_degree_test), ...
     'BRAPH:InDegree:Bug', ...
     'InDegree is not beeing calculated correctly for GraphWD')
-
-%% Test 3: InDegree get Class correctly
-g = GraphWD(A);
-in_degree = InDegree(g);
-
-assert(isequal(in_degree.getClass(), 'InDegree'), ...
-    'BRAPH:InDegree:Bug', ...
-    'InDegree is not getting class correctly')
-
-%% Test 4: InDegree get Name correctly
-g = GraphWD(A);
-in_degree = InDegree(g);
-
-assert(isequal(in_degree.getName(), 'In-Degree'), ...
-    'BRAPH:InDegree:Bug', ...
-    'InDegree is not getting class correctly')
-
-%% Test 5: InDegree get Description works
-g = GraphWD(A);
-in_degree = InDegree(g);
-
-assert(ischar(in_degree.getDescription()), ...
-    'BRAPH:InDegree:Bug', ...
-    'InDegree is not getting class correctly')
