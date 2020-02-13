f30716e9491ff8aefb60a073885a41f88ff5339b
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2020-02-13 20:28:41 +0100

    Update standardize.m

--- braph/graph/standardize.m
+++ braph/graph/standardize.m
@@ -2,8 +2,7 @@ function B = standardize(A, varargin)
 % STANDARDIZE normalizes a matrix
 %
 % B = STANDARDIZE(A) normalizes the matrix A in order to have values scaled
-% between 0 and 1 by using default RULE 'range' or 'compress'
-% using default RULE 'zero'.
+% between 0 and 1 by using the default RULE 'range'.
 %
 % B = STANDARDIZE(A, 'StandardizeRule', RULE) 
 % normalizes the matrix A using the specified RULE.
@@ -15,11 +14,12 @@ function B = standardize(A, varargin)
 %                   values scaled between 0 and 1 by using a linear function
 %                   'one' -  normalizes the matrix A following one-rule: 
 %                   by converting negative values to zero and values larger 
-%                   than 1 are converted to 1
+%                   than 1 to 1
 %                   '1' - normalizes the matrix A following one-rule
 %                   1 - normalizes the matrix A following one-rule
 %                   'threshold' - normalizes the matrix A following one-rule
 %
+% See also BINARIZE, DEDIAGONALIZE, SEMIPOSITIVIZE, SYMMETRIZE.
 
 % Author: Anna Canal Garcia
 % Date: 2020/02/12
@@ -29,9 +29,10 @@ standardize_rule = get_from_varargin('range', 'StandardizeRule', varargin{:});
 switch lower(standardize_rule)
     case {'one',1,'1','threshold'}  % one-rule
         B = A;
-        B(B <0) = 0;
+        B(B<0) = 0;
         B(B>1) = 1 ;
     otherwise  % 'range' | 'compress' - scaled interval
-        B = A - min(A)/(max(A)-min(A));
+        B = A - min(A) / (max(A) - min(A));
 end
+
 end
