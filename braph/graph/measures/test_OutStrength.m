706bed9b2c07f982fbd4f0879f2a87eb28c726c9
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2020-02-13 22:43:03 +0100

    Update test_OutStrength.m

--- braph/graph/measures/test_OutStrength.m
+++ braph/graph/measures/test_OutStrength.m
@@ -12,27 +12,3 @@ out_strength = sum(A, 2);
 assert(isequal(OutStrength.getValue(), out_strength), ...
     'BRAPH:OutStrength:Bug', ...
     'OutStrength is not beeing calculated correctly for GraphWD')
-
-%% Test 2: OutStrength get Class correctly
-g = GraphWD(A);
-OutStrength = OutStrength(g);
-
-assert(isequal(OutStrength.getClass(), 'OutStrength'), ...
-    'BRAPH:OutStrength:Bug', ...
-    'OutStrength is not getting class correctly')
-
-%% Test 3: OutStrength get Name correctly
-g = GraphWD(A);
-OutStrength = OutStrength(g);
-
-assert(isequal(OutStrength.getName(), 'Out-Strength'), ...
-    'BRAPH:OutStrength:Bug', ...
-    'OutStrength is not getting class correctly')
-
-%% Test 4: OutStrength get Description works
-g = GraphWD(A);
-OutStrength = OutStrength(g);
-
-assert(ischar(OutStrength.getDescription()), ...
-    'BRAPH:OutStrength:Bug', ...
-    'OutStrength is not getting class correctly')
