ffe3fff6d3433ca8deda32950c1549c3aac9d2f0
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-02-19 00:29:40 +0100

    Update Distance.m

--- braph/graph/measures/Distance.m
+++ braph/graph/measures/Distance.m
@@ -5,23 +5,22 @@ classdef Distance < Measure
         end
     end
     methods (Access=protected)
-        function distance = calculate(m)
+        function D = calculate(m)
             g = m.getGraph();
             graph_class = g.getClass();
-            L = g.getA();
-            calculation = zeros(size(L));
+            A = g.getA();
             
-            if graph_class == 'GraphWD' | graph_class == 'GraphWU'
-                ind = L~=0;
-                L(ind) = L(ind).^-1;
-                n=length(L);
-                D=inf(n);
-                D(1:n+1:end)=0;  % distance matrix
-                B=zeros(n);  % number of edges matrix
+            if isequal(graph_class, 'GraphWD') || isequal(graph_class, 'GraphWU')
+                ind = A~=0;
+                A(ind) = A(ind).^-1;
+                n = length(A);
+                D = inf(n);
+                D(1:n+1:end) = 0;  % distance matrix
+                B = zeros(n);  % number of edges matrix
                 
                 for u = 1:n
                     S = true(1,n);  % distance permanence (true is temporary)
-                    L1 = L;
+                    L1 = A;
                     V = u;
                     
                     while 1
@@ -30,41 +29,36 @@ classdef Distance < Measure
                         
                         for v = V
                             T = find(L1(v,:));  % neighbours of shortest nodes
-                            [d wi] = min([D(u,T);D(u,v)+L1(v,T)]);
+                            [d, wi] = min([D(u,T);D(u,v)+L1(v,T)]);
                             D(u,T) = d;  % smallest of old/new path lengths
                             ind = T(wi==2);  % indices of lengthened paths
-                            B(u,ind) = B(u,v)+1;  % increment no. of edges in lengthened paths
+                            B(u,ind) = B(u,v) + 1;  % increment no. of edges in lengthened paths
                         end
                         
                         minD = min(D(u,S));
-                        if isempty(minD) || isinf(minD),  % isempty: all nodes reached;
-                            break,  % isinf: some nodes cannot be reached
-                        end;
+                        if isempty(minD) || isinf(minD)  % isempty: all nodes reached;
+                            break  % isinf: some nodes cannot be reached
+                        end
                         
                         V = find(D(u,:)==minD);
                     end
                 end
-                
-                calculation = D;  % for weighted
             else
                 l = 1;  % path length
-                D = L;  % distance matrix
+                D = A;  % distance matrix
                 
-                Lpath = L;
+                Lpath = A;
                 Idx = true;
                 while any(Idx(:))
                     l = l+1;
-                    Lpath = Lpath*L;
-                    Idx = (Lpath~=0)&(D==0);
+                    Lpath = Lpath * A;
+                    Idx = (Lpath ~= 0) & (D == 0);
                     D(Idx) = l;
                 end
                 
                 D(~D) = inf;  % assign inf to disconnected nodes
                 D = dediagonalize(D);  % assign 0 to the diagonal
-                calculation = D;  % for binary
             end
-            
-            distance = calculation;
         end
     end
     methods (Static)
