21cfa8104425ddc5201dbae29b0426847a95fb0b
annacanal <31701548+annacanal@users.noreply.github.com>
2020-02-20 09:39:48 +0100

    Update test_Triangles.m
    #94 updated test triangles with all rules

--- braph/graph/measures/test_Triangles.m
+++ braph/graph/measures/test_Triangles.m
@@ -1,11 +1,19 @@
 % test Triangles
+<<<<<<< Updated upstream
 A_BD = [
+=======
+A_BD_in = [
+>>>>>>> Stashed changes
     0 0 1 1;
     1 0 0 1;
     0 1 0 1;
     0 0 0 0
     ];
+<<<<<<< Updated upstream
 triangles_BD = [0 0 0 3]'; % in rule
+=======
+triangles_BD_in = [0 0 0 3]'; % in rule
+>>>>>>> Stashed changes
 
 A_BU = [
     0 1 1 1;
@@ -15,6 +23,7 @@ A_BU = [
     ];
 triangles_BU = [2 1 2 1]';
 
+<<<<<<< Updated upstream
 A_test = randn(randi(10));
 
 %% Test 1: Comparison with known BD graph
@@ -26,6 +35,79 @@ assert(isequal(valueBD, triangles_BD), ...
     'Triangles is not beeing calculated correctly for GraphBD')
 
 %% Test 2: Comparison with known BU graphs
+=======
+A_BD_cycle = [
+    0 0 1 ; 
+    1 0 0 ; 
+    0 1 0 
+    ];
+triangles_BD_cycle = [1;1;1]; % cycle rule
+
+A_BD_out = [
+    0 0 0 ; 
+    1 0 0 ; 
+    1 1 0 
+    ];
+triangles_BD_out = [0;0;1]; % out rule 
+
+A_BD_mid = [
+    0 0 0 ; 
+    1 0 1 ; 
+    1 0 0 
+    ];
+triangles_BD_mid = [0;0;1]; % middleman rule 
+
+A_BD_all = [
+    0 0 0 ; 
+    1 0 1 ; 
+    1 0 0 
+    ];
+triangles_BD_all = [1;1;1]; % all rule 
+
+A_test = randn(randi(10));
+
+%% Test 1: Comparison with known BD graph -in rule
+gBD = GraphBD(A_BD_in);
+mBD = Triangles(gBD,'DirectedTrianglesRule', 'in');
+valueBD = mBD.getValue();
+assert(isequal(valueBD, triangles_BD_in), ...
+    'BRAPH:Triangles:Bug', ...
+    'Triangles [in] is not being calculated correctly for GraphBD')
+
+%% Test 2: Comparison with known BD graph -out rule
+gBD = GraphBD(A_BD_out);
+mBD = Triangles(gBD,'DirectedTrianglesRule', 'out');
+valueBD = mBD.getValue();
+assert(isequal(valueBD, triangles_BD_out), ...
+    'BRAPH:Triangles:Bug', ...
+    'Triangles [out] is not being calculated correctly for GraphBD')
+
+%% Test 3: Comparison with known BD graph -cycle rule
+gBD = GraphBD(A_BD_cycle);
+mBD = Triangles(gBD,'DirectedTrianglesRule', 'cycle');
+valueBD = mBD.getValue();
+assert(isequal(valueBD, triangles_BD_cycle), ...
+    'BRAPH:Triangles:Bug', ...
+    'Triangles [cycle] is not being calculated correctly for GraphBD')
+
+%% Test 4: Comparison with known BD graph -middleman rule
+gBD = GraphBD(A_BD_mid);
+mBD = Triangles(gBD,'DirectedTrianglesRule', 'middleman');
+valueBD = mBD.getValue();
+assert(isequal(valueBD, triangles_BD_mid), ...
+    'BRAPH:Triangles:Bug', ...
+    'Triangles [middleman] is not being calculated correctly for GraphBD')
+
+%% Test 5: Comparison with known BD graph -middleman rule
+gBD = GraphBD(A_BD_all);
+mBD = Triangles(gBD,'DirectedTrianglesRule', 'all');
+valueBD = mBD.getValue();
+assert(isequal(valueBD, triangles_BD_all), ...
+    'BRAPH:Triangles:Bug', ...
+    'Triangles [all] is not being calculated correctly for GraphBD')
+
+%% Test 6: Comparison with known BU graphs
+>>>>>>> Stashed changes
 gBU = GraphBU(A_BU);
 mBU = Triangles(gBU);
 valueBU = mBU.getValue();
@@ -33,7 +115,11 @@ assert(isequal(valueBU, triangles_BU), ...
     'BRAPH:Triangles:Bug', ...
     'Triangles is not beeing calculated correctly for GraphBU')
 
+<<<<<<< Updated upstream
 %% Test 3: Comparison with standard method for BD graphs
+=======
+%% Test 7: Comparison with standard method for BD graphs
+>>>>>>> Stashed changes
 gBD_braph2 = GraphBD(A_test); A_BD = gBD_braph2.getA();
 mBD_braph2 = Triangles(gBD_braph2, 'DirectedTrianglesRule', 'all');
 % calculate the values by Braph2 and standard methods
@@ -43,7 +129,11 @@ assert(isequal(valueBD_braph2, valueBD_std), ...
     'BRAPH:Triangles:Bug', ...
     'Triangles is not beeing calculated correctly for GraphBD')
 
+<<<<<<< Updated upstream
 %% Test 4: Comparison with standard method for BU graphs
+=======
+%% Test 8: Comparison with standard method for BU graphs
+>>>>>>> Stashed changes
 gBU_braph2 = GraphBU(A_test); A_BU = gBU_braph2.getA();
 mBU_braph2 = Triangles(gBU_braph2);
 % calculate the values by Braph2 and standard methods
@@ -53,7 +143,11 @@ assert(isequal(valueBU_braph2, valueBU_std), ...
     'BRAPH:Triangles:Bug', ...
     'Triangles is not beeing calculated correctly for GraphBU')
 
+<<<<<<< Updated upstream
 %% Test 5: Comparison with standard method for WD graphs
+=======
+%% Test 9: Comparison with standard method for WD graphs
+>>>>>>> Stashed changes
 gWD_braph2 = GraphWD(A_test); A_WD = gWD_braph2.getA();
 mWD_braph2 = Triangles(gWD_braph2, 'DirectedTrianglesRule', 'all');
 % calculate the values by Braph2 and standard methods
@@ -63,7 +157,11 @@ assert(isequal(valueWD_braph2, valueWD_std), ...
     'BRAPH:Triangles:Bug', ...
     'Triangles is not beeing calculated correctly for GraphWD')
 
+<<<<<<< Updated upstream
 %% Test 6: Comparison with standard method for WU graphs
+=======
+%% Test 10: Comparison with standard method for WU graphs
+>>>>>>> Stashed changes
 gWU_braph2 = GraphWU(A_test); A_WU = gWU_braph2.getA();
 mWU_braph2 = Triangles(gWU_braph2);
 % calculate the values by Braph2 and standard methods
