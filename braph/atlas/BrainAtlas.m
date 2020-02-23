ed2f513007ae65b204074eda579ef259d41a3729
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-02-23 14:35:21 +0100

    Update BrainAtlas.m

--- braph/atlas/BrainAtlas.m
+++ braph/atlas/BrainAtlas.m
@@ -1,11 +1,27 @@
 classdef BrainAtlas < handle & matlab.mixin.Copyable
     properties (GetAccess=protected, SetAccess=protected) 
         name  % brain atlas name
+        brdict  % dictionary with brain regions
     end
     methods
-        function ba = BrainAtlas(name, regions)
+        function ba = BrainAtlas(name, brain_regions)
+            
             ba.name = name;
             
+            ba.brdict = containers.Map;
+            for i = 1:1:length(brain_regions)
+                br = brain_regions(i);
+                ba.brdict(br.getLabel()) = br;
+            end
+        end
+        function name = getName(ba)
+            name = ba.name;
+        end
+        function bool = contains_brain_region(ba, label)
+            bool = isKey(ba.brdict, label);
+        end
+        function br = getBrainRegion(ba, label)
+            br = ba.brdict(label);
         end
     end
 end
