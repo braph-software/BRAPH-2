ae885abc46500b9b1a8aef630d1cc6eee3f573c0
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-02-26 15:24:01 +0100

    Create DataStructural.m

---
+++ braph/cohort/datas/DataStructural.m
@@ -0,0 +1,34 @@
+classdef DataStructural < Data
+    methods
+        function d = DataStructural(atlas, value)
+            
+            if nargin < 2
+                value = zeros(atlas.brainregionnumber(), 1);
+            end
+            
+            assert(isnumeric(value) && isequal(size(value), [atlas.brainregionnumber(), 1]), ...
+                'BRAPH:DataStructural:WrongValue', ...
+                [ ...
+                'The value of DataStructural must be a column vector ' ...
+                'with the same numebr of element as the BrainAtlas, ' ...
+                'in this case ' mat2str(atlas.brainregionnumber()) ...
+                ])
+            
+            d = d@Data(atlas, value);
+        end
+    end
+    methods (Static)
+        function data_class = getClass()
+            data_class = 'DataStructural';
+        end
+        function name = getName()
+            name = 'Structural Brain Data';
+        end
+        function description = getDescription()
+            description = [ ...
+                'A series of structural data corresponding ' ...
+                'to one scalar value per brain region.' ...
+                ];
+        end
+    end  
+end
