cc3c9af5554de6e4f2b43932224ad0938c66697b
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-02-28 01:22:30 +0100

    Create SubjectMRI.m

---
+++ braph/cohort/subjects/SubjectMRI.m
@@ -0,0 +1,50 @@
+classdef SubjectMRI < Subject
+    methods
+        function sub = SubjectMRI(atlas)
+
+            sub = sub@Subject('BrainAtlas', atlas);
+        end
+    end
+    methods (Access=protected)
+        function datadict = initialize_datadict(sub, varargin) %#ok<INUSL>
+
+            atlas = get_from_varargin([], 'BrainAtlas', varargin{:});
+            
+            datadict = containers.Map;
+            datadict('age') = DataScalar(atlas);
+            datadict('MRI') = DataStructural(atlas);
+        end
+    end
+    methods (Static)
+        function subject_class = getClass()
+                subject_class = 'SubjectMRI';
+        end
+        function name = getName()
+            name = 'Subject Structural MRI';
+        end
+        function description = getDescription()
+            description = [ ...
+                'Subject with structural MRI data ' ...
+                'such as cortical thickness for each brain region' ...
+                ];
+        end
+        function datalist = getDataList()
+            
+            datalist = containers.Map('KeyType', 'char', 'ValueType', 'char');
+            datalist('age') = 'DataScalar';
+            datalist('MRI') = 'DataStructural';            
+        end
+        function data_number = getDataNumber()
+            data_number = Subject.getDataNumber('SubjectMRI');
+        end
+        function data_codes = getDataCodes()
+            data_codes = Subject.getDataCodes('SubjectMRI');
+        end
+        function data_classes = getDataClasses()
+            data_classes = Subject.getDataClasses('SubjectMRI');
+        end
+        function data_class = getDataClass(data_code)
+            data_class = Subject.getDataNumber('SubjectMRI', data_code);
+        end
+    end
+end
