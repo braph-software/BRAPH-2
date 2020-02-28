0ad13469ec27f0692863f468f45c29adebda258a
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-02-28 13:00:13 +0100

    Update test_Subject.m

--- braph/cohort/test_Subject.m
+++ braph/cohort/test_Subject.m
@@ -1,7 +1,6 @@
 % test Subject
 subject_class_list = Subject.getList();
 
-%% Test 1: All subjects not abstract
 br1 = BrainRegion('BR1', 'brain region 1', 1, 11, 111);
 br2 = BrainRegion('BR2', 'brain region 2', 2, 22, 222);
 br3 = BrainRegion('BR3', 'brain region 3', 3, 33, 333);
@@ -9,6 +8,7 @@ br4 = BrainRegion('BR4', 'brain region 4', 4, 44, 444);
 br5 = BrainRegion('BR5', 'brain region 5', 5, 55, 555);
 atlas = BrainAtlas('brain atlas', {br1, br2, br3, br4, br5});
 
+%% Test 1: All subjects not abstract
 for i = 1:1:length(subject_class_list)
     subject_class = subject_class_list{i};
     sub = eval(['Subject.getSubject(subject_class' repmat(', atlas', 1, Subject.getBrainAtlasNumber(subject_class)) ')']);
