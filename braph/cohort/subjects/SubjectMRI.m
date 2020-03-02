635866723195f13b7adab5a9325d1080adcf08c6
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2020-03-02 16:07:18 +0100

    Package Cohort (#120)
    * Update braph2.m
    
    * Update test_braph2.m
    
    * Update braph2.m
    
    * Update test_braph2.m
    
    * Create Data.m
    
    * Create DataScalar.m
    
    * Update Clustering.m
    
    * Update Measure.m
    
    * Update DataScalar.m
    
    * Create test_Data.m
    
    * Update Data.m
    
    * Create test_DataScalar.m
    
    * Update braph2.m
    
    * Update test_braph2.m
    
    * Update Data.m
    
    * Update test_Data.m
    
    * Delete DataScalar.m
    
    * Delete test_DataScalar.m
    
    * Create DataScalar.m
    
    * Create DataStructural.m
    
    * Create test_DataScalar.m
    
    * Create test_DataStructural.m
    
    * Update DataStructural.m
    
    * Update test_DataStructural.m
    
    * Create DataFunctional.m
    
    * Update DataStructural.m
    
    * Update DataFunctional.m
    
    * Create test_DataFunctional.m
    
    * Create DataConnectivity.m
    
    * Create test_DataConnectivity.m
    
    * Update DataScalar.m
    
    * setData + tests
    
    * Update Data.m
    
    * Update Subject.m
    
    * Gv utils flexible varargin (#118)
    
    * Update clean_varargin.m
    
    * Update copy_varargin.m
    
    * Update get_from_varargin.m
    
    * Update test_clean_varargin.m
    
    * Update test_copy_varargin.m
    
    * Update test_get_from_varargin.m
    
    * Update Measure.m
    
    * Update Measure.m
    
    * Updated settings in all measures and graphs
    
    * Update braph2.m
    
    * Update test_braph2.m
    
    * Create test_SubjectMRI.m
    
    * Create SubjectMRI.m
    
    * Update Subject.m
    
    * Update test_Measure.m
    
    * Create test_Subject.m
    
    * Update test_SubjectMRI.m
    
    * Create test_SubjectfMRI.m
    
    * Create test_SubjectDTI.m
    
    * Update SubjectMRI.m
    
    * Create SubjectfMRI.m
    
    * Create SubjectDTI.m
    
    * Update Subject.m
    
    * Update test_Subject.m
    
    * Create Cohort.m
    
    * Create test_Cohort.m
    
    * Update Cohort.m
    
    * Update Subject.m
    
    * Update SubjectDTI.m
    
    * Update SubjectfMRI.m
    
    * Update SubjectMRI.m
    
    * Update test_Cohort.m
    
    * Update test_Cohort.m
    
    * Update test_BrainAtlas.m
    
    * Update Cohort.m
    
    * Update Cohort.m
    
    * Update test_Cohort.m
    
    * Update Subject.m
    
    * Update test_Subject.m
    
    * Update Cohort.m
    
    * Update test_Cohort.m
    
    * Update Cohort.m
    
    * Update Data.m
    
    * Update test_Data.m
    
    * Update BrainAtlas.m
    
    * Update Subject.m
    
    * Update test_Subject.m
    
    * Added uptate BrainAtlases to Subject and related changes
    
    * changed the atlas input to subjects
    
    * deep copy Cohort
    
    * Update test_Cohort.m
    
    * Adde atlases to Subject + ancillary changes
    
    * Update Cohort.m
    
    * Update Cohort.m
    
    * Update BrainAtlas.m

---
+++ braph/cohort/subjects/SubjectMRI.m
@@ -0,0 +1,74 @@
+classdef SubjectMRI < Subject
+    methods
+        function sub = SubjectMRI(atlas, varargin)
+
+            if isa(atlas, 'BrainAtlas') 
+                atlases = {atlas};
+            else
+                assert(iscell(atlas) && length(atlas)==1, ...
+                    ['BRAIN:SubjectMRI:AtlasErr'], ...
+                    ['The input must be a BrainAtlas or a cell with one BrainAtlas']) %#ok<NBRAK>
+                atlases = atlas;
+            end
+            
+            sub = sub@Subject(atlases, varargin{:});
+        end
+    end
+    methods (Access=protected)
+        function initialize_datadict(sub, varargin)
+            
+            atlases = sub.getBrainAtlases();
+            atlas = atlases{1};
+            
+            sub.datadict = containers.Map;
+            sub.datadict('age') = DataScalar(atlas);
+            sub.datadict('MRI') = DataStructural(atlas);
+        end
+        function update_brainatlases(sub, atlases)
+
+            sub.atlases = atlases;
+            atlas = atlases{1};
+
+            d1 = sub.datadict('age');
+            d1.setBrainAtlas(atlas)
+            
+            d2 = sub.datadict('MRI');
+            d2.setBrainAtlas(atlas);
+        end
+    end
+    methods (Static)
+        function subject_class = getClass()
+                subject_class = 'SubjectMRI';
+        end
+        function name = getName()
+            name = 'Subject Structural MRI';
+        end
+        function description = getDescription()
+            description = [ ...
+                'Subject with structural MRI data ' ...
+                'such as cortical thickness for each brain region' ...
+                ];
+        end
+        function atlas_number = getBrainAtlasNumber()
+            atlas_number = 1;
+        end
+        function datalist = getDataList()
+            
+            datalist = containers.Map('KeyType', 'char', 'ValueType', 'char');
+            datalist('age') = 'DataScalar';
+            datalist('MRI') = 'DataStructural';            
+        end
+        function data_number = getDataNumber()
+            data_number = Subject.getDataNumber('SubjectMRI');
+        end
+        function data_codes = getDataCodes()
+            data_codes = Subject.getDataCodes('SubjectMRI');
+        end
+        function data_classes = getDataClasses()
+            data_classes = Subject.getDataClasses('SubjectMRI');
+        end
+        function data_class = getDataClass(data_code)
+            data_class = Subject.getDataNumber('SubjectMRI', data_code);
+        end
+    end
+end
--- braph/graph/measures/Clustering.m
+++ braph/graph/measures/Clustering.m
@@ -99,6 +99,5 @@ classdef Clustering < Triangles
         function n = getCompatibleGraphNumber()
             n = Measure.getCompatibleGraphNumber('Clustering');
         end
-    end
-    
+    end  
 end
