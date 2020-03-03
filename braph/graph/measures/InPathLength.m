e26177d15de7c1d1ed57ef5b2ba86aa7095e4f04
egolol <egolol87@gmail.com>
2020-03-03 12:29:42 +0100

    Issue 97 Path Length (#103)
    * Issue 97 Path Length
    
    Created the measure and the test.
    changed nodal test for measures
    
    * update
    
    Removes for loop
    
    * Update
    
    Change code, and created Inpath and Outpath with tests
    
    * Issue 97 update
    
    update to reviews
    
    * update to in and out pl
    
    * Update
    
    Update
    
    * update
    
    * update to change branch
    
    small update
    
    * update
    
    Updated some comments in the tests, changed hoe to getSettings
    
    * update
    
    correction of settings

---
+++ braph/graph/measures/InPathLength.m
@@ -0,0 +1,74 @@
+classdef InPathLength < Measure
+    methods
+        function m = InPathLength(g, varargin)
+            settings = clean_varargin({'InPathLengthAvRule'}, varargin{:});
+            m = m@Measure(g, settings{:});
+        end
+    end
+    methods (Access = protected)
+        function in_path_length =  calculate(m)
+            g = m.getGraph();    
+
+            if g.is_measure_calculated('Distance')
+                distance = g.getMeasureValue('Distance');
+            else
+                distance = Distance(g, g.getSettings()).getValue();
+            end
+            
+            N = g.nodenumber();
+            in_path_length = zeros(N, 1);
+
+            pathLength_rule = get_from_varargin('default', 'InPathLengthAvRule', m.getSettings());
+            switch lower(pathLength_rule)
+                case {'subgraphs'}                    
+                    for u = 1:1:N
+                        Du = distance(:, u);
+                        in_path_length(u) = mean(Du(Du~=0 & Du~=Inf));
+                    end
+                    in_path_length(isnan(in_path_length)) = 0;  % node Nan corresponds to isolated nodes, pathlength is 0
+                case {'harmonic'}
+                    for u = 1:1:N
+                        Du = distance(:, u);
+                        in_path_length(u) = harmmean(Du(Du~=0));
+                    end
+                otherwise
+                    for u = 1:1:N
+                        Du = distance(:, u);
+                        in_path_length(u) = mean(Du(Du~=0));
+                    end
+            end
+        end
+    end
+    methods (Static)
+        function measure_class = getClass()
+            measure_class = 'InPathLength';
+        end
+        function name = getName()
+            name = 'In Path Length';
+        end
+        function description = getDescription()
+            description = [ ...                
+              'The in path length is the average shortest ' ...
+              'in path lengths of one node to all other nodes.' ...
+                ];
+        end
+        function bool = is_global()
+            bool = false;
+        end
+        function bool = is_nodal()
+            bool = true;
+        end
+        function bool = is_binodal()
+            bool = false;
+        end
+        function list = getCompatibleGraphList()
+            list = { ...               
+                'GraphBD', ...                
+                'GraphWD' ...
+                };
+        end
+        function n = getCompatibleGraphNumber()
+            n = Measure.getCompatibleGraphNumber('InPathLength');
+        end
+    end
+end
---
+++ braph/graph/measures/PathLength.m
@@ -0,0 +1,74 @@
+classdef PathLength < Measure
+    methods
+        function m = PathLength(g, varargin)
+            settings = clean_varargin({'PathLengthAvRule'}, varargin{:});
+            m = m@Measure(g, settings{:});
+        end
+    end
+    methods (Access = protected)
+        function path_length =  calculate(m)
+            g = m.getGraph();
+
+            if g.is_measure_calculated('Distance')
+                distance = g.getMeasureValue('Distance');
+            else
+                distance = Distance(g, g.getSettings()).getValue();
+            end
+            
+            N = g.nodenumber();
+            path_length = zeros(N, 1);            
+          
+            pathLength_rule = get_from_varargin('default', 'PathLengthAvRule', m.getSettings());
+            switch lower(pathLength_rule)
+                case {'subgraphs'}
+                    for u = 1:1:N
+                        Du = distance(:, u);
+                        path_length(u) = mean(Du(Du~=Inf & Du~=0));                        
+                    end
+                     path_length(isnan(path_length)) = 0;  % node Nan corresponds to isolated nodes, pathlength is 0
+                case {'harmonic'}
+                    for u = 1:1:N
+                        Du = distance(:, u);
+                        path_length(u) = harmmean(Du(Du~=0));
+                    end
+                otherwise
+                    for u = 1:1:N
+                        Du = distance(:, u);
+                        path_length(u) = mean(Du(Du~=0));
+                    end
+            end           
+        end
+    end
+    methods (Static)
+        function measure_class = getClass()
+            measure_class = 'PathLength';
+        end
+        function name = getName()
+            name = 'Path Length';
+        end
+        function description = getDescription()
+            description = [ ...
+                'The path length is the average shortest ' ...
+                'path lengths of one node to all other nodes.' ...
+                ];
+        end
+        function bool = is_global()
+            bool = false;
+        end
+        function bool = is_nodal()
+            bool = true;
+        end
+        function bool = is_binodal()
+            bool = false;
+        end
+        function list = getCompatibleGraphList()
+            list = { ...
+                'GraphBU', ...
+                'GraphWU' ...
+                };
+        end
+        function n = getCompatibleGraphNumber()
+            n = Measure.getCompatibleGraphNumber('PathLength');
+        end
+    end
+end
