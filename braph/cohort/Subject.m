9fac2221f627ddc0d5b53c1cd5d188544825db2b
egolol <egolol87@gmail.com>
2020-03-05 13:59:52 +0100

    Update Subject.m

--- braph/cohort/Subject.m
+++ braph/cohort/Subject.m
@@ -12,6 +12,10 @@ classdef Subject < handle & matlab.mixin.Copyable
                 ['BRAIN:Subject:AtlasErr'], ...
                 ['The input must be a cell containing BrainAtlas objects'])
             sub.atlases = atlases;
+            
+            if length(varargin) == 1
+                varargin = varargin{:};
+            end
 
             id = get_from_varargin(now(), 'SubjectID', varargin{:});
             sub.setID(id)
