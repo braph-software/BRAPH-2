2e56e00cad1dc3d25489fd83a10e5c1c621c0542
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-03-09 16:11:42 +0100

    Update Analysis.m

--- braph/analysis/Analysis.m
+++ braph/analysis/Analysis.m
@@ -1,14 +1,51 @@
 classdef Analysis < handle & matlab.mixin.Copyable
-    properties
-        name  % brain atlas name
+    properties (GetAccess=protected, SetAccess=protected) 
         cohort  % cohort
     end
     methods (Access=protected)
         function analysis = Analysis(cohort, varargin)
+            
+            assert(isa(cohort, 'Cohort') && isequal(cohort.getSubjectClass(), analysis.getSubjectClass()), ...
+                ['BRAPH:Analysis:SubjectClassErr'], ...
+                ['The first argument must be a Cohort with subjects of class ' analysis.getSubjectClass()]) %#ok<NBRAK>
+            analysis.cohort = cohort;
         end
     end
     methods
     end
     methods (Static)
-    end    
+        function analysis_list = getList()
+            analysis_list = subclasses( ...
+                'Analysis', ...
+                [fileparts(which('Analysis')) filesep 'analyses'] ...
+                );
+        end
+        function analysis_class = getClass(analysis)
+            % analysis class (same as the analysis object name)
+            
+            if isa(analysis, 'Analysis')
+                analysis_class = class(analysis);
+            else % analysis should be a string with the analysis class
+                analysis_class = analysis;
+            end
+        end
+        function subject_class = getSubjectClass(analysis)
+            % cohort class
+            
+            subject_class = eval([Analysis.getClass(analysis) '.getSubjectClass()']);
+        end
+        function name = getName(analysis)
+            % analysis name
+            
+            name = eval([Analysis.getClass(analysis) '.getName()']);
+        end
+        function description = getDescription(analysis)
+            % analysis description
+            
+            description = eval([Analysis.getClass(analysis) '.getDescription()']);
+        end
+        function analysis = getAnalysis(analysis_class, varargin)
+            analysis = eval([analysis_class '(varargin{:})']);
+        end
+    end
 end
