b95e447c996c433d94858c7c59243491ff9bf842
egolol <egolol87@gmail.com>
2020-03-10 15:14:22 +0100

    LocalEfficency
    #109 Created LocalEfficency, InLocalEfficency, OutLocalEfficency, and tests.

--- braph/graph/measures/InLocalEfficency.m
+++ braph/graph/measures/InLocalEfficency.m
@@ -1,36 +1,36 @@
-classdef LocalEfficency < Measure
+classdef InLocalEfficency < Measure
     methods
-        function m = LocalEfficency(g, varargin)
+        function m = InLocalEfficency(g, varargin)
             m = m@Measure(g, varargin{:});
         end
     end
     methods (Access = protected)
-        function le = calculate(m)
+        function in_le = calculate(m)
             g = m.getGraph();
             A = g.getA();
             n = g.nodenumber();
-            le = zeros(n,1);
+            in_le = zeros(n,1);
             
             for i = 1:1:n
                 nodes = find(A(i, :)  | A(:, i).');  % neighbours of u
                 if numel(nodes) > 1
                     sub_graph = g.subgraph(nodes);
-                    le(i) = mean(GlobalEfficency(sub_graph, g.getSettings()).getValue());
+                    in_le(i) = mean(InGlobalEfficency(sub_graph, g.getSettings()).getValue());
                 end
             end
         end
     end
     methods (Static)
         function measure_class = getClass()
-            measure_class = 'LocalEfficency';
+            measure_class = 'InLocalEfficency';
         end
         function name = getName()
-            name = 'Local Efficency';
+            name = 'In Local Efficency';
         end
         function description = getDescription()
             description = [ ...
-                'The local efficiency is the average inverse ' ...
-                'shortest path length with local nodes.' ...
+                'The in local efficiency is the average inverse ' ...
+                'shortest in path length with local nodes.' ...
                 ];
         end
         function bool = is_global()
@@ -44,12 +44,12 @@ classdef LocalEfficency < Measure
         end
         function list = getCompatibleGraphList()
             list = { ...               
-                'GraphBU', ...           
-                'GraphWU' ...
+                'GraphBD', ...           
+                'GraphWD' ...
                 };
         end
         function n = getCompatibleGraphNumber()
-            n = Measure.getCompatibleGraphNumber('LocalEfficency');
+            n = Measure.getCompatibleGraphNumber('InLocalEfficency');
         end
     end
 end
--- braph/graph/measures/OutLocalEfficency.m
+++ braph/graph/measures/OutLocalEfficency.m
@@ -1,36 +1,36 @@
-classdef LocalEfficency < Measure
+classdef OutLocalEfficency < Measure
     methods
-        function m = LocalEfficency(g, varargin)
+        function m = OutLocalEfficency(g, varargin)
             m = m@Measure(g, varargin{:});
         end
     end
     methods (Access = protected)
-        function le = calculate(m)
+        function in_le = calculate(m)
             g = m.getGraph();
             A = g.getA();
             n = g.nodenumber();
-            le = zeros(n,1);
+            in_le = zeros(n,1);
             
             for i = 1:1:n
                 nodes = find(A(i, :)  | A(:, i).');  % neighbours of u
                 if numel(nodes) > 1
                     sub_graph = g.subgraph(nodes);
-                    le(i) = mean(GlobalEfficency(sub_graph, g.getSettings()).getValue());
+                    in_le(i) = mean(OutGlobalEfficency(sub_graph, g.getSettings()).getValue());
                 end
             end
         end
     end
     methods (Static)
         function measure_class = getClass()
-            measure_class = 'LocalEfficency';
+            measure_class = 'OutLocalEfficency';
         end
         function name = getName()
-            name = 'Local Efficency';
+            name = 'Out Local Efficency';
         end
         function description = getDescription()
             description = [ ...
-                'The local efficiency is the average inverse ' ...
-                'shortest path length with local nodes.' ...
+                'The out local efficiency is the average inverse ' ...
+                'shortest out path length with local nodes.' ...
                 ];
         end
         function bool = is_global()
@@ -44,12 +44,12 @@ classdef LocalEfficency < Measure
         end
         function list = getCompatibleGraphList()
             list = { ...               
-                'GraphBU', ...           
-                'GraphWU' ...
+                'GraphBD', ...           
+                'GraphWD' ...
                 };
         end
         function n = getCompatibleGraphNumber()
-            n = Measure.getCompatibleGraphNumber('LocalEfficency');
+            n = Measure.getCompatibleGraphNumber('OutLocalEfficency');
         end
     end
 end
