a7a638a48332d6636e0c05da34c52f9c90edc8fd
annacanal <31701548+annacanal@users.noreply.github.com>
2020-03-20 16:20:20 +0100

    Update DegreeAv.m
    #166

--- braph/graph/measures/DegreeAv.m
+++ braph/graph/measures/DegreeAv.m
@@ -1,6 +1,40 @@
 classdef DegreeAv < Degree
+    % DegreeAv < Measure: Average Degree measure
+    % DegreeAv provides the average degree of a graph for binary undirected
+    % (BU) and weighted undirected (WU) graphs. It is calculated as the 
+    % average of all number of edges connected to the node.
+    % Self-connections are excluded (i.e. the diagonal of the connection
+    % matrix is removed).
+    % 
+    % DegreeAv methods:
+    %   DegreeAv      - constructor with Degree properties.
+    %
+    % DegreeAv methods (Access=protected):
+    %   calculate	- calculates the average degree of a node.
+    % 
+    % DegreeAv methods (Static)
+    %   getClass	- returns the average degree class.
+    %   getName     - returns the name of average degree measure.
+    %   getDescription - returns the description of average degree measure.
+    %   is_global	- boolean, checks if average degree measure is global.
+    %   is_nodal    - boolean, checks if average degree measure is nodal.
+    %   is_binodal	- boolean, checks if average degree measure if binodal.
+    %   getMeasure  - returns the average degree class.
+    %   getCompatibleGraphList - returns a list of compatible graphs.
+    %   getCompatibleGraphNumber - returns the number of compatible graphs.
+    %
+    % See also Measure, Degree, Graph, Strength, Distance, Efficency.
+    
     methods
         function m = DegreeAv(g, varargin)
+            % DEGREEAV(G) creates average degree with default measure properties.
+            % G is a graph (e.g, an instance of GraphBD, GraphBU,
+            % GraphWD, Graph WU). 
+            %   
+            % DEGREEAV(G, 'Settings', SETTINGS) ceates average degree measure 
+            % and initializes the property settings with SETTINGS. 
+            %   
+            % See also Degree, Measure, Graph, StrengthAv, PathLengthAv. 
 
             settings = clean_varargin({}, varargin{:});
 
@@ -9,6 +43,10 @@ classdef DegreeAv < Degree
     end
     methods (Access=protected)
         function degree_av = calculate(m)
+            % CALCULATE calculates the average degree value of a node
+            %
+            % DEGREE = CALCULATE(M) returns the value of the average degree 
+            % of a graph.
             
             g = m.getGraph();
             
@@ -23,12 +61,32 @@ classdef DegreeAv < Degree
     end
     methods(Static)
          function measure_class = getClass()
+            % GETCLASS returns the measure class 
+            %            
+            % MEASURE_CLASS = GETCLASS() returns the class of the average
+            % degree measure.
+            %
+            % See also getName(), getDescription(). 
+            
             measure_class = 'DegreeAv';
         end
         function name = getName()
+            % GETNAME returns the measure name
+            %
+            % NAME = GETNAME() returns the name of the average degree measure.
+            %
+            % See also getClass(), getDescription(). 
+            
             name = 'Average Degree';
         end
         function description = getDescription()
+            % GETDESCRIPTION returns the degree description 
+            %
+            % DESCRIPTION = GETDESCRIPTION() returns the description of the
+            % average degree measure.
+            %
+            % See also getList(), getCompatibleGraphList().
+            
             description = [ ...
                 'The average degree of a graph is ' ...
                 'the average of all number of edges connected to the node. ' ...
@@ -36,22 +94,59 @@ classdef DegreeAv < Degree
                 ];
         end
         function bool = is_global()
-            
+            % IS_GLOBAL checks if degree measure is global
+            %
+            % BOOL = IS_GLOBAL() returns true if the average degree measure 
+            % is global and false otherwise.
+            %
+            % See also is_nodal, is_binodal.
+                                            
             bool = true;
         end
         function bool = is_nodal()
+            % IS_NODAL checks if degree measure is nodal
+            %
+            % BOOL = IS_NODAL() returns true if the average degree measure 
+            % is nodal and false otherwise.
+            %
+            % See also is_global, is_binodal. 
+                       
             bool = false;
         end
         function bool = is_binodal()
+            % IS_BINODAL checks if degree measure is binodal
+            %
+            % BOOL = IS_BINODAL() returns true if the average degree measure 
+            % is binodal and false otherwise.
+            %
+            % See also is_global, is_nodal.
+                       
             bool = false;
         end
-        function list = getCompatibleGraphList()  
+        function list = getCompatibleGraphList() 
+            % GETCOMPATIBLEGRAPHLIST returns the list of compatible graphs
+            % to average degree measure
+            %
+            % LIST = GETCOMPATIBLEGRAPHLIST() returns a cell array 
+            % of compatible graph classes to average degree measure. 
+            % The measure will not work if the graph is not compatible. 
+            %
+            % See also getCompatibleGraphNumber(). 
+            
             list = { ...
                 'GraphBU', ...
                 'GraphWU' ...
                 };
         end
         function n = getCompatibleGraphNumber()
+            % GETCOMPATIBLEGRAPHNUMBER returns the number of compatible
+            % graphs to average degree measure
+            %
+            % N = GETCOMPATIBLEGRAPHNUMBER() returns the number of
+            % compatible graphs to average degree measure.
+            % 
+            % See also getCompatibleGraphList().
+                      
             n = Measure.getCompatibleGraphNumber('DegreeAv');
         end
     end
