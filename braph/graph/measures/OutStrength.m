cdd090ff75a636d4ba194f98c022801eb7d751cb
egolol <egolol87@gmail.com>
2020-03-24 11:33:46 +0100

    Documentation for Strength
    #171 Documentation: Strenth, In & Out

--- braph/graph/measures/OutStrength.m
+++ braph/graph/measures/OutStrength.m
@@ -1,27 +1,83 @@
 classdef OutStrength < Measure
+    % OutStrength < Measure: Strength measure
+    % OutStrength provides the sum of all weights of the outward edges
+    % connected to a node. For weighted undirected (WD) graphs the OutStrength
+    % is the sum over the rows of the adjacency matrix.
+    %
+    % OutStrength methods:
+    %   OutStrength                      - constructor with Measure properties.
+    %
+    % OutStrength methods (Access=protected):
+    %   calculate                   - calculates the out-strength of a node.
+    %
+    % OutStrength methods (Static)
+    %   getClass                    - returns the out-strength class.
+    %   getName                     - returns the name of out-strength measure.
+    %   getDescription              - returns the description of out-strength measure.
+    %   is_global                   - boolean, checks if out-strength measure is global.
+    %   is_nodal                    - boolean, checks if out-strength measure is nodal.
+    %   is_binodal                  - boolean, checks if out-strength measure if binodal.
+    %   getMeasure                  - returns the out-strength class.
+    %   getCompatibleGraphList      - returns a list of compatible graphs.
+    %   getCompatibleGraphNumber    - returns the number of compatible graphs.
+    %
+    % See also Measure, Graph, Degree, Distance, Efficency.
+    
     methods
         function m = OutStrength(g, varargin)
-
+            % OUTSTRENGTH(G) creates out-strength with default measure
+            % properties. G is a graph (e.g, an instance of Graph WD).
+            %
+            % OUTSTRENGTH(G, 'Settings', SETTINGS) ceates out-strength
+            % measure and initializes the property settings with SETTINGS.
+            %
+            % See also Measure, Graph, Degree, Distance, Efficency.
+            
             settings = clean_varargin({}, varargin{:});
-
+            
             m = m@Measure(g, settings{:});
         end
-    end    
+    end
     methods (Access=protected)
         function out_strength = calculate(m)
+            % CALCULATE calculates the out_strength value of a node
+            %
+            % OUTSTRENGTH = CALCULATE(M) returns the value of the out_strength
+            % of a node.
+            
             g = m.getGraph();
-            A = g.getA();           
-            out_strength = sum(A, 2);  % row sum of A        
+            A = g.getA();
+            out_strength = sum(A, 2);  % row sum of A
         end
-    end  
+    end
     methods (Static)
-         function measure_class = getClass()
+        function measure_class = getClass()
+            % GETCLASS returns the measure class
+            %
+            % MEASURE_CLASS = GETCLASS() returns the class of the out-strength
+            % measure.
+            %
+            % See also getName(), getDescription().
+            
             measure_class = 'OutStrength';
         end
         function name = getName()
+            % GETNAME returns the measure name
+            %
+            % NAME = GETNAME() returns the name of the out-strength measure.
+            %
+            % See also getClass(), getDescription().
+            
             name = 'Out-Strength';
         end
         function description = getDescription()
+            % GETDESCRIPTION returns the out-strength description
+            %
+            % DESCRIPTION = GETDESCRIPTION() returns the description of the
+            % out-strength measure.
+            %
+            % See also getList(), getCompatibleGraphList().
+            
             description = [ ...
                 'The out-strength of a node is ' ...
                 'the sum of the weights of all ' ...
@@ -31,21 +87,56 @@ classdef OutStrength < Measure
                 'the weighted connectivity matrix. ' ...
                 ];
         end
-        function bool = is_global()                        
+        function bool = is_global()
+            % IS_GLOBAL checks if out-strength measure is global (false)
+            %
+            % BOOL = IS_GLOBAL() returns false.
+            %
+            % See also is_nodal, is_binodal.
+            
             bool = false;
         end
         function bool = is_nodal()
+            % IS_NODAL checks if out-strenth measure is nodal (true)
+            %
+            % BOOL = IS_NODAL() returns true.
+            %
+            % See also is_global, is_binodal.
+            
             bool = true;
         end
         function bool = is_binodal()
+            % IS_BINODAL checks if out-strength measure is binodal (false)
+            %
+            % BOOL = IS_BINODAL() returns false.
+            %
+            % See also is_global, is_nodal.
+            
             bool = false;
         end
-        function list = getCompatibleGraphList()  
+        function list = getCompatibleGraphList()
+            % GETCOMPATIBLEGRAPHLIST returns the list of compatible graphs
+            % to out-strength.
+            %
+            % LIST = GETCOMPATIBLEGRAPHLIST() returns a cell array
+            % of compatible graph classes to out-strength.
+            % The measure will not work if the graph is not compatible.
+            %
+            % See also getCompatibleGraphNumber().
+            
             list = { ...
                 'GraphWD' ...
                 };
         end
         function n = getCompatibleGraphNumber()
+            % GETCOMPATIBLEGRAPHNUMBER returns the number of compatible
+            % graphs to out-strength.
+            %
+            % N = GETCOMPATIBLEGRAPHNUMBER() returns the number of
+            % compatible graphs to out-strength.
+            %
+            % See also getCompatibleGraphList().
+            
             n = Measure.getCompatibleGraphNumber('OutStrength');
         end
     end
