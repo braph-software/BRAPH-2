04b6185b04f29639f1c5304d65df2687c8d1cdc7
egolol <egolol87@gmail.com>
2020-04-06 15:56:38 +0200

    small commit,
    half of the way

--- braph/cohort/Subject.m
+++ braph/cohort/Subject.m
@@ -1,4 +1,48 @@
 classdef Subject < handle & matlab.mixin.Copyable
+    % Subject < handle & matlab.mixin.Copyable (Abstract): A subject 
+    % Subject provides the methods necessary for all subjects.
+    % Instances of ths class cannot be created.
+    % Use one of the subclasses (e.g., SubjectMRI, SubjectfMRI, SubjectDTI)
+    % The subclasses must be created inside the folder ./braph/cohort/subjects/.
+    %
+    % Subject properties (GetAccess=protected, SetAccess=protected):
+    %   id                      - id of the subject.
+    %   atlases                 - cell array with brain atlases.
+    %   datadict                - dictionary with subject data
+    %                             (key, value) = (char, subject data type)
+    %
+    % Subject methods (Access=protected)
+    %   Subject                 - Constructor.
+    %   sub_copy                - deep copy community structure.
+    %
+    % Subject methods:    
+    %   tostring                - returns a string representing the subject.
+    %   disp                    - displays the subject.
+    %   setID                   - sets the ID.
+    %   getID                   - returns the ID.
+    %   getData                 - returns the data from DATADICT
+    %   setBrainAtlases         - sets the brain atlases
+    %   getBrainAtlases         - returns the brain atlases from ATLASES
+    %
+    % Subject methods (Abstract, Access=protected)
+    %   initialize_datadict     - initializes the data dictionary DATADICT.
+    %   update_brainatlases     - updates the brain atlases in DATADICT.
+    %
+    % Subject methods (Static)
+    %   getList                 - returns a list with subclasses of Subject
+    %   getClass                - returns the class of the subclass.
+    %   getName                 - returns the name of the subclass.
+    %   getDescription          - returns the description of the subclass.
+    %   getBrainAtlasNumber     - returns the number of elements of Atlases
+    %   getDataList             - returns the type of data of the subclass
+    %   getDataNumber           - returns the number of elements of DATADICT
+    %   getDataCodes            - returns the key codes of the data.
+    %   getDataClasses          - returns the class of the type of all data of the subclass.
+    %   getDataClass            - returns the class of the type of a data of the subclass.
+    %   getSubject              - returns a new instantiation of a subclass
+    %
+    % See also Group, Cohort, SubjectDTI, SubjectMRI, SubjectfMRI.
+    
     properties (GetAccess=protected, SetAccess=protected)
         id  % subject id
         atlases % cell array with brain atlases
@@ -6,10 +50,17 @@ classdef Subject < handle & matlab.mixin.Copyable
     end
     methods (Access=protected)
         function sub = Subject(atlases, varargin)
+            % SUBJECT(ATLASES, VARARGIN) creates a subject with the
+            % properties of VARARGIN. ATLASES is the brain atlases that subject will use.
+            % VARARGIN contains information about the subject id and the
+            % data codes utilized in the DATADICT.
+            % This method is only accessible by the subclasses of Subject.
+            %
+            % See also Group, Cohort, SubjectMRI, SubjectfMRI, SubjectDTI
             
             assert(iscell(atlases), ...
                 ['BRAIN:Subject:AtlasErr'], ...
-                ['The input must be a cell containing BrainAtlas objects'])
+                ['The input must be a cell containing BrainAtlas objects']) %#ok<NBRAK>
             sub.atlases = atlases;
             
             id = get_from_varargin(now(), 'SubjectID', varargin{:});
@@ -25,6 +76,16 @@ classdef Subject < handle & matlab.mixin.Copyable
             end
         end
         function sub_copy = copyElement(sub)
+            % COPYELEMENT(SUB) copies elements of subject.
+            %
+            % SUB_COPY = COPYELEMENT(SUB) copies elements of the subject SUB.
+            % Makes a deep copy of the structure of subject. But it does
+            % not make a deep copy of the brain atlases in the data
+            % objects.
+            %
+            % See also Cohort, Group, Graph.
+            
+            
             % IMPORTANT! It does NOT make a deep copy of the BrainAtlas
             % atlas in the data objects
             
@@ -43,9 +104,23 @@ classdef Subject < handle & matlab.mixin.Copyable
     end
     methods
         function str = tostring(subject)
+            % TOSTRING string with information about the subject
+            %
+            % STR = TOSTRING(SUBJECT) returns string with the subject class and id.
+            %
+            % See also disp().
+            
             str = [Subject.getClass(subject) ' ' tostring(subject.getID())];
         end
         function disp(subject)
+            % DISP displays information about the subject
+            %
+            % DISP(SUBJECT) displays information about the subject.
+            % It provides information about subject class, id and data
+            % codes.
+            %
+            % See also tostring().
+            
             disp(['<a href="matlab:help ' Subject.getClass(subject) '">' Subject.getClass(subject) '</a>' ...
                 ' ID:' tostring(subject.getID())])
             data_codes = subject.getDataCodes();
@@ -56,18 +131,50 @@ classdef Subject < handle & matlab.mixin.Copyable
             end
         end
         function setID(sub, id)
+            % SETID sets the id of the subject.
+            %
+            % ID = SETID(SUBJECT, ID) sets the id of the subject.
+            %
+            % See also getID(), getData(), setBrainAtlases().
+            
             sub.id = id;
         end
         function id = getID(sub)
+            % GETID returns the id of the subject.
+            %
+            % ID = GETID(SUBJECT) returns the id of the subject.
+            %
+            % See also setID(), getData(), setBrainAtlases().
+            
             id = sub.id;
         end
         function d = getData(sub, data_code)
+            % GETDATA returns the data from the DATADICT
+            %
+            % D = GETDATA(SUB, DATACODE) returns the data from the DATADICT
+            %
+            % See also getID(), getBrainAtlases().
+            
             d = sub.datadict(data_code);
         end
         function setBrainAtlases(sub, atlases)
+            % SETBRAINATLASES sets the of the subject.
+            %
+            % ID = SETBRAINATLASES(SUBJECT, ATLASES) sets the array ATLASES
+            % with brain atlases. 
+            %
+            % See also getID(), getData(), getData().
+            
             sub.update_brainatlases(atlases);
         end
         function atlases = getBrainAtlases(sub)
+            % GETBRAINATLASES returns the brain atlases from the ATLASES.
+            %
+            % ATLASES = GETBRAINATLASES(SUB) returns the brain atlases from 
+            % the ATLASES.
+            %
+            % See also getID(), getDATA().
+            
             atlases = sub.atlases;
         end
     end
@@ -77,12 +184,29 @@ classdef Subject < handle & matlab.mixin.Copyable
     end
     methods (Static)
         function subject_list = getList()
+            % GETLIST returns the list of available subjects
+            %
+            % SUBJECT_LIST = GETLIST() returns the list of subjects
+            % (cell array) that are subclasses of Subject.
+            %
+            % See also getClass(), getName()
+            
             subject_list = subclasses( ...
                 'Subject', ...
                 [fileparts(which('Subject')) filesep 'subjects'] ...
                 );
         end
         function subject_class = getClass(sub)
+            % GETCLASS returns the class of the subject
+            %
+            % SUBJECT_CLASS = GETCLASS(SUB) returns the class of the concrete
+            % instance of subject SUB.
+            %
+            % SUBJECT_CLASS = GETCLASS(SUBJECT_CLASS) returns the class of the
+            % subject whose class is the string SUBJECT_CLASS.
+            %
+            % See also getList(), getDescription().
+            
             % subject class (same as the subject object name)
             
             if isa(sub, 'Subject')
@@ -92,42 +216,131 @@ classdef Subject < handle & matlab.mixin.Copyable
             end
         end
         function name = getName(sub)
+            % GETNAME returns the name of the subject
+            %
+            % NAME = GETNAME(SUB) returns the name (string) of the concrete instance
+            % of subject SUB.
+            %
+            % NAME = GETNAME(SUBJECT_CLASS) returns the name (string) of the
+            % subject whose class is the string SUBJECT_CLASS.
+            %
+            % See also getList(), getClass().
+            
             % subject name
             
             name = eval([Subject.getClass(sub) '.getName()']);
         end
         function description = getDescription(sub)
+            % GETDESCRIPTION returns the description of the subject
+            %
+            % DESCRIPTION = GETDESCRIPTION(SUB) returns the description
+            % (string) of the concrete instance of subject SUB
+            %
+            % DESCRIPTION = GETDESCRIPTION(SUBJECT_CLASS) returns the
+            % description (string) of the subject whose class is SUBJECT_CLASS.
+            %
+            % See also getList(), getName(), getClass().
+            
             % subject description
             
             description = eval([Subject.getClass(sub) '.getDescription()']);
         end
         function atlas_number = getBrainAtlasNumber(sub)
+            % GETBRAINATLASNUMBER returns the number of brain atlases
+            %
+            % N = GETBRAINATLASNUMBER(SUB) returns the number of
+            % brain atlases to the concrete subject SUB.
+            %
+            % N = GETBRAINATLASNUMBER(SUBJECT_CLASS) returns the number
+            % of brain atlases to the subject whose class is SUBJECT_CLASS.
+            %
+            % See also getList(), getDescription(), getName(), getClass().
+            
             % number of differetn brain atlases
             
             atlas_number = eval([Subject.getClass(sub) '.getBrainAtlasNumber()']);
         end
-        function datalist = getDataList(sub)
+        function datalist = getDataList(sub)            
+            % GETDATALIST returns the list of data
+            %
+            % CELL ARRAY = GETDATALIST(SUB) returns a cell array of
+            % subject data.
+            %
+            % CELL ARRAY = GETDATALIST(SUBJECT_CLASS) returns a 
+            % cell array of subject data to the subject whose class is
+            % SUBJECT_CLASS.
+            %
+            % See also getList()
+            
             % list of subject data keys
             
             datalist = eval([Subject.getClass(sub) '.getDataList()']);
         end
         function data_number = getDataNumber(sub)
+            % GETDATANUMBER returns the number of data.
+            %
+            % N = GETDATANUMBER(SUB) returns the number of data of the
+            % subclass SUB
+            %
+            % N = GETDATANUMBER(SUBJECT_CLASS) returns the number
+            % of  data to the subject whose class is SUBJECT_CLASS.
+            %
+            % See also getDataList(), getBrainAtlasNumber().
+            
             datalist = Subject.getDataList(sub);
             data_number = length(datalist);
         end
         function data_codes = getDataCodes(sub)
+            % GETDATACODES returns the list of data keys
+            %
+            % CELL ARRAY = GETDATACODES(SUB) returns a cell array of
+            % subject data keys.
+            %
+            % CELL ARRAY = GETDATACODES(SUBJECT_CLASS) returns a 
+            % cell array of subject data keys to the subject whose class is
+            % SUBJECT_CLASS.
+            %
+            % See also getList().
+            
             datalist = Subject.getDataList(sub);
             data_codes = keys(datalist);
         end
         function data_classes = getDataClasses(sub)
+            % GETDATACLASSES returns the list of data classes
+            %
+            % CELL ARRAY = GETDATACLASSES(SUB) returns a cell array of
+            % subject data classes.
+            %
+            % CELL ARRAY = GETDATACLASSES(SUBJECT_CLASS) returns a 
+            % cell array of subject data classes to the subject whose class is
+            % SUBJECT_CLASS.
+            %
+            % See also getList(), getDataCodes(), getDataClass()
+            
             datalist = Subject.getDataList(sub);
             data_classes = values(datalist);
         end
         function data_class = getDataClass(sub, data_code)
+            % GETDATACLASS returns the class of a data.
+            %
+            % DATA_CLASS = GETDATACLASS(SUB, DATACODE) returns the class of
+            % data with code DATACODE  
+            %
+            % See also getList(), getDataClasses().
+            
             datalist = Subject.getDataList(sub);
             data_class = datalist(data_code);
         end
         function sub = getSubject(subject_class, varargin)
+            % GETSUBJECT returns a subject 
+            %
+            % SUB = GETSUBJECT(SUBJECT_CLASS, VARARGIN) returns an instance
+            % of the class of the subject SUBJECT_CLASS with arguments
+            % VARARGIN. It initializes the new instance of the subject
+            % class.
+            %
+            % See also getList(), getDataClass().
+            
             sub = eval([subject_class '(varargin{:})']);
         end
     end
--- braph/cohort/subjects/SubjectDTI.m
+++ braph/cohort/subjects/SubjectDTI.m
@@ -1,6 +1,36 @@
 classdef SubjectDTI < Subject
+    % SubjectDTI < Subject: A subject of type DTI.
+    % SubjectDTI represents a subject of type DTI.
+    %
+    % SubjectDTI methods (Access=protected)
+    %   SubjectDTI                 - Constructor.
+    %
+    % SubjectDTI methods (Access=protected)
+    %   initialize_datadict     - initializes the data dictionary DATADICT.
+    %   update_brainatlases     - updates the brain atlases in DATADICT.
+    %
+    % SubjectDTI methods (Static)
+    %   getClass                - returns the class of SubjectDTI.
+    %   getName                 - returns the name of  SubjectDTI.
+    %   getDescription          - returns the description of SubjectDTI.
+    %   getBrainAtlasNumber     - returns the number of elements of Atlases
+    %   getDataList             - returns the type of data of SubjectDTI
+    %   getDataNumber           - returns the number of elements of DATADICT
+    %   getDataCodes            - returns the key codes of the data for SubjectDTI
+    %   getDataClasses          - returns the class of the type of all data of SubjectDTI.
+    %   getDataClass            - returns the class of the type of a data of SubjectDTI.
+    %   getSubject              - returns a new instantiation of SubjectDTI
+    %
+    % See also Group, Cohort, SubjectMRI, SubjectfMRI, Subject.
     methods
         function sub = SubjectDTI(atlas, varargin)
+            % SUBJECTDTI(ATLASES, VARARGIN) creates a subject of type dti with the 
+            % properties VARARGIN. ATLASES is the brain atlases that subject dti will use.
+            % VARARGIN contains information about the subject dti id and the
+            % data codes utilized in the DATADICT. This method passes
+            % varargin to its super constructor Subject. 
+            %
+            % See also See also Group, Cohort, SubjectMRI, SubjectfMRI, Subject.
 
             if isa(atlas, 'BrainAtlas') 
                 atlases = {atlas};
@@ -16,6 +46,12 @@ classdef SubjectDTI < Subject
     end
     methods (Access=protected)
         function initialize_datadict(sub, varargin)
+            % INITIALIZE_DATADICT initializes the data dictionary
+            %
+            % INITIALIZE_DATADICT(SUB, VARARGIN) initializes the data
+            % ditionary with data type and data code of subject dti.
+            %
+            % See also update_brainatlases().
 
             atlases = sub.getBrainAtlases();
             atlas = atlases{1};
@@ -25,6 +61,12 @@ classdef SubjectDTI < Subject
             sub.datadict('DTI') = DataConnectivity(atlas);
         end
         function update_brainatlases(sub, atlases)
+            % UPDATE_BRAINATLASES updates the atlases of the subject dti
+            % 
+            % UPDATE_BRAINATLASES(SUB, ATLASES) updates the atlases of the
+            % subject dti using the new values ATLASES.
+            % 
+            % See also initialize_datadict()
 
             sub.atlases = atlases;
             atlas = atlases{1};
@@ -38,36 +80,101 @@ classdef SubjectDTI < Subject
     end
     methods (Static)
         function subject_class = getClass()
+            % GETCLASS returns the class of the subject
+            %
+            % SUBJECT_CLASS = GETCLASS() returns the class SubjectDTI
+            %
+            % See also getList(), getDescription(), getName().
+            
                 subject_class = 'SubjectDTI';
         end
         function name = getName()
+            % GETNAME returns the name of the subject
+            %
+            % NAME = GETNAME() returns the name: Subject DTI.
+            %
+            % See also getList(), getClass().
+            
             name = 'Subject DTI';
         end
         function description = getDescription()
+            % GETDESCRIPTION returns the description of the subject
+            %
+            % DESCRIPTION = GETDESCRIPTION() returns the description
+            % of SubjectDTI
+            %
+            % See also getList(), getName(), getClass().
+            
             description = [ ...
                 'Subject with DTI connectivity matrix, ' ...
                 'such as activation timeseries for each brain region' ...
                 ];
         end
         function atlas_number = getBrainAtlasNumber()
+            % GETBRAINATLASNUMBER returns the number of brain atlases
+            %
+            % N = GETBRAINATLASNUMBER() returns the number of
+            % brain atlases, in this case 1.
+            %   
+            % See also getList(), getDescription(), getName(), getClass().
+            
             atlas_number = 1;
         end
         function datalist = getDataList()
+            % GETDATALIST returns the list of data
+            %
+            % CELL ARRAY = GETDATALIST() returns a cell array of
+            % subject data. For Subject DTI, the data list is: 
+            %   Age             -    DataScalar.
+            %   DTI             -    DataConnectivity.
+            %
+            % See also getList()
             
             datalist = containers.Map('KeyType', 'char', 'ValueType', 'char');
             datalist('age') = 'DataScalar';
             datalist('DTI') = 'DataConnectivity';            
         end
         function data_number = getDataNumber()
+            % GETDATANUMBER returns the number of data.
+            %
+            % N = GETDATANUMBER() returns the number of data.        
+            %
+            % See also getDataList(), getBrainAtlasNumber().
+            
             data_number = Subject.getDataNumber('SubjectDTI');
         end
         function data_codes = getDataCodes()
+            % GETDATACODES returns the list of data keys
+            %
+            % CELL ARRAY = GETDATACODES(SUB) returns a cell array of
+            % subject dti data keys.          
+            %
+            % See also getList(), ().
+            
             data_codes = Subject.getDataCodes('SubjectDTI');
         end
         function data_classes = getDataClasses()
+            % GETDATACLASSES returns the list of data classes
+            %
+            % CELL ARRAY = GETDATACLASSES(SUB) returns a cell array of
+            % subject dti data classes.
+            %
+            % CELL ARRAY = GETDATACLASSES(SUBJECT_CLASS) returns a 
+            % cell array of subject dti data classes to the subject whose class is
+            % SUBJECT_CLASS.
+            %
+            % See also getList(), getDataCodes(), getDataClass().
+            
             data_classes = Subject.getDataClasses('SubjectDTI');
         end
         function data_class = getDataClass(data_code)
+            % GETDATACLASS returns the class of a data.
+            %
+            % DATA_CLASS = GETDATACLASS(SUB, DATACODE) returns the class of
+            % data with code DATACODE  
+            %
+            % See also getList(), getDataClasses().
+            
             data_class = Subject.getDataNumber('SubjectDTI', data_code);
         end
     end
