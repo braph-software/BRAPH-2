e79fed8925ec48f392cf639e4ea849354fd6c26b
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-04-15 08:48:16 +0200

    Delete excludeSuffix.m

--- braph/util/excludeSuffix.m
+++
@@ -1,5 +0,0 @@
-function clean_list = excludeSuffix(suffix, superclass, varargin)
-clean_list = varargin{:};
-element_to_exclude = strcat(superclass, suffix);
-clean_list = setdiff(clean_list, {element_to_exclude});
-end
