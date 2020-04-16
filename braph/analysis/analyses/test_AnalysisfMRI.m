8023842e946614ff6a87958c78f9f7528608327e
egolol <egolol87@gmail.com>
2020-04-16 14:01:37 +0200

    update to tests
    forgot missing tests

--- braph/analysis/analyses/test_AnalysisfMRI.m
+++ braph/analysis/analyses/test_AnalysisfMRI.m
@@ -11,8 +11,28 @@ sub2 = SubjectfMRI(atlas, 'SubjectID', '2');
 sub3 = SubjectfMRI(atlas, 'SubjectID', '3');
 sub4 = SubjectfMRI(atlas, 'SubjectID', '4');
 sub5 = SubjectfMRI(atlas, 'SubjectID', '5');
+group1 = Group('SubjectfMRI', {sub1, sub2, sub3, sub4, sub5}, 'GroupName', 'GroupTestfMRI1');
+group2 = Group('SubjectfMRI', {sub1, sub2, sub3, sub4, sub5}, 'GroupName', 'GroupTestfMRI2');
 
 cohort = Cohort('Cohort fMRI', 'SubjectfMRI', atlas, {sub1, sub2, sub3, sub4, sub5});
+cohort.getGroups().add(group1.getName(), group1)
+cohort.getGroups().add(group2.getName(), group2)
 
 %% Test 1: Instantiation
+analysis = AnalysisfMRI(cohort, {}, {}, {}); %#ok<*NASGU>
+
+%% Test 2: Create correct ID
 analysis = AnalysisfMRI(cohort, {}, {}, {});
+measurement_id = analysis.getMeasurementID('Degree');
+comparison_id = analysis.getComparisonID('Distance');
+randomcomparison_id = analysis.getRandomComparisonID('PathLength');
+
+assert(ischar(measurement_id), ...
+    ['BRAPH:AnalysisDTI:getMeasurementID'], ...
+    ['.getMeasurementID() not creating an ID']) %#ok<*NBRAK>
+assert(ischar(randomcomparison_id), ...
+    ['BRAPH:AnalysisDTI:getMeasurementID'], ...
+    ['.getMeasurementID() not creating an ID']) %#ok<*NBRAK>
+assert(ischar(comparison_id), ...
+    ['BRAPH:AnalysisDTI:getMeasurementID'], ...
+    ['.getMeasurementID() not creating an ID']) %#ok<*NBRAK>
