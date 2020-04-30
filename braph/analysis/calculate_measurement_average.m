25c9d1cc7fe4975e08316d1dfd20a450afb9e319
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-04-30 15:08:23 +0200

    Update calculate_measurement_average.m

--- braph/analysis/calculate_measurement_average.m
+++ braph/analysis/calculate_measurement_average.m
@@ -1,13 +1,9 @@
-function measure_average = calculate_measurement_average(measure_code, measures)
-if Measure.is_binodal(measure_code)
-    suma = zeros(size(measures{1}));
-    for i=1:1:numel(measures)
-        suma = suma + measures{i};
-    end
-    measure_average = suma / numel(measures);
-elseif Measure.is_nodal(measure_code)
-    measure_average = mean([measures{:}], 2);
-else
-    measure_average = sum(cellfun(@sum, measures)) ./ sum(cellfun(@length, measures));
+function measure_average = calculate_measurement_average(measure_code, measure_values)
+
+suma = zeros(size(measure_values{1}));
+for i=1:1:numel(measure_values)
+    suma = suma + measure_values{i};
 end
+measure_average = suma / numel(measure_values);
+
 end
