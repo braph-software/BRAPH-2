f0aca4f856bf271185ef310e41f0da6b158f318f
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-04-30 15:25:32 +0200

    polish phase 1

--- braph/analysis/calculate_measurement_average.m
+++
@@ -1,9 +0,0 @@
-function measure_average = calculate_measurement_average(measure_code, measure_values)
-
-suma = zeros(size(measure_values{1}));
-for i=1:1:numel(measure_values)
-    suma = suma + measure_values{i};
-end
-measure_average = suma / numel(measure_values);
-
-end
