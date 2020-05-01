a591688961f1a8bf73d2a02cb1fe2b76355909e9
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-05-01 13:29:47 +0200

    initial commit

---
+++ braph/workflows/DTI/test_ComparisonDTI.m
@@ -0,0 +1,37 @@
+% test ComparisionDTI
+br1 = BrainRegion('BR1', 'brain region 1', 1, 11, 111);
+br2 = BrainRegion('BR2', 'brain region 2', 2, 22, 222);
+br3 = BrainRegion('BR3', 'brain region 3', 3, 33, 333);
+br4 = BrainRegion('BR4', 'brain region 4', 4, 44, 444);
+br5 = BrainRegion('BR5', 'brain region 5', 5, 55, 555);
+atlas = BrainAtlas('brain atlas', {br1, br2, br3, br4, br5});
+
+subject_class = 'SubjectDTI';
+
+sub1 = Subject.getSubject(subject_class, repmat({atlas}, 1, Subject.getBrainAtlasNumber(subject_class)), 'SubjectID', 1);
+sub2 = Subject.getSubject(subject_class, repmat({atlas}, 1, Subject.getBrainAtlasNumber(subject_class)), 'SubjectID', 2);
+sub3 = Subject.getSubject(subject_class, repmat({atlas}, 1, Subject.getBrainAtlasNumber(subject_class)), 'SubjectID', 3);
+sub4 = Subject.getSubject(subject_class, repmat({atlas}, 1, Subject.getBrainAtlasNumber(subject_class)), 'SubjectID', 4);
+sub5 = Subject.getSubject(subject_class, repmat({atlas}, 1, Subject.getBrainAtlasNumber(subject_class)), 'SubjectID', 5);
+group = Group(subject_class, {sub1, sub2, sub3 sub4, sub5});
+
+%% Test 1: Instantiation
+comparison = ComparisonDTI('c1', atlas,  repmat({group}, 1, ComparisonDTI.getGroupNumber()), 'ComparisonDTI.measure_code', 'Degree');
+
+%% Test 2: Comparison Initialization with values
+
+% % setup
+% dm = ;
+% dall = ;
+% p_single = ;
+% p_double = ;
+% eprcentiles = ;
+% % act
+% comparison = ComparisonDTI('c1', atlas, reptmat({group}, 1, ComparisonDTI.getGroupNumber()), ...
+%     'ComparisonDTI.measure_code', 'Degree', ...
+%     'ComparisonDTI.difference_mean', dm, ...
+%     'ComparisonDTI.difference_all', dall, ...
+%     'ComparisonDTI.p_single', p_single, ...
+%     'ComparisonDTI.p_double', p_double, ...
+%     'ComparisonDTI.percentiles', percentiles ...
+%     );
