58ea6cfb37359baa4a2685b295b0930caac10abb
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-05-01 07:08:25 +0200

    Update braph2.m

--- braph/braph2.m
+++ braph/braph2.m
@@ -12,22 +12,25 @@
 clc
 format long
 
-dir = fileparts(which('braph2'));
+directory = fileparts(which('braph2'));
 
-addpath(dir)
-addpath([dir filesep 'util'])
-addpath([dir filesep 'graph'])
-addpath([dir filesep 'graph' filesep 'graphs'])
-addpath([dir filesep 'graph' filesep 'measures'])
-addpath([dir filesep 'atlas'])
-% addpath([dir filesep 'cohort'])
-% addpath([dir filesep 'cohort' filesep 'datas'])
-% addpath([dir filesep 'cohort' filesep 'subjects'])
-% addpath([dir filesep 'analysis'])
-% addpath([dir filesep 'analysis' filesep 'analyses'])
-% addpath([dir filesep 'analysis' filesep 'measurements'])
-% addpath([dir filesep 'analysis' filesep 'comparisons'])
-% addpath([dir filesep 'analysis' filesep 'randomcomparisons'])
-addpath([dir filesep 'test'])
+addpath(directory)
+addpath([directory filesep 'util'])
+addpath([directory filesep 'graph'])
+addpath([directory filesep 'graph' filesep 'graphs'])
+addpath([directory filesep 'graph' filesep 'measures'])
+addpath([directory filesep 'atlas'])
+addpath([directory filesep 'cohort'])
+addpath([directory filesep 'cohort' filesep 'datas'])
+addpath([directory filesep 'analysis'])
+addpath([directory filesep 'test'])
 
-clear dir
+addpath([directory filesep 'workflows'])
+workflows_directories = dir([directory filesep 'workflows']);   % get the folder contents
+workflows_directories = workflows_directories([workflows_directories(:).isdir] == 1);  % remove all files (isdir property is 0)
+workflows_directories = workflows_directories(~ismember({workflows_directories(:).name}, {'.', '..'}));  % remove '.' and '..'
+for i = 1:1:length(workflows_directories)
+    addpath([directory filesep 'workflows' filesep workflows_directories(i).name])
+end
+
+clear directory workflows_directories i
