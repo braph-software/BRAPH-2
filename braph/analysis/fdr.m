4d92fc625de865f4bfe2a902992e78a253d67665
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-05-02 06:19:46 +0200

    Update fdr.m

--- braph/analysis/fdr.m
+++ braph/analysis/fdr.m
@@ -1,25 +1,24 @@
-function r = fdr(pvalues,q)
+function r = fdr(pvalues, q)
 % FDR False discovery rate
 %
-% R = FDR(PVALUES,Q) calculates the false discovery rate (FDR) for PVALUES.
+% R = FDR(PVALUES, Q) calculates the false discovery rate (FDR) for PVALUES.
 %   Q is parameter whose default value is set to 0.05.
 %
-% See also pvalue1, pvalue2.
-
-% Author: Giovanni Volpe
-% Date: 2016/01/01
+% See also pvalue1, pvalue2, bonferroni, quantiles.
 
 % q value [default = 0.05]
 if nargin<2
     q = 0.05;
 else
-    Check.isreal('Q must be a real number in (0,1]',q,'>',0,'<=',1)
+    assert(q > 0 && q <= 1, ...
+        ['BRAPH:fdr:Q'], ...
+        ['Q must be a real number in (0,1]']) %#ok<*NBRAK>
 end
 
-x = [1:1:length(pvalues)]/length(pvalues);
+x = [1:1:length(pvalues)] / length(pvalues);
 pvalues = sort(pvalues);
 
-index = sum(pvalues<=q*x);
+index = sum(pvalues <= q * x);
 if index
     r = pvalues(index);
 else
