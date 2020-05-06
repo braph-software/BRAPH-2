da543e9a6b634bb1806ceedb22dab3423126abef
egolol <egolol87@gmail.com>
2020-05-06 19:36:33 +0200

    Test for Analysis Copy

--- braph/analysis/Analysis.m
+++ braph/analysis/Analysis.m
@@ -74,7 +74,8 @@ classdef Analysis < handle & matlab.mixin.Copyable
                 measurement = analysis.getMeasurements().getValue(measurement_i);
                 measurement_copy = measurement.copy();
                 measurement_copy.setBrainAtlases(analysis_copy.cohort.getBrainAtlases());
-                measurement_copy.setGroup(analysis_copy.cohort.getGroups());
+                groups_copy = analysis_copy.cohort.getGroups().getValues();
+                measurement_copy.setGroup(groups_copy{measurement_i});
                 analysis_copy.measurement_idict.add(tostring(measurement_copy.getID()), measurement_copy, measurement_i);
             end
             
@@ -84,7 +85,8 @@ classdef Analysis < handle & matlab.mixin.Copyable
                 randomcomparison = analysis.getRandomComparisons().getValue(randomcomparisons_i);
                 randomcomparison_copy = randomcomparison.copy();
                 randomcomparison_copy.setBrainAtlases(analysis_copy.cohort.getBrainAtlases());
-                randomcomparison_copy.setGroup(analysis_copy.cohort.getGroups());
+                groups_copy = analysis_copy.cohort.getGroups().getValues();
+                randomcomparison_copy.setGroup(groups_copy{randomcomparisons_i});
                 analysis_copy.randomcomparison_idict.add(tostring(randomcomparison_copy.getID()), randomcomparison_copy, randomcomparisons_i);
             end
             
@@ -94,7 +96,8 @@ classdef Analysis < handle & matlab.mixin.Copyable
                 comparison = analysis.getComparisons().getValue(comparisons_i);
                 comparison_copy = comparison.copy();
                 comparison_copy.setBrainAtlases(analysis_copy.cohort.getBrainAtlases());
-                comparison_copy.setGroups(analysis_copy.cohort.getGroups());
+                groups_copy = analysis_copy.cohort.getGroups().getValues();
+                comparison_copy.setGroups({groups_copy{1} groups_copy{2}});
                 analysis_copy.comparison_idict.add(tostring(comparison_copy.getID()), comparison_copy, comparisons_i);
             end
         end
