b93538efb70dd83a01d035861c0ddaf86d004418
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-05-08 08:45:21 +0200

    Create BRAPH2.m

---
+++ braph2/util/BRAPH2.m
@@ -0,0 +1,46 @@
+classdef BRAPH2
+    properties (Constant)
+        % BRAPH2 ID Card
+        NAME = 'Braph 2.0'
+        STR = 'BRAPH2'
+
+        % Error codes
+        WRONG_INPUT = 'WrongInput'  % wrong input value or format in a function
+        WRONG_OUTPUT = 'WrongOutput'  % wrong output value or format in a function
+
+%         BUILD = 2020
+%         VERSION = '2.0.0'
+%         DATE = ''
+%         AUTHORS = ''
+%         COPYRIGHT = ['Copyright 2014-' datestr(now,'yyyy')]
+% 
+%         % Braph
+%         COLOR = [.9 .4 .1]
+%         FONT = 'Helvetica'
+% 
+%         % Variable type codes
+%         STRING = 1
+%         NUMERIC = 2
+%         LOGICAL = 3
+%         
+%         % file formats and dialogs
+%         MSG_GETDIR = 'Select directory'
+%         MSG_PUTDIR = 'Select directory'
+%         
+%         MAT_EXTENSION = '*.mat';
+%         MAT_MSG_GETFILE = 'Select MAT file';
+%         MAT_MSG_PUTFILE = 'Select MAT file';
+%         
+%         XLS_EXTENSION = {'*.xlsx';'*.xls'};
+%         XLS_MSG_GETFILE = 'Select Excel file';
+%         XLS_MSG_PUTFILE = 'Select Excel file';       
+% 
+%         TXT_EXTENSION = '*.txt';
+%         TXT_MSG_GETFILE = 'Select TXT file';
+%         TXT_MSG_PUTFILE = 'Select TXT file';
+%         
+%         JSON_EXTENSION = '*.json'
+%         JSON_MSG_GETFILE = 'Select JSON file'
+%         JSON_MSG_PUTFILE = 'Select JSON file'        
+    end
+end
