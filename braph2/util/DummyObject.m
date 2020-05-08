771fe46f502356b20c8693a9dc7291187de2ca1c
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-05-08 14:03:07 +0200

    Update DummyObject.m

--- braph2/util/DummyObject.m
+++ braph2/util/DummyObject.m
@@ -1,12 +1,16 @@
-classdef DummyObject
+classdef DummyObject < handle & matlab.mixin.Copyable
     % DummyObject Dummy object.
     % Dummy objects used for unit testing and debugging.
+    % It implements handle and matlab.mixin.Copyable.
     %
     % DummyObject properties:
     %   name
     %
     % DummyObject methods:
     %   DummyObject     - constructor
+    %   tostring        - returns a string describing the object
+    %
+    % See also handle, matlab.mixin.Copyable.
     
     properties
         name
@@ -16,6 +20,13 @@ classdef DummyObject
             % DummyObject(NAME) creates a dummy object with the property NAME.
 
             obj.name = name;
+        end
+        function str = tostring(obj)
+            % TOSTRING string with information about the object
+            %
+            % STR = TOSTRING(IDICT) returns string with the object name.
+            
+            str = [class(obj) ' ' tostring(obj.name)];
         end
     end
 end
