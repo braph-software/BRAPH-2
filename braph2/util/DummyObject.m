e99cb647f3b8d95cf4fe73818dee19987b589447
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-05-08 08:45:07 +0200

    Create DummyObject.m

---
+++ braph2/util/DummyObject.m
@@ -0,0 +1,20 @@
+classdef DummyObject
+    % DummyObject Dummy object.
+    %
+    % DummyObject properties:
+    %   name
+    %
+    % DummyObject methods:
+    %   DummyObject     - constructor
+    
+    properties
+        name
+    end
+    methods 
+        function obj = DummyObject(name)
+            % DummyObject(NAME) creates a dummy object with the property NAME.
+
+            obj.name = name;
+        end
+    end
+end
