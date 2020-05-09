7985cfc2e8ff109b9a34336b35668a56365fe532
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-05-09 06:46:50 +0200

    Update clean_varargin.m

--- braph2/util/clean_varargin.m
+++ braph2/util/clean_varargin.m
@@ -1,4 +1,16 @@
 function varargin_clean = clean_varargin(handles, varargin)
+% CLEAN_VARARGIN clean varargin
+%
+% CLEANED = CLEAN_VARARGIN(HANDLES, VARARGIN) shallow copy of VARARGIN
+% including only the properties in HANDLES. HANDLES is a cell array with a
+% list of property names, i.e. HANDLES = {'PropertyName1', 'PropertyName2', ...}.
+% VARARGIN is passed as a cell array.
+%
+% COPY = CLEAN_VARARGIN(HANDLES, 'PropertyName', PROPERTY, ... ) VARARGIN
+% can also be passed as a list of couples including a 'PropertyName' and a
+% PROPERTY.
+%
+% See also GET_FROM_VARARGIN, COPY_VARARGIN.
 
 if length(varargin) == 1
     varargin = varargin{:};
