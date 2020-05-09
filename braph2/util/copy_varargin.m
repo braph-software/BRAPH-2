08848b53609caa82117d210bd81bbe601d6d611e
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-05-09 06:46:54 +0200

    Update copy_varargin.m

--- braph2/util/copy_varargin.m
+++ braph2/util/copy_varargin.m
@@ -2,12 +2,11 @@ function varargin_copy = copy_varargin(varargin)
 % COPY_VARARGIN deep copy of varargin
 %
 % COPY = COPY_VARARGIN(VARARGIN) deep copy of VARARGIN, where VARARGIN is
-% passes as a cell array. All copiable objects will be deep-copied (i.e.,
+% passed as a cell array. All copiable objects will be deep-copied (i.e.,
 % the method copy() is called for all objects that have it).
 %
-% COPY = COPY_VARARGIN('PropertyName', PROPERTY, ... ) deep copy of
-% VARARGIN is passes as a list of couples including a 'PropertyName' and a
-% PROPERTY.
+% COPY = COPY_VARARGIN('PropertyName', PROPERTY, ... ) VARARGIN can also be
+% passed as a list of couples including a 'PropertyName' and a PROPERTY.
 %
 % See also GET_FROM_VARARGIN, CLEAN_VARARGIN.
 
