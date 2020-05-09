d11bfa2d3ee4921adc9cd9eb922ce419143394bf
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-05-09 06:23:17 +0200

    Update BRAPH2.m

--- braph2/util/BRAPH2.m
+++ braph2/util/BRAPH2.m
@@ -1,15 +1,27 @@
 classdef BRAPH2
-    properties (Constant)
-        % BRAPH2 ID Card
-        NAME = 'Braph 2.0'
-        STR = 'BRAPH2'
-
-        % Error codes
+    % BRAPH2 Constants
+    % BRAPH2 is a static class that contains the constants used by BRAPH2.
+    %
+    % Properties(Constant) - BRAPH2 ID Card:
+    %   NAME    - BRAPH2 full name
+    %   STR     - BRAPH2 sort string
+    %   
+    % Properties(Constant) - BRAPH2 error codes:
+	%   WRONG_INPUT     - wrong input value or format in a function
+    %   WRONG_OUTPUT    - wrong output value or format in a function
+	%   BUG_FUNC        - bug in a function (wrong behavior)
+	%   BUG_COPY        - bug while deep-copying an object
+    
+    properties (Constant)  % BRAPH2 ID Card
+        NAME = 'Braph 2.0'  % BRAPH2 full name
+        STR = 'BRAPH2'  % BRAPH2 sort string
+    end
+    properties (Constant)  % BRAPH2 error codes
         WRONG_INPUT = 'WrongInput'  % wrong input value or format in a function
         WRONG_OUTPUT = 'WrongOutput'  % wrong output value or format in a function
         BUG_FUNC = 'BugFunc'  % bug in a function (wrong behavior)
         BUG_COPY = 'BugCopy'  % bug while deep-copying an object
-
+    end
 %         BUILD = 2020
 %         VERSION = '2.0.0'
 %         DATE = ''
@@ -44,5 +56,4 @@ classdef BRAPH2
 %         JSON_EXTENSION = '*.json'
 %         JSON_MSG_GETFILE = 'Select JSON file'
 %         JSON_MSG_PUTFILE = 'Select JSON file'        
-    end
 end
