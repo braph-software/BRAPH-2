f94e670e6dbcec2283b7faf166e69a0b6abf887c
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-05-09 07:10:40 +0200

    Create Contents.m

---
+++ braph2/util/Contents.m
@@ -0,0 +1,25 @@
+% BRAPH2 Utilities
+%
+% <a href="matlab:help BRAPH2                   ">BRAPH2</a>                  (static class) BRAPH2 constants
+%
+% <a href="matlab:help IndexedDictionary        ">IndexedDictionary</a>       (class) indexed dictionary
+%
+% Functions to manage varargin:
+%   <a href="matlab:help copy_varargin          ">copy_varargin</a>         (function) copy varargin
+%   <a href="matlab:help clean_varargin         ">clean_varargin</a>        (function) clean varargin
+%   <a href="matlab:help get_from_varargin      ">get_from_varargin</a>     (function) extract property from varargin
+%
+% General tools:
+%   <a href="matlab:help tostring               ">tostring</a>              (function) converts almost anything to string
+%   <a href="matlab:help subclasses             ">subclasses</a>            (function) gets subclasses of an object
+%
+% Unit tests:
+%   <a href="matlab:help DummyObject            ">DummyObject</a>           (class) dummy object 
+%   <a href="matlab:help DummyCopiableObject    ">DummyCopiableObject</a>   (class) dummy copiable object 
+%   <a href="matlab:help test_IndexedDictionary	">test_IndexedDictionary</a>
+%   <a href="matlab:help test_copy_varargin     ">test_copy_varargin</a>
+%   <a href="matlab:help test_clean_varargin	">test_clean_varargin</a>
+%   <a href="matlab:help test_get_from_varargin	">test_get_from_varargin</a>
+
+clc
+help util
