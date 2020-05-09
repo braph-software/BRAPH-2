fa6c9427851405b993c73632a37ad7f11186a9cb
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-05-09 06:20:36 +0200

    initial commit

---
+++ braph2/util/clean_varargin.m
@@ -0,0 +1,17 @@
+function varargin_clean = clean_varargin(handles, varargin)
+
+if length(varargin) == 1
+    varargin = varargin{:};
+end
+
+varargin_clean = varargin;
+
+for n = 1:2:length(varargin)-1
+    if ~any(strcmpi(varargin{n}, handles))
+        varargin_clean{n} = [];
+        varargin_clean{n+1} = [];
+    end
+end
+varargin_clean(cellfun('isempty', varargin_clean)) = [];
+
+end
