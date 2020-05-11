773bc4d35929107441f09bf73f46a31295352de3
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-05-11 13:41:38 +0200

    Update Contents.m

---
+++ braph2/Contents.m
@@ -0,0 +1,15 @@
+% BRAPH 2.0
+%
+% <a href="matlab:help braph2       ">braph2</a>        (script) loads all BRAPH2 packages 
+%
+% BRAPH2 packages:
+%   <a href="matlab:help util       ">util</a>          (folder) general utility tools
+%   <a href="matlab:help graph      ">graph</a>         (folder) graph analysis
+%   <a href="matlab:help atlas      ">atlas</a>         (folder) brain atlas
+%   <a href="matlab:help cohort     ">cohort</a>        (folder) subjects and cohort
+%   <a href="matlab:help analysis   ">analysis</a>      (folder) analysis
+%   <a href="matlab:help workflows  ">workflows</a>     (folder) workflows
+%   <a href="matlab:help test       ">test</a>          (folder) unit test
+
+clc
+help Contents
