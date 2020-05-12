57298e69bb53f74446c7a968c25b06b9dd9693d5
egolol <egolol87@gmail.com>
2020-05-12 15:15:18 +0200

    commit

--- braph2/atlas/test_PlotBrainSurf.m
+++ braph2/atlas/test_PlotBrainSurf.m
@@ -1,16 +1,23 @@
 % test PlotBrainSurf
 
 % Test 1: Basic Functions
-
-bs = PlotBrainSurf('BrainSurfaceType', 'BrainMesh_Cerebellum.nv');
-
-bs.brain();
-bs.axis_equal();
-bs.view(PlotBrainSurf.VIEW_3D);
-bs.update_light();
+% 
+% bs = PlotBrainSurf('BrainSurfaceType', 'BrainMesh_Cerebellum.nv');
+% 
+% bs.brain();
+% bs.axis_equal();
+% bs.view(PlotBrainSurf.VIEW_3D);
+% bs.update_light();
 
 assert(ischar(bs.tostring()), ...
 	[BRAPH2.STR ':' class(bs) ':' BRAPH2.WRONG_OUTPUT], ...
     'BrainRegion.copy() does not work.')
 
-% Test 2: Get
+% Test 2: Change default properties
+
+bs = PlotBrainSurf('BrainSurfaceType', 'BrainMesh_ICBM152.nv', 'BrainSurfaceLighting', 'gouraud', 'BrainSurfaceMaterial', 'metal', 'BrainSurfaceCamLight', 'right');
+bs.brain();
+bs.axis_equal();
+bs.view(PlotBrainSurf.VIEW_3D);
+bs.update_light();
+bs.brain_settings()
