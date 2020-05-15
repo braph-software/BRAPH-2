ad4d815ab262878b68e9516e01d8906a25a9109f
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2020-05-15 15:35:44 +0200

    Gv final revision atlas (#278)
    * Create PlotBrainSurf.m
    
    * Update PlotBrainSurf.m
    
    * Create BrainRegion.m
    
    * Create test_BrainRegion.m
    
    * Update BrainRegion.m
    
    * Update test_BrainRegion.m
    
    * Create BrainAtlas.m
    
    * Create test_BrainAtlas.m
    
    * Update test_BrainAtlas.m
    
    * Update BrainAtlas.m
    
    * Update test_BrainAtlas.m
    
    * Eg final revision brain atlas plot surface (#270)
    
    * commit to save
    
    * commit to save
    
    * commit to save
    
    * commit
    
    * comit to save background color now changes, static functions
    
    * dir brainsurfs
    
    * Update BRAPH2.m
    
    * Update PlotBrainSurf.m
    
    * Update PlotBrainSurf.m
    
    * Update PlotBrainSurf.m
    
    * commit to save
    
    * commit to save
    
    * commit to save
    
    * comment brain region
    
    * commit to save
    
    * changes to comments
    
    Co-authored-by: Giovanni Volpe <giovanni.volpe@physics.gu.se>
    
    Co-authored-by: egolol <egolol87@gmail.com>

---
+++ braph2/atlas/test_PlotBrainSurf.m
@@ -0,0 +1,35 @@
+% test PlotBrainSurf
+
+%% Test 1: Basic Functions
+
+bs = PlotBrainSurf('BrainSurfaceType', 'BrainMesh_Cerebellum.nv');
+
+assert(ischar(bs.tostring()), ...
+	[BRAPH2.STR ':' class(bs) ':' BRAPH2.WRONG_OUTPUT], ...
+    'PlotBrainSurf.tostring() does not work.')
+assert(ischar(bs.getName()), ...
+	[BRAPH2.STR ':' class(bs) ':' BRAPH2.WRONG_OUTPUT], ...
+    'PlotBrainSurf.getName() does not work.')
+
+
+%% Test 2: Change default properties
+
+bs2 = PlotBrainSurf('BrainSurfaceType', 'BrainMesh_ICBM152.nv', 'PlotBrainSurf.Lighting', 'gouraud', 'PlotBrainSurf.Material', 'metal', 'PlotBrainSurf.CamLight', 'right');
+bs2.brain();
+bs2.axis_equal();
+bs2.view(PlotBrainSurf.VIEW_3D);
+bs2.update_light();
+
+
+%% Test 3: Static Functions
+dummy = PlotBrainSurf.loadBrainSurface('BrainSurfaceType',  'BrainMesh_ICBM152.nv');
+assert(~isempty(dummy), ...
+	[BRAPH2.STR ':' class(PlotBrainSurf) ':' BRAPH2.WRONG_OUTPUT], ...
+    'PlotBrainSurf.loadBrainSurface() does not work.')
+
+settings = PlotBrainSurf.getAvailableSettings();
+assert(iscell(settings), ...
+	[BRAPH2.STR ':' class(PlotBrainSurf) ':' BRAPH2.WRONG_OUTPUT], ...
+    'PlotBrainSurf.loadBrainSurface() does not work.')
+
+
