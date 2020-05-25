2af212a8a3762ff939a3d5fe0f6470be455c4fa8
annacanal <31701548+annacanal@users.noreply.github.com>
2020-05-25 13:52:09 +0200

    Create DummyMultiplex.m

---
+++ braph2/graph/graphs/DummyMultiplex.m
@@ -0,0 +1,79 @@
+classdef DummyMultiplex < Graph
+
+    methods  % Constructor
+        function g = DummyMultiplex(A, varargin)
+            
+            if isempty(A)
+                A = {
+                round(rand(4))      round(rand(4, 3))   randn(4, 2)
+                round(rand(3, 4))   round(rand(3))      round(rand(3, 2))
+                randn(2, 4)         round(rand(2, 3)) 	randn(2)
+                };
+            end
+            
+            g = g@Graph(A, varargin{:});
+        end
+    end
+    methods (Static)  % Descriptive methods
+        function graph_class = getClass()
+            % GETCLASS returns the class of the graph.
+            %
+            % GRAPH_CLASS = GETCLASS() returns the class, 'GraphBD'.
+            %
+            % See also getName().
+            
+            graph_class = 'DummyMultiplex';
+        end
+        function name = getName()
+            % GETNAME returns the name of the graph.
+            %
+            % NAME = GETCLASS() returns the name, 'Binary Directed Graph'.
+            %
+            % See also getClass().
+            
+            name = 'Dummy Multiplex';
+        end
+        function description = getDescription()
+            % GETDESCRIPTION returns the description of the graph.
+            %
+            % DESCRIPTION = GETDESCRIPTION() returns the description of GRAPHBD.
+            %
+            % See also getName().
+            
+            description = [ ...
+                'Dummy Multiplex used mainly' ...
+                'for unit testing and debugging. ' ...
+                ];
+        end
+        function graph_type = getGraphType()
+            graph_type = Graph.MULTIPLEX;
+        end
+        function connectivity_type = getConnectivityType(varargin)
+            
+            connectivity_type = [ 
+                Graph.BINARY    Graph.BINARY    Graph.WEIGHTED
+                Graph.BINARY    Graph.BINARY    Graph.BINARY 
+                Graph.WEIGHTED  Graph.BINARY    Graph.WEIGHTED
+                ];
+        end
+        function directionality_type = getDirectionalityType(varargin)
+            directionality_type = Graph.DIRECTED;
+        end
+        function selfconnectivity_type = getSelfConnectivityType(varargin)
+            selfconnectivity_type = Graph.SELFCONNECTED;
+        end
+        function negativity_type = getNegativityType(varargin)
+            % GETNEGATIVITYTYPE checks if the graph is non-negative or negative
+            %
+            % BOOL = GETNEGATIVITYTYPE() returns NONNEGATIVE for GRAPHBD.
+            %
+            % See also getConnectivityType(), getEdgeType(), getGraphType() and getSelfConnectivityType().
+            
+            negativity_type = [ 
+                Graph.NONNEGATIVE	Graph.NONNEGATIVE   Graph.NEGATIVE
+                Graph.NONNEGATIVE   Graph.NONNEGATIVE   Graph.NONNEGATIVE 
+                Graph.NEGATIVE      Graph.NONNEGATIVE   Graph.NEGATIVE
+                ];
+        end
+    end
+end
