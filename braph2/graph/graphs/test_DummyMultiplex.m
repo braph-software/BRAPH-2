7d775b27c37c1de18bf73df9d7e7b19575b52dfb
annacanal <31701548+annacanal@users.noreply.github.com>
2020-05-28 11:30:07 +0200

    updated nodeattack test

--- braph2/graph/graphs/test_DummyMultiplex.m
+++ braph2/graph/graphs/test_DummyMultiplex.m
@@ -21,7 +21,46 @@ end
 ng = g.nodeattack(g, nodes);
 
 assert( isequal(ng.getA(), A), ...
-    [BRAPH2.STR ':Graph:' BRAPH2.BUG_ERR], ...
+    [BRAPH2.STR ':DummyMultiplex:' BRAPH2.BUG_ERR], ...
+    'Graph.nodeattack() is not working for non single layer graphs')
+
+% Attack specified layers
+g = DummyMultiplex([]);
+A = g.getA();
+layernumbers = [1, 3];
+for i = layernumbers
+    M = A{i, i};
+    M(nodes(:), :) = 0;
+    M(:, nodes(:)) = 0;
+    A(i, i) = {M};
+end
+
+ng = g.nodeattack(g, nodes, layernumbers);
+
+assert(isequal(ng.getA(), A), ...
+    [BRAPH2.STR ':DummyMultiplex:' BRAPH2.BUG_ERR], ...
     'Graph.nodeattack() is not working for non single layer graphs')
 
 %% Test 3: Edge attack
+n = randi(4);
+nodes1 = [randi(n), randi(n)];
+nodes2 = [randi(n), randi(n)];
+
+% Attack all layers
+g = DummyMultiplex([]);
+A = g.getA();
+L = length(A);
+for layer = 1:1:L
+    M = A{layer, layer};
+    M(sub2ind(size(M), nodes1, nodes2)) = 0;
+    if layer < L/2 + 1
+        M(sub2ind(size(M), nodes2, nodes1)) = 0;
+    end
+    A(layer, layer) = {M};
+end    
+
+eg = g.edgeattack(g, nodes1, nodes2);
+
+assert(isequal(eg.getA(), A), ...
+    [BRAPH2.STR ':DummyMultiplex:' BRAPH2.BUG_ERR], ...
+    'Graph.edgeattack() is not working for non single layer graphs')
