632824edef30aca041593dce6356b024cee58bcf
Anna Canal Garcia <31701548+annacanal@users.noreply.github.com>
2020-06-03 13:35:27 +0200

    DummyGraph: test + Documentation  (#328)
    * Updated documentation
    
    * updated documentation
    
    * test_DummyGraph
    
    * Updated documentation in DummyGraph
    
    * Updated documentation
    
    * Documentation changes implemented
    
    * Update DummyGraph.m
    
    * Update DummyGraph.m
    
    * Update DummyGraph.m
    
    Co-authored-by: Giovanni Volpe <giovanni.volpe@physics.gu.se>

---
+++ braph2/graph/graphs/test_DummyGraph.m
@@ -0,0 +1,46 @@
+% test DummyGraph
+A_BU = [
+    0 1 0;
+    1 0 1;
+    0 1 0;
+    ];
+
+%% Test 1: Constructor
+g = DummyGraph([]); %#ok<NASGU>
+
+%% Test 2: Node attack
+nodes = 1;
+
+A_BU_attack = [
+    0 0 0;
+    0 0 1;
+    0 1 0;
+    ]; 
+
+g = DummyGraph(A_BU);
+
+% Attack 
+ng = g.nodeattack(g, nodes);
+
+assert(isequal(ng.getA(), A_BU_attack), ...
+    [BRAPH2.STR ':DummyGraph:' BRAPH2.BUG_ERR], ...
+    'Graph.nodeattack() is not working for single layer graphs')
+
+%% Test 3: Edge attack
+nodes1 = [1, 1];
+nodes2 = [2, 1];
+
+A_BU_attack = [
+    0 0 0;
+    0 0 1;
+    0 1 0;
+    ]; 
+
+% Attack all layers
+g = DummyGraph(A_BU);
+
+eg = g.edgeattack(g, nodes1, nodes2);
+
+assert(isequal(eg.getA(), A_BU_attack), ...
+    [BRAPH2.STR ':DummyGraph:' BRAPH2.BUG_ERR], ...
+    'Graph.edgeattack() is not working for single layer graphs')
