745cfea1a362d60b675f0efd4b2178cb3176baaa
egolol <egolol87@gmail.com>
2020-06-10 10:13:19 +0200

    375 eg atlas contents (#380)
    * atlas contents
    
    * Update Contents.m
    
    * Update Contents.m
    
    Co-authored-by: Giovanni Volpe <giovanni.volpe@physics.gu.se>

---
+++ braph2/atlas/Contents.m
@@ -0,0 +1,20 @@
+% BRAPH2 Brain Atlas
+%
+% <a href="matlab:help BrainAtlas               ">BrainAtlas</a>           (class) represents a collection of BrainRegions
+% <a href="matlab:help BrainRegion              ">BrainRegion</a>          (class) contains information about a brain region
+% 
+% Plotting tools:
+% <a href="matlab:help PlotBrainSurf            ">PlotBrainSurf</a>        (class) plot of a brain surface
+% <a href="matlab:help PlotBrainAtlas           ">PlotBrainAtlas</a>       (class) plot of a brain atlas
+%
+% Graphical User Interface (GUI):
+% <a href="matlab:help GUIBrainAtlas            ">GUIBrainAtlas</a>        (GUI) brain atlas editor
+% 
+% Unit tests:
+% <a href="matlab:help test_BrainAtlas          ">test_BrainAtlas</a>
+% <a href="matlab:help test_Brainregion         ">test_BrainRegion</a>
+% <a href="matlab:help test_PlotBrainAtlas      ">test_PlotBrainAtlas</a> 
+% <a href="matlab:help test_PlotBrainRegion     ">test_PlotBrainRegion</a> 
+
+clc
+help util
