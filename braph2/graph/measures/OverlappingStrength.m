39e468817d3ca104e721784e2297f34068c8f331
Anna Canal Garcia <anna.canal.garcia@ki.se>
2020-06-12 09:52:13 +0200

    Update OverlappingStrength.m

--- braph2/graph/measures/OverlappingStrength.m
+++ braph2/graph/measures/OverlappingStrength.m
@@ -1,8 +1,11 @@
 classdef OverlappingStrength < Strength
     % OverlappingStrength Overlapping strength measure
     % OverlappingStrength provides the overlapping strength of a node for  
-    % weighted undirected (WU) graphs. It is calculated as the number of 
-    % edges connected to the node. 
+    % weighted undirected (WU) multiplexes. 
+    %
+    % It is calculated as the sum of the number of weighted edges connected to the
+    % node in all layers, i.e., it is the sum of the strengths of a node in
+    % all layers.    
     % 
     % OverlappingStrength methods:
     %   OverlappingStrength         - constructor
@@ -18,29 +21,27 @@ classdef OverlappingStrength < Strength
     %   getCompatibleGraphList      - returns a list of compatible graphs
     %   getCompatibleGraphNumber    - returns the number of compatible graphs
     %
-    % See also Measure, Graph, Strength, OverlappingDegree, WeightedEdgeOverlap.
+    % See also Measure, Strength, MultiplexGraphWU.
     
     methods
         function m = OverlappingStrength(g, varargin)
-            % OVERLAPPINGSTRENGTH(G) creates overlapping strength with default Strength properties.
-            % G is a graph (e.g, an instance of GraphBD, GraphBU,
-            % GraphWD, Graph WU). 
-            %
-            % OVERLAPPINGSTRENGTH(G, 'VALUE', VALUE) creates overlapping strength, 
-            % and sets the value to VALUE. G is a graph (e.g, an instance of 
-            % GraphBD, GraphBU, GraphWD, Graph WU).
-            %   
-            % See also Measure, Graph, Strength, OverlappingDegree, WeightedEdgeOverlap. 
+            % OVERLAPPINGSTRENGTH(G) creates overlapping strength with default properties.
+            % G is a weighted undirected multiplex (e.g, an instance of
+            % MultiplexGraphWU).
+            % 
+            % See also Measure, Strength, MultiplexGraphWU.
             
             m = m@Strength(g, varargin{:});
         end
     end
     methods (Access=protected)
         function overlapping_strength = calculate(m)
-            % CALCULATE calculates the overlapping strength value of a node
+            % CALCULATE calculates the overlapping strength value of a multiplex
             %
-            % OVERLAPPINGSTRENGTH = CALCULATE(M) returns the value of the overlapping strength of a
-            % node.
+            % OVERLAPPINGSTRENGTH = CALCULATE(M) returns the value of the overlapping 
+            % strength of a multiplex.
+            %
+            % See also Measure, Strength, MultiplexGraphWU.
             
             g = m.getGraph();  % graph from measure class
             
@@ -52,12 +53,12 @@ classdef OverlappingStrength < Strength
             
             N = g.nodenumber();
             L = g.layernumber();
-            overlapping = zeros(N(1), 1);
             
+            overlapping_strength = zeros(N(1), 1);
             for li = 1:1:L
-                overlapping = overlapping + strength{li};
+                overlapping_strength = overlapping_strength + strength{li};
             end
-            overlapping_strength = {overlapping};
+            overlapping_strength = {overlapping_strength};
         end
     end  
     methods (Static)  % Descriptive methods
@@ -77,7 +78,7 @@ classdef OverlappingStrength < Strength
             %
             % See also getClass, getDescription. 
             
-            name = 'Overlapping-Strength';
+            name = 'Overlapping_Strength';
         end
         function description = getDescription()
             % GETDESCRIPTION returns the overlapping strength description 
@@ -88,8 +89,9 @@ classdef OverlappingStrength < Strength
             % See also getList, getCompatibleGraphList.
             
             description = [ ...
-                'The overlapping strength of a node is the total sum of the ' ...
-                'weights of all the edges connected to a node within all layers. ' ...
+                'The overlapping strength of a node is the sum of the number' ...
+                'of weighted edges connected to the node in all layers. It ' ...
+                'is calculated as the sum of the strengths of a node in all layers.' ...
                 ];
         end
         function available_settings = getAvailableSettings()
@@ -103,17 +105,17 @@ classdef OverlappingStrength < Strength
             available_settings = {};
         end
         function measure_format = getMeasureFormat()
-            % GETMEASUREFORMAT returns the measure format of overlapping strength
+            % GETMEASUREFORMAT returns the measure format of OverlappingStrength
             %
             % MEASURE_FORMAT = GETMEASUREFORMAT() returns the measure format
-            % of overlapping degree strength (NODAL).
+            % of overlapping strength (NODAL).
             %
             % See also getMeasureScope.
             
             measure_format = Measure.NODAL;
         end
         function measure_scope = getMeasureScope()
-            % GETMEASURESCOPE returns the measure scope of overlapping strength
+            % GETMEASURESCOPE returns the measure scope of OverlappingStrength
             %
             % MEASURE_SCOPE = GETMEASURESCOPE() returns the
             % measure scope of overlapping strength measure (SUPERGLOBAL).
@@ -123,8 +125,7 @@ classdef OverlappingStrength < Strength
             measure_scope = Measure.SUPERGLOBAL;
         end
         function list = getCompatibleGraphList()  
-            % GETCOMPATIBLEGRAPHLIST returns the list of compatible graphs
-            % to overlapping strength 
+            % GETCOMPATIBLEGRAPHLIST returns the list of compatible graphs with OverlappingStrength
             %
             % LIST = GETCOMPATIBLEGRAPHLIST() returns a cell array 
             % of compatible graph classes to overlapping strength. 
@@ -137,8 +138,7 @@ classdef OverlappingStrength < Strength
                 };
         end
         function n = getCompatibleGraphNumber()
-            % GETCOMPATIBLEGRAPHNUMBER returns the number of compatible
-            % graphs to overlapping strength 
+            % GETCOMPATIBLEGRAPHNUMBER returns the number of compatible graphs with OverlappingStrength 
             %
             % N = GETCOMPATIBLEGRAPHNUMBER() returns the number of
             % compatible graphs to overlapping strength.
