9f641fb6d1e194f9e04494216d238379745b2002
Anna Canal Garcia <anna.canal.garcia@ki.se>
2020-06-12 14:29:14 +0200

    Create test_OutStrength.m

---
+++ braph2/graph/measures/test_OutStrength.m
@@ -0,0 +1,38 @@
+% test OutStrength
+
+A11 = [
+    0   .2  1
+    0   0   .8
+    1   0   0];
+A12 = eye(3);
+A21 = eye(3);
+A22 = [
+    0 1   .6
+    1 0   .4
+    0 .4  0];
+A = {
+    A11     A12  
+    A21     A22
+    };
+
+%% Test 1: Calculation GraphWD
+known_out_strength = {[1.2, 0.8, 1]'};
+
+g = GraphWD(A11);
+out_strength = OutStrength(g);
+
+assert(isequal(out_strength.getValue(), known_out_strength), ...
+    [BRAPH2.STR ':Strength:' BRAPH2.BUG_ERR], ...
+    'Strength is not being calculated correctly for GraphWD')
+
+%% Test 2: Calculation MultiplexGraphWD
+known_out_strength = {
+                              [1.2, 0.8, 1]'
+                              [1.6, 1.4, 0.4]'};
+                                
+g = MultiplexGraphWD(A);
+out_strength = OutStrength(g);
+
+assert(isequal(out_strength.getValue(), known_out_strength), ...
+    [BRAPH2.STR ':Strength:' BRAPH2.BUG_ERR], ...
+    'Strength is not being calculated correctly for MultiplexGraphWD')
