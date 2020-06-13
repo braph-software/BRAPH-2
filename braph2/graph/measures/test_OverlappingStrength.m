30930c4a9083b29bc80048051e1531d6345baeca
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-06-13 18:22:24 +0200

    Update test_OverlappingStrength.m

--- braph2/graph/measures/test_OverlappingStrength.m
+++ braph2/graph/measures/test_OverlappingStrength.m
@@ -1,26 +1,28 @@
 % test OverlappingStrength
 
+%% Test 1: MultiplexGraphWU
 A11 = [
     0   .2  1
     .2  0   0
-    1   0   0];
+    1   0   0
+    ];
 A12= eye(3);
 A21 = eye(3);
 A22 = [
     0   1   0
     1   0   .3
-    0   .3  0];
+    0   .3  0
+    ];
 A = {
     A11     A12  
     A21     A22
     };
 
-%% Test 1: Calculation MultiplexGraphWU
-known_overlapping_strength = {[2.2, 1.5, 1.3]'};
+known_overlapping_strength = {[2.2 1.5 1.3]'};
                  
 g = MultiplexGraphWU(A);
 overlapping_strength = OverlappingStrength(g);
 
 assert(isequal(overlapping_strength.getValue(), known_overlapping_strength), ...
     [BRAPH2.STR ':OverlappingStrength:' BRAPH2.BUG_ERR], ...
-    'OverlappingStrength is not being calculated correctly for MultiplexGraphWU')
+    'OverlappingStrength is not being calculated correctly for MultiplexGraphWU.')
