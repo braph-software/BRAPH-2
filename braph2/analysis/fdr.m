c1c30a7393ce065633653b39d33225aa096113f8
egolol <egolol87@gmail.com>
2020-06-15 12:19:42 +0200

    fdr change

--- braph2/analysis/fdr.m
+++ braph2/analysis/fdr.m
@@ -5,25 +5,31 @@ function r = fdr(pvalues, q)
 % defult q parameter set to 0.05.
 %
 % R = FDR(PVALUES, Q) calculates the false discovery rate (FDR) for PVALUES.
-% Q is a threshold parameter.
+% Q is a threshold parameter. FDR returns all PValues that are less than
+% the maximum PValue 
 %
 % See also bonferroni, pvalue1, pvalue2, quantiles.
 
 % q value [default = 0.05]
-if nargin<2
+if nargin < 2
     q = 0.05;
 else
     assert(q > 0 && q <= 1, ...
-        ['BRAPH:fdr:Q'], ...
-        ['Q must be a real number in (0,1]']) %#ok<*NBRAK>
+        [BRAPH2.STR ':fdr:Q'], ...
+        ['Q must be a real number in (0, 1]']) %#ok<*NBRAK>
+end
+x = [1:1:length(pvalues)] / length(pvalues);
+critical_point = x * q;
+pvalues = sort(pvalues);
+index = 0;
+for i = 1:1:length(pvalues)
+    if pvalues(i) <= critical_point(i)
+        index = i;      
+    end
 end
 
-x = [1:1:length(pvalues)] / length(pvalues);
-pvalues = sort(pvalues);
-
-index = sum(pvalues <= q * x);
 if index
-    r = pvalues(index);
+    r = pvalues(1:index);
 else
     r = 0;
 end
--- braph2/analysis/test_fdr.m
+++ braph2/analysis/test_fdr.m
@@ -4,3 +4,21 @@
 pvalues = rand(1, 100);
 q = .05;
 r = fdr(pvalues, q);
+
+%% Test 2: Vs known values
+sample_array = [0.001, 0.008, 0.039, 0.041, 0.042, 0.060, 0.074, 0.205, 0.212, 0.216, ...
+    0.222, 0.251, 0.269, 0.275, 0.34, 0.341, 0.384, 0.569, 0.594, 0.696, 0.762, ...
+    0.94, 0.942, 0.975, 0.986];
+
+% Critical value described by Benjamini and Hochberg(1995) is (i/m)Q where
+% i is the rank of the test, m the total number of tests and Q the fdr
+% treshhold value we choose, in this case will be 0.25 We look for the
+% largest P value that is smaller than its critical value: P < (i/m)Q 
+
+fdr_output  = fdr(sample_array, 0.25);
+
+fdr_expected = [0.001, 0.008, 0.039, 0.041, 0.042, 0.060];
+
+assert(isequal(fdr_expected, fdr_output), ...
+    [BRAPH2.STR ':FDR:' BRAPH2.WRONG_OUTPUT], ...
+    'Error in bonferroni calculation')
