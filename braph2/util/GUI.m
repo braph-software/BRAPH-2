58b4eb87025ecb5dd6b93860c73fbd4b6e0de8d3
egolol <egolol87@gmail.com>
2020-06-15 09:29:19 +0200

    Update GUI.m

--- braph2/util/GUI.m
+++ braph2/util/GUI.m
@@ -1,10 +1,93 @@
 classdef GUI
-    properties (Constant)
-        
+    % GUI manages GUI's constants and functionalities
+    % GUI is a static class that contains constant properties and static
+    % functions used by all BRAPH2 graphical user interfaces.
+    %
+    % GUI 
+    %
+    % Properties (Constant)
+    % COLOR             - GUI font color
+    % FONT              - GUI font style
+    % BAE_NAME          - Brain Atlas editor window title
+    % BAE_EXTENSION     - Brain Atlas editor file extension
+    % BAE_MSG_PUTFILE   - Brain Atlas editor put file message
+    % BAE_MSG_GETFILE   - Brain Atlas editor get file message
+    % MSG_YES           - affirmtive message
+    % MSG_NO            - negation message
+    % BRA_UNITS         - left square bracket character
+    % KET_UNITS         - right square bracket character
+    % BKGCOLOR          - background color
+    % TABBKGCOLOR       - tab background color
+    % PLOT_SYMBOL_TAG   - plot symbols tag
+    % PLOT_SYMBOL_NAME  - plot symbols name
+    % PLOT_LINESTYLE_TAG - plot line style tage
+    % PLOT_LINESTYLE_NAME - plot linestyle name
+    % FONT_INTERPRETER_TAG - font interpreter tag
+    % FONT_INTERPRETER_NAME - font interpreter name
+    % FONT_TYPE_TAG     - font type tag
+    % FONT_TYPE_NAME    - font type name
+    % FONT_WEIGHT_TAG   - font weight tag
+    % FONT_WEIGHT_NAME  - font weight name
+    % MENU_FILE         - menu file string
+    % MENU_EDIT         - menu edit string
+    % MENU_VIEW         - menu view string
+    % OPEN_CMD          - open command string
+    % OPEN_SC           - open command char
+    % SAVE_CMD          - save command string
+    % SAVE_SC           - save command char
+    % SAVEAS_CMD        - save as command string
+    % IMPORT_TXT_CMD    - import '.txt' file command string
+    % IMPORT_XLS_CMD    - imort '.xls' file command string
+    % IMPORT_JSON_CMD   - improt '.json' file command string
+    % EXPORT_TXT_CMD    - export to '.txt' file command string
+    % EXPORT_XLS_CMD    - export to '.xls' file command string
+    % EXPORT_JSON_CMD   - export to '.json' file command string
+    % CLOSE_CMD         - close command string
+    % CLOSE_SC          - close command char
+    % SELECTALL_CMD     - sellect all command string
+    % CLEARSELECTION_CMD - clear selection command string
+    % ADD_CMD           - add command string
+    % ADD_SC            - add command char
+    % REMOVE_CMD        - remove command string
+    % REMOVE_SC         - remove command char
+    % MOVEUP_CMD        - move up command string
+    % MOVEUP_SC         - move up command char
+    % MOVEDOWN_CMD      - move down command string
+    % MOVEDOWN_SC       - move down command char
+    % MOVE2TOP_CMD      - move to top command string
+    % MOVE2TOP_SC       - move to top command char
+    % MOVE2BOTTOM_CMD   - move to bottom command string
+    % MOVE2BOTTOM_SC    - move to bottom command char
+    % FIGURE_CMD        - generate figure command string 
+    % FIGURE_SC         - generate figure command char
+    %
+    % GUI methods (static, constants)
+    % ACCELERATOR       - returns the corresponding OS accelerator
+    % SCREEN_WIDTH      - returns the screen size width
+    % SCREEN_HEIGHT     - returns the screen size height
+    %
+    % GUI methods (static)
+    % INIT_FIGURE       - creates and initializes the GUI figure
+    % CLOSE             - closes GUI figure
+    % SETMENUABOUT      - initializes about menu
+    % ABOUT             - message in about menu
+    % SETUNITS          - sets GUI figure units
+    % SETBACKGROUNDCOLOR - sets GUI figure background color
+    % DISABLE           - disable all children of a specified object
+    % ENABLE            - enable all children of a specified object
+    % X                 - returns the x initial position of a figure
+    % Y                 - returns the y initial position of a figure
+    % WIDTH             - returns the width of a figure 
+    % HEIGHT            - returns the height of a figure
+    %
+    % See also BRAPH2, GUIBrainAtlas
+    
+    properties (Constant)        
         % Braph
         COLOR = [.9 .4 .1]
         FONT = 'Helvetica'
         
+        % GUI windows
         BAE_NAME = 'Brain Atlas Editor'
         BAE_EXTENSION = '*.atlas'
         BAE_MSG_PUTFILE = 'Save brain atlas'
@@ -208,7 +291,7 @@ classdef GUI
             % ACCELERATOR defines accelerator
             %
             % C = ACCELERATOR() returns the corresponding accelerator depending
-            %   on whether the version is for Apple Mac OS X or form Win/Unix.
+            % on whether the version is for Apple Mac OS X or form Win/Unix.
             %
             % See also GUI, ismac.
             
@@ -225,7 +308,7 @@ classdef GUI
             %
             % See also GUI.
             
-            screensize = get(0,'screensize');
+            screensize = get(0, 'screensize');
             w = screensize(3);
         end
         function h = SCREEN_HEIGHT()
@@ -235,7 +318,7 @@ classdef GUI
             %
             % See also GUI.
             
-            screensize = get(0,'screensize');
+            screensize = get(0, 'screensize');
             h = screensize(4);
         end
     end
@@ -244,7 +327,7 @@ classdef GUI
             % INIT_FIGURE creates and intializes GUI figure
             %
             % F = INIT_FIGURE(APPNAME,WIDTH,HEIGHT,POSITION) creates a GUI figure
-            %   F with name APPNAME and initializes the following properties:
+            % F with name APPNAME and initializes the following properties:
             %   WIDTH     -  width of the figure as percentage of the screen width
             %   HEIGHT    -  height of the figure as percentage of the screen height
             %   POSITION  -  position of the figure on the screen
@@ -252,7 +335,7 @@ classdef GUI
             % Admissible POSITION initializations are:
             %   north     | south     | east      | west
             %   northeast | northwest | southeast | southwest
-            %   center    | onscreen  | [h,v]
+            %   center    | onscreen  | [h, v]
             %
             % The figure is initially hidden.
             %
@@ -273,9 +356,8 @@ classdef GUI
             % CLOSE closes GUI figure
             %
             % CLOSE(APPNAME,F) promts the user to choose whether to close the
-            %   GUI figure F with name APPNAME. If the user answers
-            %   positively, the GUI figure closes.
-            %   F can also be a vector of figures.
+            % GUI figure F with name APPNAME. If the user answers positively, 
+            % the GUI figure closes. F can also be a vector of figures.
             %
             % See also GUI.
             
@@ -292,12 +374,12 @@ classdef GUI
         function [ui_menu_about, ui_menu_about_about] = setMenuAbout(f, AppName)
             % SETMENUABOUT initializes about menu
             %
-            % [UI_MENU_ABOUT,UI_MENU_ABOUT_ABOUT] = SETMENUABOUT(APPNAME,F)
-            %   adds the menu item UI_MENU_ABOUT and its submenu item
-            %   UI_MENU_ABOUT_ABOUT to the figure F.
+            % [UI_MENU_ABOUT, UI_MENU_ABOUT_ABOUT] = SETMENUABOUT(APPNAME, F)
+            % adds the menu item UI_MENU_ABOUT and its submenu item
+            % UI_MENU_ABOUT_ABOUT to the figure F.
             %
             % UI_MENU_ABOUT_ABOUT gives general information about the
-            %   brain network toolbox.
+            % brain network toolbox.
             %
             % See also GUI.
             
@@ -311,7 +393,7 @@ classdef GUI
             % ABOUT message in about menu
             %
             % ABOUT(APPNAME) specifies the message displayed in the about
-            %   menu in the figure with name APPNAME.
+            % menu in the figure with name APPNAME.
             %
             % See also GUI.
             
@@ -322,10 +404,9 @@ classdef GUI
             % SETUNITS sets GUI figure units
             %
             % SETUNITS(H,ALSOCHILDREN) sets the units of the object with
-            %   handle H and all its children to normalized if ALSOCHILDREN
-            %   is true.
-            %   If ALSOCHILDREN is false, only the units of the object with
-            %   handle H are set to normalized.
+            % handle H and all its children to normalized if ALSOCHILDREN
+            % is true. If ALSOCHILDREN is false, only the units of the
+            % object with handle H are set to normalized.
             %
             % See also GUI.
             
@@ -342,17 +423,17 @@ classdef GUI
             % SETBACKGROUNDCOLOR sets GUI figure background color
             %
             % SETBACKGROUNDCOLOR(H,ALSOCHILDREN) sets the background color
-            %   of the object with handle H and all its children to the
-            %   brain network color if ALSOCHILDREN is true.
+            % of the object with handle H and all its children to the
+            % brain network color if ALSOCHILDREN is true.
             %
             % If ALSOCHILDREN is false, only the background color of the
-            %   object with handle H are set to normalized.
+            % object with handle H are set to normalized.
             %
             % If the child of the object H is a table, the color is
-            %   initialized to the background table tab color.
+            % initialized to the background table tab color.
             %
             % If the child of the object H is a push button, the color of
-            %   the child remains unaffected.
+            % the child remains unaffected.
             %
             % See also GUI.
             
@@ -379,16 +460,15 @@ classdef GUI
             % DISABLE disables all children of a specified object
             %
             % DISABLE(H) disables all children of the object specified by
-            % the handle H.
-            % The object H must be of the type "uipanel".
+            % the handle H. The object H must be of the type "uipanel".
             %
             % See also GUI.
             
             all = allchild(h);
             for i = 1:1:length(all)
                 ht = all(i);
-                if isa(ht,'matlab.ui.control.UIControl')
-                    set(ht,'enable','off')
+                if isa(ht, 'matlab.ui.control.UIControl')
+                    set(ht, 'enable', 'off')
                 end
             end
         end
@@ -396,16 +476,15 @@ classdef GUI
             % ENABLE enables all children of a specified object
             %
             % ENABLE(H) enables all children of the object specified by
-            % the handle H.
-            % The object H must be of the type "uipanel".
+            % the handle H. The object H must be of the type "uipanel".
             %
             % See also GUI.
             
             all = allchild(h);
             for i = 1:1:length(all)
                 ht = all(i);
-                if isa(ht,'matlab.ui.control.UIControl')
-                    set(ht,'enable','on')
+                if isa(ht, 'matlab.ui.control.UIControl')
+                    set(ht, 'enable', 'on')
                 end
             end
         end
@@ -413,31 +492,31 @@ classdef GUI
             % X initial position
             %
             % X0 = X(F,R) returns the initial position corresponding to
-            %   figure F and ratio R.
+            % figure F and ratio R.
             %
             % See also GUI.
             
-            if nargin<2
+            if nargin < 2
                 r = 1;
             end
             
-            pos = get(f,'Position');
-            x0 = r*pos(1);
+            pos = get(f, 'Position');
+            x0 = r * pos(1);
         end
         function y0 = y(f,r)
             % Y initial position
             %
             % Y0 = Y(F,R) returns the initial position corresponding to
-            %   figure F and ratio R.
+            % figure F and ratio R.
             %
             % See also GUI.
             
-            if nargin<2
+            if nargin < 2
                 r = 1;
             end
             
-            pos = get(f,'Position');
-            y0 = r*pos(2);
+            pos = get(f, 'Position');
+            y0 = r * pos(2);
         end
         function w = width(f,r)
             % WIDTH width
@@ -446,12 +525,12 @@ classdef GUI
             %
             % See also GUI.
             
-            if nargin<2
+            if nargin < 2
                 r = 1;
             end
             
-            pos = get(f,'Position');
-            w = r*pos(3);
+            pos = get(f, 'Position');
+            w = r * pos(3);
         end
         function h = height(f,r)
             % HEIGHT height
@@ -460,12 +539,12 @@ classdef GUI
             %
             % See also GUI.
             
-            if nargin<2
+            if nargin < 2
                 r = 1;
             end
             
-            pos = get(f,'Position');
-            h = r*pos(4);
+            pos = get(f, 'Position');
+            h = r * pos(4);
         end
     end
 end
