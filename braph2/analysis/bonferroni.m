0a42a10d1c1fd4de9c6e4a30921b1829d5576b26
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-06-15 20:00:37 +0200

    Update bonferroni.m

--- braph2/analysis/bonferroni.m
+++ braph2/analysis/bonferroni.m
@@ -1,8 +1,12 @@
-function r = bonferroni(pvalues, p)
+function [r, mask] = bonferroni(pvalues, p)
 % BONFERRONI Bonferroni correction
 %
 % R = BONFERRONI(PVALUES, P) calculates the Bonferroni correction for PVALUES.
 % P is the starting p-value.
+%
+% [R, MASK] = BONFERRONI(PVALUES, P) returns also the logical array MASK, 
+% which is true for the p-values that pass the test, and false for the
+% p-values that do not pass the test.
 %
 % See also fdr, pvalue1, pvalue2, quantiles.
 
