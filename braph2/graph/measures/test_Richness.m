b44779784c99cde2d85634440c66da4d5a86ca74
Anna Canal Garcia <anna.canal.garcia@ki.se>
2020-06-18 16:04:22 +0200

    Update test_Richness.m

--- braph2/graph/measures/test_Richness.m
+++ braph2/graph/measures/test_Richness.m
@@ -7,10 +7,12 @@ A = [
     1  1  0
     ];
 
-known_richness = {[1 1 0]'};
+% known_richness = {[1 1 0]'};
+known_richness = {[0 0 0]'};
 
 g = GraphBU(A);
-richness = Richness(g, 'RichnessThreshold', 1);
+% richness = Richness(g, 'RichnessThreshold', 1);
+richness = Richness(g, 'RichnessThreshold', 'default');
 
 assert(isequal(richness.getValue(), known_richness), ...
     [BRAPH2.STR ':Richness:' BRAPH2.BUG_ERR], ...
@@ -23,38 +25,6 @@ assert(isequal(richness.getValue(), known_richness), ...
 %% Test 4: GraphWD
 
 %% Test 5: MultiplexGraphBU
-A11 = [
-      0     .1  .2  .25  0; 
-      .125  0   0   0    0; 
-      .2    .5  0   .25  0; 
-      .125  10   0   0    0;
-      0     0   0   0    0
-      ];
-A12 = eye(5);
-A21 = eye(5);
-A22 = [
-      0     .1  .2  .25  0; 
-      .125  0   0   0    0; 
-      .2    .5  0   .25  0; 
-      .125  10   0   0    0;
-      0     0   0   0    0
-      ];
-A = {
-    A11     A12
-    A21     A22
-    };
-
-known_richness = {
-                 known_distance_layer
-                 known_distance_layer
-                 };
-
-g = MultiplexGraphBU(A);
-richness = Richness(g);
-
-assert(isequal(richness.getValue(), known_richness), ...
-    [BRAPH2.STR ':Richness:' BRAPH2.BUG_ERR], ...
-    'Richness is not being calculated correctly for MultiplexGraphBU')
 
 %% Test 6: MultiplexGraphBD
 
