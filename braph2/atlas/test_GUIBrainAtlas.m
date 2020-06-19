7308ab15b6b5e242393a6df3765c392c97ff5bec
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-06-19 11:58:25 +0200

    Update test_GUIBrainAtlas.m

--- braph2/atlas/test_GUIBrainAtlas.m
+++ braph2/atlas/test_GUIBrainAtlas.m
@@ -1,15 +1,12 @@
 % test GUIBrainAtlas
 
-close all
-
 %% Test 1: Basic
-% atlas = BrainAtlas.load_from_txt('File', 'desikan_atlas.txt');
-% atlas.setID('BA ID')
-% atlas.setLabel('BA name')
-% atlas.setNotes('Brain atlas notes.')
-% 
-% GUIBrainAtlas(atlas)
-GUIBrainAtlas()
+atlas = BrainAtlas.load_from_txt('File', 'desikan_atlas.txt');
+atlas.setID('BA ID')
+atlas.setLabel('BA name')
+atlas.setNotes('Brain atlas notes.')
+
+GUIBrainAtlas(atlas)
 set(gcf, 'CloseRequestFcn', 'closereq')
 
 % close(gcf)
