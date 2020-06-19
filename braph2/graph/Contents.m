90946b57a5bbe896bcc53bf53f1a58059abae529
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-06-19 13:34:34 +0200

    Update Contents.m

--- braph2/graph/Contents.m
+++ braph2/graph/Contents.m
@@ -18,11 +18,11 @@
 %   <a href="matlab:help are_compatible         ">are_compatible</a>        (function) checks whether graph and measure are compatible
 %  
 % Unit tests:
+%   <a href="matlab:help test_Graph             ">test_Graph</a>
+%   <a href="matlab:help test_Measure           ">test_Measure</a>
 %   <a href="matlab:help test_binarize          ">test_binarize</a>
 %   <a href="matlab:help test_dediagonalize     ">test_dediagonalize</a>
 %   <a href="matlab:help test_diagonalize       ">test_clean_varargin</a>
-%   <a href="matlab:help test_Graph             ">test_Graph</a>
-%   <a href="matlab:help test_Measure           ">test_Measure</a>
 %   <a href="matlab:help test_semipositivize    ">test_semipositivize</a>
 %   <a href="matlab:help test_standardize       ">test_standardize</a>
 %   <a href="matlab:help test_symmetrize        ">test_symmetrize</a>
