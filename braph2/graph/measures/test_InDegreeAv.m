8c4d57b7e853942794fd1c71c941aa000bb4d57d
egolol <egolol87@gmail.com>
2020-06-22 09:58:42 +0200

    Update test_InDegreeAv.m

--- braph2/graph/measures/test_InDegreeAv.m
+++ braph2/graph/measures/test_InDegreeAv.m
@@ -1,54 +1,42 @@
 % test InDegreeAv
-A = rand(randi(10));
-B = {A A
-    A A};
 
 %% Test 1: Graph BD
+A = rand(3);
+known_indegree_av = {mean([2 2 2])};
 g = GraphBD(A);
 in_degree_av = InDegreeAv(g);
 
-A(1:length(A)+1:end) = 0;
-A(A<0) = 0;
-A(A>0) = 1;
-A = max(A, A');
-in_degree_test = sum(A, 2);
-out_degree_test_av = {mean(in_degree_test)};
 
-assert(isequal(in_degree_av.getValue(), out_degree_test_av), ...
+assert(isequal(in_degree_av.getValue(), known_indegree_av), ...
     [BRAPH2.STR ':InDegreeAv:' BRAPH2.BUG_ERR], ...
     'InDegreeAv is not being calculated correctly for GraphBD')
 
 %% Test 2: GraphWD
+A = rand(3);
+known_indegree_av = {mean([2 2 2])};
 g = GraphWD(A);
 in_degree_av = InDegreeAv(g);
 
-A(1:length(A)+1:end) = 0;
-A(A < 0) = 0;
-A(A > 0) = 1;
-A = max(A, A');
-in_degree_test = sum(A, 2);
-out_degree_test_av = {mean(in_degree_test)};
-
-assert(isequal(in_degree_av.getValue(), out_degree_test_av), ...
+assert(isequal(in_degree_av.getValue(), known_indegree_av), ...
     [BRAPH2.STR ':InDegreeAv:' BRAPH2.BUG_ERR], ...
     'InDegreeAv is not being calculated correctly for GraphWD')
 
 %% Test 3: MultiplexGraphBD
-g = MultiplexGraphBD(B);
+A = rand(3);
+B = eye(3);
+C = [A B
+     B A];
+ 
+known_indegree_av = {
+    mean([2 2 2])
+    mean([2 2 2])
+    };
+
+g = MultiplexGraphBD(C);
 in_degree_av = InDegreeAv(g);
 
-in_degree_av_test = cell(2, 1);
-for i=1:1:2
-    Aii = B{i, i};
-    Aii(1:length(Aii)+1:end) = 0;
-    Aii(Aii<0) = 0;
-    Aii = max(Aii, Aii');
-    Aii(Aii>0) = 1;
-    in_degree_test = sum(Aii, 2);
-    in_degree_av_test(i) = {mean(in_degree_test)};
-end
 
-assert(isequal(in_degree_av.getValue(), in_degree_av_test), ...
+assert(isequal(in_degree_av.getValue(), known_indegree_av), ...
     [BRAPH2.STR ':DegreeAv:' BRAPH2.BUG_ERR], ...
     'DegreeAv is not being calculated correctly for MultiplexGraphBU')
 
