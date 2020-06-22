67cc1d27f7ff4c36853379bb004ab085277fd627
Anna Canal Garcia <anna.canal.garcia@ki.se>
2020-06-22 08:59:48 +0200

    Update test_MultiplexInParticipation.m

--- braph2/graph/measures/test_MultiplexInParticipation.m
+++ braph2/graph/measures/test_MultiplexInParticipation.m
@@ -18,14 +18,14 @@ A = {
     A21     A22
     };
 
-known_multiplex_in_participation = {[8/9, 1, 0]'};
+known_multiplex_in_participation = {[8/9 1 0]'};
 
 g = MultiplexGraphBD(A);
 multiplex_in_participation = MultiplexInParticipation(g);
                  
 assert(isequal(multiplex_in_participation.getValue(), known_multiplex_in_participation), ...
     [BRAPH2.STR ':MultiplexInParticipation:' BRAPH2.BUG_ERR], ...
-    'MultiplexInParticipation is not being calculated correctly for MultiplexGraphBD')
+    'MultiplexInParticipation is not being calculated correctly for MultiplexGraphBD.')
 
 %% Test 2: MultiplexGraphWD
 A11 = [
@@ -45,11 +45,11 @@ A = {
     A21     A22
     };
 
-known_multiplex_in_participation = {[1, 8/9, 0]'};
+known_multiplex_in_participation = {[1 8/9 0]'};
 
 g = MultiplexGraphWD(A);           
 multiplex_in_participation = MultiplexInParticipation(g);
 
 assert(isequal(multiplex_in_participation.getValue(), known_multiplex_in_participation), ...
     [BRAPH2.STR ':MultiplexInParticipation:' BRAPH2.BUG_ERR], ...
-    'MultiplexInParticipation is not being calculated correctly for MultiplexGraphWD')
+    'MultiplexInParticipation is not being calculated correctly for MultiplexGraphWD.')
