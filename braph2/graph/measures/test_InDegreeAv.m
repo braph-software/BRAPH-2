154e0e70c54dbf5181c998a0288300f78d4c24f1
egolol <egolol87@gmail.com>
2020-06-22 10:04:17 +0200

    final test

--- braph2/graph/measures/test_InDegreeAv.m
+++ braph2/graph/measures/test_InDegreeAv.m
@@ -24,8 +24,8 @@ assert(isequal(in_degree_av.getValue(), known_indegree_av), ...
 %% Test 3: MultiplexGraphBD
 A = rand(3);
 B = eye(3);
-C = [A B
-     B A];
+C = [{A} {B}
+     {B} {A}];
  
 known_indegree_av = {
     mean([2 2 2])
@@ -35,26 +35,24 @@ known_indegree_av = {
 g = MultiplexGraphBD(C);
 in_degree_av = InDegreeAv(g);
 
-
 assert(isequal(in_degree_av.getValue(), known_indegree_av), ...
     [BRAPH2.STR ':DegreeAv:' BRAPH2.BUG_ERR], ...
     'DegreeAv is not being calculated correctly for MultiplexGraphBU')
 
 %% Test 4: MultiplexGraphWU
-g = MultiplexGraphWD(B);
+A = rand(3);
+B = eye(3);
+C = [{A} {B}
+     {B} {A}];
+ 
+known_indegree_av = {
+    mean([2 2 2])
+    mean([2 2 2])
+    };
+
+g = MultiplexGraphWD(C);
 in_degree_av = InDegreeAv(g);
 
-in_degree_av_test = cell(2, 1);
-for i=1:1:2
-    Aii = B{i, i};
-    Aii(1:length(Aii)+1:end) = 0;
-    Aii(Aii<0) = 0;
-    Aii = max(Aii, Aii');
-    Aii(Aii>0) = 1;
-    in_degree_test = sum(Aii, 2);
-    in_degree_av_test(i) = {mean(in_degree_test)};
-end
-
-assert(isequal(in_degree_av.getValue(), in_degree_av_test), ...
+assert(isequal(in_degree_av.getValue(), known_indegree_av), ...
     [BRAPH2.STR ':InDegreeAv:' BRAPH2.BUG_ERR], ...
     'InDegreeAv is not being calculated correctly for MultiplexGraphWD')
--- braph2/graph/measures/test_OutDegreeAv.m
+++ braph2/graph/measures/test_OutDegreeAv.m
@@ -1,72 +1,58 @@
 % test OutDegreeAv
-A = rand(randi(10));
-B = {A A
-    A A};
 
 %% Test 1: Graph BD
+A = rand(3);
+known_indegree_av = {mean([2 2 2])};
 g = GraphBD(A);
 out_degree_av = OutDegreeAv(g);
 
-A(1:length(A)+1:end) = 0;
-A(A<0) = 0;
-A(A>0) = 1;
-A = max(A, A');
-out_degree_test = sum(A, 2);
-out_degree_test_av = {mean(out_degree_test)};
 
-assert(isequal(out_degree_av.getValue(), out_degree_test_av), ...
+assert(isequal(out_degree_av.getValue(), known_indegree_av), ...
     [BRAPH2.STR ':OutDegreeAv:' BRAPH2.BUG_ERR], ...
     'OutDegreeAv is not being calculated correctly for GraphBD')
 
 %% Test 2: GraphWD
+A = rand(3);
+known_indegree_av = {mean([2 2 2])};
 g = GraphWD(A);
 out_degree_av = OutDegreeAv(g);
 
-A(1:length(A)+1:end) = 0;
-A(A < 0) = 0;
-A(A > 0) = 1;
-A = max(A, A');
-out_degree_test = sum(A, 2);
-out_degree_test_av = {mean(out_degree_test)};
-
-assert(isequal(out_degree_av.getValue(), out_degree_test_av), ...
+assert(isequal(out_degree_av.getValue(), known_indegree_av), ...
     [BRAPH2.STR ':OutDegreeAv:' BRAPH2.BUG_ERR], ...
     'OutDegreeAv is not being calculated correctly for GraphWD')
 
 %% Test 3: MultiplexGraphBD
-g = MultiplexGraphBD(B);
+A = rand(3);
+B = eye(3);
+C = [{A} {B}
+     {B} {A}];
+ 
+known_indegree_av = {
+    mean([2 2 2])
+    mean([2 2 2])
+    };
+
+g = MultiplexGraphBD(C);
 out_degree_av = OutDegreeAv(g);
 
-out_degree_av_test = cell(2, 1);
-for i=1:1:2
-    Aii = B{i, i};
-    Aii(1:length(Aii)+1:end) = 0;
-    Aii(Aii<0) = 0;
-    Aii = max(Aii, Aii');
-    Aii(Aii>0) = 1;
-    out_degree_test = sum(Aii, 2);
-    out_degree_av_test(i) = {mean(out_degree_test)};
-end
-
-assert(isequal(out_degree_av.getValue(), out_degree_av_test), ...
+assert(isequal(out_degree_av.getValue(), known_indegree_av), ...
     [BRAPH2.STR ':DegreeAv:' BRAPH2.BUG_ERR], ...
     'DegreeAv is not being calculated correctly for MultiplexGraphBU')
 
 %% Test 4: MultiplexGraphWU
-g = MultiplexGraphWD(B);
+A = rand(3);
+B = eye(3);
+C = [{A} {B}
+     {B} {A}];
+ 
+known_outdegree_av = {
+    mean([2 2 2])
+    mean([2 2 2])
+    };
+
+g = MultiplexGraphWD(C);
 out_degree_av = OutDegreeAv(g);
 
-out_degree_av_test = cell(2, 1);
-for i=1:1:2
-    Aii = B{i, i};
-    Aii(1:length(Aii)+1:end) = 0;
-    Aii(Aii<0) = 0;
-    Aii = max(Aii, Aii');
-    Aii(Aii>0) = 1;
-    out_degree_test = sum(Aii, 2);
-    out_degree_av_test(i) = {mean(out_degree_test)};
-end
-
-assert(isequal(out_degree_av.getValue(), out_degree_av_test), ...
+assert(isequal(out_degree_av.getValue(), known_outdegree_av), ...
     [BRAPH2.STR ':OutDegreeAv:' BRAPH2.BUG_ERR], ...
     'OutDegreeAv is not being calculated correctly for MultiplexGraphWD')
