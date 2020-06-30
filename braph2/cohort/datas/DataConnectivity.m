0348b18b2231f9fe3e638cb473f804e7f0685081
egolol <egolol87@gmail.com>
2020-06-30 18:10:23 +0200

    Eg cohort data documentation (#460)
    * done
    
    * correction
    
    * datacategorical and test
    
    * moved datacategorical
    
    * removed
    
    * datas with varargin
    
    * Update Data.m
    
    * data connectivity
    
    * functional
    
    * scalar
    
    * structural
    
    * correction to review
    
    * Update Data.m
    
    Co-authored-by: Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>

--- braph2/cohort/datas/DataConnectivity.m
+++ braph2/cohort/datas/DataConnectivity.m
@@ -1,6 +1,34 @@
 classdef DataConnectivity < Data
+    % DataConnectivity A data connectivity matrix
+    % DataConnectivity implements Data and serves as a container for matrix
+    % type data.
+    % It is a subclass of Data.
+    %
+    % DataConnectivity implements Data and serves as a container for matrix
+    % type data. It checks if the value of the data being saved is numeric
+    % and has the same dimensions as the Brain Atlas.
+    %
+    % DataConnectivity methods:
+    %   DataConnectivity        - Constructor
+    %
+    % DataConnectivity abstract methods:
+    %   setValue                - checks the value and sets it to DataConnectivity
+    %
+    % DataConnectivity static mehtods
+    %   getClass                - returns the class
+    %   getName                 - returns the name of the class
+    %   getDescription          - returns the description of the class
+    %   getAvailableSettings    - returns the available settings
+    %
+    % See also Data, DataFunctional, DataScalar, DataStructural.
+    
     methods
         function d = DataConnectivity(atlas, value, varargin)
+            % DATACONNECTIVITY(ATLAS, VALUE, 'PROPERTYVALUE1', 'VALUE1', ...)
+            % creates DataConnectivity object, checks if value is present,
+            % and calls for the superclass.
+            %
+            % See also Data, DataFunctional, DataScalar, DataStructural.
             
             if nargin < 2
                 value = zeros(atlas.getBrainRegions().length(), atlas.getBrainRegions().length());
@@ -11,6 +39,14 @@ classdef DataConnectivity < Data
     end
     methods
         function setValue(d, value)
+            % SETVALUE sets the value of the data into the object
+            %
+            % SETVALUE(D, VALUE) checks that the value of the data is
+            % numeric and that it has the correct dimensions. If incorrect
+            % it throws and error, if correct it sets the value to the
+            % object.
+            %
+            % See also getValue.
 
             regionnumber = d.getBrainAtlas().getBrainRegions().length();
             assert(isnumeric(value) && isequal(size(value), [regionnumber, regionnumber]), ...
@@ -26,17 +62,43 @@ classdef DataConnectivity < Data
     end
     methods (Static)
         function data_class = getClass()
+            % GETCLASS returns the class of the data 
+            %
+            % DATA_CLASS = GETCLASS() returns the class of the data.
+            %
+            % See also  getName, getDescription, getAvailableSettings.
+            
             data_class = 'DataConnectivity';
         end
         function name = getName()
+            % GETNAME returns the name of the data
+            %
+            % NAME = GETNAME(D) returns the name of the data.
+            % 
+            % See also getClass, getDescription, getAvailableSettings.
+            
             name = 'Connectivity Matrix';
         end
         function description = getDescription()
+            % GETDESCRIPTION returns the description of the data
+            %
+            % DESCRIPTION = GETDESCRIPTION(D) returns the description of
+            % the data.
+            % 
+            % See also getClass, getName, getAvailableSettings.
+            
             description = [ ...
                 'A precalculated connectivity matrix.' ...
                 ]; %#ok<NBRAK>
         end
         function available_settings = getAvailableSettings(d) %#ok<INUSD>
+            % GETAVAILABLESETTINGS returns the available settings of the data
+            %
+            % AVAILABLE_SETTINGS = GETAVAILABLESETTINGS(D) returns the
+            % available settings of the data.
+            % 
+            % See also getClass, getName, getDescription.
+            
             available_settings = {};
         end
     end  
--- braph2/cohort/datas/DataStructural.m
+++ braph2/cohort/datas/DataStructural.m
@@ -1,6 +1,34 @@
 classdef DataStructural < Data
+    % DataStructurral A data column vector
+    % DataStructurral implements Data and serves as a container for column
+    % type data.
+    % It is a subclass of Data.
+    %
+    % DataStructurral implements Data and serves as a container for column
+    % type data. It checks if the value of the data being saved is numeric
+    % and has the same number of elements as the Brain Atlas.
+    %
+    % DataStructurral methods:
+    %   DataStructurral         - Constructor
+    %
+    % DataStructurral abstract methods:
+    %   setValue                - checks the value and sets it to DataStructurral
+    %
+    % DataStructurral static mehtods
+    %   getClass                - returns the class
+    %   getName                 - returns the name of the class
+    %   getDescription          - returns the description of the class
+    %   getAvailableSettings    - returns the available settings
+    %
+    % See also Data, DataFunctional, DataScalar, DataConnectivity.
+    
     methods
         function d = DataStructural(atlas, value, varargin)
+            % DATASTRUCTURAL(ATLAS, VALUE, 'PROPERTYVALUE1', 'VALUE1', ...)
+            % creates DataStructurral object, checks if value is present,
+            % and calls for the super class.
+            %
+            % See also Data, DataFunctional, DataScalar, DataConnectivity.
             
             if nargin < 2
                 value = zeros(atlas.getBrainRegions().length(), 1);
@@ -11,7 +39,15 @@ classdef DataStructural < Data
     end
     methods
         function setValue(d, value)
-                        
+            % SETVALUE sets the value of the data into the object
+            %
+            % SETVALUE(D, VALUE) checks that the value of the data is
+            % numeric and that it has the correct dimensions. If incorrect
+            % it throws and error, if correct it sets the value to the
+            % object.
+            %
+            % See also getValue.
+            
             regionnumber = d.getBrainAtlas().getBrainRegions().length();
             assert(isnumeric(value) && isequal(size(value), [regionnumber, 1]), ...
                 [BRAPH2.STR ':DataStructural:' BRAPH2.WRONG_INPUT], ...
@@ -26,18 +62,44 @@ classdef DataStructural < Data
     end
     methods (Static)
         function data_class = getClass()
+            % GETCLASS returns the class of the data 
+            %
+            % DATA_CLASS = GETCLASS() returns the class of the data.
+            %
+            % See also  getName, getDescription, getAvailableSettings.
+            
             data_class = 'DataStructural';
         end
         function name = getName()
+            % GETNAME returns the name of the data
+            %
+            % NAME = GETNAME(D) returns the name of the data.
+            % 
+            % See also getClass, getDescription, getAvailableSettings.
+            
             name = 'Structural Brain Data';
         end
         function description = getDescription()
+            % GETDESCRIPTION returns the description of the data
+            %
+            % DESCRIPTION = GETDESCRIPTION(D) returns the description of
+            % the data.
+            % 
+            % See also getClass, getName, getAvailableSettings.
+            
             description = [ ...
                 'A series of structural data corresponding ' ...
                 'to one scalar value per brain region.' ...
                 ];
         end
          function available_settings = getAvailableSettings(d) %#ok<INUSD>
+            % GETAVAILABLESETTINGS returns the available settings of the data
+            %
+            % AVAILABLE_SETTINGS = GETAVAILABLESETTINGS(D) returns the
+            % available settings of the data.
+            % 
+            % See also getClass, getName, getDescription.
+            
             available_settings = {};
         end
     end  
