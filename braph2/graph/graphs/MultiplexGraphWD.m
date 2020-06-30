1d19d94c0af3b2e0cbaf3d00ce3eeab84394a6e0
Anna Canal Garcia <anna.canal.garcia@ki.se>
2020-06-30 15:03:29 +0200

    Update MultiplexGraphWD.m

--- braph2/graph/graphs/MultiplexGraphWD.m
+++ braph2/graph/graphs/MultiplexGraphWD.m
@@ -210,6 +210,82 @@ classdef MultiplexGraphWD < Graph
                 };
         end
     end
+    methods
+        function [randomized_graph, correlation_coefficents] = randomize_graph(g, varargin)
+            % get rules
+            number_of_weights = get_from_varargin(10, 'NumberOfWeights', varargin{:});
+            
+            W = g.getA();            
+            multiplex_graph_BD = MultiplexGraphBD(W);
+            [A, ~] = multiplex_graph_BD.randomize_graph(varargin{:});
+            L = g.layernumber();
+            correlation_coefficents = cell(L, 1);
+            randomized_graph = A;
+            
+            for li = 1:1:L
+                Aii = A{li, li};
+                Wii = W{li, li};       
+                % remove self connections
+                Aii(1:length(Aii)+1:numel(Aii)) = 0;
+                Wii(1:length(Wii)+1:numel(Wii)) = 0;
+                W_bin = Wii > 0;
+                N = size(Aii, 1);  % number of nodes
+                randomized_graph_layer = zeros(N);  % initialize null model matrix
+                S_in = sum(Wii, 1).';  % nodal in-strength
+                S_out = sum(Wii, 2);  % nodal out-strength
+                W_sorted = sort(Wii(W_bin));  % sorted weights vector
+                % find all the edges
+                [I_edges, J_edges] = find(Aii);
+                edges = I_edges + (J_edges - 1)*N;
+                % expected weights matrix
+                P = (S_out*S_in.');
+
+                for m = numel(W_sorted):-number_of_weights:1
+
+                    % sort the expected weights matrix
+                    [~, ind] = sort(P(edges));
+
+                    % random index of sorted expected weight
+                    selected_indices = randperm(m, min(m,number_of_weights)).';
+                    selected_edges = ind(selected_indices);
+
+                    % assign corresponding sorted weight at this index
+                    randomized_graph_layer(edges(selected_edges)) = W_sorted(selected_indices);
+
+                    % recalculate expected weight for node I_edges(selected_edge)
+                    WAi = accumarray(I_edges(selected_edges), W_sorted(selected_indices), [N,1]);
+                    Iu = any(WAi,2);
+                    % readjust expected weight probabilities
+                    F = 1 - WAi(Iu)./S_out(Iu);
+                    P(Iu,:) = P(Iu,:).*F(:,ones(1,N));
+                    % readjust in-strength
+                    S_out(Iu) = S_out(Iu) - WAi(Iu);
+
+                    % recalculate expected weight for node J_edges(selected_edge)
+                    WAj = accumarray(J_edges(selected_edges), W_sorted(selected_indices), [N,1]);
+                    Ju = any(WAj,2);
+                    % readjust expected weight probabilities
+                    F = 1 - WAj(Ju)./S_in(Ju);
+                    P(:,Ju) = P(:,Ju).*F(:,ones(1,N)).';
+                    % readjust out-strength
+                    S_in(Ju) = S_in(Ju) - WAj(Ju);
+
+                    % remove the edge/weight from further consideration
+                    selected_edges = ind(selected_indices);
+                    edges(selected_edges) = [];
+                    I_edges(selected_edges) = [];
+                    J_edges(selected_edges) = [];
+                    W_sorted(selected_indices) = [];
+                end
+
+                % calculate correlation of original vs reassinged in/out strength
+                rpos_in = corrcoef(sum(Wii,1), sum(randomized_graph_layer,1));
+                rpos_out = corrcoef(sum(Wii,2), sum(randomized_graph_layer,2));
+                correlation_coefficents(li) = {[rpos_in(2) rpos_out(2)]};
+                randomized_graph(li, li) = {randomized_graph_layer};
+            end
+        end
+    end
 %     methods
 %         function g = GraphWD(A, varargin)
 %             % GRAPHWD(A) creates a GRAPHWD class with adjacency matrix A.
@@ -233,74 +309,6 @@ classdef MultiplexGraphWD < Graph
 %             A = standardize(A, varargin{:});  % ensures all weights are between 0 and 1
 %             
 %             g = g@Graph(A, varargin{:});
-%         end
-%     end
-%     methods
-%         function [randomized_graph, correlation_coefficents] = randomize_graph(g, varargin)
-%             % get rules
-%             number_of_weights = get_from_varargin(10, 'NumberOfWeights', varargin{:});
-%             
-%             W = g.getA();
-%             graph_BD = GraphBD(W);
-%             [A, ~] = graph_BD.randomize_graph(varargin{:});
-%             % remove self connections
-%             A(1:length(A)+1:numel(A)) = 0;
-%             W(1:length(W)+1:numel(W)) = 0;
-%             W_bin = W > 0;
-%             N = size(A,1); % number of nodes
-%             randomized_graph = zeros(N); % initialize null model matrix
-%             
-%             S_in = sum(W,1).'; % nodal in-strength
-%             S_out = sum(W,2);  % nodal out-strength
-%             W_sorted = sort(W(W_bin)); % sorted weights vector
-%             % find all the edges
-%             [I_edges, J_edges] = find(A);
-%             edges = I_edges + (J_edges-1)*N;
-%             % expected weights matrix
-%             P = (S_out*S_in.');
-%             
-%             for m = numel(W_sorted):-number_of_weights:1
-%                 
-%                 % sort the expected weights matrix
-%                 [~, ind] = sort(P(edges));
-%                 
-%                 % random index of sorted expected weight
-%                 selected_indices = randperm(m, min(m,number_of_weights)).';
-%                 selected_edges = ind(selected_indices);
-%                 
-%                 % assign corresponding sorted weight at this index
-%                 randomized_graph(edges(selected_edges)) = W_sorted(selected_indices);
-%                 
-%                 % recalculate expected weight for node I_edges(selected_edge)
-%                 WAi = accumarray(I_edges(selected_edges), W_sorted(selected_indices), [N,1]);
-%                 Iu = any(WAi,2);
-%                 % readjust expected weight probabilities
-%                 F = 1 - WAi(Iu)./S_out(Iu);
-%                 P(Iu,:) = P(Iu,:).*F(:,ones(1,N));
-%                 % readjust in-strength
-%                 S_out(Iu) = S_out(Iu) - WAi(Iu);
-%                 
-%                 % recalculate expected weight for node J_edges(selected_edge)
-%                 WAj = accumarray(J_edges(selected_edges), W_sorted(selected_indices), [N,1]);
-%                 Ju = any(WAj,2);
-%                 % readjust expected weight probabilities
-%                 F = 1 - WAj(Ju)./S_in(Ju);
-%                 P(:,Ju) = P(:,Ju).*F(:,ones(1,N)).';
-%                 % readjust out-strength
-%                 S_in(Ju) = S_in(Ju) - WAj(Ju);
-%                 
-%                 % remove the edge/weight from further consideration
-%                 selected_edges = ind(selected_indices);
-%                 edges(selected_edges) = [];
-%                 I_edges(selected_edges) = [];
-%                 J_edges(selected_edges) = [];
-%                 W_sorted(selected_indices) = [];
-%             end
-%             
-%             % calculate correlation of original vs reassinged in/out strength
-%             rpos_in = corrcoef(sum(W,1), sum(randomized_graph,1));
-%             rpos_out = corrcoef(sum(W,2), sum(randomized_graph,2));
-%             correlation_coefficents = [rpos_in(2) rpos_out(2)];
 %         end
 %     end
 %     methods (Static)
