0d6e39eaadabefef28618bc97f6573a3047bb5e3
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-07-07 06:40:48 +0200

    Update test_Comparison.m

--- braph2/analysis/test_Comparison.m
+++ braph2/analysis/test_Comparison.m
@@ -1,12 +1,13 @@
-% test Comparisons
-br1 = BrainRegion('BR1', 'brain region 1', 1, 11, 111);
-br2 = BrainRegion('BR2', 'brain region 2', 2, 22, 222);
-br3 = BrainRegion('BR3', 'brain region 3', 3, 33, 333);
-br4 = BrainRegion('BR4', 'brain region 4', 4, 44, 444);
-br5 = BrainRegion('BR5', 'brain region 5', 5, 55, 555);
-atlas = BrainAtlas('brain atlas', {br1, br2, br3, br4, br5});
+% test Comparison
 
 comparison_class_list =  Comparison.getList();
+
+br1 = BrainRegion('BR1', 'brain region 1', 'notes 1', 1, 1.1, 1.11);
+br2 = BrainRegion('BR2', 'brain region 2', 'notes 2', 2, 2.2, 2.22);
+br3 = BrainRegion('BR3', 'brain region 3', 'notes 3', 3, 3.3, 3.33);
+br4 = BrainRegion('BR4', 'brain region 4', 'notes 4', 4, 4.4, 4.44);
+br5 = BrainRegion('BR5', 'brain region 5', 'notes 5', 5, 5.5, 5.55);
+atlas = BrainAtlas('BA', 'brain atlas', 'notes', 'BrainMesh_ICBM152.nv', {br1, br2, br3, br4, br5});
 
 %% Test 1 Instantiation
 for i = 1:1:length(comparison_class_list)
