e12968ccb7b53b07b079aaee49ccc230ff803b17
Anna Canal Garcia <anna.canal.garcia@ki.se>
2020-07-07 11:59:15 +0200

    updated multiplex tests

--- braph2/graph/graphs/test_DummyOrderedMultiplex.m
+++ braph2/graph/graphs/test_DummyOrderedMultiplex.m
@@ -159,3 +159,35 @@ sg = g.subgraph(g, nodes);
 assert(isequal(sg.getA(), A_subgraph), ...
     [BRAPH2.STR ':DummyOrderedMultiplex:' BRAPH2.BUG_ERR], ...
     'Graph.subgraph() is not working for non single layer graphs')
+
+% Different nodes per layer
+nodes = {
+        [2, 3]
+        [2, 3]
+        [1, 2]
+        };
+    
+A_WD_subgraph = [
+                1  .2;
+                0  1;
+                ];
+
+A_BD_subgraph = [
+                1 1;
+                0 1;
+                ];
+            
+diag3 = diag(ones(3, 1));  
+
+A_subgraph = {
+    A_BD_subgraph       diag(ones(2, 1))            {}  
+    diag(ones(2, 1))    A_BD_subgraph               diag3(nodes{2}, nodes{3})  
+    {}                  diag3(nodes{3}, nodes{2})   A_WD_subgraph               
+    };
+
+g = DummyOrderedMultiplex(A);
+sg = g.subgraph(g, nodes);
+
+assert(isequal(sg.getA(), A_subgraph), ...
+    [BRAPH2.STR ':DummyOrderedMultiplex:' BRAPH2.BUG_ERR], ...
+    'Graph.subgraph() is not working for non single layer graphs')
