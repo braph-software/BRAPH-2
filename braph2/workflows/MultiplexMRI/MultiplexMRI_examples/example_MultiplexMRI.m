c2bbb88bb063e29182bf2c2a07aca252b0b7cfe9
Anna Canal Garcia <anna.canal.garcia@ki.se>
2020-07-08 11:20:22 +0200

    Create example_MultiplexMRI.m

---
+++ braph2/workflows/MultiplexMRI/MultiplexMRI_examples/example_MultiplexMRI.m
@@ -0,0 +1,48 @@
+% Example MultiplexMRI Workflow
+
+%% Load Atlas
+atlas_path = [fileparts(which('AnalysisMultiplexMRI')) filesep 'MultiplexMRI_examples\exampleAtlas.xlsx'];
+atlas = BrainAtlas.load_from_xls('File', atlas_path);
+
+%% Load Subject Data
+% Data from group 1 
+cohort_group1_layer1 = [fileparts(which('AnalysisMultiplexMRI')) filesep 'MultiplexMRI_examples\xls\Group1_MRI_data_layer1.xlsx'];
+cohort_group1_layer2 = [fileparts(which('AnalysisMultiplexMRI')) filesep 'MultiplexMRI_examples\xls\Group1_MRI_data_layer2.xlsx'];
+cohort_group1 = SubjectMultiplexMRI.load_from_xls('SubjectMultiplexMRI', atlas, 'File1', cohort_group1_layer1, 'File2', cohort_group1_layer2);
+% Data from group 2
+cohort_group2_layer1 = [fileparts(which('AnalysisMultiplexMRI')) filesep 'MultiplexMRI_examples\xls\Group2_MRI_data_layer1.xlsx'];
+cohort_group2_layer2 = [fileparts(which('AnalysisMultiplexMRI')) filesep 'MultiplexMRI_examples\xls\Group2_MRI_data_layer2.xlsx'];
+cohort_group2 = SubjectMultiplexMRI.load_from_xls('SubjectMultiplexMRI', atlas, 'File1', cohort_group2_layer1, 'File2', cohort_group2_layer2);
+
+
+% Init Cohort 
+% group11 = Group('SubjectMultiplexMRI', 'GroupName1', 'TestGroup1', 'notes1', {});
+% group22 = Group('SubjectMultiplexMRI', 'GroupName2', 'TestGroup2', 'notes2', {});
+cohort = Cohort('Cohort Multiplex MRI', 'Cohort Label', 'Cohort notes' , 'SubjectMultiplexMRI', atlas, {});
+% cohort.getGroups().add(group11.getID(), group11);
+% cohort.getGroups().add(group22.getID(), group22);
+
+% Integrating both cohort groups to Cohort
+for i = 1:1:cohort_group1.getSubjects().length()
+    subject = cohort_group1.getSubjects().getValue(i);
+    cohort.getSubjects().add(subject.getID(), subject);
+end
+group1 = cohort_group1.getGroups().getValue(1);
+cohort.getGroups().add(group1.getID(), group1)
+
+for i = 1:1:cohort_group2.getSubjects().length()
+    subject = cohort_group2.getSubjects().getValue(i);
+    cohort.getSubjects().add(subject.getID(), subject);
+end
+group2 = cohort_group2.getGroups().getValue(1);
+cohort.getGroups().add(group2.getID(), group2)
+
+%% Show groups data
+groups = cohort.getGroups().getValues();
+group = groups{1}
+group = groups{2}
+
+%% Create Analysis
+analysis = AnalysisMultiplexMRI('analysis id', 'analysis label', 'analysis notes', cohort, {}, {}, {});
+
+analysis.getSettings()
