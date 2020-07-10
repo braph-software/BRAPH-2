ba64e9c9ec1f0b617d96fc69e6e566395254f412
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-07-10 20:52:27 +0200

    Update quantiles.m

--- braph2/analysis/quantiles.m
+++ braph2/analysis/quantiles.m
@@ -2,9 +2,10 @@ function Q = quantiles(values, P)
 % QUANTILES Calculates the quantiles of a distribution
 %
 % Q = QUANTILES(VALUES, P) calculates the P-quantiles of VALUES.
-% VALUES must be a cell array of samples, where each cell contains a 
-% scalar, a vector or a matrix with random variables. Q is a cell array with
-% the quantiles from 0 to Q.
+% VALUES must be a cell array of samples, where each cell contains a
+% scalar, a vector or a matrix with random variables, or a combination thereof.
+% Q is a cell array with the quantiles from 0 to Q, whose format is the
+% same as the cells of VALUES.
 %
 % Q = QUANTILES(VALUES) calculates the percentiles of VALUES. It is
 % equivalent to QUANTILES(VALUES, 100).
@@ -18,15 +19,30 @@ else
     P = ceil(abs(P));  % enforces that P is a positive integer
 end
 
-M = numel(values);  %#ok<NASGU> % number of samples (per variable)
+if ~iscell(values{1})
+    Q = for_quantiles(values, P);
+else
+    Q = cell(size(values{1}));
+    for r = 1:1:size(values, 1)
+        for c = 1:1:size(values, 2)
+            Q(r, c) = {for_quantiles(cellfun(@(x) x{r, c}, values, 'UniformOutput', false), P)};
+        end
+    end
+end
 
-row_number = size(values{1}, 1);
-column_number = size(values{1}, 2);
-
-Q = cell(row_number, column_number);
-for i = 1:1:row_number
-    for j = 1:1:column_number
-        current_values = cellfun(@(x) x(i, j), values);
-        Q{i, j} = [min(current_values), quantile(current_values, P - 1), max(current_values)];
+    function Q = for_quantiles(values, P)
+        
+        M = numel(values);  %#ok<NASGU> % number of samples (per variable)
+        
+        row_number = size(values{1}, 1);
+        column_number = size(values{1}, 2);
+        
+        Q = cell(row_number, column_number);
+        for i = 1:1:row_number
+            for j = 1:1:column_number
+                current_values = cellfun(@(x) x(i, j), values);
+                Q{i, j} = [min(current_values), quantile(current_values, P - 1), max(current_values)];
+            end
+        end
     end
 end
