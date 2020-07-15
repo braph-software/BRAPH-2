4430a985efeffbe9b35244c05811967ce6704157
egolol <egolol87@gmail.com>
2020-07-15 13:50:30 +0200

    script

--- braph2/workflows/MRI/example_workflow_MRI_WU.m
+++ braph2/workflows/MRI/example_workflow_MRI_WU.m
@@ -41,15 +41,27 @@ cohort = SubjectMRI.load_from_xls(cohort', 'File', group2_file);
 disp(['Loaded Group 2: ' cohort.tostring()])
 
 %% Create Analysis
-analysis = AnalysisMRI_WU('analysis example ID', 'analysis example label', 'analysis example notes', cohort_group_2, {}, {}, {});
-disp(['Analysis created: ' analysis.tostring()])
+analysis = AnalysisMRI_WU('analysis example ID', 'analysis example label', 'analysis example notes', cohort, {}, {}, {});
+disp('AnalysisMRI_WU created.')
 
-% % Create Measurement
-% measure = 'Degree';
-% groups = analysis.getGroups();
-%
-% measurement = MeasurementMRI_WU('ID example measure 1', 'label example measure l', 'notes example measure 1', atlas, measure, group);
-%
-%
-% Create Comparison
-% Create RandomComparison
+%% Create Measurement
+measure = 'Degree';
+groups = analysis.getCohort().getGroups();
+
+measurement_group_1 = analysis.getMeasurement(measure, groups.getValue(1));
+measurement_group_2 = analysis.getMeasurement(measure, groups.getValue(2));
+
+disp([measure ' for group 1: ' measurement_group_1.getMeasureValue()])
+disp([measure ' for group 2: ' measurement_group_2.getMeasureValue()])
+
+%% Create Comparison
+number_of_permutations = 10;
+comparison = analysis.getComparison(measure, groups.getValue(1), groups.getValue(2), 'PermutationNumber', number_of_permutations);
+disp([measure ' comparison between groups: ' groups.getValue(1).getID() ' and ' groups.getValue(2).getID() ' : ' comparison.getDifference()])
+
+%% Create RandomComparison
+number_of_permutations = 10;
+random_comparison = analysis.getRandomComparison(measure, groups.getValue(1), groups.getValue(2), 'PermutationNumber', number_of_permutations);
+disp([measure ' random comparison between groups: ' groups.getValue(1).getID() ' and ' groups.getValue(2).getID() ' : ' random_comparison.getDifference()])
+
+disp('Script has finished!')
