700432518250b2f0221b77649e2abda69f145eb5
egolol <egolol87@gmail.com>
2020-07-21 16:12:22 +0200

    review

--- braph2/graph/test_binarize.m
+++ braph2/graph/test_binarize.m
@@ -17,3 +17,36 @@ assert(isequal(B, ...
     ]), ...
     [BRAPH2.STR ':binarize:' BRAPH2.WRONG_OUTPUT], ...
     'Binarize does not work correctly.')
+
+%% Test 2 Threshold
+
+A = [1 2 3 4 5; 5 4 3 2 1; 2 3 4 5 1; 3 4 5 1 2; 4 5 1 2 3;];
+T = 3;
+T_A = binarize(A, 'threshold', T);
+
+assert(isequal(T_A, ...
+    [
+    0 0 0 1 1; 
+    1 1 0 0 0; 
+    0 0 1 1 0;
+    0 1 1 0 0;
+    1 1 0 0 0;
+    ]), ...
+    [BRAPH2.STR ':threshold:' BRAPH2.WRONG_OUTPUT], ...
+    'threshold does not work correctly.')
+
+%% Test 3 Density
+A = [1 2 3 4 5; 5 4 3 2 1; 2 3 4 5 1; 3 4 5 1 2; 4 5 1 2 3;];
+D = 2;
+D_A = binarize(A, 'density', D);
+
+assert(isequal(D_A, ...
+    [
+    0, 1, 1, 1, 1;
+    1, 1, 1, 1, 0;
+    1, 1, 1, 1, 0;
+    1, 1, 1, 0, 1;
+    1, 1, 0, 1, 1;
+    ]), ...
+    [BRAPH2.STR ':threshold:' BRAPH2.WRONG_OUTPUT], ...
+    'density does not work correctly.')
