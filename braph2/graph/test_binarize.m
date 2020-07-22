a25b62f6e63457a1d7f1ae7285873352bc587ca2
egolol <egolol87@gmail.com>
2020-07-22 10:44:54 +0200

    update test

--- braph2/graph/test_binarize.m
+++ braph2/graph/test_binarize.m
@@ -20,7 +20,13 @@ assert(isequal(B, ...
 
 %% Test 2 Threshold
 
-A = [1 2 3 4 5; 5 4 3 2 1; 2 3 4 5 1; 3 4 5 1 2; 4 5 1 2 3;];
+A = [
+    1 2 3 4 5;
+    5 4 3 2 1;
+    2 3 4 5 1;
+    3 4 5 1 2;
+    4 5 1 2 3;
+    ];
 T = 3;
 T_A = binarize(A, 'threshold', T);
 
@@ -32,21 +38,41 @@ assert(isequal(T_A, ...
     0 1 1 0 0;
     1 1 0 0 0;
     ]), ...
-    [BRAPH2.STR ':threshold:' BRAPH2.WRONG_OUTPUT], ...
+    [BRAPH2.STR ':binarize:' BRAPH2.WRONG_OUTPUT], ...
     'threshold does not work correctly.')
 
 %% Test 3 Density
-A = [1 2 3 4 5; 5 4 3 2 1; 2 3 4 5 1; 3 4 5 1 2; 4 5 1 2 3;];
-D = 2;
+A = [
+    0.001 0.002 0.003 0.004 0.005; 
+    0.005 0.004 0.003 0.002 0.001;
+    0.002 0.003 0.004 0.005 0.001;
+    0.003 0.004 0.005 0.001 0.002;
+    0.004 0.005 0.001 0.002 0.003;
+    ];
+D = 2;  % 2 percent
 D_A = binarize(A, 'density', D);
 
 assert(isequal(D_A, ...
     [
-    0, 1, 1, 1, 1;
-    1, 1, 1, 1, 0;
-    1, 1, 1, 1, 0;
-    1, 1, 1, 0, 1;
-    1, 1, 0, 1, 1;
+    0, 0, 0, 0, 0;
+    0, 0, 0, 0, 0;
+    0, 0, 0, 0, 0;
+    0, 0, 0, 0, 0;
+    0, 0, 0, 0, 0;
     ]), ...
-    [BRAPH2.STR ':threshold:' BRAPH2.WRONG_OUTPUT], ...
+    [BRAPH2.STR ':binarize:' BRAPH2.WRONG_OUTPUT], ...
+    'density does not work correctly.')
+
+D = 60;  % 60 percent
+D_A = binarize(A, 'density', D);
+
+assert(isequal(D_A, ...
+    [
+    0, 0, 0, 1, 1;
+    1, 1, 0, 0, 0;
+    0, 0, 1, 1, 0;
+    0, 1, 1, 0, 0;
+    1, 1, 0, 0, 0;
+    ]), ...
+    [BRAPH2.STR ':binarize:' BRAPH2.WRONG_OUTPUT], ...
     'density does not work correctly.')
