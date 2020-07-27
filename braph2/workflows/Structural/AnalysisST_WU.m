b2fa13c7822a18f9b07c5d781a70720f292ecdfe
egolol <egolol87@gmail.com>
2020-07-27 20:30:25 +0200

    small fix

--- braph2/workflows/Structural/AnalysisST_WU.m
+++ braph2/workflows/Structural/AnalysisST_WU.m
@@ -125,9 +125,7 @@ classdef AnalysisST_WU < Analysis
             correlation_rule = analysis.getSettings('AnalysisST.CorrelationRule');
             negative_weight_rule = analysis.getSettings('AnalysisST.NegativeWeightRule');
             A = Correlation.getAdjacencyMatrix(data, correlation_rule, negative_weight_rule);
-            
-            A = binarize(A, varargin{:});
-            
+                        
             graph_type = AnalysisST_WU.getGraphType();
             g = Graph.getGraph(graph_type, A);
         end
