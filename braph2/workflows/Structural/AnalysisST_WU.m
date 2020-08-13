95ac3ec9a1efe6bd2ff71845430eb6a63aa5c76a
egolol <egolol87@gmail.com>
2020-08-13 12:20:10 +0200

    Update AnalysisST_WU.m

--- braph2/workflows/Structural/AnalysisST_WU.m
+++ braph2/workflows/Structural/AnalysisST_WU.m
@@ -714,7 +714,7 @@ classdef AnalysisST_WU < Analysis
                     A = analysis.get_weighted_correlation_matrix(subjects, varargin{:});
                     
                     if get(ui_matrix_histogram_checkbox, 'Value')                        
-                        matrix_plot = GraphBU.plot(A, 'Graph.PlotType', graph_type_value); Graph.plo
+                        matrix_plot = GraphBU.plot(A, 'Graph.PlotType', graph_type_value); 
                     else         
                         % get atlas labels
                         atlases = analysis.getCohort().getBrainAtlases();
