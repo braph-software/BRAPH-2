e99a1f7d87aad0143d38c9e0c08d9da2d4779979
annacanal <annacg@kth.se>
2020-08-27 13:24:06 +0200

    update MeasurementST_WU

--- braph2/graph/measures/Richness.m
+++ braph2/graph/measures/Richness.m
@@ -109,7 +109,7 @@ classdef Richness < Degree
                     deg = (in_degree{li} + out_degree{li})/2;
                 end
                 
-                richness_threshold = get_from_varargin(0, 'RichnessThreshold', m.getSettings());
+                richness_threshold = get_from_varargin(1, 'RichnessThreshold', m.getSettings());
                 assert(mod(richness_threshold, 1) == 0, ...
                     [BRAPH2.STR ':Richness:' BRAPH2.WRONG_INPUT], ...
                     ['Richness threshold must be an integer value ' ...
@@ -222,20 +222,21 @@ classdef Richness < Degree
             
             parametricity = Measure.PARAMETRIC;
         end
-        function [name, values] = getParameterInformation(varargin)
+        function [name, values] = getParameterInformation(m)
             % GETPARAMETERINFORMATION returns the name and the values of the richness parameter
             %
             % NAME, VALUES = GETPARAMETERINFORMATION() returns the name (string) and 
             % the values of the richness parameter.
             
             name = 'Richness threshold';
-            
-            if isempty(varargin)
-                richness_threshold = 1;
-            else
-                richness_threshold = varargin{1};
+            richness_threshold = get_from_varargin(1, 'RichnessThreshold', m.getSettings());
+            if richness_threshold > 0  % for positive threshold value, k = value
+                values = 1:1:richness_threshold;
+            else  % for negative threshold, k = max degree - threshold (default -1)
+                %k_level2 = max(deg) - abs(richness_threshold);  % problem knowing the degree
+                %values = 1:1:k_level2;
+                values = k_level; % will gives errors to negative thresholds now
             end
-            values = 1:1:richness_threshold;
         end
         function list = getCompatibleGraphList()  
             % GETCOMPATIBLEGRAPHLIST returns the list of compatible graphs with Richness 
