e833055a2f6aadfad48e8cc8e6235cbd38b99807
egolol <egolol87@gmail.com>
2020-08-31 19:37:23 +0200

    save test plot

--- braph2/atlas/test_PlotBrainGraph.m
+++ braph2/atlas/test_PlotBrainGraph.m
@@ -5,5 +5,9 @@ atlas = BrainAtlas.load_from_xls('File', atlas_file);
 
 %% Init 
 bg = PlotBrainGraph(atlas);
+figure
+bg.hold_on()
+bg.view(PlotBrainSurf.VIEW_3D)
+bg.axis_equal()
 bg.link_edges();
 bg.link_edges_on();
