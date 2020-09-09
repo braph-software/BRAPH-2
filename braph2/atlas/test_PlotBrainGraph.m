bb4c43245ed035d3ee7c84b00b0997c9b6547b26
egolol <egolol87@gmail.com>
2020-09-09 17:52:07 +0200

    tests

--- braph2/atlas/test_PlotBrainGraph.m
+++ braph2/atlas/test_PlotBrainGraph.m
@@ -1,6 +1,6 @@
 % test PlotBrainGraph
 
-atlas_file = [fileparts(which('example_workflow_ST_WU.m')) filesep() 'example data ST (MRI)' filesep() 'desikan_atlas.xlsx'];
+atlas_file = [fileparts(which('example_workflow_ST_WU.m')) filesep() 'example data ST (MRI)' filesep() 'desikan_atlas_braph2.xlsx'];
 atlas = BrainAtlas.load_from_xls('File', atlas_file);
 
 %% Init edges liness
