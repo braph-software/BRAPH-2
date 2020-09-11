9be54af2c798d4467e147bc2bfed9942aef3012d
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-09-11 15:51:52 +0200

    Update test_PlotBrainGraph.m

--- braph2/atlas/test_PlotBrainGraph.m
+++ braph2/atlas/test_PlotBrainGraph.m
@@ -3,7 +3,7 @@
 atlas_file = [fileparts(which('example_workflow_ST_WU.m')) filesep() 'example data ST (MRI)' filesep() 'desikan_atlas.xlsx'];
 atlas = BrainAtlas.load_from_xls('File', atlas_file);
 
-%% Init edges liness
+%% Init edges lines
 bg = PlotBrainGraph(atlas);
 figure
 bg.hold_on()
@@ -16,30 +16,30 @@ bg.link_edges_settings();
 close(gcf)
 close(gcf)
 
-%% Edges arrows
-
-bg = PlotBrainGraph(atlas);
-figure
-bg.hold_on()
-bg.view(PlotBrainSurf.VIEW_3D)
-bg.axis_equal()
-bg.arrow_edges();
-bg.arrow_edges_on();
-bg.arrow_edges_settings();
-
-close(gcf)
-close(gcf)
-
-%% Edges Cylinders
-
-bg = PlotBrainGraph(atlas);
-figure
-bg.hold_on()
-bg.view(PlotBrainSurf.VIEW_3D)
-bg.axis_equal()
-bg.cylinder_edges();
-bg.cylinder_edges_on();
-bg.cylinder_edges_settings();
-
-close(gcf)
-close(gcf)
+% %% Edges arrows
+% 
+% bg = PlotBrainGraph(atlas);
+% figure
+% bg.hold_on()
+% bg.view(PlotBrainSurf.VIEW_3D)
+% bg.axis_equal()
+% bg.arrow_edges();
+% bg.arrow_edges_on();
+% bg.arrow_edges_settings();
+% 
+% close(gcf)
+% close(gcf)
+% 
+% %% Edges Cylinders
+% 
+% bg = PlotBrainGraph(atlas);
+% figure
+% bg.hold_on()
+% bg.view(PlotBrainSurf.VIEW_3D)
+% bg.axis_equal()
+% bg.cylinder_edges();
+% bg.cylinder_edges_on();
+% bg.cylinder_edges_settings();
+% 
+% close(gcf)
+% close(gcf)
