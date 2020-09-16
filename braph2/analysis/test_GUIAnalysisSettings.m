a65d3e66a5b90c7639664cd0b6d294be668b423f
egolol <egolol87@gmail.com>
2020-09-16 17:42:39 +0200

    to save

--- braph2/analysis/test_GUIAnalysisSettings.m
+++ braph2/analysis/test_GUIAnalysisSettings.m
@@ -6,7 +6,7 @@ GUIAnalysisSettings('AnalysisST_WU')
 set(gcf, 'CloseRequestFcn', 'closereq')
 close(gcf)
 
-%% Pass a cohort
+%% Pass a ST cohort
 cohort_file = [fileparts(which('example_workflow_ST_WU.m')) filesep() 'example data ST (MRI)' filesep() 'cohort_example.cohort'];
 temp = load(cohort_file, '-mat', 'cohort', 'selected_group', 'selected_subjects', 'BUILD');
 cohort = temp.cohort;
@@ -15,3 +15,13 @@ GUIAnalysisSettings(cohort, 'AnalysisST_BUD')
 
 set(gcf, 'CloseRequestFcn', 'closereq')
 close(gcf)
+
+%% Pass a FNC cohort
+cohort_file = [fileparts(which('example_workflow_FNC_WU.m')) filesep() 'example data FNC (fMRI)' filesep() 'cohort_example.cohort'];
+temp = load(cohort_file, '-mat', 'cohort', 'selected_group', 'selected_subjects', 'BUILD');
+cohort = temp.cohort;
+
+GUIAnalysisSettings(cohort, 'AnalysisFNC_BUD')
+
+% set(gcf, 'CloseRequestFcn', 'closereq')
+% close(gcf)
