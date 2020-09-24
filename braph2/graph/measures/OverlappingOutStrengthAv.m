54f30529fc77d4bc8226ad5575f1129f107ecfa5
Yu-Wei Chang <30530538+maverickyuwei@users.noreply.github.com>
2020-09-24 16:27:28 +0200

    Update OverlappingOutStrengthAv.m

--- braph2/graph/measures/OverlappingOutStrengthAv.m
+++ braph2/graph/measures/OverlappingOutStrengthAv.m
@@ -1,63 +1,62 @@
 classdef OverlappingOutStrengthAv < OverlappingOutStrength
-    % OverlappingOutStrengthAv Average overlapping in-strength measure
-    % OverlappingOutStrengthAv provides the average overlapping in-strength of a graph
+    % OverlappingOutStrengthAv Average overlapping out-strength measure
+    % OverlappingOutStrengthAv provides the average overlapping out-strength of a graph
     % for weighted directed (WD) multiplexes. 
     %
-    % It is calculated as the average of the sum of the in-strengths of a node in all layers.
+    % It is calculated as the average of the sum of the out-strengths of a node in all layers.
     % 
     % OverlappingOutStrengthAv methods:
-    %   OverlappingOutStrengthAv     - constructor
+    %   OverlappingOutStrengthAv    - constructor
     %
     % OverlappingOutStrengthAv methods (Static)
-    %   getClass                    - returns the average overlapping in-strength class
-    %   getName                     - returns the name of average overlapping in-strength measure
-    %   getDescription              - returns the description of average overlapping in-strength measure
+    %   getClass                    - returns the average overlapping out-strength class
+    %   getName                     - returns the name of average overlapping out-strength measure
+    %   getDescription              - returns the description of average overlapping out-strength measure
     %   getAvailableSettings        - returns the settings available to the class
     %   getMeasureFormat            - returns the measure format
     %   getMeasureScope             - returns the measure scope
     %   getParametricity            - returns the parametricity of the measure 
-    %   getMeasure                  - returns the average overlapping in-strength class
+    %   getMeasure                  - returns the average overlapping out-strength class
     %   getCompatibleGraphList      - returns a list of compatible graphs
     %   getCompatibleGraphNumber    - returns the number of compatible graphs
     %
-    % See also Measure, OverlappingOutStrength, InStrength, MultiplexGraphWD.
+    % See also Measure, OverlappingOutStrength, OutStrength, MultiplexGraphWD.
     
     methods
         function m = OverlappingOutStrengthAv(g, varargin)
-            % OverlappingOutStrengthAv(G) creates average overlapping in-strength with default properties.
-            % G is a directed multiplex (i.e., an instance of
-            % MultiplexGraphBD or MultiplexGraphWD).
+            % OVERLAPPINGOUTSTRENGTHAV(G) creates average overlapping out-strength with default properties.
+            % G is a directed multiplex (i.e., an instance of MultiplexGraphWD).
             %
-            % See also Measure, OverlappingOutStrength, InStrength, MultiplexGraphWD.
+            % See also Measure, OverlappingOutStrength, OutStrength, MultiplexGraphWD.
             
             m = m@OverlappingOutStrength(g, varargin{:});
         end
     end
     methods (Access=protected)
-        function overlapping_in_strength_av = calculate(m)
-            % CALCULATE calculates the average overlapping in-strength value of a multiplex
+        function overlapping_out_strength_av = calculate(m)
+            % CALCULATE calculates the average overlapping out-strength value of a multiplex
             %
-            % OverlappingOutStrengthAv = CALCULATE(M) returns the value of the average overlapping
-            % in-strength of a multiplex.
+            % OVERLAPPINGOUTSTRENGTHAV = CALCULATE(M) returns the value of the average overlapping
+            % out-strength of a multiplex.
             %
-            % See also Measure, OverlappingOutStrength, InStrength, MultiplexGraphBU, MultiplexGraphWU.
+            % See also Measure, OverlappingOutStrength, OutStrength, MultiplexGraphWD.
             
             g = m.getGraph();  % graph from measure class
             
             if g.is_measure_calculated('OverlappingOutStrength')
-                overlapping_in_strength = g.getMeasureValue('OverlappingOutStrength');
+                overlapping_out_strength = g.getMeasureValue('OverlappingOutStrength');
             else
-                overlapping_in_strength = calculate@OverlappingOutStrength(m);
+                overlapping_out_strength = calculate@OverlappingOutStrength(m);
             end
             
-            overlapping_in_strength_av = {mean(overlapping_in_strength{1})};
+            overlapping_out_strength_av = {mean(overlapping_out_strength{1})};
         end
     end  
     methods (Static)  % Descriptive methods
         function measure_class = getClass()
             % GETCLASS returns the measure class 
             %            
-            % MEASURE_CLASS = GETCLASS() returns the class of the average overlapping in-strength measure.
+            % MEASURE_CLASS = GETCLASS() returns the class of the average overlapping out-strength measure.
             %
             % See also getName, getDescription. 
             
@@ -66,23 +65,25 @@ classdef OverlappingOutStrengthAv < OverlappingOutStrength
         function name = getName()
             % GETNAME returns the measure name
             %
-            % NAME = GETNAME() returns the name of the average overlapping in-strength measure.
+            % NAME = GETNAME() returns the name of the average overlapping out-strength measure.
             %
             % See also getClass, getDescription. 
             
-            name = 'Average overlapping in-strength';
+            name = 'Average overlapping out-strength';
         end
         function description = getDescription()
-            % GETDESCRIPTION returns the average overlapping in-strength description 
+            % GETDESCRIPTION returns the average overlapping out-strength description 
             %
             % DESCRIPTION = GETDESCRIPTION() returns the description of the
-            % overlapping in-strength measure.
+            % overlapping out-strength measure.
             %
             % See also getClass, getName.
             
             description = [ ...
-                'The average overlapping in-strength of a network is the average ' ...
-                'of the sum of the in-strengths of a node in all layers. ' ...
+                'The average overlapping out-strength of a network is the average ' ...
+                'of the sum of the outward weighted edges connected to the node in all layers. ' ...
+                'It is calculated as the average of the sum of the out-strengths of a node ' ...
+                'in all layers.' ...
                 ];
         end
         function available_settings = getAvailableSettings()
@@ -97,7 +98,7 @@ classdef OverlappingOutStrengthAv < OverlappingOutStrength
             % GETMEASUREFORMAT returns the measure format of OverlappingOutStrengthAv
             %
             % MEASURE_FORMAT = GETMEASUREFORMAT() returns the measure format
-            % of average overlapping in-strength measure (GLOBAL).
+            % of average overlapping out-strength measure (GLOBAL).
             %
             % See also getMeasureScope.
             
@@ -107,7 +108,7 @@ classdef OverlappingOutStrengthAv < OverlappingOutStrength
             % GETMEASURESCOPE returns the measure scope of OverlappingOutStrengthAv
             %
             % MEASURE_SCOPE = GETMEASURESCOPE() returns the
-            % measure scope of average overlapping in-strength measure (SUPERGLOBAL).
+            % measure scope of average overlapping out-strength measure (SUPERGLOBAL).
             %
             % See also getMeasureFormat.
             
@@ -117,7 +118,7 @@ classdef OverlappingOutStrengthAv < OverlappingOutStrength
             % GETPARAMETRICITY returns the parametricity of OverlappingOutStrengthAv
             %
             % PARAMETRICITY = GETPARAMETRICITY() returns the
-            % parametricity of average overlapping in-strength measure (NONPARAMETRIC).
+            % parametricity of average overlapping out-strength measure (NONPARAMETRIC).
             %
             % See also getMeasureFormat, getMeasureScope.
             
@@ -127,7 +128,7 @@ classdef OverlappingOutStrengthAv < OverlappingOutStrength
             % GETCOMPATIBLEGRAPHLIST returns the list of compatible graphs with OverlappingOutStrengthAv 
             %
             % LIST = GETCOMPATIBLEGRAPHLIST() returns a cell array 
-            % of compatible graph classes to average overlapping in-strength. 
+            % of compatible graph classes to average overlapping out-strength. 
             % The measure will not work if the graph is not compatible. 
             %
             % See also getCompatibleGraphNumber. 
@@ -140,7 +141,7 @@ classdef OverlappingOutStrengthAv < OverlappingOutStrength
             % GETCOMPATIBLEGRAPHNUMBER returns the number of compatible graphs with OverlappingOutStrengthAv
             %
             % N = GETCOMPATIBLEGRAPHNUMBER() returns the number of
-            % compatible graphs with average overlapping in-strength.
+            % compatible graphs with average overlapping out-strength.
             % 
             % See also getCompatibleGraphList.
             
