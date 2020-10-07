17af723ef232abf3c80f5e3dafab1bcd67109bff
annacanal <annacg@kth.se>
2020-10-07 09:57:41 +0200

    Updated MultiplexTriangles with triangles between each pair of layers

--- braph2/graph/measures/MultiplexTriangles.m
+++ braph2/graph/measures/MultiplexTriangles.m
@@ -3,11 +3,8 @@ classdef MultiplexTriangles < Measure
     % MultiplexTriangles provides the number of multiplex triangles of a node for 
     % binary undirected (BU) and weighted undirected (WU) multiplexes. 
     %
-    % It is calculated as the number of a node's neighbor pairs that are   
-    % connected to each other between two layers. 
-    % In weighted graphs, the triangles are 
-    % calculated as geometric mean of the weights of the edges forming
-    % the triangle.
+    % It is calculated as the total number of a node's neighbor pairs that are   
+    % connected to each other between each pair of layers. 
     % 
     % MultiplexTriangles methods:
     %   MultiplexTriangles          - constructor
@@ -31,21 +28,6 @@ classdef MultiplexTriangles < Measure
             % MULTIPLEXTRIANGLES(G) creates multiplex triangles with default properties.
             % G is an undirected multiplex (e.g, an instance of 
             % MultiplexGraphBU or MultiplexGraphWU).  
-            %
-            % MULTIPLEXTRIANGLES(G, 'MultiplexTrianglesLayers', MULTIPLEXTRIANGLESLAYERS)
-            % creates multiplex triangles measure and initializes the property 
-            % MultiplexTrianglesLayers with MULTIPLEXTRIANGLESLAYERS. 
-            % Admissible RULE options are:
-            % MULTIRPLEXTRIANGLESLAYERS = [1, 2] (default) - MULTIPLEXTRIANGLES  
-            %                    layers' indexes will be set to 1 and 2.
-            %                    values - MULTIPLEXTRIANGLES layers' indexes 
-            %                    will be set to the values specified if the 
-            %                    values are integers between 1 and the
-            %                    number of layers.
-            %           
-            % MULTIPLEXTRIANGLES(G, 'VALUE', VALUE) creates multiplex triangles, and sets
-            % the value to VALUE. G is an undirected multiplex (e.g, an instance of
-            % MultiplexGraphBU or MultiplexGraphWU). 
             %   
             % See also Measure, Graph, MultiplexGraphBU, MultiplexGraphWU.
             
@@ -64,20 +46,17 @@ classdef MultiplexTriangles < Measure
             g = m.getGraph();  % graph from measure class
             A = g.getA();  % adjency matrix of the graph
             L = g.layernumber();
-
-            multiplex_triangles_layers = get_from_varargin([1, 2], 'MultiplexTrianglesLayers', m.getSettings());
-            assert(length(multiplex_triangles_layers) == 2, ...
-                [BRAPH2.STR ':MultiplexTriangles' BRAPH2.WRONG_INPUT], ...
-                ['Multiplex triangles layers must contain the index of ' ...
-                'two layers (' tostring(L) ') while it contains ' tostring(length(multiplex_triangles_layers))])
-            assert(all(ismember(multiplex_triangles_layers, 1:L)), ...
-                [BRAPH2.STR ':MultiplexTriangles:' BRAPH2.WRONG_INPUT], ...
-                ['Multiplex triangles layers indexes must be integers and be between 1 and the ' ...
-                'number of layers (' tostring(L) ') while they are ' tostring(multiplex_triangles_layers)])          
+            N = g.nodenumber();
             
-            A11 = A{multiplex_triangles_layers(1), multiplex_triangles_layers(1)};
-            A22 = A{multiplex_triangles_layers(2), multiplex_triangles_layers(2)};
-            multiplex_triangles = {diag(A11.^(1/3)*A22.^(1/3)*A11.^(1/3)) + diag(A22.^(1/3)*A11.^(1/3)*A22.^(1/3))};
+            multiplex_triangles = zeros(N(1), 1);
+            for i=1:1:L-1
+                A11 = A{i, i};
+                for j=i+1:1:L
+                    A22 = A{j, j};
+                    multiplex_triangles = multiplex_triangles + diag(A11.^(1/3)*A22.^(1/3)*A11.^(1/3)) + diag(A22.^(1/3)*A11.^(1/3)*A22.^(1/3));
+                end
+            end
+            multiplex_triangles = {multiplex_triangles};
         end
     end
     methods (Static)
@@ -109,11 +88,8 @@ classdef MultiplexTriangles < Measure
             
             description = [ ...
                 'The multiplex triangles of a node are ' ...
-                'the number of its neighbor pairs' ...
-                'that are connected to each other within a layer. ' ...
-                'In weighted graphs, the triangles are calculated' ...
-                'as geometric mean of the weights of the edges' ...
-                'forming the triangle.' ...
+                'the total number of its neighbor pairs' ...
+                'that are connected to each other between each pair of layers. ' ...
                 ];
         end
         function available_settings = getAvailableSettings()
@@ -121,17 +97,11 @@ classdef MultiplexTriangles < Measure
             % MultiplexTriangles
             %
             % AVAILABLESETTINGS = GETAVAILABLESETTINGS() returns the
-            % settings available to MultiplexTriangles. 
-            % MULTIRPLEXTRIANGLESLAYERS = [1, 2] (default) - MULTIPLEXTRIANGLES  
-            %                    layers' indexes will be set to 1 and 2.
-            %                    values - MULTIPLEXTRIANGLES layers' indexes 
-            %                    will be set to the values specified if the 
-            %                    values are integers between 1 and the
-            %                    number of layers.
-
-            available_settings = {
-                 'MultiplexTrianglesLayers', BRAPH2.NUMERIC, [1, 2], {};
-                };
+            % settings available to MultiplexTriangles.  Empty array in this case.
+            % 
+            % See also getCompatibleGraphList.
+            
+            available_settings = {};
         end
         function measure_format = getMeasureFormat()
             % GETMEASUREFORMAT returns the measure format of MultiplexTriangles
