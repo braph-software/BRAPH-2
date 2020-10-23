e0ab9f8b5751c6971c6e7bbde778387999a9e401
Yu-Wei Chang <30530538+maverickyuwei@users.noreply.github.com>
2020-10-23 20:22:07 +0200

    Create test_WeightedMultiplexOutParticipation.m

---
+++ braph2/graph/measures/test_WeightedMultiplexOutParticipation.m
@@ -0,0 +1,28 @@
+% test WeightedMultiplexOutParticipation
+
+%% Test 1: MultiplexGraphWD
+A11 = [
+    0   .5  1
+    .5  0   0
+    1   0   0
+    ];
+A12 = eye(3);
+A21 = eye(3);
+A22 = [
+    0   1   0
+    1   0   .5
+    0   .5  0
+    ];
+A = {
+    A11     A12
+    A21     A22
+    };
+
+known_weighted_multiplex_participation = {[24/25 3/4 8/9]'};
+
+g = MultiplexGraphWD(A);           
+weighted_multiplex_out_participation = WeightedMultiplexOutParticipation(g);
+
+assert(isequal(weighted_multiplex_out_participation.getValue(), known_weighted_multiplex_participation), ...
+    [BRAPH2.STR ':WeightedMultiplexOutParticipation:' BRAPH2.BUG_ERR], ...
+    'WeightedMultiplexOutParticipation is not being calculated correctly for MultiplexGraphWD.')
