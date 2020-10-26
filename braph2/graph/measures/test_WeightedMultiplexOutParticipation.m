767e338bbde50a4433f077ddaf26bac16f1bc232
annacanal <annacg@kth.se>
2020-10-26 09:40:56 +0100

    Updated test

--- braph2/graph/measures/test_WeightedMultiplexOutParticipation.m
+++ braph2/graph/measures/test_WeightedMultiplexOutParticipation.m
@@ -2,7 +2,7 @@
 
 %% Test 1: MultiplexGraphWD
 A11 = [
-    0   .5  1
+    0   .5  0
     .5  0   0
     1   0   0
     ];
@@ -11,14 +11,14 @@ A21 = eye(3);
 A22 = [
     0   1   0
     1   0   .5
-    0   .5  0
+    0   0   0
     ];
 A = {
     A11     A12
     A21     A22
     };
 
-known_weighted_multiplex_participation = {[24/25 3/4 8/9]'};
+known_weighted_multiplex_participation = {[8/9 3/4 0]'};
 
 g = MultiplexGraphWD(A);           
 weighted_multiplex_out_participation = WeightedMultiplexOutParticipation(g);
