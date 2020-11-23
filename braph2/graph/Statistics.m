2440c2e323765929b37e4dc0e27fdf453dbd7e05
egolol <egolol87@gmail.com>
2020-11-23 14:25:17 +0100

    Statistics and Permutation Test

---
+++ braph2/graph/Statistics.m
@@ -0,0 +1,91 @@
+classdef Statistics < handle & matlab.mixin.Copyable
+    properties
+        groups  % structure with groups involved in the statistic
+        settings  % structure with the constructor varargin
+        stat_dict  % all statistic return a dict first entry has keys of the rest of entries
+    end
+    methods (Access = protected)
+        function s = Statistics(varargin)
+            if length(varargin) == 1
+                varargin = varargin{:};
+            end
+            
+            available_settings = Statistics.getAvailableSettings(class(s));
+            settings = cell(1, size(available_settings, 1));
+            number_groups = 1;
+            for i = 1:1:size(available_settings, 1)
+                available_setting_code = available_settings{i, 1};
+                available_setting_default = available_settings{i, 3};
+                settings{2 * i - 1} = available_setting_code;
+                settings{2 * i} = get_from_varargin(available_setting_default, available_setting_code, varargin{:});
+                
+                if isequal(available_setting_code, 'number_groups')
+                    number_groups = settings{2 * i};
+                end
+            end
+            s.settings = settings;
+            
+            groups = cell(1, number_groups);
+            for i = 1:1:number_groups
+                group_key = ['Group_' i];
+                groups{2 * i -1} = group_key;
+                groups{2 * i} = get_from_varargin([], group_key, varargin{:});
+            end
+            s.groups = groups;
+        end
+    end
+    methods  % Basic methods
+        function groups = getGroups(s)
+            groups = s.groups;
+        end
+        function res = getSettings(s, setting_code)
+            if nargin < 2
+                res = s.settings;
+            else
+                res = get_from_varargin([], setting_code, s.settings{:});
+            end
+        end
+        function stat_dict = getStatistics(s, varargin)
+            % check for defaults, this may need to be recalculated every
+            % time its a new defaul, for example for perm test M and
+            % verbose vary.
+            if ~s.is_dict_init()
+                s.stat_dict = s.init_data_dict(varargin{:});
+            end
+            stat_dict = s.stat_dict;
+        end
+    end
+    methods (Abstract, Access = protected)
+        init_data_dict(s)
+    end
+    methods (Static)
+        function statistic_class_list = getList()
+            statistic_class_list = subclasses( ...
+                'Statistics', ...
+                [fileparts(which('Statistics')) filesep 'statistics'] ...
+                );
+        end
+        function statistic_class = getClass(s)
+            if isa(g, 'Statistics')
+                statistic_class = class(s);
+            else % g should be a string with the graph code
+                statistic_class = s;
+            end
+        end
+        function name = getName(s)
+            name = eval([Statistics.getClass(s) '.getName()']);
+        end
+        function name = getDescription(s)
+            name = eval([Statistics.getClass(s) '.getDescription()']);
+        end
+        function available_settings = getAvailableSettings(s)
+            available_settings = eval([Statistics.getClass(s) '.getAvailableSettings()']);
+        end
+        function s = getStatistic(stat_code, varargin)
+            s = eval([stat_code '(varargin{:})']);
+        end
+        function list = getCompatibleAnalysis(s)
+            list = eval([Statistics.getClass(s) '.getCompatibleGraphList()']);
+        end
+    end
+end
---
+++ braph2/graph/statistics/PermutationTest.m
@@ -0,0 +1,108 @@
+classdef PermutationTest < Statistics
+    methods
+        function s = PermutationTest(varargin)
+            s = s@Statistics(varargin{:});
+        end
+    end
+    methods (Access = protected)
+        function init_data_dict(s, varargin)
+            
+            % varargin
+            calling_class = get_from_varargin([], 'CallingClass', varargin{:});
+            M = get_from_varargin(1e+3, 'PermutationNumber', varargin{:});
+            verbose = get_from_varargin(false, 'Verbose', varargin{:});
+            interruptible = get_from_varargin(0.001, 'Interruptible', varargin{:});
+            values_1 = get_from_varargin([], 'Val1', varargin{:});
+            values_2 = get_from_varargin([], ' Val2', varargin{:});
+            res_1 = get_from_varargin([], 'Res1', varargin{:});
+            res_2 = get_from_varargin([], 'Res2', varargin{:});
+            
+            % settings
+            is_longitudinal = s.getSettings('Longitudinal');
+            
+            % Operation
+            all_permutations_1 = cell(1, M);
+            all_permutations_2 = cell(1, M);
+            
+            start = tic;
+            for i = 1:1:M
+                if verbose
+                    disp(['** PERMUTATION TEST - sampling #' int2str(i) '/' int2str(M) ' - ' int2str(toc(start)) '.' int2str(mod(toc(start),1)*10) 's'])
+                end
+                
+                [permutation_1, permutation_2] = permutation(values_1, values_2, is_longitudinal);
+                
+                mean_permutated_1 = mean(reshape(cell2mat(permutation_1), [size(permutation_1{1}, 1), size(permutation_1{1}, 2), group_1.subjectnumber()]), 3);
+                mean_permutated_2 = mean(reshape(cell2mat(permutation_2), [size(permutation_2{1}, 1), size(permutation_2{1}, 2), group_2.subjectnumber()]), 3);
+                
+                all_permutations_1(1, i) = {mean_permutated_1};
+                all_permutations_2(1, i) = {mean_permutated_2};
+                
+                difference_all_permutations{1, i} = mean_permutated_1 - mean_permutated_2; %#ok<AGROW>
+                if interruptible
+                    pause(interruptible)
+                end
+            end
+            
+            difference_mean = {res_2{1} - res_1{1}};  % difference of the mean values of the non permutated groups
+            difference_all_permutations = cellfun(@(x) [x], difference_all_permutations, 'UniformOutput', false);  %#ok<NBRAK> % permutated group 1 - permutated group 2
+            
+            % Statistical analysis
+            p1 = {pvalue1(difference_mean{1}, difference_all_permutations)};  % singe tail,
+            p2 = {pvalue2(difference_mean{1}, difference_all_permutations)};  % double tail
+            
+            qtl = quantiles(difference_all_permutations, 40);
+            ci_lower = {cellfun(@(x) x(2), qtl)};
+            ci_upper = {cellfun(@(x) x(40), qtl)};
+            
+            % init data_dict
+            data_dict = containers.Map;
+            data_dict('stat_keys') = { ...
+                [calling_class '.PermutationNumber'], ...
+                [calling_class '.values_1'], ...
+                [calling_class '.average_values_1'], ...
+                [calling_class '.values_2'], ...
+                [calling_class '.average_values_2'], ...
+                [calling_class '.difference'], ...
+                [calling_class '.all_differences'], ...
+                [calling_class '.p1'], ...
+                [calling_class '.p2'], ...
+                [calling_class '.confidence_min'], ...
+                [calling_class '.confidence_max'] ...
+                };
+            data_dict([calling_class '.PermutationNumber']) = M;
+            data_dict([calling_class '.values_1']) = values_1;
+            data_dict([calling_class '.average_values_1']) = res_1;
+            data_dict([calling_class '.values_2']) = values_2;
+            data_dict([calling_class '.average_values_2']) = res_2;
+            data_dict([calling_class '.difference']) = difference_mean;
+            data_dict([calling_class '.all_differences']) = difference_all_permutations;
+            data_dict([calling_class '.p1']) = p1;
+            data_dict([calling_class '.p2']) = p2;
+            data_dict([calling_class '.confidence_min']) = ci_lower;
+            data_dict([calling_class '.confidence_max']) = ci_upper;
+            
+            s.data_dict = data_dict;
+            
+        end
+    end
+    methods (Static)
+        function name = getName()
+            name = 'Permutation Test';
+        end
+        function des = getDescription()
+            des = [ ...
+                'A permutation test is a type of statistical significance test ' ...
+                'in which distribution of the test statistic under the null' ...
+                ' hypothesis is obtained by calculating all possible values ' ...
+                'of the test statistic under all possible rearrangements ' ...
+                'of the observed data points. ' ...
+                ];
+        end
+        function available_settings = getAvailableSettings()
+            available_settings = {
+                {'Longitudinal', BRAPH2.LOGICAL, false, {false, true}} ...                
+                };
+        end
+    end
+end
