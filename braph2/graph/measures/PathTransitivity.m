2cf72107f84ed529a8d723b89fd189a5365881d1
Yu-Wei Chang <30530538+maverickyuwei@users.noreply.github.com>
2020-11-27 02:13:48 +0100

    Update PathTransitivity.m
    revise the content

--- braph2/graph/measures/PathTransitivity.m
+++ braph2/graph/measures/PathTransitivity.m
@@ -1,5 +1,5 @@
 classdef PathTransitivity < Measure
-    % PathTransitivity PathTransitivity measure
+    % PathTransitivity Path transitivity measure
     % PathTransitivity provides the density of triangles that are
     % available along the shortest-paths between pairs of nodes. 
     % 
@@ -22,37 +22,109 @@ classdef PathTransitivity < Measure
     
     methods
         function m = PathTransitivity(g, varargin)
-            % DEGREE(G) creates path transitivity with default properties.
+            % PATHTRANSITIVITY(G) creates path transitivity with default properties.
             % G is a graph (e.g, an instance of GraphBU, GraphWU,
             % MultiplexGraphBU or MultiplexGraphBU). 
             %  
-            % See also Measure, GraphBU, GraphWU, MultiplexGraphBU, MultiplexGraphWU.
+            % See also Measure, GraphBU, MultiplexGraphBU.
             
             m = m@Measure(g, varargin{:});
         end
     end
     methods (Access=protected)
-        function path transitivity = calculate(m)
-            % CALCULATE calculates the path transitivity value of a graph
+        function path_transitivity = calculate(m)
+            % CALCULATE calculates the path transitivity pairwise matrix of
+            % graph
             %
-            % DEGREE = CALCULATE(M) returns the value of the path transitivity of a
-            % binary undirected (BU) or weighted undirected (WU) graph or multiplex.
+            % PATH_TRANSITIVITY = CALCULATE(M) returns the value of the path transitivity pairwise matrix
+            % of a binary undirected (BU) graph or multiplex.
             %
-            % See also Measure, GraphBU, GraphWU, MultiplexGraphBU, MultiplexGraphWU.
+            % See also Measure, GraphBU, MultiplexGraphBU.
             
             g = m.getGraph();  % graph from measure class
             A = g.getA();  % adjacency matrix (for graph) or 2D-cell array (for multiplex)
-            
-            path transitivity = cell(g.layernumber(), 1);
-            
-            for li = 1:1:g.layernumber()
+            N = g.nodenumber();
+            L = g.layernumber();
+            path_transitivity = cell(L, 1);
+            for li = 1:1:L
                 if g.is_graph(g)
                     Aii = A;
                 else
                     Aii = A{li, li};
                 end
-                Aii = binarize(Aii);  % binarizes the adjacency matrix
-                path transitivity(li) = {sum(Aii, 2)};  % calculates the path transitivity of a node for layer li
+                m = zeros(N(li),N(li));
+                path_transitivity_single = zeros(N(li),N(li));
+                for i=1:N(li)-1
+                    for j=i+1:N(li)
+                        x=0;
+                        y=0;
+                        z=0;
+                        for k=1:N(li)
+                            if Aii(i, k)~=0 && Aii(j, k)~=0 && k~=i && k~=j
+                                x = x + Aii(i, k) + Aii(j, k);
+                            end
+                            if k~=j
+                                y = y + Aii(i, k);
+                            end
+                            if k~=i
+                                z = z + Aii(j, k);
+                            end
+                        end
+                        m(i,j) = x/(y+z);
+                    end
+                end
+                m = m+m';
+                % Calculate hops and Pmat
+                SPL = Aii;  % Unweighted/Weighted shortest path-length matrix.
+                SPL(SPL == 0) = inf;
+                n = size(Aii,2);    
+                hops = double(Aii ~= 0);  % Number of edges in the shortest path matrix. 
+                Pmat = 1:n;  % Elements {i,j} of this matrix indicate the next node in the hortest path between i and j. 
+                Pmat = Pmat(ones(n,1),:);
+                for k = 1:n
+                    i2k_k2j = bsxfun(@plus, SPL(:,k), SPL(k,:));
+                    path = bsxfun(@gt, SPL, i2k_k2j);
+                    [i,j] = find(path);
+                    hops(path) = hops(i,k) + hops(k,j)';
+                    Pmat(path) = Pmat(i,k);
+                    SPL = min(SPL, i2k_k2j);
+                end
+
+                hops(eye(n)>0)=0;
+                Pmat(eye(n)>0)=0;
+                % Calculate path transitivity 
+                n = length(Aii);
+                for i=1:n-1
+                    for j=i+1:n
+                        x=0;
+                        % Retrieve shortest path
+                        s = i;
+                        t = j;
+                        path_length = hops(s,t);
+                        if path_length ~= 0
+                            path = nan(path_length+1,1);
+                            path(1) = s;
+                            for ind = 2:length(path)
+                                s = Pmat(s,j);
+                                path(ind) = t;
+                            end
+                        else
+                            path = [];
+                        end
+
+                        K=length(path);
+
+                        for t=1:K-1
+                            for l=t+1:K
+                                x=x+m(path(t),path(l));
+                            end
+                        end
+                        path_transitivity_single(i,j)=2*x/(K*(K-1));
+                    end
+                end
+                path_transitivity_single = path_transitivity_single + path_transitivity_single';
+                path_transitivity_single(isnan(path_transitivity_single)) = 0; % Should return zeros, not NaN
+                path_transitivity(li) = {path_transitivity_single};
             end
         end
     end  
@@ -73,7 +145,7 @@ classdef PathTransitivity < Measure
             %
             % See also getClass, getDescription. 
             
-            name = 'PathTransitivity';
+            name = 'Path transitivity';
         end
         function description = getDescription()
             % GETDESCRIPTION returns the path transitivity description 
@@ -81,12 +153,12 @@ classdef PathTransitivity < Measure
             % DESCRIPTION = GETDESCRIPTION() returns the description of the
             % path transitivity measure.
             %
-            % See also getList, getCompatibleGraphList.
+            % See also getClass, getName.
             
             description = [ ...
-                'The path transitivity of a node is the number of ' ...
-                'edges connected to a node within a layer. ' ...
-                'Connection weights are ignored in calculations.' ...
+                'The path transitivity of a graph is the density of ' ...
+                'triangles that are available along the ' ...
+                'shortest-paths between all pairs of nodes.' ...
                 ];
         end
         function available_settings = getAvailableSettings()
@@ -103,11 +175,11 @@ classdef PathTransitivity < Measure
             % GETMEASUREFORMAT returns the measure format of PathTransitivity
             %
             % MEASURE_FORMAT = GETMEASUREFORMAT() returns the measure format
-            % of path transitivity measure (NODAL).
+            % of path transitivity measure (BINODAL).
             %
             % See also getMeasureScope.
             
-            measure_format = Measure.NODAL;
+            measure_format = Measure.BINODAL;
         end
         function measure_scope = getMeasureScope()
             % GETMEASURESCOPE returns the measure scope of PathTransitivity
@@ -141,9 +213,7 @@ classdef PathTransitivity < Measure
             
             list = { ...
                 'GraphBU', ...
-                'GraphWU', ...
                 'MultiplexGraphBU', ...
-                'MultiplexGraphWU' ...
                 };
         end
         function n = getCompatibleGraphNumber()
