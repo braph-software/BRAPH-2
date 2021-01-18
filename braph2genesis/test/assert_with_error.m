c41e8650d23e1c2ce22943b45975878306f65ff8
egolol <egolol87@gmail.com>
2021-01-18 18:30:01 +0100

    Assert with error comments
    Comments for assert_wth_error class

--- braph2genesis/test/assert_with_error.m
+++ braph2genesis/test/assert_with_error.m
@@ -1,14 +1,22 @@
 function assert_with_error(code, error_identifier, varargin)
+%ASSERTWITHERROR checks if the error is correctly thrown by the code.
+%
+% ASSERTWITHERROR(CODE, ERRORIDENTIFIER, VARARGIN) checks if the correct
+%  error with ERRORIDENTIFIER is thrown by the CODE utilizing the format 
+%  given by the VARARGIN. 
+%
+% See also TESTBRAPH2.
 
-try 
+try % tries the code, it is expected to throw an error    
     clear e
     eval(code)
-catch e
+catch e  % catches the thrown error e, it compares the identifier with the expected error_identifier
     assert(isequal(e.identifier, error_identifier), ...
         [BRAPH2.STR ':Graph:' BRAPH2.BUG_ERR], ...
         [BRAPH2.STR ':Graph:' BRAPH2.BUG_ERR ' ' ...
         'Expected error: ' error_identifier '. Instead, thrown error ' e.identifier])    
 end
+% checks that the variable e exists in the workspace 
 assert(exist('e', 'var') == 1, ...
     [BRAPH2.STR ':Format:' BRAPH2.BUG_ERR], ...
 	[BRAPH2.STR ':Graph:' BRAPH2.BUG_ERR ' ' ...
