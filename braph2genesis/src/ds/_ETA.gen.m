ffcbde5264388a65dd97466c1ff3957f9ac6abc1
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2021-01-19 20:08:36 +0100

    Create _ETA.gen.m

---
+++ braph2genesis/src/ds/_ETA.gen.m
@@ -0,0 +1,55 @@
+%% ¡header!
+ETA < Element (et, test Element A) tests props.
+
+%%% ¡description!
+Tests the methods set, get, and check for all properties categories and formats.
+
+%%% ¡seealso!
+Element, Category, Format
+
+%% ¡props!
+
+%%% ¡prop!
+PROP_EMPTY (metadata, empty) is a metadata, empty.
+
+%%% ¡prop!
+PROP_STRING (metadata, string) is a metadata, string.
+
+%%% ¡prop!
+PROP_LOGICAL (metadata, logical) is a metadata, logical.
+
+%%% ¡prop!
+PROP_OPTION (metadata, option) is a metadata, option.
+
+%%% ¡prop!
+PROP_CLASS (metadata, class) is a metadata, class.
+
+%%% ¡prop!
+PROP_CLASSLIST (metadata, classlist) is a metadata, classlist.
+
+%%% ¡prop!
+PROP_ITEM (metadata, item) is a metadata, item.
+
+%%% ¡prop!
+PROP_ITEMLIST (metadata, itemlist) is a metadata, itemlist.
+
+%%% ¡prop!
+PROP_IDICT (metadata, idict) is a metadata, idict.
+
+%%% ¡prop!
+PROP_SCALAR (metadata, scalar) is a metadata, scalar.
+
+%%% ¡prop!
+PROP_RVECTOR (metadata, rvector) is a metadata, rvector.
+
+%%% ¡prop!
+PROP_CVECTOR (metadata, cvector) is a metadata, cvector.
+
+%%% ¡prop!
+PROP_MATRIX (metadata, matrix) is a metadata, matrix.
+
+%%% ¡prop!
+PROP_SMATRIX (metadata, smatrix) is a metadata, smatrix.
+
+%%% ¡prop!
+PROP_CELL (metadata, cell) is a metadata, cell.
