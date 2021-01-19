501d4fe2eb3303d6f4174eb1297b2a92c6be23ff
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2021-01-19 21:32:20 +0100

    Update _ETA.gen.m

--- braph2genesis/src/ds/_ETA.gen.m
+++ braph2genesis/src/ds/_ETA.gen.m
@@ -10,46 +10,181 @@ Element, Category, Format
 %% ¡props!
 
 %%% ¡prop!
-PROP_EMPTY (metadata, empty) is a metadata, empty.
+PROP_EMPTY_M (metadata, empty) is a metadata, empty.
 
 %%% ¡prop!
-PROP_STRING (metadata, string) is a metadata, string.
+PROP_STRING_M (metadata, string) is a metadata, string.
 
 %%% ¡prop!
-PROP_LOGICAL (metadata, logical) is a metadata, logical.
+PROP_LOGICAL_M (metadata, logical) is a metadata, logical.
 
 %%% ¡prop!
-PROP_OPTION (metadata, option) is a metadata, option.
+PROP_OPTION_M (metadata, option) is a metadata, option.
 
 %%% ¡prop!
-PROP_CLASS (metadata, class) is a metadata, class.
+PROP_CLASS_M (metadata, class) is a metadata, class.
 
 %%% ¡prop!
-PROP_CLASSLIST (metadata, classlist) is a metadata, classlist.
+PROP_CLASSLIST_M (metadata, classlist) is a metadata, classlist.
 
 %%% ¡prop!
-PROP_ITEM (metadata, item) is a metadata, item.
+PROP_ITEM_M (metadata, item) is a metadata, item.
 
 %%% ¡prop!
-PROP_ITEMLIST (metadata, itemlist) is a metadata, itemlist.
+PROP_ITEMLIST_M (metadata, itemlist) is a metadata, itemlist.
 
 %%% ¡prop!
-PROP_IDICT (metadata, idict) is a metadata, idict.
+PROP_IDICT_M (metadata, idict) is a metadata, idict.
 
 %%% ¡prop!
-PROP_SCALAR (metadata, scalar) is a metadata, scalar.
+PROP_SCALAR_M (metadata, scalar) is a metadata, scalar.
 
 %%% ¡prop!
-PROP_RVECTOR (metadata, rvector) is a metadata, rvector.
+PROP_RVECTOR_M (metadata, rvector) is a metadata, rvector.
 
 %%% ¡prop!
-PROP_CVECTOR (metadata, cvector) is a metadata, cvector.
+PROP_CVECTOR_M (metadata, cvector) is a metadata, cvector.
 
 %%% ¡prop!
-PROP_MATRIX (metadata, matrix) is a metadata, matrix.
+PROP_MATRIX_M (metadata, matrix) is a metadata, matrix.
 
 %%% ¡prop!
-PROP_SMATRIX (metadata, smatrix) is a metadata, smatrix.
+PROP_SMATRIX_M (metadata, smatrix) is a metadata, smatrix.
 
 %%% ¡prop!
-PROP_CELL (metadata, cell) is a metadata, cell.
+PROP_CELL_M (metadata, cell) is a metadata, cell.
+
+%%% ¡prop!
+PROP_EMPTY_P (parameter, empty) is a parameter, empty.
+
+%%% ¡prop!
+PROP_STRING_P (parameter, string) is a parameter, string.
+
+%%% ¡prop!
+PROP_LOGICAL_P (parameter, logical) is a parameter, logical.
+
+%%% ¡prop!
+PROP_OPTION_P (parameter, option) is a parameter, option.
+
+%%% ¡prop!
+PROP_CLASS_P (parameter, class) is a parameter, class.
+
+%%% ¡prop!
+PROP_CLASSLIST_P (parameter, classlist) is a parameter, classlist.
+
+%%% ¡prop!
+PROP_ITEM_P (parameter, item) is a parameter, item.
+
+%%% ¡prop!
+PROP_ITEMLIST_P (parameter, itemlist) is a parameter, itemlist.
+
+%%% ¡prop!
+PROP_IDICT_P (parameter, idict) is a parameter, idict.
+
+%%% ¡prop!
+PROP_SCALAR_P (parameter, scalar) is a parameter, scalar.
+
+%%% ¡prop!
+PROP_RVECTOR_P (parameter, rvector) is a parameter, rvector.
+
+%%% ¡prop!
+PROP_CVECTOR_P (parameter, cvector) is a parameter, cvector.
+
+%%% ¡prop!
+PROP_MATRIX_P (parameter, matrix) is a parameter, matrix.
+
+%%% ¡prop!
+PROP_SMATRIX_P (parameter, smatrix) is a parameter, smatrix.
+
+%%% ¡prop!
+PROP_CELL_P (parameter, cell) is a parameter, cell.
+
+%%% ¡prop!
+PROP_EMPTY_D (data, empty) is a data, empty.
+
+%%% ¡prop!
+PROP_STRING_D (data, string) is a data, string.
+
+%%% ¡prop!
+PROP_LOGICAL_D (data, logical) is a data, logical.
+
+%%% ¡prop!
+PROP_OPTION_D (data, option) is a data, option.
+
+%%% ¡prop!
+PROP_CLASS_D (data, class) is a data, class.
+
+%%% ¡prop!
+PROP_CLASSLIST_D (data, classlist) is a data, classlist.
+
+%%% ¡prop!
+PROP_ITEM_D (data, item) is a data, item.
+
+%%% ¡prop!
+PROP_ITEMLIST_D (data, itemlist) is a data, itemlist.
+
+%%% ¡prop!
+PROP_IDICT_D (data, idict) is a data, idict.
+
+%%% ¡prop!
+PROP_SCALAR_D (data, scalar) is a data, scalar.
+
+%%% ¡prop!
+PROP_RVECTOR_D (data, rvector) is a data, rvector.
+
+%%% ¡prop!
+PROP_CVECTOR_D (data, cvector) is a data, cvector.
+
+%%% ¡prop!
+PROP_MATRIX_D (data, matrix) is a data, matrix.
+
+%%% ¡prop!
+PROP_SMATRIX_D (data, smatrix) is a data, smatrix.
+
+%%% ¡prop!
+PROP_CELL_D (data, cell) is a data, cell.
+
+% %%% ¡prop!
+% PROP_EMPTY_R (result, empty) is a result, empty.
+% 
+% %%% ¡prop!
+% PROP_STRING_R (result, string) is a result, string.
+% 
+% %%% ¡prop!
+% PROP_LOGICAL_R (result, logical) is a result, logical.
+% 
+% %%% ¡prop!
+% PROP_OPTION_R (result, option) is a result, option.
+% 
+% %%% ¡prop!
+% PROP_CLASS_R (result, class) is a result, class.
+% 
+% %%% ¡prop!
+% PROP_CLASSLIST_R (result, classlist) is a result, classlist.
+% 
+% %%% ¡prop!
+% PROP_ITEM_R (result, item) is a result, item.
+% 
+% %%% ¡prop!
+% PROP_ITEMLIST_R (result, itemlist) is a result, itemlist.
+% 
+% %%% ¡prop!
+% PROP_IDICT_R (result, idict) is a result, idict.
+% 
+% %%% ¡prop!
+% PROP_SCALAR_R (result, scalar) is a result, scalar.
+% 
+% %%% ¡prop!
+% PROP_RVECTOR_R (result, rvector) is a result, rvector.
+% 
+% %%% ¡prop!
+% PROP_CVECTOR_R (result, cvector) is a result, cvector.
+% 
+% %%% ¡prop!
+% PROP_MATRIX_R (result, matrix) is a result, matrix.
+% 
+% %%% ¡prop!
+% PROP_SMATRIX_R (result, smatrix) is a result, smatrix.
+% 
+% %%% ¡prop!
+% PROP_CELL_R (result, cell) is a result, cell.
