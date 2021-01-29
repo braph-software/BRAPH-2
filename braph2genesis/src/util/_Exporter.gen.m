6f1d3155add98e95dee4f59fed5015497d936c39
annacanal <annacg@kth.se>
2021-01-29 14:42:38 +0100

    Init exporters/importers

---
+++ braph2genesis/src/util/_Exporter.gen.m
@@ -0,0 +1,13 @@
+%% ¡header!
+Exporter < Element (ex, exporter to a file) exports an element to a file.
+
+%% ¡props!
+
+%%% ¡prop!
+ID (data, string) is a few-letter code for the exporter.
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the exporter.
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the exporter.
