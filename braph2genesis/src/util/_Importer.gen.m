0ac99676197d31c0f1ce4c6f7b37dbd8c99bafa6
egolol <egolol87@gmail.com>
2021-01-29 15:11:29 +0100

    exporter/importer

---
+++ braph2genesis/src/util/_Importer.gen.m
@@ -0,0 +1,13 @@
+%% ¡header!
+Importer < Element (im, importer from a file) imports an element from a file.
+
+%% ¡props!
+
+%%% ¡prop!
+ID (data, string) is a few-letter code for the importer.
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the importer.
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the importer.
