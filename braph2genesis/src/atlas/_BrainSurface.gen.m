65deee8948331a513f440b2c27a088c3fb021123
egolol <egolol87@gmail.com>
2021-02-01 09:33:07 +0100

    Update _BrainSurface.gen.m

--- braph2genesis/src/atlas/_BrainSurface.gen.m
+++ braph2genesis/src/atlas/_BrainSurface.gen.m
@@ -23,13 +23,13 @@ LABEL (metadata, string) is an extended label of the brain surface.
 NOTES (metadata, string) are some specific notes about the brain surface.
 
 %%% ¡prop!
-VERTEX_NUMBER (data, cvector) is the number of triangles vertices.
+VERTEX_NUMBER (data, scalar) is the number of triangles vertices.
 
 %%% ¡prop!
 COORDINATES (data, cvector) is the coordinates of the triangles vertices.
 
 %%% ¡prop!
-TRIANGLES_NUMBER (data, cvector) is the number of triangles.
+TRIANGLES_NUMBER (data, scalar) is the number of triangles.
 
 %%% ¡prop!
 TRIANGLES (data, cvector) is the triad of vertex identifiers to create a triangle.
