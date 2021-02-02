2d0dff2215850f3cb0685acbe043fcf8989445e8
annacanal <annacg@kth.se>
2021-02-02 19:09:48 +0100

    Updated subject

--- braph2genesis/src/cohort/_Subject.gen.m
+++ braph2genesis/src/cohort/_Subject.gen.m
@@ -21,7 +21,47 @@ NOTES (metadata, string) are some specific notes about the subject.
 ATLASES (data, cell) is a cell array with brain atlases.
 
 %%% ¡prop!
-DATADICT (data, idict) is a dictionary with subject data.
+DATA_DICT (data, idict) is a dictionary with subject data.
 
 %%% ¡prop!
 h_panel
+
+%% ¡methods!
+function subject_list = getList()
+    % GETLIST returns the list of available subjects
+    %
+    % SUBJECT_LIST = GETLIST() returns the list of subjects
+    % (cell array) that are subclasses of Subject.
+    %
+    % See also getClass, getName, getDescription.
+
+    subject_list = subclasses('Subject');
+end
+function atlas_number = getBrainAtlasNumber(sub)
+    % GETBRAINATLASNUMBER returns the number of brain atlases
+    %
+    % N = GETBRAINATLASNUMBER(SUB) returns the number of
+    % brain atlases to the concrete subject SUB.
+    %
+    % N = GETBRAINATLASNUMBER(SUBJECT_CLASS) returns the number
+    % of brain atlases to the subject whose class is SUBJECT_CLASS.
+    %
+    % See also getList, getDescription, getName, getClass.
+
+    % number of differetn brain atlases
+
+    atlas_number = length(sub.get('ATLASES'));
+end
+function data_number = getDataNumber(sub)
+    % GETDATANUMBER returns the number of data
+    %
+    % N = GETDATANUMBER(SUB) returns the number of data of the
+    % subclass SUB
+    %
+    % N = GETDATANUMBER(SUBJECT_CLASS) returns the number
+    % of data to the subject whose class is SUBJECT_CLASS.
+    %
+    % See also getDataList, getBrainAtlasNumber, getDataCodes
+
+    data_number = length(gr.get('DATA_DICT'));
+end
