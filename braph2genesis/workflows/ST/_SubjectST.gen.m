82af09f0c68d6e4bbe6f10027adf05742c16591b
annacanal <annacg@kth.se>
2021-02-03 17:04:24 +0100

    change workflows folder dir

--- braph2genesis/workflows/ST/_SubjectST.gen.m
+++ braph2genesis/workflows/ST/_SubjectST.gen.m
@@ -1,9 +1,9 @@
 %% ¡header!
-SubjectST < Subject (sub, subject with structural data) is a subject with structural data (e.g. MRI).
+SubjectST < Subject (sub, subject with structural data) is a subject with structural data (e.g. sMRI).
 
 %%% ¡description!
 Subject with structural data (such as cortical thickness) for each brain region.
-For example, structural data can be MRI.
+For example, structural data can be structural MRI.
 
 %% ¡props!
 
@@ -11,14 +11,16 @@ For example, structural data can be MRI.
 BA (data, item) is a brain atlas.
 %%%% ¡settings!
 'BrainAtlas'
-%%%% ¡default!
-BrainAtlas()
 
 %%% ¡prop!
 ST (data, cvector) is a column vector with data for each brain region.
-%%%% ¡check!
+%%%% ¡check_value!
 br_number = sub.get('BA').get('BR_DICT').length();
 check = (iscolumn(value) && isequal(size(value), [br_number, 1])) || (isempty(value) && br_number == 0); % Format.checkFormat(Format.CVECTOR, value) already checked
-msg = ['ST must be a column vector with the same number of element as the brain regions (' int2str(br_number) ').'];
+if check
+    msg = 'All ok!';
+else   
+    msg = ['ST must be a column vector with the same number of element as the brain regions (' int2str(br_number) ').'];
+end
 %%%% ¡default!
 []
