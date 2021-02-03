a7808e67c20249ff3cf01d3bfc6da43d554ff185
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2021-02-03 15:52:30 +0100

    Create _PlotBrainSurface.gen.m

---
+++ braph2genesis/src/atlas/_PlotBrainSurface.gen.m
@@ -0,0 +1,59 @@
+%% ¡header!
+PlotBrainSurface < Plot (pl, plot brain surface) is a plot of a brain surfce.
+
+%%% ¡description!
+...
+
+%%% ¡seealso!
+Plot, BrainSurface
+
+%% ¡props!
+
+%%% ¡prop!
+SURF (metadata, item) is the brain surface to be plotted.
+%%%% ¡settings!
+'BrainSurface'
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+Basic use
+%%%% ¡code!
+pl = PlotBrainSurface('SURF', ImporterBrainSurfaceNV('FILE', 'human_ICBM152.nv').get('SURF'));
+
+
+% pbs = PlotBrainSurf(bs);
+% 
+% pbs.set_axes()
+% h = pbs.get_axes();
+% 
+% pbs.brain()
+% pbs.brain_off()
+% pbs.brain_on()
+% 
+% pbs.brain_settings();
+% 
+% pbs.hold_on()
+% pbs.hold_off()
+% 
+% pbs.grid_off()
+% pbs.grid_on()
+% 
+% pbs.axis_off()
+% pbs.axis_on()
+% 
+% pbs.axis_equal()
+% 
+% pbs.axis_tight()
+% 
+% pbs.view([45 45])
+% 
+% pbs.lighting('phong')
+% pbs.material('shiny')
+% pbs.camlight('left')
+% pbs.shading('interp')
+% pbs.colormap('jet')
+% 
+% close(gcf)
+% close(gcf)
