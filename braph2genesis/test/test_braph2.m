2318e00cea6d8d5284b7549835d3cbb33c55a67e
annacanal <annacg@kth.se>
2021-02-03 17:40:58 +0100

    update tes_braph2

--- braph2genesis/test/test_braph2.m
+++ braph2genesis/test/test_braph2.m
@@ -19,15 +19,15 @@ directories_to_test = { ...
     [braph2_dir filesep 'measures'] ...
     };
 
-% workflows_dir = [fileparts(which('braph2')) filesep 'workflows'];
-% 
-% addpath(workflows_dir)
-% workflows_dir_list = dir(workflows_dir);   % get the folder contents
-% workflows_dir_list = workflows_dir_list([workflows_dir_list(:).isdir] == 1);  % remove all files (isdir property is 0)
-% workflows_dir_list = workflows_dir_list(~ismember({workflows_dir_list(:).name}, {'.', '..'}));  % remove '.' and '..'
-% for i = 1:1:length(workflows_dir_list)
-%     directories_to_test{end + 1} = [workflows_dir filesep workflows_dir_list(i).name]; %#ok<SAGROW>
-% end
+workflows_dir = [fileparts(which('braph2')) filesep 'workflows'];
+
+addpath(workflows_dir)
+workflows_dir_list = dir(workflows_dir);   % get the folder contents
+workflows_dir_list = workflows_dir_list([workflows_dir_list(:).isdir] == 1);  % remove all files (isdir property is 0)
+workflows_dir_list = workflows_dir_list(~ismember({workflows_dir_list(:).name}, {'.', '..'}));  % remove '.' and '..'
+for i = 1:1:length(workflows_dir_list)
+    directories_to_test{end + 1} = [workflows_dir filesep workflows_dir_list(i).name]; %#ok<SAGROW>
+end
 
 clear braph2_dir workflows_dir workflows_dir_list i
 
