d3f3139d67bb9c27cc3aa6480847d5a063a502da
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2021-02-03 17:09:28 +0100

    Update _Plot.gen.m

--- braph2genesis/src/util/_Plot.gen.m
+++ braph2genesis/src/util/_Plot.gen.m
@@ -11,32 +11,58 @@ uipanel, ishandle
 %% ¡properties!
 h % panel graphical handle
 
-%% ¡methods!
-function setPanel(pl, h)
-    %SETPANEL sets graphic panel.
-    %
-    % SETPANEL(PL, H) sets the graphic panel to H. 
-    %  If H is not a uipanel, it throws an error.
-    %  Error id: [BRAPH2:<element class>:WrongInput]
-    %
-    % See also getPanel, uipanel.
+%% ¡props!
 
-    assert(isgraphics(h, 'uipanel'), ...
-        [BRAPH2.STR ':' pl.getClass() ':' BRAPH2.WRONG_INPUT], ...
-        [BRAPH2.STR ':' pl.getClass() ':' BRAPH2.WRONG_INPUT ' ' ...
-        'The value of the handle must be a handle of an active uipanel.'])
-
+%%% ¡prop!
+Plot (result, empty) is an empty property to plot.
+%%%% ¡conditioning!
+if isgraphics(value, 'uipanel')
     pl.h = h;
 end
-function h = getPanel(pl)
-    %GETPANEL gets graphic panel.
-    %
-    % H = GETPANEL(PL) returns a handle to the graphic panel used to plot.
-    %  If the panel has not been inizialized it returns an empty vector.
-    %  If the panel has been cancelled, it returns a handle to a deleted
-    %  panel.
-    %
-    % See also setPanel, uipanel.
 
-    h = pl.h;
+value = NoValue.getNoValue();
+%%%% ¡calculate!
+if isempty(pl.h) || ~isgraphics(pl.h, 'uipanel')
+    pl.h = uipanel();
 end
+
+value = [];
+
+%% ¡methods!
+% function setPanel(pl, h)
+%     %SETPANEL sets graphic panel.
+%     %
+%     % SETPANEL(PL, H) sets the graphic panel to H. 
+%     %  If H is not a uipanel, it throws an error.
+%     %  Error id: [BRAPH2:<element class>:WrongInput]
+%     %
+%     % See also getPanel, uipanel.
+% 
+%     assert(isgraphics(h, 'uipanel'), ...
+%         [BRAPH2.STR ':' pl.getClass() ':' BRAPH2.WRONG_INPUT], ...
+%         [BRAPH2.STR ':' pl.getClass() ':' BRAPH2.WRONG_INPUT ' ' ...
+%         'The value of the handle must be a handle of an active uipanel.'])
+% 
+%     pl.h = h;
+% end
+% function h = getPanel(pl)
+%     %GETPANEL gets graphic panel.
+%     %
+%     % H = GETPANEL(PL) returns a handle to the graphic panel used to plot.
+%     %  If the panel has not been inizialized it returns an empty vector.
+%     %  If the panel has been cancelled, it returns a handle to a deleted
+%     %  panel.
+%     %
+%     % See also setPanel, uipanel.
+% 
+%     h = pl.h;
+% end
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+Basics
+%%%% ¡code!
+pl = Plot();
+pl.get('PLOT');
