3d3d2d81ebee8a3139fdb364a21a4a1ca42cb84c
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2021-02-04 18:13:39 +0100

    Update _Plot.gen.m

--- braph2genesis/src/util/_Plot.gen.m
+++ braph2genesis/src/util/_Plot.gen.m
@@ -21,33 +21,6 @@ function h_panel = draw(pl, varargin)
         h_panel = pl.h_panel;
     end
 end
-% function setPanel(pl, h)
-%     %SETPANEL sets graphic panel.
-%     %
-%     % SETPANEL(PL, H) sets the graphic panel to H. 
-%     %  If H is not a uipanel, it throws an error.
-%     %  Error id: [BRAPH2:<element class>:WrongInput]
-%     %
-%     % See also getPanel, uipanel.
-% 
-%     if nargin < 2 || ~isgraphics(h, 'uipanel')
-%         h = uipanel('Units', 'normalized', 'Position', [0 0 1 1]);
-%     end
-% 
-%     pl.h = h;
-% end
-% function h = getPanel(pl)
-%     %GETPANEL gets graphic panel.
-%     %
-%     % H = GETPANEL(PL) returns a handle to the graphic panel used to plot.
-%     %  If the panel has not been inizialized it returns an empty vector.
-%     %  If the panel has been cancelled, it returns a handle to a deleted
-%     %  panel.
-%     %
-%     % See also setPanel, uipanel.
-% 
-%     h = pl.h;
-% end
 
 %% ¡tests!
 
