92b0982c384cf99bd31c803ecdaddddbac2ed3de
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2021-02-04 07:43:29 +0100

    Update _PlotBrainSurface.gen.m

--- braph2genesis/src/atlas/_PlotBrainSurface.gen.m
+++ braph2genesis/src/atlas/_PlotBrainSurface.gen.m
@@ -9,13 +9,7 @@ Plot, BrainSurface
 
 %% ¡properties!
 h_axes % handle for the axes
-
-%% ¡props_update!
-
-%%% ¡prop!
-PLOT (result, empty) is an empty property to plot.
-%%%% ¡calculate!
-value = calculateValue@Plot(pl, prop);
+h_brain % handle for brain surface
 
 %% ¡props!
 
@@ -24,13 +18,35 @@ SURF (metadata, item) is the brain surface to be plotted.
 %%%% ¡settings!
 'BrainSurface'
 
+%% ¡methods!
+function setPanel(pl, varargin)
+
+    setPanel@Plot(pl, varargin{:})
+    
+    pl.h_axes = axes(pl.getPanel());
+end
+function h_axes = getAxes(pl)
+    % GET_AXES gets current axes
+    %
+    % H = GET_AXES(BS) returns a handle to the axes used to plot
+    % the brain surf.
+    %
+    % See also set_axes.
+
+    h_axes = pl.h_axes;
+end
+
 %% ¡tests!
 
 %%% ¡test!
 %%%% ¡name!
 Basics
 %%%% ¡code!
+fig = figure();
+
 pl = PlotBrainSurface('SURF', ImporterBrainSurfaceNV('FILE', 'human_ICBM152.nv').get('SURF'));
+pl.setPanel(uipanel('Units', 'normalized', 'Position', [.1 .1 .8 .8]));
+h = pl.getPanel();
 
 % pbs.set_axes()
 % h = pbs.get_axes();
@@ -64,3 +80,5 @@ pl = PlotBrainSurface('SURF', ImporterBrainSurfaceNV('FILE', 'human_ICBM152.nv')
 % 
 % close(gcf)
 % close(gcf)
+
+close(fig)
