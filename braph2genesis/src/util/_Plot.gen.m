2785d223f8cb1f1d27bd65374b564c8433a6a32c
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2021-02-04 14:18:28 +0100

    Update _Plot.gen.m

--- braph2genesis/src/util/_Plot.gen.m
+++ braph2genesis/src/util/_Plot.gen.m
@@ -11,41 +11,45 @@ uipanel, ishandle
 h % panel graphical handle
 
 %% ¡methods!
-function setPanel(pl, h)
-    %SETPANEL sets graphic panel.
-    %
-    % SETPANEL(PL, H) sets the graphic panel to H. 
-    %  If H is not a uipanel, it throws an error.
-    %  Error id: [BRAPH2:<element class>:WrongInput]
-    %
-    % See also getPanel, uipanel.
+function h = draw(pl, varargin)
 
-    if nargin < 2 || ~isgraphics(h, 'uipanel')
-        h = uipanel('Units', 'normalized', 'Position', [0 0 1 1]);
+    if isempty(pl.h) || ~isgraphics(pl.h, 'uipanel')
+        h = uipanel(varargin{:});
     end
-
-    pl.h = h;
+    
+    if nargout == 0
+        clear h
+    end
 end
-function h = getPanel(pl)
-    %GETPANEL gets graphic panel.
-    %
-    % H = GETPANEL(PL) returns a handle to the graphic panel used to plot.
-    %  If the panel has not been inizialized it returns an empty vector.
-    %  If the panel has been cancelled, it returns a handle to a deleted
-    %  panel.
-    %
-    % See also setPanel, uipanel.
-
-    h = pl.h;
-end
+% function setPanel(pl, h)
+%     %SETPANEL sets graphic panel.
+%     %
+%     % SETPANEL(PL, H) sets the graphic panel to H. 
+%     %  If H is not a uipanel, it throws an error.
+%     %  Error id: [BRAPH2:<element class>:WrongInput]
+%     %
+%     % See also getPanel, uipanel.
+% 
+%     if nargin < 2 || ~isgraphics(h, 'uipanel')
+%         h = uipanel('Units', 'normalized', 'Position', [0 0 1 1]);
+%     end
+% 
+%     pl.h = h;
+% end
+% function h = getPanel(pl)
+%     %GETPANEL gets graphic panel.
+%     %
+%     % H = GETPANEL(PL) returns a handle to the graphic panel used to plot.
+%     %  If the panel has not been inizialized it returns an empty vector.
+%     %  If the panel has been cancelled, it returns a handle to a deleted
+%     %  panel.
+%     %
+%     % See also setPanel, uipanel.
+% 
+%     h = pl.h;
+% end
 
 %% ¡tests!
-
-%%% ¡test!
-%%%% ¡name!
-Close figure
-%%%% ¡code!
-close(gcf)
 
 %%% ¡test!
 %%%% ¡name!
@@ -54,11 +58,9 @@ Basics
 fig = figure();
 
 pl1 = Plot();
-pl1.setPanel();
-h1 = pl1.getPanel();
+pl1.draw()
 
 pl2 = Plot();
-pl2.setPanel(uipanel('Units', 'normalized', 'Position', [.25 .25 .50 .50]));
-h2 = pl2.getPanel();
+h2 = pl2.draw('Units', 'normalized', 'Position', [.25 .25 .50 .50]);
 
 close(fig)
