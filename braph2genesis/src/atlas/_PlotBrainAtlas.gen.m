cbf13e20fad014e202816bb833f848aefecf6972
egolol <egolol87@gmail.com>
2021-02-09 11:07:33 +0100

    review

--- braph2genesis/src/atlas/_PlotBrainAtlas.gen.m
+++ braph2genesis/src/atlas/_PlotBrainAtlas.gen.m
@@ -46,6 +46,7 @@ PLOT_SYMBOL_TAG = { ...
 
 %% ¡properties!
 symbols % handle for the symbols structure
+spheres % handle for the spheres structure
 
 %% ¡props!
 
@@ -55,14 +56,14 @@ ATLAS (metadata, item) is the brain atlas with the brain regions
 'BrainAtlas'
 
 %%% ¡prop!
-SYMS (metadata, rvector) is symbol visibility.
+SYMS (metadata, cvector) is symbol visibility.
 %%%% ¡check_value!
 check = length(value) == 1 || length(value) == pl.get('ATLAS').get('BR_DICT').length();
 %%%% ¡default!
 1
 
 %%% ¡prop!
-SYMS_SIZE (metadata, rvector) is the symbol size.
+SYMS_SIZE (metadata, cvector) is the symbol size.
 %%%% ¡conditioning!
 value = abs(value);
 %%%% ¡check_value!
@@ -71,25 +72,43 @@ check = length(value) == 1 || length(value) == pl.get('ATLAS').get('BR_DICT').le
 10
 
 %%% ¡prop!
-SYMS_MARKER (metadata, option) is the symbol option.
+SYMS_MARKER (metadata, cvector) is the symbol option.
 %%%% ¡settings!
-{'.' 'o' 'x' '+' '*' 's' 'd' 'v' '^' '>' '<' 'p' 'h' 'none'}
+1:length(PlotBrainAtlas.PLOT_SYMBOL_TAG)
+%%%% ¡check_value!
+check = 1 < value &&  value < length(PlotBrainAtlas.PLOT_SYMBOL_TAG);
 %%%% ¡default!
-'o'
+2
 
 %%% ¡prop!
-SYMS_FACE_COLOR (metadata, cell) is the symbol face color.
+SYMS_FACE_COLOR (metadata, matrix) is the symbol face color.
 %%%% ¡check_value!
-check = size(value, 1) == 1 || size(value, 1) == pl.get('ATLAS').get('BR_DICT').length();
+check = (size(value, 1) == 1 &&  size(value, 2) == 3)|| (size(value, 1) == pl.get('ATLAS').get('BR_DICT').length() &&  size(value, 2) == 3);
 %%%% ¡default!
-{[0 0 0]}
+[0 0 0]
 
 %%% ¡prop!
-SYMS_EDGE_COLOR (metadata, cell) is the symbol edge color.
+SYMS_EDGE_COLOR (metadata, matrix) is the symbol edge color.
 %%%% ¡check_value!
-check = size(value, 1) == 1 || size(value, 1) == pl.get('ATLAS').get('BR_DICT').length();
+check = (size(value, 1) == 1 &&  size(value, 2) == 3)|| (size(value, 1) == pl.get('ATLAS').get('BR_DICT').length() &&  size(value, 2) == 3);
 %%%% ¡default!
-{[0 0 0]} 
+[0 0 0]
+
+%%% ¡prop!
+SPHS (metadata, cvector) is sphere visibility.
+%%%% ¡check_value!
+check = length(value) == 1 || length(value) == pl.get('ATLAS').get('BR_DICT').length();
+%%%% ¡default!
+1
+
+%%% ¡prop!
+SPHS_SIZE (metadata, cvector) is the sphere size.
+%%%% ¡conditioning!
+value = abs(value);
+%%%% ¡check_value!
+check = length(value) == 1 || length(value) == pl.get('ATLAS').get('BR_DICT').length();
+%%%% ¡default!
+1
 
 %% ¡methods!
 function h_panel = draw(pl, varargin)
@@ -114,49 +133,76 @@ function h_panel = draw(pl, varargin)
     if isempty(pl.symbols)
         pl.symbols = cell(1, pl.get('ATLAS').get('BR_DICT').length);
     end
+    if isempty(pl.spheres)
+        pl.spheres = cell(1, pl.get('ATLAS').get('BR_DICT').length);
+    end
     
+    % get coordinates
     X = cellfun(@(x) x.get('X'), pl.get('ATLAS').get('BR_DICT').get('IT_LIST'), 'UniformOutput', false);
     Y = cellfun(@(x) x.get('Y'), pl.get('ATLAS').get('BR_DICT').get('IT_LIST'), 'UniformOutput', false);
     Z = cellfun(@(x) x.get('Z'), pl.get('ATLAS').get('BR_DICT').get('IT_LIST'), 'UniformOutput', false);
     
-    SYM_SIZE = pl.get('syms_size');
-    SYM_SHOW = pl.get('syms');
-    SYM_FACE_COLOR = pl.get('syms_face_color');
-    SYM_EDGE_COLOR = pl.get('syms_edge_color');
-    if length(SYM_SIZE) == 1
-        SYM_SIZE = repmat(SYM_SIZE, 1, pl.get('ATLAS').get('BR_DICT').length);
+    % get values & complete vector size
+    SYMS_SIZE = pl.get('syms_size');
+    if length(SYMS_SIZE) == 1
+        SYMS_SIZE = repmat(SYMS_SIZE, pl.get('ATLAS').get('BR_DICT').length, 1);
     end
-    if length(SYM_SHOW) == 1
-        SYM_SHOW = repmat(SYM_SHOW, 1, pl.get('ATLAS').get('BR_DICT').length);
+    SYMS_SHOW = pl.get('syms');
+    if length(SYMS_SHOW) == 1
+        SYMS_SHOW = repmat(SYMS_SHOW, pl.get('ATLAS').get('BR_DICT').length, 1);
     end
-    if length(SYM_FACE_COLOR) == 1
-        sym_hold = SYM_FACE_COLOR{1};
-        SYM_FACE_COLOR = cell(1, pl.get('ATLAS').get('BR_DICT').length);
-        SYM_FACE_COLOR(:) = {sym_hold};
+    SYMS_FACE_COLOR = pl.get('syms_face_color');
+    if  size(SYMS_FACE_COLOR, 1) == 1        
+        SYMS_FACE_COLOR = repmat(SYMS_FACE_COLOR, pl.get('ATLAS').get('BR_DICT').length, 1);
     end
-    if length(SYM_EDGE_COLOR) == 1
-        sym_hold = SYM_EDGE_COLOR{1};
-        SYM_EDGE_COLOR = cell(1, pl.get('ATLAS').get('BR_DICT').length);
-        SYM_EDGE_COLOR(:) = {sym_hold};
+    SYMS_EDGE_COLOR = pl.get('syms_edge_color');
+    if  size(SYMS_EDGE_COLOR, 1) == 1
+        SYMS_EDGE_COLOR = repmat(SYMS_EDGE_COLOR, pl.get('ATLAS').get('BR_DICT').length, 1);
     end
+    SPHS_SHOW = pl.get('sphs');
+    if length(SPHS_SHOW) == 1
+        SPHS_SHOW = repmat(SPHS_SHOW, pl.get('ATLAS').get('BR_DICT').length, 1);
+    end
+    SPHS_SIZE = pl.get('SPHS_SIZE'); 
+    if length(SPHS_SIZE) == 1
+        SPHS_SIZE = repmat(SPHS_SIZE, pl.get('ATLAS').get('BR_DICT').length, 1);
+    end    
     
-    for i = 1:1:pl.get('ATLAS').get('BR_DICT').length
-        
-        if ~ishandle(pl.symbols(i))
-            pl.symbols{i} = plot3(X{i}, Y{i}, Z{i}, ...
-                'Marker', pl.get('syms_marker'));
-        end
-        set(pl.symbols{i}, ...
-            'MarkerSize', SYM_SIZE(i), ...
-            'MarkerFaceColor', cell2mat(SYM_FACE_COLOR(i)), ...
-            'MarkerEdgeColor', cell2mat(SYM_EDGE_COLOR(i)) ...
-            );
-        
-        if SYM_SHOW(i)
+    % for loop for plots and sets
+    for i = 1:1:pl.get('ATLAS').get('BR_DICT').length        
+        % symbols
+        if SYMS_SHOW(i)
+            % plotting
+            if ~ishandle(pl.symbols(i))
+                pl.symbols{i} = plot3(X{i}, Y{i}, Z{i});
+            end
+            % set
+            m = PlotBrainAtlas.PLOT_SYMBOL_TAG(pl.get('syms_marker'));
             set(pl.symbols{i}, ...
-                'Visible', 'on');
+                'Visible', 'on', ...
+                'Marker', m{1}, ...
+                'MarkerSize', SYMS_SIZE(i), ...
+                'MarkerFaceColor', SYMS_FACE_COLOR(i, :), ...
+                'MarkerEdgeColor', SYMS_EDGE_COLOR(i, :) ...
+                );
         else
             set(pl.symbols{i}, ...
+                'Visible', 'off');
+        end
+        
+        % spheres
+        if SPHS_SHOW(i)
+            % plotting
+            if ~ishandle(pl.spheres(i))
+                [sx, sy, sz] = sphere();
+                pls.spheres{i} = surf(X{i} * SPHS_SIZE(i) *sx, Y{i} * SPHS_SIZE(i) * sy, ...
+                    Z{i} * SPHS_SIZE(i) * sz);
+            end
+            % set
+            set(pl.spheres{i}, ...
+                'Visible', 'on');
+        else
+            set(pl.spheres{i}, ...
                 'Visible', 'off');
         end
     end
@@ -188,8 +234,9 @@ br_dict =  IndexedDictionary( ...
     'it_list', {br1, br2, br3, br4, br5});
 atlas = BrainAtlas('ID', 'BA ID', 'Label', 'Brain Atlas Label', 'Notes', 'Brain atlas notes', 'SURF', bs, 'BR_DICT', br_dict);
 pl = PlotBrainAtlas('atlas', atlas, ...
-    'syms', 1, 'SYMS_SIZE', [20:10:60], ...
-    'SYMS_FACE_COLOR', {[0 0 0]}, 'SYMS_EDGE_COLOR', {[0 0 0]}, ...
+    'syms', 1, 'SYMS_SIZE', [20:10:60]', ...
+    'SYMS_FACE_COLOR', [0 0 0], 'SYMS_EDGE_COLOR', [0 0 0], ...
+    'SPHS', 1, 'SPHS_SIZE', [1:2:10]', ...
     'SURF', ImporterBrainSurfaceNV('FILE', 'human_ICBM152.nv').get('SURF'));
 pl.draw()
 
