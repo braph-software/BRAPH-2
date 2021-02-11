21401154512ae41c954261d94aa3c1448a0543de
egolol <egolol87@gmail.com>
2021-02-11 12:09:37 +0100

    Strength and StrengthAV

---
+++ braph2genesis/measures/_Strength.gen.m
@@ -0,0 +1,89 @@
+%% ¡header!
+Strength < Measure (m, strength) is the graph strength.
+
+%%% ¡description!
+The strength of a graph is the sum of all weights of the edges connected to
+a node within a layer. 
+
+%%% ¡shape!
+shape = Measure.NODAL;
+
+%%% ¡scope!
+scope = Measure.UNILAYER;
+
+%%% ¡parametricity!
+parametricity = Measure.NONPARAMETRIC;
+
+%%% ¡compatible_graphs!
+GraphWU
+MultiplexGraphWU
+
+%% ¡props_update!
+
+%%% ¡prop!
+M (result, cell) is the strength.
+%%%% ¡calculate!
+g = s.get('G');  % graph from measure class
+A = g.get('A');  % adjency matrix (for graph) or 2D-cell array (for multiplex)
+
+strength = cell(g.layernumber(), 1);
+for li = 1:1:g.layernumber()
+    if g.is_graph(g)
+        Aii = A;
+    else
+        Aii = A{li, li};
+    end
+    strength(li) = {sum(Aii, 2)};  % calculates the strength of a node for layer li
+end
+value = strength;
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+GraphWU
+%%%% ¡code!
+B = [
+    0  .2 1
+    .2 0  0
+    1  0  0
+    ];
+known_strength = {[1.2 0.2 1]'};
+g = GraphWU('B', B);
+s_outside_g = Strength('G', g);
+
+assert(isequal(s_outside_g.getValue('M'), known_strength), ...
+    [BRAPH2.STR ':Strength:' BRAPH2.BUG_ERR], ...
+    'Strength is not being calculated correctly for GraphWU.')
+
+%%% ¡test!
+%%%% ¡name!
+MultiplexGraphWU
+%%%% ¡code!
+B11 = [
+    0  .2 1
+    .2 0  0
+    1  0  0
+    ];
+B12 = eye(3);
+B21 = eye(3);
+B22 = [
+    0 1  0
+    1 0  .4
+    0 .4 0
+    ];
+B = {
+    B11     B12  
+    B21     B22
+    };
+
+known_strength = {
+                 [1.2 .2  1]'
+                 [1   1.4 .4]'
+                 };
+                                
+g = MultiplexGraphWU('B', B);
+strength = Strength('G', g);
+
+assert(isequal(strength.get('M'), known_strength), ...
+    [BRAPH2.STR ':Strength:' BRAPH2.BUG_ERR], ...
+    'Strength is not being calculated correctly for MultiplexGraphWU.')
