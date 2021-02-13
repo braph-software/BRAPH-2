4cf11d003567ac98c96aa14c60da06410c1983e2
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2021-02-13 07:37:14 +0100

    Update GUI.m

--- braph2genesis/src/gui/GUI.m
+++ braph2genesis/src/gui/GUI.m
@@ -1,3 +1,10 @@
-function GUI(el)
-
+classdef GUI
+    properties (Access=protected)
+        el % element to be visualized
+    end
+    methods % constructor
+        function gui = GUI(el)
+            gui.el = el;
+        end
+    end
 end
