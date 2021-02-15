1d450657775bfa02a6a6fadea4240345af510b69
annacanal <annacg@kth.se>
2021-02-15 13:35:53 +0100

    Add Overlapping Strength

---
+++ braph2genesis/measures/_OverlappingStrength.gen.m
@@ -0,0 +1,62 @@
+%% ¡header!
+OverlappingStrength < Strength (m, overlapping strength) is the graph overlapping strength.
+
+%%% ¡description!
+The overlapping strength of a graph is the sum of the strengths of a node in
+all layers. 
+  
+%%% ¡shape!
+shape = Measure.NODAL;
+
+%%% ¡scope!
+scope = Measure.SUPERGLOBAL;
+
+%%% ¡parametricity!
+parametricity = Measure.NONPARAMETRIC;
+
+%%% ¡compatible_graphs!
+MultiplexGraphWU
+
+%% ¡props_update!
+
+%%% ¡prop!
+M (result, cell) is the overlapping strength.
+%%%% ¡calculate!
+g = m.get('G');  % graph from measure class
+A = g.get('A');  % cell array with adjacency matrix 
+L = g.layernumber();
+N = length(A{1, 1});
+
+strength = calculateValue@Strength(m, prop);	
+overlapping_strength = zeros(N, 1);
+for li = 1:1:L  
+    overlapping_strength = overlapping_strength + strength{li};
+end
+value = {overlapping_strength};
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+MultiplexGraphWU
+%%%% ¡code!
+B11 = [
+    0   .2  1
+    .2  0   0
+    1   0   0
+    ];
+B22 = [
+    0   1   0
+    1   0   .3
+    0   .3  0
+    ];
+B = {B11 B22 };
+
+known_overlapping_strength = {[2.2 1.5 1.3]'};
+
+g = MultiplexGraphWU('B', B);
+overlapping_strength = Strength('G', g);
+
+assert(isequal(overlapping_strength.get('M'), known_overlapping_strength), ...
+    [BRAPH2.STR ':OverlappingStrength:' BRAPH2.BUG_ERR], ...
+    'OverlappingStrength is not being calculated correctly for MultiplexGraphWU.')
