32d580911b61663b6377d1a32eee98e3ea02f6d9
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2021-02-20 22:57:34 +0100

    Update GUI.m

--- braph2genesis/src/gui/GUI.m
+++ braph2genesis/src/gui/GUI.m
@@ -1,174 +1,150 @@
-classdef GUI
+classdef GUI2
     properties (Access=protected)
         el % element to be visualized
         
         name % app name
         
-        x0 % widow left corner
-        y0 % widow lower corner
-        width % window width
-        height % window height
+        f % figure
+        p % panel
+        s % slider
         
-        border % standard border
-        
-        ui_panel_prop % property panel
-        ui_slider_prop % propety slider
-        
-        ui_panel_props % property panel list
-        ui_text_props_tag
+        prop_panel % prop panel cell array
+        prop_text_tag
     end
     properties (Constant)
-        BKGCOLOR = [.98 .98 .98]
-        FRGCOLOR = [.80 .80 .80]
-        FFGCOLOR = [.95 .95 .95]
-        
-        MSG_YES = 'Yes'
-        MSG_NO = 'No'
-    end
-    methods (Static)
-        function c = ACCELERATOR()
-            %ACCELERATOR defines the accelerator.
-            %
-            % C = ACCELERATOR() returns the corresponding accelerator depending
-            % on whether the version is for Apple Mac OS X or for Win/Unix.
-            %
-            % See also GUI, ismac.
-            
-            if ismac  % Mac
-                c = 'Cmd';
-            else  % Win/Unix
-                c = 'Crtl';
-            end
-        end
-        function w = SCREEN_WIDTH()
-            %SCREEN_WIDTH defines the screen width
-            %
-            % W = SCREEN_WIDTH() returns the width of the screen.
-            %
-            % See also SCREEN_HEIGHT.
-            
-            screensize = get(0, 'screensize');
-            w = screensize(3);
-        end
-        function h = SCREEN_HEIGHT()
-            %SCREEN_HEIGHT defines the screen height.
-            %
-            % W = SCREEN_HEIGHT() returns the height of the screen.
-            %
-            % See also SCREEN_WIDTH.
-            
-            screensize = get(0, 'screensize');
-            h = screensize(4);
-        end
-        function close(AppName, f)
-            %CLOSE closes GUI figure.
-            %
-            % CLOSE(APPNAME,F) promts the user to choose whether to close the
-            % GUI figure F with name APPNAME. If the user answers positively,
-            % the GUI figure closes. F can also be a vector of figures.
-            
-            selection = questdlg(['Do you want to close ' AppName '?'], ...
-                ['Close ' AppName], ...
-                GUI.MSG_YES, GUI.MSG_NO, GUI.MSG_YES);
-            switch selection
-                case GUI.MSG_YES
-                    delete(f)
-                case GUI.MSG_NO
-                    return
-            end
-        end
+        BKGCOLOR = [.80 .80 .80]
+        FRGCOLOR = [.98 .98 .98]
     end
     methods % constructor
-        function gui = GUI(el, varargin)
+        function gui = GUI2(el, varargin)
             gui.el = el;
-            
-            % parameters
+
+            % name
             if el.existsTag('ID')
-                gui.name = get_from_varargin([el.getClass() ' - ' el.get('ID')], 'x0', varargin);
+                gui.name = get_from_varargin([el.getClass() ' - ' el.get('ID')], 'name', varargin);
             else
-                gui.name = get_from_varargin(el.getClass(), 'x0', varargin);
+                gui.name = get_from_varargin(el.getClass(), 'name', varargin);
             end
             
-            gui.x0 = get_from_varargin(.02, 'x0', varargin);
-            gui.y0 = get_from_varargin(.30, 'y0', varargin);
-            gui.width = get_from_varargin(.70, 'width', varargin);
-            gui.height = get_from_varargin(.60, 'height', varargin);
+            position = get_from_varargin([.02 .30 .30 .80], 'position', varargin);
 
-            gui.border = get_from_varargin(.01, 'border', varargin);
-            
             % initializes GUI
-            f = figure( ...
+            gui.f = figure( ...
                 'Visible', 'off', ...
                 'NumberTitle', 'off', ...
                 'Name', [gui.name ' - ' BRAPH2.STR], ...
                 'Units', 'normalized', ...
-                'Position', [gui.x0 gui.y0 gui.width gui.height], ...
+                'Position', position, ...
+                'Units', 'character', ...
                 'DockControls', 'off', ...
-                'Color', GUI.BKGCOLOR, ...
-                'CloseRequestFcn', ['GUI.close(''' gui.name ''', gcf)'] ...
+                'Color', GUI2.BKGCOLOR, ...
+                'SizeChangedFcn', {@resize}, ...
+                'CloseRequestFcn', {@close} ...
                 );
             
-            % properties
-            props_w = .9;
-            for prop = 1:1:el.getPropNumber()
-                props_h(prop) = .3; %#ok<AGROW>
+            function close(~, ~)
+                selection = questdlg(['Do you want to close ' gui.name '?'], ...
+                    ['Close ' gui.name], ...
+                    'Yes', 'No', 'Yes');
+                switch selection
+                    case 'Yes'
+                        delete(gui.f)
+                    case 'No'
+                        return
+                end
             end
-            props_x0 = gui.border;
-            props_y0 = sum(props_h + gui.border) - cumsum(props_h + gui.border) + gui.border;
+            function resize(~, ~)
+                set(gui.p, 'Position', [0 0 w(gui.f) h(gui.f)])
+                                
+                dw = 1; % border along x
+                dh = .5; % border along y
+                
+                prop_panel_w = w(gui.p) - 2 * dw - w(gui.s);
+                prop_panel_h = cellfun(@(x) h(x), gui.prop_panel);
+                prop_panel_x0 = dw;
+                prop_panel_y0 = sum(prop_panel_h + dh) - cumsum(prop_panel_h + dh) + dh;
+                for prop = 1:1:el.getPropNumber() %#ok<FXUP>
+                    set(gui.prop_panel{prop}, 'Position', [prop_panel_x0 prop_panel_y0(prop) prop_panel_w prop_panel_h(prop)])
+                    set(gui.prop_text_tag{prop}, 'Position', [0 prop_panel_h(prop)-h(gui.prop_text_tag{prop}) prop_panel_w h(gui.prop_text_tag{prop})])
+                end
+
+                h_p = sum(prop_panel_h + dh) + dh;
+                set(gui.p, 'Position', [0 max(0, h(gui.f) - h_p) w(gui.f) h_p])
+                
+                if h(gui.f) >= h(gui.p)
+                    set(gui.s, 'Visible', 'off')
+                else
+                    set(gui.s, ...
+                        'Visible', 'on', ...
+                        'Position', [w(gui.p)-w(gui.s) 0 w(gui.s) h(gui.f)], ...
+                        'Min', h(gui.f) - h(gui.p) ...
+                        )
+                end
+            end
             
-            ui_panel_prop_w = .4;
-            ui_panel_prop_h = sum(props_h + gui.border) + gui.border;
-            
-            props_h = props_h / ui_panel_prop_h;
-            props_y0 = props_y0 / ui_panel_prop_h;
-            
-            ui_panel_prop = uipanel( ...
-                'Parent', f, ...
-                'Units', 'normalized', ...
-                'Position', [0 1-ui_panel_prop_h ui_panel_prop_w ui_panel_prop_h], ...
-                'BackgroundColor', GUI.FRGCOLOR);
-            
-            for prop = 1:1:el.getPropNumber()
-                gui.ui_panel_props{prop} = uipanel( ...
-                    'Parent', ui_panel_prop, ...
-                    'Units', 'normalized', ...
-                    'Position', [props_x0 props_y0(prop) props_w props_h(prop)], ...
-                    'BackgroundColor', GUI.FFGCOLOR, ...
+            gui.p = uipanel( ...
+                'Parent', gui.f, ...
+                'Units', 'character', ...
+                'BackgroundColor', GUI2.BKGCOLOR, ...
+                'BorderType', 'none' ...
+                );
+
+            for prop = 1:1:el.getPropNumber() %#ok<FXUP>
+                gui.prop_panel{prop} = uipanel( ...
+                    'Parent', gui.p, ...
+                    'Units', 'character', ...
+                    'Position', [0 0 eps 2], ... % defines prop panel height
+                    'BackgroundColor', GUI2.FRGCOLOR, ...
                     'BorderType', 'none' ...
                     );
                 
-                gui.ui_text_props_tag{prop} = uicontrol( ...
+                gui.prop_text_tag{prop} =  uicontrol( ...
                     'Style', 'text', ...
-                    'Parent', gui.ui_panel_props{prop}, ...
-                    'Units', 'normalized', ...
+                    'Parent', gui.prop_panel{prop}, ...
+                    'Units', 'character', ...
+                    'Position', [0 0 eps 1], ... % defines prop text tag height
                     'String', upper(el.getPropTag(prop)), ...
                     'HorizontalAlignment', 'left', ...
-                    'Tooltip', [num2str(el.getPropProp(prop)) ' ' el.getPropDescription(prop)] ...
+                    'Tooltip', [num2str(el.getPropProp(prop)) ' ' el.getPropDescription(prop)], ...
+                    'BackgroundColor', GUI2.FRGCOLOR ...
                     );
-                pos = get(gui.ui_text_props_tag{prop}, 'Position');
-                set(gui.ui_text_props_tag{prop}, 'Position', [0 1-pos(4) 1 pos(4)])
             end
             
-            ui_slider_prop = uicontrol( ...
+            gui.s = uicontrol( ...
                 'Style', 'slider', ...
-                'Parent', f, ...
-                'Units', 'normalized', ...
-                'Position', [0 0 ui_panel_prop_w 1], ...
-                'Min', 1 - ui_panel_prop_h, ...
+                'Parent', gui.f, ...
+                'Units', 'character', ...
+                'Position', [0 0 5 eps], ... % defines slider width
+                'Min', -eps, ...
                 'Max', 0, ...
                 'Value', 0, ...
-                'Callback', {@cb_slider_prop});
-            
-            function cb_slider_prop(~, ~)
-                offset = get(ui_slider_prop, 'Value');
-                position = get(ui_panel_prop, 'Position');
-                set(ui_panel_prop, 'Position', [position(1) 1-ui_panel_prop_h-offset position(3) position(4)]);
-            end
-            
-            % details
-            
+                'Callback', {@cb_s});
+                
+            function cb_s(~, ~)
+                offset = get(gui.s, 'Value');
+                set(gui.p, 'Position', [x0(gui.p) h(gui.f)-h(gui.p)-offset w(gui.p) h(gui.p)]);
+            end               
+
             % shows GUI
-            set(f, 'Visible', 'on')
+            set(gui.f, 'Visible', 'on')
+            
+            % auxiliary functions
+            function r = x0(h)
+                r = get(h, 'Position');
+                r = r(1);
+            end
+            function r = y0(h) %#ok<DEFNU>
+                r = get(h, 'Position');
+                r = r(2);
+            end
+            function r = w(h)
+                r = get(h, 'Position');
+                r = r(3);
+            end
+            function r = h(h)
+                r = get(h, 'Position');
+                r = r(4);
+            end
         end
     end
 end
