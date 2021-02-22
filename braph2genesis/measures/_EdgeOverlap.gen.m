8632193105b72a10c52645fc8b8dd3a57f23ca10
annacanal <annacg@kth.se>
2021-02-22 15:56:05 +0100

    Add edge overlap, (av-)degree overlap

---
+++ braph2genesis/measures/_EdgeOverlap.gen.m
@@ -0,0 +1,163 @@
+%% ¡header!
+EdgeOverlap < Measure (m, edge overlap) is the graph edge overlap.
+
+%%% ¡description!
+The edge overlap of a graph is the fraction of layers in which an edge between
+a pair of nodes exists. Connection weights are ignored in calculations.
+    
+%%% ¡shape!
+shape = Measure.BINODAL;
+
+%%% ¡scope!
+scope = Measure.SUPERGLOBAL;
+
+%%% ¡parametricity!
+parametricity = Measure.NONPARAMETRIC;
+
+%%% ¡compatible_graphs!
+MultiplexGraphBD
+MultiplexGraphBU
+MultiplexGraphWD
+MultiplexGraphWU
+
+%% ¡props_update!
+
+%%% ¡prop!
+M (result, cell) is the edge overlap.
+%%%% ¡calculate!
+g = m.get('G'); % graph from measure class
+A = g.get('A'); % cell with adjacency matrix (for graph) or 2D-cell array (for multigraph, multiplex, etc.)
+N = g.nodenumber();
+L = g.layernumber();
+
+if isempty(N)
+    edge_overlap = zeros(1);
+else
+    edge_overlap = zeros(N(1));
+end
+
+for li = 1:1:L
+    Aii = A{li, li};
+    Aii = binarize(Aii);  % binarizes the adjacency matrix
+    edge_overlap = edge_overlap + Aii;
+end
+
+value = {edge_overlap / L};
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+MultiplexGraphBU
+%%%% ¡code!
+B11 = [
+    0   1   1
+    1   0   0
+    1   0   0
+    ];
+B22 = [
+    0   1   0
+    1   0   1
+    0   1   0
+    ];
+B = {B11 B22};
+
+known_edge_overlap = {[
+    0   1   .5
+    1   0   .5
+    .5  .5  0
+    ]};
+
+g = MultiplexGraphBU('B', B);
+edge_overlap = EdgeOverlap('G', g);
+ 
+assert(isequal(edge_overlap.get('M'), known_edge_overlap), ...
+    [BRAPH2.STR ':EdgeOverlap:' BRAPH2.BUG_ERR], ...
+    'EdgeOverlap is not being calculated correctly for MultiplexGraphBU.')
+
+%%% ¡test!
+%%%% ¡name!
+MultiplexGraphBD
+%%%% ¡code!
+B11 = [
+    0   1   0
+    1   0   0
+    1   0   0
+    ];
+B22 = [
+    0   1   0
+    1   0   1
+    0   1   0
+    ];
+B = {B11 B22};
+
+known_edge_overlap = {[
+    0   1   0
+    1   0   .5
+    .5  .5  0
+    ]};
+
+g = MultiplexGraphBD('B', B);
+edge_overlap = EdgeOverlap('G', g);
+
+assert(isequal(edge_overlap.get('M'), known_edge_overlap), ...
+    [BRAPH2.STR ':EdgeOverlap:' BRAPH2.BUG_ERR], ...
+    'EdgeOverlap is not being calculated correctly for MultiplexGraphBD')
+
+%%% ¡test!
+%%%% ¡name!
+MultiplexGraphWU
+%%%% ¡code!
+B11 = [
+    0   .2  1
+    .2  0   0
+    1   0   0
+    ];
+B22 = [
+    0   1   0
+    1   0   .3
+    0   .3  0
+    ];
+B = {B11 B22};
+
+known_edge_overlap = {[
+    0   1   .5
+    1   0   .5
+    .5  .5  0
+    ]};
+
+g = MultiplexGraphWU('B', B);
+edge_overlap = EdgeOverlap('G', g);
+
+assert(isequal(edge_overlap.get('M'), known_edge_overlap), ...
+    [BRAPH2.STR ':EdgeOverlap:' BRAPH2.BUG_ERR], ...
+    'EdgeOverlap is not being calculated correctly for MultiplexGraphWU')
+
+%%% ¡test!
+%%%% ¡name!
+MultiplexGraphWD
+%%%% ¡code!
+B11 = [
+    0   .2  0
+    .2  0   0
+    1   0   0
+    ];
+B22 = [
+    0   1   0
+    1   0   .3
+    0   .3  0
+    ];
+B = {B11 B22};
+
+known_edge_overlap = {[
+    0   1   0
+    1   0   .5
+    .5  .5  0
+    ]};
+
+g = MultiplexGraphWD('B', B);
+edge_overlap = EdgeOverlap('G', g);
+
+assert(isequal(edge_overlap.get('M'), known_edge_overlap), ...
+    [BRAPH2.STR ':EdgeOverlap:' BRAPH2.BUG_ERR], ...
+    'EdgeOverlap is not being calculated correctly for MultiplexGraphWD')
