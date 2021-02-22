53a0e97ee36feb7d4dcba7d0486fc8a9002f7d6d
annacanal <annacg@kth.se>
2021-02-22 09:54:09 +0100

    Add in-strength av

---
+++ braph2genesis/measures/_InStrengthAv.gen.m
@@ -0,0 +1,81 @@
+%% ¡header!
+InStrengthAv < InStrength (m, average in-strength) is the graph average in-strength.
+
+%%% ¡description!
+The average in-strength of a graph is the average of the sum of all weights of
+the inward edges connected to a node within a layer. 
+
+%%% ¡shape!
+shape = Measure.GLOBAL;
+
+%%% ¡scope!
+scope = Measure.UNILAYER;
+
+%%% ¡parametricity!
+parametricity = Measure.NONPARAMETRIC;
+
+%%% ¡compatible_graphs!
+GraphWD
+MultiplexGraphWD
+
+%% ¡props_update!
+
+%%% ¡prop!
+M (result, cell) is the average in-strength.
+%%%% ¡calculate!
+in_strength = calculateValue@InStrength(m, prop);					
+g = m.get('G');  % graph from measure class
+
+in_strength_av = cell(g.layernumber(), 1);
+for li = 1:1:length(in_strength_av)
+    in_strength_av(li) = {mean(in_strength{li})};
+end
+
+value = in_strength_av;
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+GraphWD
+%%%% ¡code!
+B = [
+    0  .2 1
+    .2 0  0
+    1  0  0
+    ];
+known_in_strength_av = {mean([1.2 0.2 1])'};
+g = GraphWD('B', B);
+s_outside_g = InStrengthAv('G', g);
+
+assert(isequal(s_outside_g.get('M'), known_in_strength_av), ...
+    [BRAPH2.STR ':InStrengthAv:' BRAPH2.BUG_ERR], ...
+    'InStrengthAv is not being calculated correctly for GraphWD.')
+
+%%% ¡test!
+%%%% ¡name!
+MultiplexGraphWD
+%%%% ¡code!
+B11 = [
+    0  .2 1
+    .2 0  0
+    1  0  0
+    ];
+B22 = [
+    0 1  0
+    1 0  .4
+    0 .4 0
+    ];
+B = {B11 B22};
+
+known_in_strength_av = {
+                 mean([1.2 .2  1])
+                 mean([1   1.4 .4])
+                 };
+                                
+g = MultiplexGraphWD('B', B);
+in_strength_av = InStrengthAv('G', g);
+
+assert(isequal(in_strength_av.get('M'), known_in_strength_av), ...
+    [BRAPH2.STR ':InStrengthAv:' BRAPH2.BUG_ERR], ...
+    'InStrengthAv is not being calculated correctly for MultiplexGraphWD.')
