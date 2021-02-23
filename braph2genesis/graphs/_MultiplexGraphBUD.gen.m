593bb1ecf8582eefef168ddd574bb1449cabfc25
annacanal <annacg@kth.se>
2021-02-23 11:27:30 +0100

    Add Multiplex Graph BUD

---
+++ braph2genesis/graphs/_MultiplexGraphBUD.gen.m
@@ -0,0 +1,80 @@
+%% ¡header!
+MultiplexGraphBUD < MultiplexGraphWU (g, multiplex binary undirected multigraph with fixed densities) is a multiplex binary undirected multigraph with fixed densities.
+
+%%% ¡description!
+In a multiplex binary undirected multigraph with fixed densities (BUD), 
+all the layers consist of binary undirected (BU) multiplex graphs 
+derived from the same weighted supra-connectivity matrices 
+binarized at different densities.
+
+%%% ¡ensemble!
+false
+
+%%% ¡graph!
+graph = Graph.MULTIGRAPH;
+
+%%% ¡connectivity!
+connectivity = Graph.BINARY * ones(layernumber);
+
+%%% ¡directionality!
+directionality = Graph.UNDIRECTED * ones(layernumber);
+
+%%% ¡selfconnectivity!
+selfconnectivity = Graph.SELFCONNECTED * ones(layernumber);
+selfconnectivity(1:layernumber+1:end) = Graph.NONSELFCONNECTED;                
+
+%%% ¡negativity!
+negativity = Graph.NONNEGATIVE * ones(layernumber);
+
+%% ¡props!
+
+%%% ¡prop!
+DENSITIES (data, rvector) is the vector of densities.
+
+%% ¡props_update!
+
+%%% ¡prop!
+A (result, cell) is the cell array containing the multiplex binary adjacency matrices of the multiplex binary undirected multigraph. 
+%%%% ¡calculate!
+A_WU = calculateValue@MultiplexGraphWU(g, prop);
+
+densities = g.get('DENSITIES');
+L = length(A_WU); %% number of layers
+A = cell(length(densities)*L);
+
+for i = 1:1:length(densities)
+    density = densities(i);
+    layer = 1;
+    for j = (i*2) - 1:1: (i*2) + L - 2     
+        A{j, j} = binarize(A_WU{layer, layer}, 'density', density);
+        layer = layer + 1;
+    end
+end
+
+value = A;
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+Constructor
+%%%% ¡code!
+A = [
+     0 .1 .2 .3 .4
+    .1  0 .5 .6 .7
+    .2 .5  0 .8 .9
+    .3 .6 .8  0  1
+    .4 .7 .9  1  0
+    ];
+B = {A, A};
+
+g = MultiplexGraphBUD('B', B, 'DENSITIES', [0 55 100]);
+
+A = g.get('A');
+
+assert( ...
+    sum(sum(A{1, 1})) == 0 && ...
+    sum(sum(A{2, 2})) == 10 && ...
+    sum(sum(A{3, 3})) == 20, ...
+    [BRAPH2.STR ':MultigraphBUD:' BRAPH2.BUG_ERR], ...
+    'MultigraphBUD is not constructing well.')
