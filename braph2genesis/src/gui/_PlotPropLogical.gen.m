5f1cf97508d5a7f07df8ffe2f0d3f01efbc5b924
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2021-02-27 17:08:38 +0100

    logical

--- braph2genesis/src/gui/_PlotPropLogical.gen.m
+++ braph2genesis/src/gui/_PlotPropLogical.gen.m
@@ -1,23 +1,23 @@
 %% ¡header!
-PlotPropScalar < PlotProp (pl, plot property scalar) is a plot of a property scalar.
+PlotPropLogical < PlotProp (pl, plot property logical) is a plot of a property logical.
 
 %%% ¡description!
-PlotProp plots a property scalar of an element in a panel.
+PlotProp plots a property logical of an element in a panel.
 
 %%% ¡seealso!
 GUI, PlotElement, PlotProp
 
 %% ¡properties!
 pp
-edit_value
+checkbox_value
 
 %% ¡methods!
 function h_panel = draw(pl, varargin)
-    %DRAW draws the scalar property graphical panel.
+    %DRAW draws the logical property graphical panel.
     %
-    % DRAW(PL) draws the scalar property graphical panel.
+    % DRAW(PL) draws the logical property graphical panel.
     %
-    % H = DRAW(PL) returns a handle to the scalar property graphical panel.
+    % H = DRAW(PL) returns a handle to the logical property graphical panel.
     %
     % DRAW(PL, 'Property', VALUE, ...) sets the properties of the graphical
     %  panel with custom property-value couples.
@@ -33,22 +33,22 @@ function h_panel = draw(pl, varargin)
     
     pl.pp = draw@PlotProp(pl, varargin{:});
     
-    if isempty(pl.edit_value) || ~isgraphics(pl.edit_value, 'edit')
-        pl.edit_value = uicontrol( ...
-            'Style', 'edit', ...
+    if isempty(pl.checkbox_value) || ~isgraphics(pl.checkbox_value, 'checkbox')
+        pl.checkbox_value = uicontrol( ...
+            'Style', 'checkbox', ...
             'Parent', pl.pp, ...
             'Units', 'normalized', ...
             'Position', [.01 .10 .29 .40], ...
-            'HorizontalAlignment', 'left', ...
-            'BackgroundColor', 'w', ...
+            'HorizontalAlignment', 'center', ...
+            'BackgroundColor', pl.get('BKGCOLOR'), ...
             'Tooltip', [num2str(el.getPropProp(prop)) ' ' el.getPropDescription(prop)], ...
-            'Callback', {@cb_edit_value} ...
+            'Callback', {@cb_checkbox_value} ...
             );
     end
 
     % callback
-    function cb_edit_value(src, ~)
-        el.set(prop, str2double(get(src, 'String')))
+    function cb_checkbox_value(src, ~)
+        el.set(prop, logical(get(src, 'Value')))
    
         pl.update()
     end
@@ -71,34 +71,34 @@ function update(pl)
     prop = pl.get('PROP');
     
     if el.isLocked(prop)
-        set(pl.edit_value, 'Enable', 'off')
+        set(pl.checkbox_value, 'Enable', 'off')
     end
 
     switch el.getPropCategory(prop)
         case Category.METADATA
-            set(pl.edit_value, 'String', mat2str(el.get(prop)))
+            set(pl.checkbox_value, 'Value', el.get(prop))
 
         case {Category.PARAMETER, Category.DATA}
-            set(pl.edit_value, 'String', mat2str(el.get(prop)))
+            set(pl.checkbox_value, 'Value', el.get(prop))
 
             value = el.getr(prop);
             if isa(value, 'Callback')
-                set(pl.edit_value, 'Enable', 'off')
+                set(pl.checkbox_value, 'Enable', 'off')
             end
 
         case Category.RESULT
             value = el.getr(prop);
 
             if isa(value, 'NoValue')
-                set(pl.edit_value, ...
-                    'String', mat2str(el.getPropDefault(prop)), ...
+                set(pl.checkbox_value, ...
+                    'Value', el.getPropDefault(prop), ...
                     'Enable', 'off' ...
                     )
             else
-                set(pl.edit_value, ...
-                    'String', mat2str(el.get(prop)), ...
-                    'Enable', 'off' ...
-                    )
+            set(pl.checkbox_value, ...
+                'Value', el.get(prop), ...
+                'Enable', 'off' ...
+                )
             end
     end
 end
@@ -111,5 +111,5 @@ function resize(pl, varargin)
     %
     % See also draw, update, refresh.
     
-    pl.resize@PlotProp('Height', 2.50)
+    pl.resize@PlotProp('Height', 2.5)
 end
