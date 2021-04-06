7d176c8e0e4e4efcd01923cae2e7f821059e01a0
annacanal <annacg@kth.se>
2021-04-06 17:00:03 +0200

    Add layernumber method in MultiplexBUT

--- braph2genesis/graphs/_MultiplexBUT.gen.m
+++ braph2genesis/graphs/_MultiplexBUT.gen.m
@@ -56,6 +56,22 @@ end
 
 value = A;
 
+%% ¡methods!
+function [l, ls] = layernumber(g)
+    %LAYERNUMBER returns the number of layers in the graph.
+    %
+    % L = LAYERNUMBER(G) returns the number of layers in graph G. L is a scalar.
+    %
+    % [~, LS] = LAYERNUMBER(G) returns the number of layers in the partitions
+    %  of graph G. LS is a vector of integers.
+    %
+    % See also nodenumber.
+
+    l = length(g.get('A'));
+    thresholds = g.get('THRESHOLDS');
+    ls = ones(1, l/length(thresholds));
+end
+
 %% ¡tests!
 
 %%% ¡test!
