31d31325a3a62346723d8e24cae66597c86556be
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2021-04-07 15:21:42 +0200

    AnalysisGroup ST WU

---
+++ braph2genesis/workflows/structural/_AnalysisGroup_ST_WU.gen.m
@@ -0,0 +1,58 @@
+%% ¡header!
+AnalysisGroup_ST_WU < AnalysisGroup (a, graph analysis with structural data) is a graph analysis using structural data.
+
+%% ¡description!
+This graph analysis uses structural data and analyzes them using weighted undirected graphs,
+binary undirected multigraphs with fixed thresholds,
+or binary undirected multigraphs with fixed densities.
+
+%% ¡props!
+
+%%% ¡prop!
+CORRELATION_RULE (parameter, option) is the correlation type.
+%%%% ¡settings!
+Correlation.CORRELATION_RULE_LIST
+%%%% ¡default!
+Correlation.CORRELATION_RULE_LIST{1}
+
+%%% ¡prop!
+NEGATIVE_WEIGHT_RULE (parameter, option) determines how to deal with negative weights.
+%%%% ¡settings!
+Correlation.NEGATIVE_WEIGHT_RULE_LIST
+%%%% ¡default!
+Correlation.NEGATIVE_WEIGHT_RULE_LIST{1}
+
+%% ¡props_update!
+
+%%% ¡prop!
+GR (data, item) is the subject group, which also defines the subject class SubjectST.
+%%%% ¡default!
+Group('SUB_CLASS', 'SubjectST')
+
+%%% ¡prop!
+G (result, item) is the graph obtained from this analysis.
+%%%% ¡settings!
+'GraphWU'
+%%%% ¡default!
+GraphWU()
+%%%% ¡calculate!
+gr = a.get('GR');
+data_list = cellfun(@(x) x.get('ST'), gr.get('SUB_DICT').getItems, 'UniformOutput', false);
+data = cat(2, data_list{:})'; % correlation is a column based operation
+
+A = Correlation.getAdjacencyMatrix(data, a.get('CORRELATION_RULE'), a.get('NEGATIVE_WEIGHT_RULE'));
+
+g = GraphWU( ...
+    'ID', ['g ' gr.get('ID')], ...
+    'B', A ...
+    );
+
+value = g;
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+Example
+%%%% ¡code!
+% example_ST_WU
