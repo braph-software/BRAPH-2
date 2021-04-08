430f2cbcc4ea68e37e30320e69dec7246f8f4438
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2021-04-08 10:03:03 +0200

    Update _ComparisonGroup.gen.m

--- braph2genesis/src/analysis/_ComparisonGroup.gen.m
+++ braph2genesis/src/analysis/_ComparisonGroup.gen.m
@@ -46,48 +46,29 @@ PERM_SEEDS (result, rvector) is the list of seeds for the random permutations.
 value = randi(intmax('uint32'), 1, c.get('P'));
 
 %%% ¡prop!
-A1_PERM (result, idict) is the list of permuted analyses for the first analysis.
+A1_PERM_DICT (result, idict) is the list of permuted analyses for the first analysis.
 %%%% ¡settings!
 'AnalysisGroup'
 %%%% ¡calculate!
 value = IndexedDictionary('IT_CLASS', 'AnalysisGroup', 'IT_KEY', 1);
 
 %%% ¡prop!
-A2_PERM (result, idict) is the list of permuted analyses for the second analysis.
+A2_PERM_DICT (result, idict) is the list of permuted analyses for the second analysis.
 %%%% ¡settings!
 'AnalysisGroup'
 %%%% ¡calculate!
 value = IndexedDictionary('IT_CLASS', 'AnalysisGroup', 'IT_KEY', 1);
 
 %% ¡methods!
-function [a1_perm, a2_perm] = getPerm(c, i)
-
-    seeds = c.get('PERM_SEEDS');
-    rng(seeds(i), 'twister')
-    
-    subs1 = c.get('A1').get('GR').get('SUB_DICT').get('IT_LIST');
-    subs2 = c.get('A2').get('GR').get('SUB_DICT').get('IT_LIST');
-
-    [subs1_perm, subs2_perm] = permutation(subs1, subs2, c.get('LONGITUDINAL'));
-
-    a1_perm = c.get('A1').clone();
-    a1_perm.set('GR', c.get('A1').get('GR').clone())
-    a1_perm.get('GR').set('SUB_DICT', c.get('A1').get('GR').get('SUB_DICT').clone())
-    a1_perm.get('GR').get('SUB_DICT').set('IT_LIST', subs1_perm)
-
-    a2_perm = c.get('A2').clone();
-    a2_perm.set('GR', c.get('A2').get('GR').clone())
-    a2_perm.get('GR').set('SUB_DICT', c.get('A2').get('GR').get('SUB_DICT').clone())
-    a2_perm.get('GR').get('SUB_DICT').set('IT_LIST', subs2_perm)
-end
 function [p1, p2, ci_lower, ci_upper, m1, m2, diff, m1_perms, m2_perms, diff_perms] = getComparison(c, measure_code, varargin)
 
     verbose = get_from_varargin(false, 'Verbose', varargin{:});
     interruptible = get_from_varargin(0.001, 'Interruptible', varargin{:});
+    memorize = get_from_varargin(false, 'Memorize', varargin{:});
 
     % Measure for groups 1 and 2, and their difference
-    m1 = c.get('A1').get('G').getMeasure(measure_code).get('M');
-    m2 = c.get('A2').get('G').getMeasure(measure_code).get('M');
+    m1 = c.get('A1').get('G').getMeasure(measure_code).memorize('M');
+    m2 = c.get('A2').get('G').getMeasure(measure_code).memorize('M');
     diff = cellfun(@(x, y) y - x, m1, m2, 'UniformOutput', false);
 
     % Permutations
@@ -99,19 +80,18 @@ function [p1, p2, ci_lower, ci_upper, m1, m2, diff, m1_perms, m2_perms, diff_per
 
     start = tic;
     for i = 1:1:P
-        if verbose
-            disp(['** PERMUTATION TEST - sampling #' int2str(i) '/' int2str(P) ' - ' int2str(toc(start)) '.' int2str(mod(toc(start), 1) * 10) 's'])
-        end
+        [a1_perm, a2_perm] = get_perm(i);
 
-        [a1_perm, a2_perm] = c.getPerm(i);
-
-        m1_perms{1, i} = a1_perm.get('G').getMeasure(measure_code).get('M');
-        m2_perms{1, i} = a2_perm.get('G').getMeasure(measure_code).get('M');
+        m1_perms{1, i} = a1_perm.get('G').getMeasure(measure_code).memorize('M');
+        m2_perms{1, i} = a2_perm.get('G').getMeasure(measure_code).memorize('M');
         diff_perms{1, i} = cellfun(@(x, y) y - x, m1_perms{1, i}, m2_perms{1, i}, 'UniformOutput', false);
 
         if interruptible
             pause(interruptible)
         end
+        if verbose
+            disp(['** PERMUTATION TEST - sampling #' int2str(i) '/' int2str(P) ' - ' int2str(toc(start)) '.' int2str(mod(toc(start), 1) * 10) 's'])
+        end        
     end
 
     % Statistical analysis
@@ -129,4 +109,45 @@ function [p1, p2, ci_lower, ci_upper, m1, m2, diff, m1_perms, m2_perms, diff_per
         end
     end
     
+    function [a1_perm, a2_perm] = get_perm(i)
+
+        a1_perm_dict = c.memorize('A1_PERM_DICT');
+        a2_perm_dict = c.memorize('A2_PERM_DICT');
+        if a1_perm_dict.containsIndex(i) && a2_perm_dict.containsIndex(i)
+            % retrieves if already memorized
+            a1_perm = a1_perm_dict.getItem(i);
+            a2_perm = a2_perm_dict.getItem(i);
+        else    
+            % perutation
+            seeds = c.get('PERM_SEEDS');
+            rng(seeds(i), 'twister')
+
+            subs1 = c.get('A1').get('GR').get('SUB_DICT').get('IT_LIST');
+            subs2 = c.get('A2').get('GR').get('SUB_DICT').get('IT_LIST');
+
+            [subs1_perm, subs2_perm] = permutation(subs1, subs2, c.get('LONGITUDINAL'));
+
+            a1_perm = c.get('A1').clone();
+            a1_perm.set( ...
+                'ID', [c.get('A1').get('ID') ' permutation ' int2str(i)], ...
+                'GR', c.get('A1').get('GR').clone() ...
+                )
+            a1_perm.get('GR').set('SUB_DICT', c.get('A1').get('GR').get('SUB_DICT').clone())
+            a1_perm.get('GR').get('SUB_DICT').set('IT_LIST', subs1_perm)
+
+            a2_perm = c.get('A2').clone();
+            a2_perm.set( ...
+                'ID', [c.get('A2').get('ID') ' permutation ' int2str(i)], ...
+                'GR', c.get('A2').get('GR').clone() ...
+                )
+            a2_perm.get('GR').set('SUB_DICT', c.get('A2').get('GR').get('SUB_DICT').clone())
+            a2_perm.get('GR').get('SUB_DICT').set('IT_LIST', subs2_perm)
+
+            % memorize if required
+            if memorize
+                a1_perm_dict.add(a1_perm)
+                a2_perm_dict.add(a2_perm)
+            end
+        end
+    end
 end
