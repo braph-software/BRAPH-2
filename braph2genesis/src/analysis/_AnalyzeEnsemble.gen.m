7a5c21a9c72ab83ce0caf191665a388f1573d2ab
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2021-04-11 07:38:15 +0200

    Update _AnalyzeEnsemble.gen.m

--- braph2genesis/src/analysis/_AnalyzeEnsemble.gen.m
+++ braph2genesis/src/analysis/_AnalyzeEnsemble.gen.m
@@ -31,3 +31,47 @@ G_DICT (result, idict) is the graph enemble obtained from this analysis.
 'Graph'
 %%%% ¡calculate!
 value = IndexedDictionary('IT_CLASS', 'Graph');
+
+%%% ¡prop!
+ME_DICT (result, idict) contains the calculated measures of the graph ensemble.
+%%%% ¡settings!
+'MeasureEnsemble'
+%%%% ¡calculate!
+value = IndexedDictionary('IT_CLASS', 'MeasureEnsemble', 'IT_KEY', 1);
+
+%% ¡methods!
+function me = getMeasureEnsemble(a, measure_class, varargin)
+    %GETMEASURE returns measure.
+    %
+    % ME = GETMEASURE(A, MEASURE_CLASS) checks if the measure ensemble exists in the
+    %  property ME_DICT. If not it creates a new measure M of class MEASURE_CLASS
+    %  with properties defined by the graph settings. The user must call
+    %  getValue() for the new measure M to retrieve the value of measure M.
+
+    me_dict = a.memorize('ME_DICT');
+    if me_dict.containsKey(measure_class)
+        me = me_dict.getItem(measure_class);
+    else
+        m_list = cellfun(@(x) x.get('M'), a.get('G_DICT').getItems, 'UniformOutput', false);
+
+        if isempty(m_list)
+            m_av = {};
+        else
+            m_av = cell(size(m_list{1}));
+            for i = 1:1:size(m_list{1}, 1)
+                for j = 1:1:size(m_list{1}, 2)
+                    m_ij_list = cellfun(@(x) x{i, j}, m_list, 'UniformOutput', false);
+                    m_av{i, j} = mean(cat(ndims(m_ij_list{1}), m_ij_list{:}), ndims(m_ij_list{1}));
+                end
+            end
+        end
+       
+        me = MeasureEnsemble( ...
+            'ID', measure_class, ...
+            'A', g, ...
+            'M', m_av, ...
+            varargin{:} ...
+            );
+        me_dict.add(me);
+    end
+end
