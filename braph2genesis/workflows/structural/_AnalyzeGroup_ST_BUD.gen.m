9923aa2b14c03194148096f99f579bf9e18e26bb
annacanal <annacg@kth.se>
2021-04-13 10:22:49 +0200

    Add Analyze ST BUD, Re-add MultigraphBUD

---
+++ braph2genesis/workflows/structural/_AnalyzeGroup_ST_BUD.gen.m
@@ -0,0 +1,64 @@
+%% ¡header!
+AnalyzeGroup_ST_BUD < AnalyzeGroup (a, graph analysis with structural data of fixed density) is a graph analysis using structural data of fixed density.
+
+%% ¡description!
+This graph analysis uses structural data of fixed density and analyzes them using binary undirected graphs.
+
+%% ¡props!
+
+%%% ¡prop!
+CORRELATION_RULE (parameter, option) is the correlation type.
+%%%% ¡settings!
+Correlation.CORRELATION_RULE_LIST
+%%%% ¡default!
+Correlation.CORRELATION_RULE_LIST{1}
+
+%%% ¡prop!
+NEGATIVE_WEIGHT_RULE (parameter, option) determines how to deal with negative weights.
+%%%% ¡settings!
+Correlation.NEGATIVE_WEIGHT_RULE_LIST
+%%%% ¡default!
+Correlation.NEGATIVE_WEIGHT_RULE_LIST{1}
+
+%%% ¡prop!
+DENSITIES (parameter, rvector) is the vector of densities.
+%%%% ¡default!
+0
+
+%% ¡props_update!
+
+%%% ¡prop!
+GR (data, item) is the subject group, which also defines the subject class SubjectST.
+%%%% ¡default!
+Group('SUB_CLASS', 'SubjectST')
+
+%%% ¡prop!
+G (result, item) is the graph obtained from this analysis.
+%%%% ¡settings!
+'GraphBU'
+%%%% ¡default!
+GraphBU()
+%%%% ¡calculate!
+gr = a.get('GR');
+data_list = cellfun(@(x) x.get('ST'), gr.get('SUB_DICT').getItems, 'UniformOutput', false);
+data = cat(2, data_list{:})'; % correlation is a column based operation
+
+A = Correlation.getAdjacencyMatrix(data, a.get('CORRELATION_RULE'), a.get('NEGATIVE_WEIGHT_RULE'));
+
+densities = a.get('DENSITIES'); % this is a vector
+
+g = MultigraphBUD( ...
+    'ID', ['g ' gr.get('ID')], ...
+    'B', A, ...
+    'DENSITIES', densities ...
+    );
+
+value = g;
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+Example
+%%%% ¡code!
+example_ST_BUD
