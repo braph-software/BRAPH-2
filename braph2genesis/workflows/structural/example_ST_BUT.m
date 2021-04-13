2b21b3723733894202df3167eb0df771707035fd
egolol <egolol87@gmail.com>
2021-04-13 11:42:50 +0200

    Update example_ST_BUT.m

--- braph2genesis/workflows/structural/example_ST_BUT.m
+++ braph2genesis/workflows/structural/example_ST_BUT.m
@@ -24,39 +24,39 @@ im_gr2 = ImporterGroupSubjectSTXLS( ...
 gr2 = im_gr2.get('GR');
 
 %% Analysis BUT
-a_WU1 = AnalyzeGroup_ST_BUT('GR', gr1, 'Thresholds', [.5 .6]);
+a_BUT1 = AnalyzeGroup_ST_BUT('GR', gr1, 'Thresholds', [.5 .6]);
 
-a_WU2 = AnalyzeGroup_ST_BUT('GR', gr2, 'Thresholds', [.5 .6]);
+a_BUT2 = AnalyzeGroup_ST_BUT('GR', gr2, 'Thresholds', [.5 .6]);
 
 % measure calculation
-g_WU1 = a_WU1.get('G');
-degree_WU1 = g_WU1.getMeasure('Degree').get('M');
-degree_av_WU1 = g_WU1.getMeasure('DegreeAv').get('M');
-distance_WU1 = g_WU1.getMeasure('Distance').get('M');
+g_BUT1 = a_BUT1.get('G');
+degree_BUT1 = g_BUT1.getMeasure('Degree').get('M');
+degree_av_BUT1 = g_BUT1.getMeasure('DegreeAv').get('M');
+distance_BUT1 = g_BUT1.getMeasure('Distance').get('M');
 
-g_WU2 = a_WU2.get('G');
-degree_WU1 = g_WU2.getMeasure('Degree').get('M');
-degree_av_WU1 = g_WU2.getMeasure('DegreeAv').get('M');
-distance_WU1 = g_WU2.getMeasure('Distance').get('M');
+g_BUT2 = a_BUT2.get('G');
+degree_BUT1 = g_BUT2.getMeasure('Degree').get('M');
+degree_av_BUT1 = g_BUT2.getMeasure('DegreeAv').get('M');
+distance_BUT1 = g_BUT2.getMeasure('Distance').get('M');
 
 % comparison
-c_WU = CompareGroup( ...
+c_BUT = CompareGroup( ...
     'P', 10, ...
-    'A1', a_WU1, ...
-    'A2', a_WU2 ...
+    'A1', a_BUT1, ...
+    'A2', a_BUT2 ...
     );
 
-degree_WU_p1 = c_WU.getComparison('Degree').get('P1');
-degree_WU_p2 = c_WU.getComparison('Degree').get('P2');
-degree_WU_cil = c_WU.getComparison('Degree').get('CIL');
-degree_WU_ciu = c_WU.getComparison('Degree').get('CIU');
+degree_BUT_p1 = c_BUT.getComparison('Degree').get('P1');
+degree_BUT_p2 = c_BUT.getComparison('Degree').get('P2');
+degree_BUT_cil = c_BUT.getComparison('Degree').get('CIL');
+degree_BUT_ciu = c_BUT.getComparison('Degree').get('CIU');
 
-degree_av_WU_p1 = c_WU.getComparison('DegreeAv').get('P1');
-degree_av_WU_p2 = c_WU.getComparison('DegreeAv').get('P2');
-degree_av_WU_cil = c_WU.getComparison('DegreeAv').get('CIL');
-degree_av_WU_ciu = c_WU.getComparison('DegreeAv').get('CIU');
+degree_av_BUT_p1 = c_BUT.getComparison('DegreeAv').get('P1');
+degree_av_BUT_p2 = c_BUT.getComparison('DegreeAv').get('P2');
+degree_av_BUT_cil = c_BUT.getComparison('DegreeAv').get('CIL');
+degree_av_BUT_ciu = c_BUT.getComparison('DegreeAv').get('CIU');
 
-distance_WU_p1 = c_WU.getComparison('Distance').get('P1');
-distance_WU_p2 = c_WU.getComparison('Distance').get('P2');
-distance_WU_cil = c_WU.getComparison('Distance').get('CIL');
-distance_WU_ciu = c_WU.getComparison('Distance').get('CIU');
+distance_BUT_p1 = c_BUT.getComparison('Distance').get('P1');
+distance_BUT_p2 = c_BUT.getComparison('Distance').get('P2');
+distance_BUT_cil = c_BUT.getComparison('Distance').get('CIL');
+distance_BUT_ciu = c_BUT.getComparison('Distance').get('CIU');
