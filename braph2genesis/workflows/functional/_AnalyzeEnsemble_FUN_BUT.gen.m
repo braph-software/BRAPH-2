f77a95175f30d9e325f58cbca8966c3495f67132
egolol <egolol87@gmail.com>
2021-04-14 15:31:06 +0200

    but

---
+++ braph2genesis/workflows/functional/_AnalyzeEnsemble_FUN_BUT.gen.m
@@ -0,0 +1,97 @@
+%% ¡header!
+AnalyzeEnsemble_FUN_BUT < AnalyzeEnsemble (a, graph analysis with functional data of fixed threshold) is a graph analysis using functional data of fixed threshold.
+
+%% ¡description!
+This graph analysis uses functional data of fixed threshold and analyzes them using binary undirected graphs.
+
+%%% ¡seealso!
+AnalyzeGroup_FUN_WU, AnalyzeGroup_FUN_BUD, Subject_FUN, MultigraphBUT.
+
+%% ¡props!
+%%% ¡prop!
+REPETITION(parameter, scalar) is the number of repetitions
+%%%% ¡default!
+1
+
+%%% ¡prop!
+FREQUENCYRULEMIN(parameter, scalar) is the minimum frequency value
+%%%% ¡default!
+0
+
+%%% ¡prop!
+FREQUENCYRULEMAX(parameter, scalar) is the maximum frequency value
+%%%% ¡default!
+Inf
+
+%%% ¡prop!
+CORRELATION_RULE (parameter, option) is the correlation type.
+%%%% ¡settings!
+Correlation.CORRELATION_RULE_LIST
+%%%% ¡default!
+Correlation.CORRELATION_RULE_LIST{1}
+
+%%% ¡prop!
+NEGATIVE_WEIGHT_RULE (parameter, option) determines how to deal with negative weights.
+%%%% ¡settings!
+Correlation.NEGATIVE_WEIGHT_RULE_LIST
+%%%% ¡default!
+Correlation.NEGATIVE_WEIGHT_RULE_LIST{1}
+
+%%% ¡prop!
+THRESHOLDS (parameter, rvector) is the vector of thresholds.
+%%%% ¡default!
+0
+
+%% ¡props_update!
+
+%%% ¡prop!
+GR (data, item) is the subject group, which also defines the subject class SubjectFUN.
+%%%% ¡default!
+Group('SUB_CLASS', 'SubjectFUN')
+
+%%% ¡prop!
+G_DICT (result, idict) is the graph (MultigraphBUT) ensemble obtained from this analysis.
+%%%% ¡settings!
+'MultigraphBUT'
+%%%% ¡default!
+IndexedDictionary('IT_CLASS', 'MultigraphBUT')
+%%%% ¡calculate!
+g_dict = IndexedDictionary('IT_CLASS', 'MultigraphBUT');
+
+gr = a.get('GR');
+T = a.get('REPETITION');
+fmin = a.get('FREQUENCYRULEMIN');
+fmax = a.get('FREQUENCYRULEMAX');
+thresholds = a.get('THRESHOLDS'); % this is a vector
+for i = 1:1:gr.get('SUB_DICT').length()
+	sub = gr.get('SUB_DICT').getItem(i);
+    data = sub.getr('FUN');
+    fs = 1 / T;
+    
+    if fmax > fmin && T > 0
+        NFFT = 2 * ceil(size(data, 1) / 2);
+        ft = fft(data, NFFT);  % Fourier transform
+        f = fftshift(fs * abs(-NFFT / 2:NFFT / 2 - 1) / NFFT);  % absolute frequency
+        ft(f < fmin | f > fmax, :) = 0;
+        data = ifft(ft, NFFT);
+    end
+    
+    A = Correlation.getAdjacencyMatrix(data, a.get('CORRELATION_RULE'), a.get('NEGATIVE_WEIGHT_RULE'));      
+    
+    g = MultigraphBUT( ...
+        'ID', ['g ' sub.get('ID')], ...
+        'B', A, ...
+        'THRESHOLDS', thresholds ...
+        );
+    g_dict.add(g)
+end
+
+value = g_dict;
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+Example
+%%%% ¡code!
+example_FUN_BUT
