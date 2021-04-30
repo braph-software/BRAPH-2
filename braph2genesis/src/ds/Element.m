be26ab311accb8e8ed7ee57ed83ce03552f35c7a
egolol <egolol87@gmail.com>
2021-04-30 11:42:01 +0200

    Update Element.m

--- braph2genesis/src/ds/Element.m
+++ braph2genesis/src/ds/Element.m
@@ -1394,7 +1394,7 @@ classdef Element < Category & Format & matlab.mixin.Copyable
                 for prop = 1:1:el.getPropNumber()
                     value = struct{i}.props(prop).value;
                     if isnumeric(value)
-                        if el.getPropFormat(prop) & ~isa(el_list{value}, 'NoValue') %#ok<AND2> % also case Format.ITEMLIST
+                        if isequal(el.getPropFormat(prop), Format.ITEMLIST) & ~isa(el_list{value}, 'NoValue') %#ok<AND2> % also case Format.ITEMLIST
                             indices = value;
                             el.props{prop}.value = el_list(indices)';
                         else  % also case {Format.ITEM, Format.IDICT}
