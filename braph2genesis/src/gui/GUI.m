3bcccd8354f326e079c7067d319c59b0c48225a3
egolol <egolol87@gmail.com>
2021-05-08 22:45:03 +0200

    icon

--- braph2genesis/src/gui/GUI.m
+++ braph2genesis/src/gui/GUI.m
@@ -43,7 +43,7 @@ f = init();
         
         warning('off')
         javaFrame    = get(f, 'JavaFrame'); %#ok<JAVFM>
-        iconFilePath = [fileparts(which('braph2')) filesep 'src' filesep 'util' filesep 'icon_brain.png'];
+        iconFilePath = [fileparts(which('braph2')) filesep 'src' filesep 'util' filesep 'braphicon_notext.png'];
         javaFrame.setFigureIcon(javax.swing.ImageIcon(iconFilePath));
         warning('on')
         
