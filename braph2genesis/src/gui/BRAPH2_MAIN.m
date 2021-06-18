152b78c4946df130209d2378a5d90ca805524eaf
egolol <egolol87@gmail.com>
2021-06-18 13:16:54 +0200

    updates

--- braph2genesis/src/gui/BRAPH2_MAIN.m
+++ braph2genesis/src/gui/BRAPH2_MAIN.m
@@ -1,8 +1,8 @@
 function BRAPH2_MAIN(varargin)
 % constants
-f_position = get_from_varargin([.02 .1 .6 .80], 'Position', varargin);
-name = BRAPH2.STR;
-BKGCOLOR = get_from_varargin([.98 .95 .95], 'BackgroundColor', varargin);
+f_position = get_from_varargin([.02 .1 .5 .65], 'Position', varargin);
+name = 'BRAPH 2.0';
+BKGCOLOR = get_from_varargin([1 .9725 .929], 'BackgroundColor', varargin);
 close_request = 1; % true
 
 % init
@@ -17,7 +17,7 @@ f = init();
             'Units', 'character', ...
             'MenuBar', 'none', ...
             'DockControls', 'off', ...
-            'Color', BKGCOLOR ...
+            'SizeChangedFcn', {@update_position} ...
             );
         set_icon(f)
         
@@ -39,23 +39,38 @@ f = init();
 % main
 % search
 warning('off', 'all')
-hContainer = handle(uipanel('BorderType','none', 'Parent', f)); 
 jPanelObj = com.mathworks.widgets.SearchTextField('Enter search filter:');
 jAssetComponent = jPanelObj.getComponent;
-[jhPanel,hContainer] = javacomponent(jAssetComponent, [10,500,200,25], f);
+[jhPanel, hContainer] = javacomponent(jAssetComponent, [20, 375, 350, 25], f);
 hjSearchField = handle(jAssetComponent.getComponent(0), 'CallbackProperties');
 set(hjSearchField, 'KeyPressedCallback', {@updateSearch, jPanelObj});
+warning('on', 'all')
 
     function updateSearch(~, ~, ~)  
         update_listbox()
     end
+    function update_position(~, ~)
+        pos = get_figure_position();
+        x = pos(1);
+        y = pos(2);
+        w = pos(3);
+        h = pos(4);
+        
+        
+        jhPanel.setLocation(1, 1);
+        jhPanel.setSize(1,1);
+    end
+    function pos = get_figure_position()
+        pos = getpixelposition(f); %// gives x left, y bottom, width, height
+    end
+
 
 % list
 workflow_list = uicontrol( ...
     'Parent', f, ...
     'Style', 'listbox', ...
     'Units', 'normalized', ...
-    'Position', [.02 .1 .3 .5], ...
+    'Position', [.02 .1 .46 .5], ...
     'String', '', ...
     'Callback', {@cb_wf_list_box});
 
@@ -75,6 +90,7 @@ workflow_list = uicontrol( ...
         paths = files_paths(workflow_indices);
         
         workflow_names = cellfun(@(x, y) erase(x, [y filesep()]), workflow_names, paths, 'UniformOutput', false);
+        workflow_names = cellfun(@(x) erase(x, '.m'), workflow_names, 'UniformOutput', false);
         
         if ~isempty(jPanelObj.getSearchText.toCharArray')
             filter = jPanelObj.getSearchText.toCharArray';
@@ -158,38 +174,74 @@ workflow_list = uicontrol( ...
 panel_logo = uipanel( ...
     'Parent', f, ...
     'Units', 'normalized', ...
-    'Position', [.52 .1 .48 .8], ...
-    'BackgroundColor', [0 0 0],...
+    'Position', [.02 .75 .6 .25], ...
+    'BackgroundColor', BKGCOLOR,...
     'BorderType', 'none');
-logo = imread([fileparts(which('braph2')) filesep 'src' filesep 'util' filesep 'braph2icon.png']);
+logo = imread([fileparts(which('braph2')) filesep 'src' filesep 'util' filesep 'head_main.png']);
 pax = axes(panel_logo);
 image(pax, logo);
 axis off
 
+% rotate
+panel_rotate = uipanel( ...
+    'Parent', f, ...
+    'Units', 'normalized', ...
+    'Position', [.52 .1 .46 .5], ...
+    'BackgroundColor', BKGCOLOR,...
+    'BorderType', 'none');
+
+ui_checkbox_bottom_animation = uicontrol(f, 'Style', 'checkbox', 'Visible', 'off', 'Value', true);
+pl = PlotBrainSurface('SURF', ImporterBrainSurfaceNV('FILE', 'human_ICBM152.nv').get('SURF'));
+pl.set(...
+    'AXESCOLOR', BKGCOLOR, ...
+    'LIGHTING', 'phong', ...
+    'MATERIAL', 'shiny', ...
+    'CAMLIGHT', 'left', ...
+    'SHADING', 'interp', ...
+    'COLORMAP', autumn)
+h_panel = pl.draw('Parent', panel_rotate, 'Units', 'normalized', 'Position', [.0 .0 1 1], 'BackgroundColor', BKGCOLOR);
+a = get(h_panel, 'Children');
+pl_axes = a;
+axis off
+pl.set('axis', false);
+    function rotate()        
+        try        
+            while get(ui_checkbox_bottom_animation, 'Value')
+                camorbit(pl_axes, 2, 2, 'camera')                
+                drawnow
+                pause(0.1)
+            end
+        catch
+            % nothing
+        end
+    end
+
 % menu
 menu()
     function menu()
+        ui_menu_about = uimenu(f, 'Label', 'About');
+        uimenu(ui_menu_about, ...
+            'Label', 'License ...', ...
+            'Callback', {@cb_license})
+        uimenu(ui_menu_about, ...
+            'Label', 'About ...', ...
+            'Callback', {@cb_about})
+        function cb_license(~, ~)
+            BRAPH2_LICENSE()
+        end
+        function cb_about(~, ~)
+            BRAPH2_ABOUT();
+        end
     end
 % toolbar
 toolbar()
     function toolbar()
-        set(f, 'Toolbar', 'figure')
-
-        ui_toolbar = findall(f, 'Tag', 'FigureToolBar');
-
-        delete(findall(ui_toolbar, 'Tag', 'Standard.NewFigure'))        
-        delete(findall(ui_toolbar, 'Tag', 'Standard.PrintFigure'))
-        delete(findall(ui_toolbar, 'Tag', 'Standard.EditPlot'))
-        delete(findall(ui_toolbar, 'Tag', 'Standard.OpenInspector'))
-        delete(findall(ui_toolbar, 'Tag', 'Exploration.Brushing'))
-        delete(findall(ui_toolbar, 'Tag', 'DataManager.Linking'))
-        delete(findall(ui_toolbar, 'Tag', 'Annotation.InsertColorbar'))
-        delete(findall(ui_toolbar, 'Tag', 'Annotation.InsertLegend'))
-        delete(findall(ui_toolbar, 'Tag', 'Plottools.PlottoolsOff'))
-        delete(findall(ui_toolbar, 'Tag', 'Plottools.PlottoolsOn'))
+        set(f, 'Toolbar', 'none')
     end
 % auxiliary
 update_listbox()
 set(f, 'Visible', 'on')
-warning('on', 'all')
+set(f, 'Color', BKGCOLOR);
+rotate()
+
 end
