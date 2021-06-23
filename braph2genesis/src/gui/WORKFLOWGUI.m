6795b5b06e99eb2c11831c6f08ed7d3b43affada
egolol <egolol87@gmail.com>
2021-06-23 17:20:29 +0200

    functionality

--- braph2genesis/src/gui/WORKFLOWGUI.m
+++ braph2genesis/src/gui/WORKFLOWGUI.m
@@ -1,17 +1,47 @@
 function WORKFLOWGUI(file, varargin)
-% constants
+%% constants
 screen_size = get(0, 'ScreenSize');
 h_f = screen_size(4) * 0.65;
 w_f = h_f * 1.61;
-x_f = screen_size(3) * 0.3;
-y_f = screen_size(4) * .3; 
+x_f = screen_size(3) * 0.2;
+y_f = screen_size(4) * .2; 
 f_position = get_from_varargin([x_f y_f w_f h_f], 'Position', varargin);
 name = 'Workflow';
 BKGCOLOR = get_from_varargin([1 .9725 .929], 'BackgroundColor', varargin);
-close_request = 1; % true
+close_request = 0; % false has to be true after debugs
+tokens = [];
 % get info of file
+txt = fileread(file);
+cycles = getFileCycles(txt);
+for i = 1:cycles
+    tokens{i} = getGUIToken(txt, i);
+end
+    function cycles = getFileCycles(txt)
+        splits = regexp(txt, ['(^|' newline() ')%%\s*'], 'split');
+        splits = splits(~cellfun('isempty', splits));
+        cycles = length(splits);
+    end
+    function token = getGUIToken(txt, cycle)
+        splits = regexp(txt, ['(^|' newline() ')%%\s*'], 'split');
+        splits = splits(~cellfun('isempty', splits));
+        for j = 1:1:length(splits)
+            split_tmp = splits{j};
+            if j == cycle
+                in_splits = regexp(split_tmp, ['(^|' newline() ')%\s*'], 'split');
+                in_splits = in_splits(~cellfun('isempty', in_splits));
+                token = in_splits{1};                
+                break
+            end
+        end
+    end
+    function [vars, vars_per_token] = getExecutable(token)
+        vars = regexp(token, ['(^|' newline() ')'], 'split');
+        vars = cellfun(@(x) strtrim(x), vars, 'UniformOutput', false);
+        vars = vars(~cellfun('isempty', vars));
+        vars_per_token = length(vars);
+    end
 
-% figure
+%% figure
 f = init();
     function f = init()
         f = figure(...
@@ -23,8 +53,7 @@ f = init();
             'Units', 'character', ...
             'MenuBar', 'none', ...
             'DockControls', 'off', ...
-            'Color', BKGCOLOR, ...
-            'SizeChangedFcn', {@update_position} ...
+            'Color', BKGCOLOR ...            
             );
         set_icon(f)
         
@@ -44,8 +73,133 @@ f = init();
         end
     end
 
-% atlas panel
-% group panel
-% analysis / comparison panel
+%% atlas panel
+atlas_vars = getExecutable(tokens{2});
+% declare atlas variables
+ba = [];
 
+atlas_panel = uipanel(f);
+if length(atlas_vars) >= 2
+    ui_atlas_panel_name = uicontrol(atlas_panel);
+    ui_atlas_btn = uicontrol(atlas_panel);
+end
+
+init_atlas_panel()
+    function init_atlas_panel()
+        set(atlas_panel, ...
+            'Units', 'normalized', ...
+            'Position', [.02 .02 .31 .96], ...
+            'BackgroundColor', BKGCOLOR)
+        if length(atlas_vars) >= 2
+            
+            set(ui_atlas_panel_name, ...
+                'Style', 'text', ...
+                'Units', 'normalized', ...
+                'String', atlas_vars{1}, ...
+                'BackgroundColor', BKGCOLOR, ...
+                'Position', [.3 .9 .3 .08] ...
+                )
+            
+             set(ui_atlas_btn, ...
+                'Style', 'pushbutton', ...
+                'String', 'BrainAtlas', ...
+                'Units', 'normalized', ...
+                'Position', [.3 .7 .3 .08], ...
+                'Callback', {@cb_atlas_btn} ...
+                )
+        end
+        
+    end
+    function cb_atlas_btn(~, ~)
+        executable = split(atlas_vars{2}, '=');
+        eval(['ba = ' executable{2}])        
+        if ~isempty(ba) && isa(ba, 'BrainAtlas')
+           GUI(ba)
+           enable_panel(group_panel)
+        end        
+    end
+    
+%% group panel
+group_vars = getExecutable(tokens{3});
+% declare atlas variables
+grs = cell(length(group_vars) - 1 , 1);
+group_panel = uipanel(f);
+if length(group_vars) >= 2
+    ui_group_panel_name = uicontrol(group_panel);
+    ui_group_btns = zeros(length(group_vars) - 1 , 1);
+    for k = 2:length(group_vars)
+        ui_group_btns(k, 1) = uicontrol(group_panel);
+    end    
+end
+init_group_panel()
+    function init_group_panel()
+        set(group_panel, ...
+            'Units', 'normalized', ...
+            'Position', [.33 .02 .33 .96], ...
+            'BackgroundColor', BKGCOLOR)
+        if length(group_vars) >= 2
+            
+            set(ui_group_panel_name, ...
+                'Style', 'text', ...
+                'Units', 'normalized', ...
+                'String', group_vars{1}, ...
+                'BackgroundColor', BKGCOLOR, ...
+                'Position', [.3 .9 .3 .08] ...
+                )
+            
+            inner_panel_height = .7 / length(group_vars);
+            for k = 2:length(group_vars)
+                y_correction = 0.05;
+                inner_panel_y = 1 - k * inner_panel_height + y_correction;
+                set(ui_group_btns(k), ...
+                'Style', 'pushbutton', ...
+                'String', ['Group ' num2str(k - 1)], ...
+                'Units', 'normalized', ...
+                'Position', [.3 inner_panel_y .3 .08], ...
+                'Callback', {@cb_group_btn} ...
+                )
+            end
+        end
+    end
+    function cb_group_btn(src, ~)
+        source_group = str2double(erase(src.String, 'Group '));
+        executable = split(group_vars{source_group + 1}, '=');
+        eval(['grs{source_group} = ' executable{2}])
+        GUI(grs{source_group})
+        if ~isempty(grs{source_group}) && isa(grs{source_group}, 'Group')
+           GUI(grs{source_group})
+        end  
+        if ~any(cellfun(@(x) isempty(x), grs))
+            enable_panel(analysis_panel, ba)
+        end
+    end
+
+%% analysis / comparison panel
+analysis_panel = uipanel(f);
+init_analysis_panel()
+    function init_analysis_panel()
+        set(analysis_panel, ...
+            'Units', 'normalized', ...
+            'Position', [.66 .02 .31 .96], ...
+            'BackgroundColor', BKGCOLOR)
+    end
+
+%% auxiliary
+set(f, 'Visible', 'on')
+disable_panel(group_panel)
+disable_panel(analysis_panel)
+    function enable_panel(panel)
+        set(panel, 'BackgroundColor', BKGCOLOR)
+        childs = get(panel, 'Children');
+        for i = 1:length(childs)
+            set(childs(i), 'Enable', 'on');
+        end
+    end
+    function disable_panel(panel)
+        set(panel, 'BackgroundColor', [220,220,220]/255)
+        childs = get(panel, 'Children');
+        for i = 1:length(childs)
+            set(childs(i), 'Enable', 'off');
+        end
+    end
 end
