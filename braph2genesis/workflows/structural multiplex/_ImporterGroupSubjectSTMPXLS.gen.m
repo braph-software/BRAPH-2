98c0c006b9f06c0882d1518fad0700dde691073d
egolol <egolol87@gmail.com>
2021-07-23 12:55:04 +0200

    waitbar nums correction

--- braph2genesis/workflows/structural multiplex/_ImporterGroupSubjectSTMPXLS.gen.m
+++ braph2genesis/workflows/structural multiplex/_ImporterGroupSubjectSTMPXLS.gen.m
@@ -116,7 +116,7 @@ if isfolder(directory)
         
         % cycle over subjects, add subjects
         for i = 1:1:size(all_subjects_data, 2)
-            waitbar(.5, f, ['Processing your data: ' num2str(i) '/' num2str(size(all_subjects_data, 2)) ' ...'])
+            waitbar(.5, f, ['Processing your data: ' num2str(i) '/' num2str(size(all_subjects_data, 2)-1) ' ...'])
             layer_subject = reshape(all_subjects_data(:, i, :), [layers_number br_number]);
             for l = 1:1:layers_number
                 ST_MP(l) = {cell2mat(layer_subject(l, :)')};
