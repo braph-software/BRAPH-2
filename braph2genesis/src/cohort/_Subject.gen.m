9b9e2fb7858114d3d10062cd3bf9887744ee684a
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2021-08-04 17:02:04 +0200

    Update _Subject.gen.m

--- braph2genesis/src/cohort/_Subject.gen.m
+++ braph2genesis/src/cohort/_Subject.gen.m
@@ -21,24 +21,20 @@ LABEL (metadata, string) is an extended label of the subject.
 NOTES (metadata, string) are some specific notes about the subject.
 
 %% ¡methods!
-function str = tostring(el, varargin)
-    %TOSTRING string with information about the element.
+function str = tostring(sub, varargin)
+    %TOSTRING string with information about the subject.
     %
-    % STR = TOSTRING(EL) returns a string with information about the element.
+    % STR = TOSTRING(SUB) returns a string with information about the subject.
     %
-    % STR = TOSTRING(EL, N) trims the string to the first N characters.
+    % STR = TOSTRING(SUB, N) trims the string to the first N characters.
     %
-    % STR = TOSTRING(EL, N, ENDING) ends the string with ENDING if it has
+    % STR = TOSTRING(SUB, N, ENDING) ends the string with ENDING if it has
     %  been trimmed.
     %
     % See also disp, tree.
 
-    if el.getPropNumber() > 0
-        % str = char(join([class(el) 'with properties' cellfun(@(prod) el.getPropTag(prod), num2cell(Element.getProps(el)'), 'UniformOutput', false)]));
-        str = [class(el) ' ' el.getPropTag(1) ' = ' tostring(el.get(1)) '.'];
-    else
-        str = [class(el) ' without properties.'];
-    end
+    str = [class(el) ' ' el.getPropTag(1) ' = ' tostring(el.get(1)) '.'];
+    
     str = tostring(str, varargin{:});
     str = str(2:1:end-1);
 end
