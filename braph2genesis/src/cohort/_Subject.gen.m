72da951e6857095cd831de60eedb70c8ee2f7776
egolol <egolol87@gmail.com>
2021-08-04 15:13:06 +0200

    Update _Subject.gen.m

--- braph2genesis/src/cohort/_Subject.gen.m
+++ braph2genesis/src/cohort/_Subject.gen.m
@@ -19,3 +19,26 @@ LABEL (metadata, string) is an extended label of the subject.
 
 %%% ¡prop!
 NOTES (metadata, string) are some specific notes about the subject.
+
+%% ¡methods!
+function str = tostring(el, varargin)
+    %TOSTRING string with information about the element.
+    %
+    % STR = TOSTRING(EL) returns a string with information about the element.
+    %
+    % STR = TOSTRING(EL, N) trims the string to the first N characters.
+    %
+    % STR = TOSTRING(EL, N, ENDING) ends the string with ENDING if it has
+    %  been trimmed.
+    %
+    % See also disp, tree.
+
+    if el.getPropNumber() > 0
+        % str = char(join([class(el) 'with properties' cellfun(@(prod) el.getPropTag(prod), num2cell(Element.getProps(el)'), 'UniformOutput', false)]));
+        str = [class(el)  ' = ' tostring(el.get(1)) '.'];
+    else
+        str = [class(el) ' without properties.'];
+    end
+    str = tostring(str, varargin{:});
+    str = str(2:1:end-1);
+end
