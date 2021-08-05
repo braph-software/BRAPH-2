31dcdccaf8afdc7a87b250b5bfb9b804eb27f4c1
egolol <egolol87@gmail.com>
2021-08-05 11:02:11 +0200

    Update _Subject.gen.m

--- braph2genesis/src/cohort/_Subject.gen.m
+++ braph2genesis/src/cohort/_Subject.gen.m
@@ -33,7 +33,7 @@ function str = tostring(sub, varargin)
     %
     % See also disp, tree.
 
-    str = [class(el) ' ' el.getPropTag(1) ' = ' tostring(el.get(1)) '.'];
+    str = [class(sub) ' ' sub.getPropTag(1) ' = ' tostring(sub.get(1)) '.'];
     
     str = tostring(str, varargin{:});
     str = str(2:1:end-1);
