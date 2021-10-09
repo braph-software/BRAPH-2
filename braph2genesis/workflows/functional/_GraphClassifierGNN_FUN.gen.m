e0eaf872a9a29f95ed26da2b5c0654ee88a2e1cd
Yu-Wei Chang <hejyuwei@gmail.com>
2021-10-09 17:51:28 +0200

    finish the GNN for functional data

--- braph2genesis/workflows/functional/_GraphClassifierGNN_FUN.gen.m
+++ braph2genesis/workflows/functional/_GraphClassifierGNN_FUN.gen.m
@@ -2,7 +2,9 @@
 GraphClassifierGNN_FUN < ClassifierGNN (gnn, graph classifier with functional data) is a graph neural network classifier using functional data for classification.
 
 %% ¡description!
-This classifier uses functional data and trains graph neural network for classification
+This classifier classifies graphs using functional data with graph neural network. The graph classification task is one where an algorithm, in this example, a GNN has to predict the labels of unlabelled graphs.
+In this example, a graph is represented by a connectom of individual. Nodal graph measures represent nodes in the graph and the correlation betweeens brain regions obtained from functional time series represent links in the graph.
+Graph labels are the diagnosis of individual, for example, MCI. As such, input to the GNN are connectom along with graph nodal measures and the outputs are predictions of the type of diagnosis.
 
 %% ¡props!
 %%% ¡prop!
@@ -10,38 +12,62 @@ GR1 (data, item) is the subject group 1, which also defines the subject class Su
 %%%% ¡settings!
 'Group'
 %%%% ¡default!
-Group('SUB_CLASS', 'SubjectST')
+Group('SUB_CLASS', 'SubjectFUN')
 
 %%% ¡prop!
 GR2 (data, item) is the subject group 2, which also defines the subject class SubjectST.
 %%%% ¡settings!
 'Group'
 %%%% ¡default!
-Group('SUB_CLASS', 'SubjectST')
+Group('SUB_CLASS', 'SubjectFUN')
 
 %%% ¡prop!
-X_TBLTRAIN (result, matrix) is the training dataset separated from the whole dataset.
+FEATURES1 (data, CELL) is the cell consists of features matrix for each individual in group 1.
+
+%%% ¡prop!
+FEATURES2 (data, CELL) is the cell consists of features matrix for each individual in group 2.
+
+%%% ¡prop!
+LINKS1 (data, CELL) is the links for the nodes in group 1.
+
+%%% ¡prop!
+LINKS2 (data, CELL) is the links for the nodes in group 2.
+
+%%% ¡prop!
+FEATURES_TRAIN (result, matrix) is the training features separated from the whole dataset.
 %%%% ¡calculate!
 analysis = gnn.get('NEURAL_NETWORK_ANALYSIS');
 value = analysis{2};
 
 %%% ¡prop!
-Y_TBLTRAIN (result, matrix) is the labels for the training dataset.
+LINKS_TRAIN (result, matrix) is the training links separated from the whole dataset.
 %%%% ¡calculate!
 analysis = gnn.get('NEURAL_NETWORK_ANALYSIS');
 value = analysis{3};
 
 %%% ¡prop!
-X_TBLTEST (result, matrix) is the test dataset separated from the whole dataset.
+Y_TBLTRAIN (result, matrix) is the labels for the training dataset.
 %%%% ¡calculate!
 analysis = gnn.get('NEURAL_NETWORK_ANALYSIS');
 value = analysis{4};
 
 %%% ¡prop!
-Y_TBLTEST (result, matrix) is the labels for the test dataset.
+FEATURES_TEST (result, matrix) is the test features separated from the whole dataset.
 %%%% ¡calculate!
 analysis = gnn.get('NEURAL_NETWORK_ANALYSIS');
 value = analysis{5};
+
+%%% ¡prop!
+LINKS_TEST (result, matrix)is the test links separated from the whole dataset.
+%%%% ¡calculate!
+analysis = gnn.get('NEURAL_NETWORK_ANALYSIS');
+value = analysis{6};
+
+%%% ¡prop!
+Y_TBLTEST (result, matrix) is the labels for the test dataset.
+%%%% ¡calculate!
+analysis = gnn.get('NEURAL_NETWORK_ANALYSIS');
+value = analysis{7};
 
 %%% ¡prop!
 TRAINED_NET (result, rvector) is the trained neural network.
@@ -51,16 +77,16 @@ value = analysis{1};
 
 %% ¡props_update!
 %%% ¡prop!
-TRAINING_ACCURACY (result, scalar) is the root mean square error obtained from training dataset.
+TRAINING_ACCURACY (result, scalar) is the training accuracy obtained from training dataset.
 %%%% ¡calculate!
 analysis = gnn.get('NEURAL_NETWORK_ANALYSIS');
-value = analysis{6};
+value = analysis{8};
 
 %%% ¡prop!
-TEST_ACCURACY (result, scalar) is the root mean square error obtained from test dataset.
+TEST_ACCURACY (result, scalar) is the test accuracy obtained from test dataset.
 %%%% ¡calculate!
 analysis = gnn.get('NEURAL_NETWORK_ANALYSIS');
-value = analysis{7};
+value = analysis{9};
 
 %%% ¡prop!
 NEURAL_NETWORK_ANALYSIS (result, cell) is the machine learning analysis.
@@ -68,10 +94,10 @@ NEURAL_NETWORK_ANALYSIS (result, cell) is the machine learning analysis.
 value = calculate_results(gnn);
 
 %% ¡methods!
-function dlY = model(dlX, A, parameters)
+function dlY = model(gnn, dlX, A, parameters)
 
     % Normalize adjacency matrix
-    L = normalizeAdjacency(A);
+    L = gnn.normalize_adjacency(A);
 
     n = 64;
     Z1 = dlX;
@@ -88,40 +114,76 @@ function dlY = model(dlX, A, parameters)
 
     Z4 = L * Z3 * parameters.W3;
     Z4 = dlarray(Z4,'ST');
-    dlY = avgpool(Z4, 90, 'PoolFormat','S','Stride',90);
+    dlY = avgpool(Z4, 200, 'PoolFormat','S','Stride',200);
     dlY = reshape(dlY, [size(dlY,1), size(dlY,2)]);
     dlY = softmax(dlY, 'DataFormat', 'BC');
+end
+
+function adj = getBatchedAdjacency(gnn, links)
+    adj = [];
+    for i = 1:length(links)
+        link = links{i};
+        istart = 1+(i-1)*size(link,1);
+        iend = (i)*size(link,1);
+        adj(istart:iend,istart:iend) = link;
+    end
+end
+
+function feature_vector = getBatchedFeatureVector(gnn, features)
+    feature_vector = [];
+    for i = 1:length(features)
+        feature = features{i};
+        feature_vector = [feature_vector; feature];
+    end
 end
 
 function value = calculate_results(gnn)
     % import the data
     gr1 = gnn.get('GR1');
-    data_list = cellfun(@(x) x.get('ST'), gr1.get('SUB_DICT').getItems, 'UniformOutput', false);
-    data = cat(2, data_list{:})'; 
-    age_list = cellfun(@(x) x.get('age'), gr1.get('SUB_DICT').getItems, 'UniformOutput', false);
-    label = cat(2, age_list{:})';
-
+    labels = repmat(string(gr1.get('ID')), gr1.get('SUB_DICT').length(), 1);
+    
     gr2 = gnn.get('GR2');
-    data_list = cellfun(@(x) x.get('ST'), gr1.get('SUB_DICT').getItems, 'UniformOutput', false);
-    data = [data; cat(2, data_list{:})'];
-    age_list = cellfun(@(x) x.get('age'), gr2.get('SUB_DICT').getItems, 'UniformOutput', false);
-    label = [label; cat(2, age_list{:})'];
+    labels = [labels; repmat(string(gr2.get('ID')), gr2.get('SUB_DICT').length(), 1)];
+    
+    labels = categorical(labels');
+    
+    features1 = gnn.get('FEATURES1');
+    features2 = gnn.get('FEATURES2');
+    features = [features1 features2];
 
-    dataset = array2table(data);
+    links1 = gnn.get('LINKS1');
+    links2 = gnn.get('LINKS2');
+    links = [links1 links2];
+    
+    % split the data into training set and test set (90:10)
+    numObservations = size(labels, 2);
+    numObservationsTrain = floor(0.8*numObservations);
+    numObservationsTest = numObservations - numObservationsTrain;
+
+    idx = randperm(numObservations);
+    idxTrain = idx(1:numObservationsTrain);
+    idxTest = idx(numObservationsTrain+1:end);
+
+    featuresTrain = features(idxTrain);
+    featuresTest = features(idxTest);
+    labelTrain = labels(idxTrain);
+    labelTest = labels(idxTest);
+    linksTrain = links(idxTrain);
+    linksTest = links(idxTest);
     
     features = cell(1,2);
     labels = cell(1,2);
     adjacency = cell(1,2);
-    %features{1} = getBatchedFeatureVector(subjTrain);
-    %features{2} = getBatchedFeatureVector(subjTest);
-    %labels{1} = labelTrain';
-    %labels{2} = labelTest';
-    %adjacency{1} = getBatchedAdjacency(subjTrain);
-    %adjacency{2} = getBatchedAdjacency(subjTest);
+    features{1} = gnn.getBatchedFeatureVector(featuresTrain);
+    features{2} = gnn.getBatchedFeatureVector(featuresTest);
+    labels{1} = labelTrain';
+    labels{2} = labelTest';
+    adjacency{1} = gnn.getBatchedAdjacency(linksTrain);
+    adjacency{2} = gnn.getBatchedAdjacency(linksTest);
+    
     features = gnn.normalize_features(features);
     
-    
-    if(~isempty(features{1}))
+    if(~isempty(features))
         featureTrain = features{1};
         adjacencyTrain = adjacency{1};
         targetTrain = labels{1};
@@ -133,8 +195,8 @@ function value = calculate_results(gnn)
         labelsAll = cat(1,labels{:});
         classes = categories(labelsAll)
 
-        figure
-        histogram(labelsAll)
+        %figure
+        %histogram(labelsAll)
         xlabel('Category')
         ylabel('Frequency')
         title('Label Counts')
@@ -145,17 +207,17 @@ function value = calculate_results(gnn)
         sz = [numInputFeatures numHiddenFeatureMaps];
         numOut = numHiddenFeatureMaps;
         numIn = numInputFeatures;
-        parameters.W1 = initializeGlorot(sz,numOut,numIn,'double');
+        parameters.W1 = gnn.initialize_glorot(sz,numOut,numIn,'double');
 
         sz = [numHiddenFeatureMaps numHiddenFeatureMaps];
         numOut = numHiddenFeatureMaps;
         numIn = numHiddenFeatureMaps;
-        parameters.W2 = initializeGlorot(sz,numOut,numIn,'double');
+        parameters.W2 = gnn.initialize_glorot(sz,numOut,numIn,'double');
 
         sz = [numHiddenFeatureMaps numOutputFeatures];
         numOut = numOutputFeatures;
         numIn = numHiddenFeatureMaps;
-        parameters.W3 = initializeGlorot(sz,numOut,numIn,'double');
+        parameters.W3 = gnn.initialize_glorot(sz,numOut,numIn,'double');
         parameters
         numEpochs = 250;
         learnRate = 0.01;
@@ -204,7 +266,7 @@ function value = calculate_results(gnn)
 
             % Evaluate the model gradients and loss using dlfeval and the
             % modelGradients function.
-            [gradients, loss, dlYPred] = dlfeval(@modelGradients, dlX, adjacencyTrain, T, parameters);
+            [gradients, loss, dlYPred] = dlfeval(@gnn.model_gradients, dlX, adjacencyTrain, T, parameters);
 
             % Update the network parameters using the Adam optimizer.
             [parameters,trailingAvg,trailingAvgSq] = adamupdate(parameters,gradients, ...
@@ -220,7 +282,7 @@ function value = calculate_results(gnn)
                 addpoints(lineLossTrain,epoch,double(gather(extractdata(loss))))
 
                 % Accuracy score.
-                score = accuracy(dlYPred, targetTrain, classes);
+                score = gnn.accuracy(dlYPred, targetTrain, classes);
                 addpoints(lineAccuracyTrain,epoch,double(gather(score)))
 
                 drawnow
@@ -228,26 +290,35 @@ function value = calculate_results(gnn)
                 % Display validation metrics.
                 if epoch == 1 || mod(epoch,validationFrequency) == 0
                     % Loss.
-                    dlYPredValidation = model(dlXValidation, adjacencyValidation, parameters);
+                    dlYPredValidation = gnn.model(dlXValidation, adjacencyValidation, parameters);
                     lossValidation = crossentropy(dlYPredValidation, TValidation, 'DataFormat', 'BC');
                     addpoints(lineLossValidation,epoch,double(gather(extractdata(lossValidation))))
 
                     % Accuracy score.
-                    scoreValidation = accuracy(dlYPredValidation, targetValidation, classes);
+                    scoreValidation = gnn.accuracy(dlYPredValidation, targetValidation, classes);
                     addpoints(lineAccuracyValidation,epoch,double(gather(scoreValidation)))
 
                     drawnow
                 end
             end
         end
-
+        
+        % get training accuracy
+        featureTrain = features{1};
+        adjacencyTrain = adjacency{1};
+        targetTrain = labels{1};
+        dlXTest = dlarray(featureTrain);
+        dlYPredTrain = gnn.model(dlXTest, adjacencyTrain, parameters);
+        [scoreTrain, predTrain] = gnn.accuracy(dlYPredTrain, targetTrain, classes);
+        training_accuracy = sum(targetTrain==predTrain)/length(predTrain);
+        
+        % get test accuracy
         featureTest = features{2};
         adjacencyTest = adjacency{2};
         targetTest = labels{2};
         dlXTest = dlarray(featureTest);
-        dlYPredTest = model(dlXTest, adjacencyTest, parameters);
-        [scoreTest, predTest] = accuracy(dlYPredTest, targetTest, classes);
-        [X,Y,T,AUC] = perfcurve(targetTest,dlYPredTest(:,idx_pos),'amyloid_p');
+        dlYPredTest = gnn.model(dlXTest, adjacencyTest, parameters);
+        [scoreTest, predTest] = gnn.accuracy(dlYPredTest, targetTest, classes);
         scoreTest
         numOfSamples = numel(targetTest);
         classTarget = zeros(numOfSamples, numOutputFeatures);
@@ -261,34 +332,29 @@ function value = calculate_results(gnn)
         classAccuracy = sum(classPred == classTarget)./numOfSamples;
 
         % Visualize class-wise accuracy score
-        figure
-        [~,idx] = sort(classAccuracy,'descend');
-        histogram('Categories',classes(idx), ...
-            'BinCounts',classAccuracy(idx), ...
-            'Barwidth',0.8)
-        xlabel("Category")
-        ylabel("Accuracy")
-        title("Class Accuracy Score")
         [confusionMatrix, order] = confusionmat(targetTest, predTest);
         figure
         cm = confusionchart(confusionMatrix, classes, ...
             'ColumnSummary','column-normalized', ...
             'RowSummary','row-normalized', ...
-            'Title', 'ADNI Confusion Chart');
+            'Title', 'Confusion Chart');
         
+        test_accuracy = sum(targetTest==predTest)/length(predTest);
         % get string format of nn
-        neural_network = gnn.net_binary_transformer(net);
+        %neural_network = gnn.net_binary_transformer(net);
     else
-        neural_network = [];
-        X_tblTrain = [];
-        y_tblTrain = [];
-        X_tblTest = [];
-        y_tblTest = [];
+        parameters = [];
+        featureTrain = [];
+        adjacencyTrain = [];
+        targetTrain = [];
+        featureTest = [];
+        adjacencyTest = [];
+        targetTest = [];
         training_accuracy = 0;
         test_accuracy = 0;
     end
     % return all the values
-    value = {neural_network, X_tblTrain, y_tblTrain, X_tblTest, y_tblTest, training_accuracy, test_accuracy};
+    value = {parameters, featureTrain, adjacencyTrain, targetTrain, featureTest, adjacencyTest, targetTest, training_accuracy, test_accuracy};
 end
 
 %% ¡tests!
@@ -296,4 +362,4 @@ end
 %%%% ¡name!
 Example
 %%%% ¡code!
-%example_nn_regression_ST
+example_gnn_graph_classification_FUN_BUD
