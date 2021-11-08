1cf28a38f44052464405ca55eb03d21233ae41cd
egolol <egolol87@gmail.com>
2021-11-08 10:23:10 -0600

    update

---
+++ braph2genesis/workflows/functional/_GNNFeatureSelector.gen.m
@@ -0,0 +1,66 @@
+%% ¡header!
+GNNFeatureSelector < ClassifierGNN (gnn, graph classifier with functional data) is a graph neural network classifier using functional data for classification.
+
+%% ¡description!
+This classifier classifies graphs using functional data with graph neural network. The graph classification task is one where an algorithm, in this example, a GNN has to predict the labels of unlabelled graphs.
+In this example, a graph is represented by a connectom of individual. Nodal graph measures represent nodes in the graph and the correlation betweeens brain regions obtained from functional time series represent links in the graph.
+Graph labels are the diagnosis of individual, for example, MCI. As such, input to the GNN are connectom along with graph nodal measures and the outputs are predictions of the type of diagnosis.
+
+%% ¡props!
+%%% ¡prop!
+GR1 (data, item) is the subject group 1, which also defines the subject class SubjectST.
+%%%% ¡settings!
+'Group'
+%%%% ¡default!
+Group('SUB_CLASS', 'SubjectFUN')
+
+%%% ¡prop!
+GR2 (data, item) is the subject group 2, which also defines the subject class SubjectST.
+%%%% ¡settings!
+'Group'
+%%%% ¡default!
+Group('SUB_CLASS', 'SubjectFUN')
+
+%%% ¡prop!
+A_1 (data, item) is the analysis 1
+%%%% ¡settings!
+'AnalyzeEnsemble'
+
+%%% ¡prop!
+A_2 (data, item) is the analysis 2
+%%%% ¡settings!
+'AnalyzeEnsemble'
+
+%%% ¡prop!
+GNN_Classification (result, item) is the GNN object
+%%%% ¡calculate!
+a_1 = el.get('A_1');
+a_BUD1_adjs = a_1.get('G_DICT').getItems();
+features1 = {};
+links1 = {};
+for i = 1:length(a_BUD1_adjs)
+    features1{end+1} = cell2mat(a_BUD1_adjs{i}.getMeasure('Degree').get('M'));
+    links1{end+1} = cell2mat(a_BUD1_adjs{i}.get('A'));
+end
+
+a_2 = el.get('A_2');
+a_BUD2_adjs = a_2.get('G_DICT').getItems();
+features2 = {};
+links2 = {};
+for i = 1:length(a_BUD2_adjs)
+    features2{end+1} = cell2mat(a_BUD2_adjs{i}.getMeasure('Degree').get('M'));
+    links2{end+1} = cell2mat(a_BUD2_adjs{i}.get('A'));
+end
+
+gr1 = el.get('GR1');
+gr2 = el.get('GR2');
+
+value = GraphClassifierGNN_FUN( ...
+    'GR1', gr1, ...
+    'GR2', gr2, ...
+    'FEATURES1', features1, ...
+    'FEATURES2', features2, ...
+    'LINKS1', links1, ...
+    'LINKS2', links2 ...
+    );
+
