0d2c2ac817554b8b9fe39ee94397c788b9832e71
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2021-12-28 11:53:01 +0100

    Update _Graph.gen.m

--- braph2genesis/src/gt/_Graph.gen.m
+++ braph2genesis/src/gt/_Graph.gen.m
@@ -37,8 +37,8 @@ Graph.checkDirectionality(Graph.getDirectionalityType(g, length(A)), A);
 Graph.checkSelfConnectivity(Graph.getSelfConnectivityType(g, length(A)), A);
 Graph.checkNegativity(Graph.getNegativityType(g, length(A)), A);
 check = true; % only if no error is thrown by the previous code!
-%%%% ¡gui!
-pl = PPGraphA('EL', g, 'PROP', Graph.A, varargin{:});
+%%%% ¡gui__!
+% % % pl = PPGraphA('EL', g, 'PROP', Graph.A, varargin{:});
 
 %%% ¡prop!
 M_DICT (result, idict) contains the calculated measures of the graph.
