942015bda6b3d8320e1069c1db116257e9765569
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2022-01-03 21:28:03 +0100

    GUI initial

---
+++ braph2genesis/src/gui/_GUI.gen.m
@@ -0,0 +1,19 @@
+%% ¡header!
+GUI < Element (gui, graphical user interface) is a graphical user interface for an element.
+
+%%% ¡description!
+% % % FIXME
+
+%%% ¡seealso!
+Element, PlotElement
+
+%% ¡props!
+
+%%% ¡prop!
+ID (data, string) is a few-letter code for the graphical user interface.
+
+%%% ¡prop!
+PE (metadata, item) is a plot element.
+%%%% ¡settings!
+'PlotElement'
+
--- braph2genesis/tmp.m
+++ braph2genesis/tmp.m
@@ -9,13 +9,13 @@ clc
 % delete('/Users/giovannivolpe/Documents/GitHub/Braph-2.0-Matlab/braph2/src/gui/PlotElement.m')
 % create_Element('/Users/giovannivolpe/Documents/GitHub/Braph-2.0-Matlab/braph2genesis/src/gui/_PlotElement.gen.m', '/Users/giovannivolpe/Documents/GitHub/Braph-2.0-Matlab/braph2/src/gui')
 
-braph2genesis
-
-et = ETA();
-pe = PlotElement('EL', et);
-pp = pe.draw();
-
-get(pp, 'UserData')
+% braph2genesis
+% 
+% et = ETA();
+% pe = PlotElement('EL', et);
+% pp = pe.draw();
+% 
+% get(pp, 'UserData')
 
 % create_Element('/Users/giovannivolpe/Documents/GitHub/Braph-2.0-Matlab/braph2genesis/src/gui/_PlotPropMatrix.gen.m', '/Users/giovannivolpe/Documents/GitHub/Braph-2.0-Matlab/braph2/src/gui')
 % 
