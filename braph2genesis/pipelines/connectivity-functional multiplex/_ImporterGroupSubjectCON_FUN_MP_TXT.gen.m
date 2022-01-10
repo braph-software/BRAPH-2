dce325ae1c2948904e6db349623ff4fc61d73ab6
egolol <egolol87@gmail.com>
2022-01-10 15:26:56 -0600

    new importer and exporters + separetorconfun

---
+++ braph2genesis/pipelines/connectivity-functional multiplex/_ImporterGroupSubjectCON_FUN_MP_TXT.gen.m
@@ -0,0 +1,97 @@
+%% ¡header!
+ImporterGroupSubjectCON_FUN_MP_TXT < Importer (im, importer of CON_FUN_MP subject group from TXT) imports a group of subjects with connectivity data from a series of TXT file.
+
+%%% ¡description!
+ImporterGroupSubjectCON_FUN_MP_TXT imports a group of subjects with connectivity data from a series of TXT file and their covariates (optional) from another TXT file.
+All these files must be in the same folder; also, no other files should be in the folder.
+Each file contains a table of values corresponding to the adjacency matrix.
+The TXT file containing the covariates must be inside another folder in the same group directory 
+and consists of the following columns:
+Subject ID (column 1), Subject AGE (column 2), and Subject SEX (column 3).
+The first row contains the headers and each subsequent row the values for each subject.
+
+%%% ¡seealso!
+Element, Importer, ExporterGroupSubjectCON_TXT
+
+%% ¡props!
+
+%%% ¡prop!
+DIRECTORY_CON (data, string) is the directory containing the CON_FUN_MP subject group files of CON type  from which to load the subject group.
+
+%%% ¡prop!
+DIRECTORY_FUN (data, string) is the directory containing the CON_FUN_MP subject group files of FUN type from which to load the subject group.
+
+%%% ¡prop!
+BA (data, item) is a brain atlas.
+%%%% ¡settings!
+'BrainAtlas'
+
+%%% ¡prop!
+GR (result, item) is a group of subjects with connectivity data.
+%%%% ¡settings!
+'Group'
+%%%% ¡check_value!
+check = any(strcmp(value.get(Group.SUB_CLASS_TAG), subclasses('SubjectCON_FUN_MP', [], [], true))); % Format.checkFormat(Format.ITEM, value, 'Group') already checked
+%%%% ¡default!
+Group('SUB_CLASS', 'SubjectCON_FUN_MP', 'SUB_DICT', IndexedDictionary('IT_CLASS', 'SubjectCON'))
+%%%% ¡calculate!
+% creates empty Group
+gr = Group( ...
+    'SUB_CLASS', 'SubjectCON_FUN_MP', ...
+    'SUB_DICT', IndexedDictionary('IT_CLASS', 'SubjectCON_FUN_MP') ...
+    );
+
+gr.lock('SUB_CLASS');
+
+% first CON
+directory_CON = im.get('DIRECTORY_CON');
+if ~isfolder(directory_CON)&& ~braph2_testing()
+    im.uigetdir('DIR_TYPE', 'DIRECTORY_CON')
+    directory_CON = im.get('DIRECTORY_CON');
+end
+if isfolder(directory_CON)  
+    gr_con = ImporterGroupSubjectCON_TXT( ...
+        'DIRECTORY',  directory_CON, ...
+        'BA', ba, ...
+        'WAITBAR', im.get('WAITBAR') ...
+        );
+elseif ~braph2_testing()
+    error(BRAPH2.IM_ERR);
+end
+
+% second FUN
+directory_FUN = im.get('DIRECTORY_FUN');
+if ~isfolder(directory_FUN)&& ~braph2_testing()
+    im.uigetdir('DIR_TYPE', 'DIRECTORY_FUN')
+    directory_FUN = im.get('DIRECTORY_FUN');
+end
+if isfolder(directory_FUN)  
+    gr_fun = ImporterGroupSubjectFUN_TXT( ...
+        'DIRECTORY',  directory_FUN, ...
+        'BA', ba, ...
+        'WAITBAR', im.get('WAITBAR') ...
+        );
+elseif ~braph2_testing()
+    error(BRAPH2.IM_ERR);
+end
+
+co_gr = CombineGroups_CON_FUN( ...
+    'GR1', gr_con, ...
+    'GR2', gr_fun, ...
+    'WAITBAR', true ...
+    );
+
+gr = co_gr.get('GR');
+
+value = gr;
+
+%% ¡methods!
+function uigetdir(im, varargin)
+    % UIGETDIR opens a dialog box to set the directory from where to load the TXT files of the CON_FUN_MP subject group.
+
+    directory = uigetdir('Select directory');
+    dir_type = get_from_varargin('', 'DIR_TYPE', varargin{:});
+    if ischar(directory) && isfolder(directory)
+        im.set(dir_type, directory);
+    end
+end
---
+++ braph2genesis/pipelines/connectivity-functional multiplex/_SepareteGroupsCON_FUN.gen.m
@@ -0,0 +1,91 @@
+%% ¡header!
+SepareteGroupsCON_FUN < Element (se, separetes the CON subject group and the FUN subject group from a CON_FUN group) separetes a group of subjects with connectivity data with a group of subjects with functional data.
+%%% ¡description!
+SepareteGroups_CON_FUN separetes a group of subjects with connectivity data with a group of subjects with functional data.
+Separetes the data uploaded through the connectivity and functional workflows. The ID and covariates of subjects are
+copied from the first group, in this case connectivity.
+
+%%% ¡seealso!
+Element
+
+%% ¡props!
+
+%%% ¡prop!
+ID (data, string) is a few-letter code for the group combiner.
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the group combiner.
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the group combiner.
+
+%%% ¡prop!
+WAITBAR (metadata, logical) determines whether to show the waitbar.
+
+%%% ¡prop!
+GRP_CON_FUN_MP (data, item) is a group of subjects with functional and connectivity data
+%%%% ¡default!
+Group('SUB_CLASS', 'SubjectCON_FUN_MP')
+
+%%% ¡prop!
+GRS (result, cell) is a idict containing two group of subjects with connectivity data and functional data.
+%%%% ¡calculate!
+if se.get('WAITBAR')
+    wb = waitbar(0, 'Separating subject groups ...', 'Name', BRAPH2.NAME);
+    set_braph2_icon(wb)
+end
+
+gr = se.get('GRP_CON_FUN_MP');
+ba = gr.getItem(1).get('BA');
+
+% declare variables
+mixed_data{i} = cell(gr.get('SUB_DICT').length(), 1);
+gr_con = Group( ...
+    'SUB_CLASS', 'SubjectCON', ...
+    'SUB_DICT', IndexedDictionary('IT_CLASS', 'SubjectCON') ...
+    );
+subdict_con = gr_con.get('SUB_DICT');
+gr_fun = Group( ...
+    'SUB_CLASS', 'SubjectFUN', ...
+    'SUB_DICT', IndexedDictionary('IT_CLASS', 'SubjectFUN') ...
+    );
+subdict_fun = gr_fun.get('SUB_DICT');
+
+% cycle for data and creation of different groups
+for i = 1:1:gr.get('SUB_DICT').length()
+    if se.get('WAITBAR')
+        waitbar(.30 + .70 * i / gr.get('SUB_DICT').length(), wb, ['Separating subject ' num2str(i) ' of ' num2str(gr.get('SUB_DICT').length()) ' ...'])
+    end
+    sub = gr.getItem(i);
+    mixed_data{i} = sub.get('CON_FUN_MP');
+    sub_con = SubjectCON( ...
+        'ID', sub.get('ID'), ...
+        'BA', ba, ...
+        'age', sub.get('AGE'), ...
+        'sex', sub.get('SEX'), ...
+        'CON', mixed_data{1} ...
+        );
+    sub_fun = SubjectFUN( ...
+        'ID', sub.get('ID'), ...
+        'BA', ba, ...
+        'age', sub.get('AGE'), ...
+        'sex', sub.get('SEX'), ...
+        'CON', mixed_data{2} ...
+        );
+    subdict_con.add(sub_con);
+    subdict_fun.add(sub_fun);
+end
+
+gr_con.set('sub_dict', subdict_con);
+gr_fun.set('sub_dict', subdict_fun);
+
+% declare result variable and set it with result
+grs = cell(2, 1);
+grs{1} = gr_con;
+grs{2} = gr_fun;
+
+if se.get('WAITBAR')
+    close(wb)
+end
+
+value = grs;
