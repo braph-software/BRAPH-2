49468470521f96e3017bcad5f6d2d646b26bc4d0
c-yuwei <hejyuwei@gmail.com>
2022-01-19 23:35:41 +0100

    Update _ModelEvaluator.gen.m

--- braph2genesis/src/nn/_ModelEvaluator.gen.m
+++ braph2genesis/src/nn/_ModelEvaluator.gen.m
@@ -1,13 +1,18 @@
 %% ¡header!
-ModelEvaluator < Element (me, model evaluator of a neural network model) evaluates the performance of a neural network model with specific dataset.
+ModelEvaluator < Element (me, model evaluator of a neural network model) evaluates the performance of a neural network model with a specific dataset.
 
 %% ¡description!
 This model evaluator evaluates the performance of a neural network model.
 Instances of this class should not be created. 
 Use one of its subclasses instead.
 
-%% ¡prop!
+%% ¡props!
 %%% ¡prop!
-DATASET_PREPROCESS(data, item) is the dataset for testing the trained neural network model.
+DATASET_PROCESSOR (data, item) is a dataset processor contains the dataset and the neural network model.
 %%%% ¡settings!
 'DatasetProcessor'
+
+%%% ¡prop!
+TRAINED_MODEL (data, item) is a trained neural network model.
+%%%% ¡settings!
+'BaseNN'
