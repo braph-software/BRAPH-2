f0de691f6bb25c1b9d8d12fb809dc031a86d1620
egolol <egolol87@gmail.com>
2022-02-22 15:07:14 +0100

    changes to comparison ensemble diff

--- braph2genesis/src/analysis/_PPComparisonEnsemble_Diff.gen.m
+++ braph2genesis/src/analysis/_PPComparisonEnsemble_Diff.gen.m
@@ -41,7 +41,7 @@ function h_panel = draw(pr, varargin)
     % see also update, redraw, refresh, settings, uipanel, isgraphics.
 
     pr.p = draw@PlotPropMatrix(pr, varargin{:});
-    
+
     % retrieves the handle of the table
     children = get(pr.p, 'Children');
     for i = 1:1:length(children)
@@ -49,15 +49,20 @@ function h_panel = draw(pr, varargin)
             pr.comparison_tbl = children(i);
         end
     end
-    
+
     % create panel with slider
     el = pr.get('EL');
     prop = pr.get('PROP');
     value = el.get(prop);
     L = size(value, 1);
-    L2 = size(value, 2);
-    label = el.get('C').get('A1').getPropTag(7);
-    
+    label = el.get('C').get('A1').getPropTag(12);
+    layer_dim_element = el.get('C').get('A1').get('G_DICT').getItem(1);
+    L2 = size(layer_dim_element.get('b'), 2);
+
+    if L2 > 1
+        L = L/L2;
+    end
+
     % set on first layer
     pr.slider = uicontrol( ...
         'Parent', pr.p, ...
@@ -74,7 +79,7 @@ function h_panel = draw(pr, varargin)
         'Parent', pr.p, ...
         'Style', 'text', ...
         'Units', 'characters', ...
-        'HorizontalAlignment', 'center', ...        
+        'HorizontalAlignment', 'center', ...
         'Visible', 'off', ...
         'FontUnits', BRAPH2.FONTUNITS, ...
         'FontSize', BRAPH2.FONTSIZE, ...
@@ -82,11 +87,11 @@ function h_panel = draw(pr, varargin)
         'String', [label ' ' num2str(round(get(pr.slider, 'Value')))], ...
         'BackgroundColor', pr.get('BKGCOLOR') ...
         );
-    
-    function cb_slider(~, ~)
-        pr.update()
-    end
-    
+
+        function cb_slider(~, ~)
+            pr.update()
+        end
+
     if L2 > 1
         pr.second_slider = uicontrol( ...
             'Parent', pr.p, ...
@@ -97,7 +102,7 @@ function h_panel = draw(pr, varargin)
             'Max', L2, ...
             'SliderStep', [1 1], ...
             'Callback', {@cb_slider_2} ...
-            );    
+            );
         pr.second_slider_text = uicontrol(...
             'Parent', pr.p, ...
             'Style', 'text', ...
@@ -106,14 +111,14 @@ function h_panel = draw(pr, varargin)
             'FontUnits', BRAPH2.FONTUNITS, ...
             'FontSize', BRAPH2.FONTSIZE, ...
             'FontWeight', 'bold', ...
-            'String', [label ' ' num2str(round(get(pr.second_slider, 'Value')))], ...
+            'String', ['Layer ' num2str(round(get(pr.second_slider, 'Value')))], ...
             'BackgroundColor', pr.get('BKGCOLOR') ...
             );
     end
-    
-    function cb_slider_2(~, ~)
-        pr.update()
-    end
+
+        function cb_slider_2(~, ~)
+            pr.update()
+        end
 
     brain_view = uicontrol('Parent', pr.p, ...
         'Style', 'pushbutton', ...
@@ -144,28 +149,34 @@ function update(pr)
 
     el = pr.get('EL');
     prop = pr.get('PROP');
-    value = el.getr(prop);  
-    label = el.get('C').get('A1').getPropTag(7);
+    value = el.getr(prop);
+    label = el.get('C').get('A1').getPropTag(12);
     fdr_q_value = 0.05;
     fdr_style = [1 1 0];
-   
+    layer_dim_elemnt = el.get('C').get('A1').get('G_DICT').getItem(1);
+
+    L2 = size(layer_dim_elemnt.get('b'), 2);
+
     if el.isLocked(prop)
         set(pr.comparison_tbl, ...
             'Enable', pr.get('ENABLE'), ...
             'ColumnEditable', false ...
             )
     end
-    
-    if  size(value, 2) > 2
+
+    if  L2 > 1
         set(pr.slider_text, ...
             'String', [label ' ' num2str(round(get(pr.slider, 'Value')))]);
         set(pr.second_slider_text, ...
-            'String', [label ' ' num2str(round(get(pr.second_slider, 'Value')))]);
-       
-         % set p values mask
-        tmp_value = value{round(get(pr.slider, 'Value')), round(get(pr.second_slider, 'Value'))};
+            'String', ['Layer ' num2str(round(get(pr.second_slider, 'Value')))]);
+
+        % set p values mask
+        D_T = round(get(pr.slider, 'Value'));
+        layer_sel = round(get(pr.second_slider, 'Value'));
+        tmp_diff = L2-layer_sel;
+        tmp_value = value{D_T*L2-tmp_diff};
         p1 = el.get('P1');
-        p1 = p1{round(get(pr.slider, 'Value')), round(get(pr.second_slider, 'Value'))};
+        p1 = p1{D_T*L2-tmp_diff};
 
         if Measure.is_nodal(el.get('measure'))
             p1 = p1';
@@ -174,7 +185,7 @@ function update(pr)
         else
             [~, mask] = fdr(p1, fdr_q_value);
         end
-        
+
         for i = 1:size(tmp_value, 1)
             for j = 1:size(tmp_value, 2)
                 if mask(i, j)
@@ -187,16 +198,16 @@ function update(pr)
                 end
             end
         end
-        
+
         set(pr.comparison_tbl, ...
             'Data', tmp_value, ...
             'ColumnFormat', repmat({'long'}, 1, size(el.get(prop), 2)), ...
-            'ColumnEditable', false)        
-    else        
+            'ColumnEditable', false)
+    else
         set(pr.slider_text, ...
             'String', [label ' ' num2str(round(get(pr.slider, 'Value')))]);
-        
-        
+
+
         % set p values mask
         tmp_value = value{round(get(pr.slider, 'Value'))};
         p1 = el.get('P1');
@@ -222,7 +233,7 @@ function update(pr)
                 end
             end
         end
-        
+
         set(pr.comparison_tbl, ...
             'Data', tmp_value, ...
             'ColumnFormat', repmat({'long'}, 1, size(el.get(prop), 2)), ...
@@ -236,7 +247,7 @@ function update(pr)
             'ColumnEditable', false ...
             )
     end
-    
+
 end
 function redraw(pr, varargin)
     %REDRAW redraws the element graphical panel.
@@ -246,64 +257,69 @@ function redraw(pr, varargin)
     % REDRAW(PR, 'Height', HEIGHT) sets the height of PR (by default HEIGHT=3.3).
     %
     % See also draw, update, refresh.
-    
+
     [h, varargin] = get_and_remove_from_varargin(1.8, 'Height', varargin);
     [Sh, varargin] = get_and_remove_from_varargin(2.0, 'SHeight', varargin);
     [Th, varargin] = get_and_remove_from_varargin(2.0, 'THeight', varargin);
     [Dh, varargin] = get_and_remove_from_varargin(20, 'DHeight', varargin);
-    
-    
+
+
     el = pr.get('EL');
     prop = pr.get('PROP');
     value = el.get(prop);
     L = size(value, 1);
-    L2 = size(value, 2);
-    
+    layer_dim_elemnt = el.get('C').get('A1').get('G_DICT').getItem(1);
+    L2 = size(layer_dim_elemnt.get('b'), 2);
+
     if L2 > 1
         pr.redraw@PlotProp('Height', h + Sh + Sh + Th + Th + Dh, varargin{:})
         set(pr.slider, ...
             'Units', 'normalized', ...
-            'Position', [.01 (Sh+Th+Dh)/(h+Sh+Sh+Th+Th+Dh) .97 (Th/(h+Sh+Sh+Th+Th+Dh)-.02)] ...
+            'Visible', 'on', ...
+            'Position', [.01 (Sh+Th+Dh+h)/(h+Sh+Sh+Th+Th+Dh) .97 (Th/(h+Sh+Sh+Th+Th+Dh)-.02)] ...
             );
-        
+
         set(pr.slider_text, ...
             'Units', 'normalized', ...
-            'Position', [.01 (Sh+Th+Th+Dh)/(h+Sh+Sh+Th+Th+Dh) .97 (Th/(h+Sh+Sh+Th+Th+Dh)-.02)] ...
+            'Visible', 'on', ...
+            'Position', [.01 (Sh+Th+Th+Dh+h)/(h+Sh+Sh+Th+Th+Dh) .97 (Th/(h+Sh+Sh+Th+Th+Dh)-.02)] ...
             );
-        
+
         set(pr.second_slider, ...
             'Units', 'normalized', ...
-            'Position', [.01 (Dh)/(h+Sh+Sh+Th+Th+Dh) .97 (Th/(h+Sh+Sh+Th+Th+Dh)-.02)] ...
+            'Visible', 'on', ...
+            'Position', [.01 (Dh+h)/(h+Sh+Sh+Th+Th+Dh) .97 (Th/(h+Sh+Sh+Th+Th+Dh)-.02)] ...
             );
-        
+
         set(pr.second_slider_text, ...
             'Units', 'normalized', ...
-            'Position', [.01 (Dh+Th)/(h+Sh+Sh+Th+Th+Dh) .97 (Th/(h+Sh+Sh+Th+Th+Dh)-.02)] ...
+            'Visible', 'on', ...
+            'Position', [.01 (Dh+Th+h)/(h+Sh+Sh+Th+Th+Dh) .97 (Th/(h+Sh+Sh+Th+Th+Dh)-.02)] ...
             );
-        
+
         set(pr.comparison_tbl, ...
             'Visible', 'on', ...
             'Units', 'normalized', ...
-            'Position', [.01 .02 .97 (Dh/(h+Sh+Sh+Th+Th+Dh)-.02)] ...
+            'Position', [.01 .02 .97 ((Dh+h)/(h+Sh+Sh+Th+Th+Dh)-.02)] ...
             )
     else
         pr.redraw@PlotProp('Height', h + Sh + Th + Dh, varargin{:})
         set(pr.slider, ...
-            'Units', 'normalized', ...            
+            'Units', 'normalized', ...
             'Visible', 'on', ...
-            'Position', [.01 Dh/(h+Sh+Th+Dh) .97 (Th/(h+Sh+Th+Dh)-.02)] ...
+            'Position', [.01 (Dh+h)/(h+Sh+Th+Dh) .97 (Th/(h+Sh+Th+Dh)-.02)] ...
             );
-        
+
         set(pr.slider_text, ...
             'Units', 'normalized', ...
             'Visible', 'on', ...
-            'Position', [.01 (Th+Dh)/(h+Sh+Th+Dh) .97 (Th/(h+Sh+Th+Dh)-.02)] ...
+            'Position', [.01 (Th+Dh+h)/(h+Sh+Th+Dh) .97 (Th/(h+Sh+Th+Dh)-.02)] ...
             );
-        
+
         set(pr.comparison_tbl, ...
             'Visible', 'on', ...
             'Units', 'normalized', ...
-            'Position', [.01 .02 .97 (Dh/(h+Sh+Th+Dh)-.02)] ...
+            'Position', [.01 .02 .97 ((Dh+h)/(h+Sh+Th+Dh)-.02)] ...
             )
     end
 end
@@ -323,7 +339,7 @@ function cb_brain_view_fig(pr)
     h = f_ba_h / 1.5;
     y = f_ba_y + f_ba_h - h;
     w = screen_w - x;
-    
+
     if isempty(pr.f_br) || ~check_graphics(pr.f_br, 'figure')
         pr.f_br = figure( ...
             'NumberTitle', 'off', ...
@@ -336,16 +352,16 @@ function cb_brain_view_fig(pr)
             );
         set_braph2_icon(pr.f_br)
         menu_about = BRAPH2.add_menu_about(pr.f_br);
-        
+
         ui_toolbar = findall(pr.f_br, 'Tag', 'FigureToolBar');
         delete(findall(ui_toolbar, 'Tag', 'Standard.NewFigure'))
         delete(findall(ui_toolbar, 'Tag', 'Standard.FileOpen'))
-        
+
         el = pr.get('EL');
         prop = pr.get('PROP');
-        
+
         g = el.get('c').get('a1').get('g_dict').getItem(1);
-        
+
         if isequal(g.getClass(), 'MultigraphBUD') || isequal(g.getClass(), 'MultiplexBUD')
             type = 'Densities';
         elseif isequal(g.getClass(), 'MultigraphBUT') || isequal(g.getClass(), 'MultiplexBUT')
@@ -353,15 +369,15 @@ function cb_brain_view_fig(pr)
         else
             type = 'Weighted';
         end
-        
+
         prop_tag = el.getPropTag(prop);
-        
+
         pbv = PlotBrainView('COMP', el, ...
             'Atlas', g.get('brainatlas'), ...
             'PROPTAG', prop_tag, ...
             'Type', type);
 
-        
+
         pbv.draw('Parent', pr.f_br );
         f_settings = pbv.settings();
         set(f_settings, 'Position', [x/screen_w f_ba_y/screen_h w/screen_w (f_ba_h-h)/screen_h])
@@ -371,11 +387,11 @@ function cb_brain_view_fig(pr)
         gui = get(pr.f_br, 'UserData');
         gui.cb_bring_to_front()
     end
-    
-    function cb_f_br_close(~, ~)
-        delete(pr.f_br);
-        pr.update()
-    end
+
+        function cb_f_br_close(~, ~)
+            delete(pr.f_br);
+            pr.update()
+        end
 
     pr.update()
 end
@@ -389,12 +405,12 @@ function cb_bring_to_front(pr)
 
     % brings to front settings panel
     pr.cb_bring_to_front@PlotProp();
-    
+
     % bring to front plot graph
     if check_graphics(pr.f_br, 'figure')
         gui = get(pr.f_br, 'UserData');
         gui.cb_bring_to_front()
-    end      
+    end
 end
 function cb_hide(pr)
     %CB_HIDE hides the figure and its settings figure.
@@ -405,13 +421,12 @@ function cb_hide(pr)
 
     % hides settings panel
     pr.cb_hide@PlotProp();
-    
+
     % bring to front plot graph
     if check_graphics(pr.f_br, 'figure')
         gui = get(pr.f_br, 'UserData');
         gui.cb_hide();
-    end 
-
+    end
 end
 function cb_close(pr)
     %CB_CLOSE closes the figure.
@@ -420,8 +435,8 @@ function cb_close(pr)
     %
     % See also cb_bring_to_front, cd_hide.
 
-    % close adj graph figure
+    % close br graph figure
     if ~isempty(pr.f_br) && check_graphics(pr.f_br, 'figure')
         delete(pr.f_br);
-    end     
+    end
 end
