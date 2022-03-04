fb94246dcb3fac7b9d64539b8b2b27111c887acd
c-yuwei <hejyuwei@gmail.com>
2022-03-04 23:35:38 +0100

    Update tmp.m

--- braph2genesis/tmp.m
+++ braph2genesis/tmp.m
@@ -11,16 +11,13 @@ files_for_copy = { ...
     '\pipelines\structural NN\example_NNCV_ST_Classification.m'
     '\pipelines\structural NN\example_NN_ST_Classification.m'
     }
+
 % list the genesis files to be compiled to braph2 folder
 gen_files = { ...
     '\src\nn\_NNBase.gen.m' ...
     '\src\nn\_NNData.gen.m' ...
     '\src\nn\_NNEvaluator.gen.m'};
 
-%% Delete the files
-for i = 1:length(gen_files)
-    delete([braph_path extractBefore(gen_files{i}, "\_") filesep char(extractBetween(gen_files{i}, "\_", ".gen")) '.m']);
-end 
 
 %% Compile and run the test
 for i = 1:length(files_for_copy)
@@ -36,8 +33,6 @@ for i = 1:length(gen_files)
     eval(['test_' char(extractBetween(gen_files{i}, "\_", ".gen"))])
 end
 
-%% test on GUI
-GUI('pe', Pipeline()).draw()
 
 %% archived codes
 % delete('/Users/giovannivolpe/Documents/GitHub/Braph-2.0-Matlab/braph2/src/gui/PlotElement.m')
@@ -68,24 +63,33 @@ GUI('pe', Pipeline()).draw()
 
 lf = load('pip_c.b2', '-mat');
 pip = lf.el;
-% lf = load('pip_c.b2', '-mat');
-% pip = lf.el;
 
 % gui = GUI( ...
 %     'PE', pip, ...
-@@ -77,6 +83,11 @@
-    'CLOSEREQ', false ...
-    );
-gui.draw()
-% gui = GUI( ...
-%     'PE', pip, ...
+%     'MENU_FILE', false, ...
+%     'MENU_IMPORT', false, ...
+%     'MENU_EXPORT', false, ...
+%     'MENU_PERSONALIZE', false, ...
+%     'MENU_ABOUT', false, ...
+%     'TOOL_FIG', true, ...
 %     'CLOSEREQ', false ...
 %     );
-% gui.draw()
+gui = GUI( ...
+    'PE', pip, ...
+gui.draw()
 
 % % % delete('/Users/giovannivolpe/Documents/GitHub/Braph-2.0-Matlab/braph2/src/gui/PPPipeline_PSDict.m')
 % % % create_Element('/Users/giovannivolpe/Documents/GitHub/Braph-2.0-Matlab/braph2genesis/src/gui/_PPPipeline_PSDict.gen.m', '/Users/giovannivolpe/Documents/GitHub/Braph-2.0-Matlab/braph2/src/gui')
-@@ -93,377 +104,9 @@
+% % % create_Element('/Users/giovannivolpe/Documents/GitHub/Braph-2.0-Matlab/braph2genesis/src/gui/_PPPipeline_PSDict.gen.m', '/Users/giovannivolpe/Documents/GitHub/Braph-2.0-Matlab/braph2/src/gui')
+% % 
+% % lf = load('pip_c.b2', '-mat');
+% % pip = lf.el;
+% % 
+% % % im = ImporterPipelineBRAPH2(...
+% % %     'FILE', [fileparts(which('SubjectST')) filesep 'pipeline_structural_comparison_wu.braph2'], ...
+% % %     'WAITBAR', false ...
+% % %     ); 
+% % % pip = im.get('PIP');
 % % 
 % % gui = GUI('PE', pip, 'CLOSEREQ', false);
 % % gui.draw()
@@ -462,5 +466,3 @@ gui.draw()
 % %     'WAITBAR', true ...
 % %     );
 % % ex.get('SAVE');
-
-% create_Element('/Users/giovannivolpe/Documents/GitHub/Braph-2.0-Matlab/braph2genesis/src/gui/_PPPipeline_PSDict.gen.m', '/Users/giovannivolpe/Documents/GitHub/Braph-2.0-Matlab/braph2/src/gui') 
