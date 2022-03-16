0694a24283d006e3fd4c3c288c4b04ca9b3e1b74
Yu-Wei Chang <hejyuwei@gmail.com>
2022-03-16 02:12:10 +0100

    CON pipeline with NNGroup and NNSubject

--- braph2genesis/pipelines/connectivity NN/_NNClassifierData_CON_WU.gen.m
+++ braph2genesis/pipelines/connectivity NN/_NNClassifierData_CON_WU.gen.m
@@ -10,119 +10,162 @@ when constructing the INPUTS. The TARGETS cell holds the target values for
 the trianing samples. 
 
 %% ¡props_update!
-%%% ¡prop!
-TRAIN_G_DICT_1 (result, idict) is the graph (GraphWU) from subject group 1 in training set.
-%%%% ¡settings!
-'GraphWU'
-%%%% ¡default!
-IndexedDictionary('IT_CLASS', 'GraphWU')
-%%%% ¡calculate!
-g_dict = IndexedDictionary('IT_CLASS', 'GraphWU');
-gr = nnd.get('TRAIN_GR1');
-atlas = BrainAtlas();
-if ~isempty(gr) && ~isa(gr, 'NoValue') && gr.get('SUB_DICT').length > 0 
-    atlas = gr.get('SUB_DICT').getItem(1).get('BA');
-end
-gr = nnd.get('TRAIN_GR1');
-for i = 1:1:gr.get('SUB_DICT').length()
-	sub = gr.get('SUB_DICT').getItem(i);
-    g = GraphWU( ...
-        'ID', ['g ' sub.get('ID')], ...
-        'BRAINATLAS', atlas, ...
-        'B', Callback('EL', sub, 'TAG', 'CON') ...
-        );
-    g_dict.add(g)
-end
-
-value = g_dict;
 
 %%% ¡prop!
-TRAIN_G_DICT_2 (result, idict) is the graph (GraphWU) from subject group 2 in training set.
-%%%% ¡settings!
-'GraphWU'
-%%%% ¡default!
-IndexedDictionary('IT_CLASS', 'GraphWU')
-%%%% ¡calculate!
-g_dict = IndexedDictionary('IT_CLASS', 'GraphWU');
-gr = nnd.get('TRAIN_GR2');
-atlas = BrainAtlas();
-if ~isempty(gr) && ~isa(gr, 'NoValue') && gr.get('SUB_DICT').length > 0 
-    atlas = gr.get('SUB_DICT').getItem(1).get('BA');
-end
-gr = nnd.get('TRAIN_GR2');
-for i = 1:1:gr.get('SUB_DICT').length()
-	sub = gr.get('SUB_DICT').getItem(i);
-    g = GraphWU( ...
-        'ID', ['g ' sub.get('ID')], ...
-        'BRAINATLAS', atlas, ...
-        'B', Callback('EL', sub, 'TAG', 'CON') ...
-        );
-    g_dict.add(g)
-end
-
-value = g_dict;
-
-%%% ¡prop!
-VAL_G_DICT_1 (result, idict) is the graph (GraphWU) from subject group 1 in validation set.
-%%%% ¡settings!
-'GraphWU'
-%%%% ¡default!
-IndexedDictionary('IT_CLASS', 'GraphWU')
-%%%% ¡calculate!
-g_dict = IndexedDictionary('IT_CLASS', 'GraphWU');
-gr = nnd.get('VAL_GR1');
-atlas = BrainAtlas();
-if ~isempty(gr) && ~isa(gr, 'NoValue') && gr.get('SUB_DICT').length > 0 
-    atlas = gr.get('SUB_DICT').getItem(1).get('BA');
-end
-gr = nnd.get('VAL_GR1');
-for i = 1:1:gr.get('SUB_DICT').length()
-	sub = gr.get('SUB_DICT').getItem(i);
-    g = GraphWU( ...
-        'ID', ['g ' sub.get('ID')], ...
-        'BRAINATLAS', atlas, ...
-        'B', Callback('EL', sub, 'TAG', 'CON') ...
-        );
-    g_dict.add(g)
-end
-
-value = g_dict;
-
-%%% ¡prop!
-VAL_G_DICT_2 (result, idict) is the graph (GraphWU) from subject group 2 in validation set.
-%%%% ¡settings!
-'GraphWU'
-%%%% ¡default!
-IndexedDictionary('IT_CLASS', 'GraphWU')
-%%%% ¡calculate!
-g_dict = IndexedDictionary('IT_CLASS', 'GraphWU');
-gr = nnd.get('VAL_GR2');
-atlas = BrainAtlas();
-if ~isempty(gr) && ~isa(gr, 'NoValue') && gr.get('SUB_DICT').length > 0 
-    atlas = gr.get('SUB_DICT').getItem(1).get('BA');
-end
-gr = nnd.get('VAL_GR2');
-for i = 1:1:gr.get('SUB_DICT').length()
-	sub = gr.get('SUB_DICT').getItem(i);
-    g = GraphWU( ...
-        'ID', ['g ' sub.get('ID')], ...
-        'BRAINATLAS', atlas, ...
-        'B', Callback('EL', sub, 'TAG', 'CON') ...
-        );
-    g_dict.add(g)
-end
-
-value = g_dict;
-
-%%% ¡prop!
-GR1 (data, item) is the subject group 1, which also defines the subject class SubjectCON.
+GR1 (data, item) is a group of subjects defined as SubjectCON class.
 %%%% ¡default!
 Group('SUB_CLASS', 'SubjectCON')
 
 %%% ¡prop!
-GR2 (data, item) is the subject group 2, which also defines the subject class SubjectCON.
+GR2 (data, item) is a group of subjects defined as SubjectCON class.
 %%%% ¡default!
 Group('SUB_CLASS', 'SubjectCON')
+
+%%% ¡prop!
+NN_GR1 (result, group) is a group of subjects defined as NNSubject class.
+%%%% ¡settings!
+'NNGroup'
+%%%% ¡default!
+NNGroup('SUB_CLASS', 'NNSubject', 'SUB_DICT', IndexedDictionary('IT_CLASS', 'NNSubject'))
+%%%% ¡calculate!
+gr = nnd.get('GR1');
+nn_gr = NNGroup( ...
+    'SUB_CLASS', 'NNSubject', ...
+    'SUB_DICT', IndexedDictionary('IT_CLASS', 'NNSubject') ...
+    );
+
+nn_gr.lock('SUB_CLASS');
+
+nn_gr.set( ...
+    'ID', gr.get('ID'), ...
+    'LABEL', gr.get('LABEL'), ...
+    'NOTES', gr.get('NOTES') ...
+    );
+
+atlas = BrainAtlas();
+if ~isempty(gr) && ~isa(gr, 'NoValue') && gr.get('SUB_DICT').length > 0 
+    atlas = gr.get('SUB_DICT').getItem(1).get('BA');
+end
+
+nn_sub_dict = nn_gr.get('SUB_DICT');
+
+for i = 1:1:gr.get('SUB_DICT').length()
+	sub = gr.get('SUB_DICT').getItem(i);
+    g = GraphWU( ...
+        'ID', ['g ' sub.get('ID')], ...
+        'BRAINATLAS', atlas, ...
+        'B', Callback('EL', sub, 'TAG', 'CON') ...
+        );
+    
+    if string(nnd.get('INPUT_TYPE')) == "adjacency_matrices"
+        input = g.get('A'); 
+
+    elseif string(nnd.get('INPUT_TYPE')) == "graph_measures"
+        input_nodal = [];
+        input_binodal = [];
+        input_global = [];
+        mlist = nnd.get('MEASURES');
+        for j = 1:length(mlist)
+            if Measure.is_nodal(mlist{j})
+                input_nodal = [input_nodal; cell2mat(g.getMeasure(mlist{j}).get('M'))];
+            elseif Measure.is_global(mlist{j})
+                input_global = [input_global; cell2mat(g.getMeasure(mlist{j}).get('M'))];
+            else
+                input_binodal = [input_binodal; cell2mat(g.getMeasure(mlist{j}).get('M'))];
+            end
+        end
+        input = {input_nodal input_global input_binodal};
+    end
+
+    nn_sub = NNSubject( ...
+        'ID', [sub.get('ID') ' in ' gr.get('ID')], ...
+        'BA', atlas, ...
+        'age', sub.get('age'), ...
+        'sex', sub.get('sex'), ...
+        ...
+        'G', g, ...
+        'INPUT', input, ...
+        'TARGET', gr.get('ID') ...
+        );
+    nn_sub_dict.add(nn_sub);
+end
+nn_gr.set('sub_dict', nn_sub_dict);
+
+value = nn_gr;
+
+%%% ¡prop!
+NN_GR2 (result, group)  is a group of subjects defined as NNSubject class.
+%%%% ¡settings!
+'NNGroup'
+%%%% ¡default!
+NNGroup('SUB_CLASS', 'NNSubject', 'SUB_DICT', IndexedDictionary('IT_CLASS', 'NNSubject'))
+%%%% ¡calculate!
+gr = nnd.get('GR2');
+nn_gr = NNGroup( ...
+    'SUB_CLASS', 'NNSubject', ...
+    'SUB_DICT', IndexedDictionary('IT_CLASS', 'NNSubject') ...
+    );
+
+nn_gr.lock('SUB_CLASS');
+
+nn_gr.set( ...
+    'ID', gr.get('ID'), ...
+    'LABEL', gr.get('LABEL'), ...
+    'NOTES', gr.get('NOTES') ...
+    );
+
+atlas = BrainAtlas();
+if ~isempty(gr) && ~isa(gr, 'NoValue') && gr.get('SUB_DICT').length > 0 
+    atlas = gr.get('SUB_DICT').getItem(1).get('BA');
+end
+
+nn_sub_dict = nn_gr.get('SUB_DICT');
+
+for i = 1:1:gr.get('SUB_DICT').length()
+	sub = gr.get('SUB_DICT').getItem(i);
+    g = GraphWU( ...
+        'ID', ['g ' sub.get('ID')], ...
+        'BRAINATLAS', atlas, ...
+        'B', Callback('EL', sub, 'TAG', 'CON') ...
+        );
+    
+    if string(nnd.get('INPUT_TYPE')) == "adjacency_matrices"
+        input = g.get('A'); 
+
+    elseif string(nnd.get('INPUT_TYPE')) == "graph_measures"
+        input_nodal = [];
+        input_binodal = [];
+        input_global = [];
+        mlist = nnd.get('MEASURES');
+        for j = 1:length(mlist)
+            if Measure.is_nodal(mlist{j})
+                input_nodal = [input_nodal; cell2mat(g.getMeasure(mlist{j}).get('M'))];
+            elseif Measure.is_global(mlist{j})
+                input_global = [input_global; cell2mat(g.getMeasure(mlist{j}).get('M'))];
+            else
+                input_binodal = [input_binodal; cell2mat(g.getMeasure(mlist{j}).get('M'))];
+            end
+        end
+        input_nodal(isnan(input_nodal)) = 0;
+        input_binodal(isnan(input_binodal)) = 0;
+        input_global(isnan(input_global)) = 0;
+        input = {input_nodal input_global input_binodal};
+    end
+
+    nn_sub = NNSubject( ...
+        'ID', [sub.get('ID') ' in ' gr.get('ID')], ...
+        'BA', atlas, ...
+        'age', sub.get('age'), ...
+        'sex', sub.get('sex'), ...
+        'G', g, ...
+        'INPUT', input, ...
+        'TARGET', gr.get('ID') ...
+        );
+    nn_sub_dict.add(nn_sub);
+end
+nn_gr.set('sub_dict', nn_sub_dict);
+
+value = nn_gr;
 
 %% ¡tests!
 %%% ¡test!
