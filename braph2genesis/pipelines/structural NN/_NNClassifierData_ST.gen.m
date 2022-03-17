9e62658df216f351225c68f0288aaa275d3fb0f8
Yu-Wei Chang <hejyuwei@gmail.com>
2022-03-18 00:03:51 +0100

    classification for CON, FUN and ST

--- braph2genesis/pipelines/structural NN/_NNClassifierData_ST.gen.m
+++ braph2genesis/pipelines/structural NN/_NNClassifierData_ST.gen.m
@@ -2,21 +2,19 @@
 NNClassifierData_ST < NNClassifierData (nnd, data of a neural network classifier with structural data) produces a dataset to a neural netowrk classifier using structural data. 
 
 %% ¡description!
-This dataset can be used to train or test a neural network classifier. The
-INPUTS and TARGETS are the two cells for trianing a neurla network. The INPUTS 
-cell is obtained direcly from structural data, and it is represented as 
-floating point feature cell. Feature selection procedure can be implemented 
-when constructing the INPUTS. The TARGETS cell holds the target values for 
-the trianing samples. 
+This dataset produces NN groups with structural data.
+The dataset produces NN groups for trianing or testing a neurla network
+with structural data. 
 
 %% ¡props_update!
+
 %%% ¡prop!
-GR1 (data, item) is the subject group 1, which also defines the subject class SubjectST.
+GR1 (data, item) is a group of subjects defined as SubjectST class.
 %%%% ¡default!
 Group('SUB_CLASS', 'SubjectST')
 
 %%% ¡prop!
-GR2 (data, item) is the subject group 2, which also defines the subject class SubjectST.
+GR2 (data, item) is a group of subjects defined as SubjectST class.
 %%%% ¡default!
 Group('SUB_CLASS', 'SubjectST')
 
@@ -26,66 +24,94 @@ INPUT_TYPE (data, option) is the input type for training or testing the NN.
 'structural_data'
 
 %%% ¡prop!
-FEATURE_MASK_ANALYSIS (result, cvector) is an analysis for generating mask for selected features.
+NN_GR1 (result, group) is a group of NN subjects.
+%%%% ¡settings!
+'NNGroup'
+%%%% ¡default!
+NNGroup('SUB_CLASS', 'NNSubject', 'SUB_DICT', IndexedDictionary('IT_CLASS', 'NNSubject'))
 %%%% ¡calculate!
-density = nnd.get('FEATURE_MASK');
-data1 = nnd.data_construction(nnd.get('TRAIN_GR1'));
-data2 = nnd.data_construction(nnd.get('TRAIN_GR2'));
-data = [data1 data2];
+gr = nnd.get('GR1');
+nn_gr = NNGroup( ...
+    'SUB_CLASS', 'NNSubject', ...
+    'SUB_DICT', IndexedDictionary('IT_CLASS', 'NNSubject') ...
+    );
 
-if(isempty(data))
-    value = [];
-else
-    y = nnd.get('TARGETS');
-    y = y{1};
-    for j = 1:size(data{1},1)
-        for k = 1:size(data{1},2)
-            data_per_feature = cellfun(@(v)v(j, k), data);
-            label = y';
-            mask(j, k) = nnd.mutual_information_analysis(data_per_feature, label, 5);
-        end
-    end
-    [~,idx_all] = sort(mask(:), 'descend');
-    num_top_idx = floor(density * size(mask, 1) * size(mask, 2));
+nn_gr.lock('SUB_CLASS');
 
-    value = idx_all(1:num_top_idx);
+nn_gr.set( ...
+    'ID', gr.get('ID'), ...
+    'LABEL', gr.get('LABEL'), ...
+    'NOTES', gr.get('NOTES') ...
+    );
+
+atlas = BrainAtlas();
+if ~isempty(gr) && ~isa(gr, 'NoValue') && gr.get('SUB_DICT').length > 0 
+    atlas = gr.get('SUB_DICT').getItem(1).get('BA');
 end
+
+nn_sub_dict = nn_gr.get('SUB_DICT');
+
+for i = 1:1:gr.get('SUB_DICT').length()
+	input = gr.get('SUB_DICT').getItem(i);
+    nn_sub = NNSubject( ...
+        'ID', [sub.get('ID') ' in ' gr.get('ID')], ...
+        'BA', atlas, ...
+        'age', sub.get('age'), ...
+        'sex', sub.get('sex'), ...
+        'G', g, ...
+        'INPUT', input, ...
+        'TARGET', gr.get('ID') ...
+        );
+    nn_sub_dict.add(nn_sub);
+end
+nn_gr.set('sub_dict', nn_sub_dict);
+
+value = nn_gr;
 
 %%% ¡prop!
-INPUTS (result, cell) is the inputs for training or testing a neural network.
+NN_GR2 (result, group) is a group of NN subjects.
+%%%% ¡settings!
+'NNGroup'
+%%%% ¡default!
+NNGroup('SUB_CLASS', 'NNSubject', 'SUB_DICT', IndexedDictionary('IT_CLASS', 'NNSubject'))
 %%%% ¡calculate!
-mask = nnd.get('FEATURE_MASK');
-if length(mask) == 1 && abs(mask) <= 1
-    mask = nnd.get('FEATURE_MASK_ANALYSIS');
-end
-value = nnd.input_construction(nnd.get('TRAIN_GR1'), nnd.get('TRAIN_GR2'), mask);
+gr = nnd.get('GR2');
+nn_gr = NNGroup( ...
+    'SUB_CLASS', 'NNSubject', ...
+    'SUB_DICT', IndexedDictionary('IT_CLASS', 'NNSubject') ...
+    );
 
-%%% ¡prop!
-VAL_INPUTS (result, cell) is the inputs from validation set for testing a neural network.
-%%%% ¡calculate!
-mask = nnd.get('FEATURE_MASK');
-if length(mask) == 1 && abs(mask) <= 1
-    mask = nnd.get('FEATURE_MASK_ANALYSIS');
-end
-value = nnd.input_construction(nnd.get('VAL_GR1'), nnd.get('VAL_GR2'), mask);
+nn_gr.lock('SUB_CLASS');
 
-%% ¡methods!
-function inputs = input_construction(nnd, gr1, gr2, mask)
-    %INPUT_CONSTRUCTION constructs the inputs for neural networks.
-    % 
-    % INPUTS = INPUT_CONSTRUCTION(NN, GR1, GR2, MASK) 
-    %  constructs the input for training or testing neural networks. The 
-    %  features for each subject will be extracted from thu SUB_DICT in 
-    %  GR1 and GR2 the group. Then the extracted features will be masked by MASK the 
-    %  feature mask. The selected features will construct the eventual 
-    %  inputs INPUTS for the neural network.
-    
-    data_gr1 = nnd.data_construction(gr1);  
-    data_gr2 = nnd.data_construction(gr2);  
-    data_gr = [data_gr1 data_gr2];
-    inputs = cellfun(@(v)v(mask), data_gr, 'UniformOutput', false);
-    inputs = {cat(2, inputs{:})};
+nn_gr.set( ...
+    'ID', gr.get('ID'), ...
+    'LABEL', gr.get('LABEL'), ...
+    'NOTES', gr.get('NOTES') ...
+    );
+
+atlas = BrainAtlas();
+if ~isempty(gr) && ~isa(gr, 'NoValue') && gr.get('SUB_DICT').length > 0 
+    atlas = gr.get('SUB_DICT').getItem(1).get('BA');
 end
+
+nn_sub_dict = nn_gr.get('SUB_DICT');
+
+for i = 1:1:gr.get('SUB_DICT').length()
+	input = gr.get('SUB_DICT').getItem(i);
+    nn_sub = NNSubject( ...
+        'ID', [sub.get('ID') ' in ' gr.get('ID')], ...
+        'BA', atlas, ...
+        'age', sub.get('age'), ...
+        'sex', sub.get('sex'), ...
+        'G', g, ...
+        'INPUT', input, ...
+        'TARGET', gr.get('ID') ...
+        );
+    nn_sub_dict.add(nn_sub);
+end
+nn_gr.set('sub_dict', nn_sub_dict);
+
+value = nn_gr;
 
 %% ¡tests!
 %%% ¡test!
