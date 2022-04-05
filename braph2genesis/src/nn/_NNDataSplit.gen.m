2882851729195dae62f2c5fab4cd912ea25bc374
c-yuwei <hejyuwei@gmail.com>
2022-04-05 10:15:10 +0200

    moniker correction

--- braph2genesis/src/nn/_NNDataSplit.gen.m
+++ braph2genesis/src/nn/_NNDataSplit.gen.m
@@ -53,7 +53,7 @@ GR_TRAIN_FS (result, item) is a group of NN subjects with feature mask for the t
 %%%% ¡default!
 NNGroup('SUB_CLASS', 'NNSubject', 'SUB_DICT', IndexedDictionary('IT_CLASS', 'NNSubject'))
 %%%% ¡gui!
-pr = PPNNData_GR_NN('EL', nnd, 'PROP', NNDataSplit.GR_TRAIN_FS, varargin{:});
+pr = PPNNData_GR_NN('EL', nnds, 'PROP', NNDataSplit.GR_TRAIN_FS, varargin{:});
 
 %%% ¡prop!
 GR_VAL_FS (result, item) is a group of NN subjects with feature mask for the validation set.
@@ -62,7 +62,7 @@ GR_VAL_FS (result, item) is a group of NN subjects with feature mask for the val
 %%%% ¡default!
 NNGroup('SUB_CLASS', 'NNSubject', 'SUB_DICT', IndexedDictionary('IT_CLASS', 'NNSubject'))
 %%%% ¡gui!
-pr = PPNNData_GR_NN('EL', nnd, 'PROP', NNDataSplit.GR_VAL_FS, varargin{:});
+pr = PPNNData_GR_NN('EL', nnds, 'PROP', NNDataSplit.GR_VAL_FS, varargin{:});
 
 %% ¡methods!
 function score = mutual_information_analysis(nnds, X, Y, n)
