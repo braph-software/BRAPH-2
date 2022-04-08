58a75b895e64740f3818f7637ae0fe74f3bc553b
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2022-04-08 14:20:58 +0200

    Update _ETC.gen.m

--- Braph2 final/braph2genesis/src/ds/_ETC.gen.m
+++ Braph2 final/braph2genesis/src/ds/_ETC.gen.m
@@ -2,7 +2,7 @@
 ETC < Element (et, test Element C) tests props.
 
 %%% ¡description!
-Tests conditioning, postprocessing, and checking multiple propos at once.
+ETC tests conditioning.
 
 %%% ¡seealso!
 Element, Category, Format
@@ -17,41 +17,66 @@ value = value(2);
 [0:1:10]
 
 %%% ¡prop!
-SM (metadata, string) is a scalar, metadata.
-%%%% ¡conditioning!
-value = tostring(value);
-
-%%% ¡prop!
 NP (parameter, scalar) is a scalar, parameter.
 %%%% ¡conditioning!
 value = value(3);
 %%%% ¡default!
 [0:1:10]
 
+%%% ¡prop!
+ND (data, scalar) is a scalar, data.
+%%%% ¡conditioning!
+value = value(4);
+%%%% ¡default!
+[0:1:10]
 
-% % % %%% ¡prop!
-% % % ND (data, scalar) is a scalar, data.
-% % % 
-% % % %%% ¡prop!
-% % % NG (gui, scalar) is a scalar, gui.
-% % % 
-% % % %%% ¡prop!
-% % % NF (figure, scalar) is a scalar, figure.
-% % % 
-% % % %%% ¡prop!
-% % % NR (result, scalar) is a scalar, result.
-% % % 
-% % % %%% ¡prop!
-% % % SP (parameter, string) is a scalar, parameter.
-% % % 
-% % % %%% ¡prop!
-% % % SD (data, string) is a scalar, data.
-% % % 
-% % % %%% ¡prop!
-% % % SR (result, string) is a scalar, result.
-% % % 
-% % % %%% ¡prop!
-% % % SG (gui, string) is a scalar, gui.
-% % % 
-% % % %%% ¡prop!
-% % % SF (figure, string) is a scalar, figure.
+%%% ¡prop!
+NG (gui, scalar) is a scalar, gui.
+%%%% ¡conditioning!
+value = value(5);
+%%%% ¡default!
+[0:1:10]
+
+%%% ¡prop!
+NF (figure, scalar) is a scalar, figure.
+%%%% ¡conditioning!
+value = value(6);
+%%%% ¡default!
+[0:1:10]
+
+%%% ¡prop!
+NR (result, scalar) is a scalar, result.
+%%%% ¡conditioning!
+value = value(7);
+%%%% ¡default!
+[0:1:10]
+
+%%% ¡prop!
+SM (metadata, string) is a scalar, metadata.
+%%%% ¡conditioning!
+value = tostring(value);
+
+%%% ¡prop!
+SP (parameter, string) is a scalar, parameter.
+%%%% ¡conditioning!
+value = tostring(value);
+
+%%% ¡prop!
+SD (data, string) is a scalar, data.
+%%%% ¡conditioning!
+value = tostring(value);
+
+%%% ¡prop!
+SG (gui, string) is a scalar, gui.
+%%%% ¡conditioning!
+value = tostring(value);
+
+%%% ¡prop!
+SF (figure, string) is a scalar, figure.
+%%%% ¡conditioning!
+value = tostring(value);
+
+%%% ¡prop!
+SR (result, string) is a scalar, result.
+%%%% ¡conditioning!
+value = tostring(value);
