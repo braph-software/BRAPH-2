b30b71a678e95a2b26ed769625d87995672bf414
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2022-04-08 11:40:56 +0200

    ETC

---
+++ Braph2 final/braph2genesis/src/ds/_ETC.gen.m
@@ -0,0 +1,50 @@
+%% ¡header!
+ETC < Element (et, test Element C) tests props.
+
+%%% ¡description!
+Tests conditioning, postprocessing, and checking multiple propos at once.
+
+%%% ¡seealso!
+Element, Category, Format
+
+%% ¡props!
+
+%%% ¡prop!
+NM (metadata, scalar) is a scalar, metadata.
+%%%% ¡conditioning!
+value = value(2);
+%%%% ¡default!
+[1 2 3]
+
+%%% ¡prop!
+NP (parameter, scalar) is a scalar, parameter.
+
+%%% ¡prop!
+ND (data, scalar) is a scalar, data.
+
+%%% ¡prop!
+NR (result, scalar) is a scalar, result.
+
+%%% ¡prop!
+NG (gui, scalar) is a scalar, gui.
+
+%%% ¡prop!
+NF (figure, scalar) is a scalar, figure.
+
+%%% ¡prop!
+SM (metadata, string) is a scalar, metadata.
+
+%%% ¡prop!
+SP (parameter, string) is a scalar, parameter.
+
+%%% ¡prop!
+SD (data, string) is a scalar, data.
+
+%%% ¡prop!
+SR (result, string) is a scalar, result.
+
+%%% ¡prop!
+SG (gui, string) is a scalar, gui.
+
+%%% ¡prop!
+SF (figure, string) is a scalar, figure.
