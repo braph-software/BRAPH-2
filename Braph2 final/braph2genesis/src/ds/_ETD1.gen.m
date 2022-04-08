ea9251fa281a94a857039f8835d640c0febb6ff4
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2022-04-08 22:05:40 +0200

    ETDx

--- Braph2 final/braph2genesis/src/ds/_ETD1.gen.m
+++ Braph2 final/braph2genesis/src/ds/_ETD1.gen.m
@@ -1,11 +1,11 @@
 %% ¡header!
-ETD < Element (et, test Element D) tests multiple values with checks.
+ETD1 < Element (et, test Element D1) tests multiple values with checks.
 
 %%% ¡description!
-ETD tests multiple values with checks.
+ETD1 tests multiple values with checks.
 
 %%% ¡seealso!
-Element, Category, Format, ETE
+Element, Category, Format, ETD2
 
 %% ¡props!
 
@@ -45,22 +45,22 @@ end
 %%%% ¡name!
 Multiple values
 %%%% ¡code!
-et = ETD();
+et = ETD1();
 
 assert_with_error("varargin{1}.set('UNO', 1)", ...
-    [BRAPH2.STR ':ETD:' BRAPH2.WRONG_INPUT], ...
+    [BRAPH2.STR ':ETD1:' BRAPH2.WRONG_INPUT], ...
     et)
 
 assert_with_error("varargin{1}.set('DUE', 2)", ...
-    [BRAPH2.STR ':ETD:' BRAPH2.WRONG_INPUT], ...
+    [BRAPH2.STR ':ETD1:' BRAPH2.WRONG_INPUT], ...
     et)
 
 assert_with_error("varargin{1}.set('TRE', 3)", ...
-    [BRAPH2.STR ':ETD:' BRAPH2.WRONG_INPUT], ...
+    [BRAPH2.STR ':ETD1:' BRAPH2.WRONG_INPUT], ...
     et)
 
 assert_with_error("varargin{1}.set('UNO', 1, 'DUE', 2, 'TRE', 3)", ...
-    [BRAPH2.STR ':ETD:' BRAPH2.WRONG_INPUT], ...
+    [BRAPH2.STR ':ETD1:' BRAPH2.WRONG_INPUT], ...
     et)
 
 et.set('UNO', 3.14, 'DUE', 3.14, 'TRE', 3.14)
