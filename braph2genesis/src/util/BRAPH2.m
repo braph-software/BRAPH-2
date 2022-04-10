7ae6ad00f4679eb5933f0013ebf8e6619641c05d
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2022-04-10 02:56:21 +0200

    Update BRAPH2.m

--- braph2genesis/src/util/BRAPH2.m
+++ braph2genesis/src/util/BRAPH2.m
@@ -5,58 +5,65 @@ classdef BRAPH2
     % Properties (Constant) - BRAPH2 ID Card:
     %  NAME         - BRAPH2 full name
     %  VERSION      - BRAPH2 version
-    %  WEB          - BRAPH2 website
-    %  STR          - BRAPH2 sort string
+    %  STR          - BRAPH2 short string
     %  BUILD        - BRAPH2 build number
     %  AUTHORS      - BRAPH2 authors
     %  COPYRIGHT    - BRAPH2 copyright
+    %  WEB          - BRAPH2 website
+    %  TWITTER      - BRAPH2 twitter handle
     %
     % Properties (Constant) - BRAPH2 extensions
     %  EXT_ELEMENT  - BRAPH2 element extension (*.b2)
     %  EXT_PIPELINE - BRAPH2 pipeline extension (*.braph2)
-    %  EXT_WORKSPACE - BRAPH2 workspace extension (*.mat)
     %
     % Properties (Constant) - BRAPH2 error codes:
+	%  ADDON        - addon not installed
     %  WRONG_INPUT  - wrong input value or format in a function
     %  WRONG_OUTPUT - wrong output value or format in a function
     %  BUG_FUNC     - bug in a function (wrong behavior)
     %  BUG_COPY     - bug while deep-copying an object
     %  BUG_CLONE    - bug while cloning an object
-    %  BUG_IO       - bug while loading/saving
+    %  BUG_IO       - bug while loading/saving files
     %  BUG_JSON     - bug in JSON encoding/deconding
     %  BUG_ERR      - bug in the handling of errors
-    %  IM_ERR       - error while importing a file
-    %  EX_ERR       - error while exporting a file
     %
     % Properties (Constant) - BRAPH2 GUI constants:
     %  FONTUNITS    - sets the units of the font
     %  FONTSIZE     - sets the size of the font
-    %  COL_FIG      - figure background color
+    %  COL          - official BRAPH2 color
+    %  COL_FIG      - standard figure background color
+    %  COL_M        - standard metadata prop background color
+    %  COL_P        - standard parameter prop background color
+    %  COL_D        - standard data prop background color
+    %  COL_R        - standard result prop background color
+    %  COL_F        - standard figure prop background color
+    %  COL_G        - standard gui prop background color
     %
     % Methods (Static) - BRAPH2 info panels:
-    %  credits        - provides information about the authors of BRAPH2
+    %  credits      - provides information about the authors of BRAPH2
     %  license      - provides information about the license of BRAPH2
     %  web          - opens the BRAPH2 website
     %  forum        - opens the BRAPH2 forum
     %  twitter      - opens the BRAPH2 Twitter
-    %  add_menu_about - adds the menu about to a figure
+    %  add_menu_about - adds the about menu to a figure
     %  add_tool_about - adds the about tools to a toolbar
     
     properties (Constant) % BRAPH2 ID Card
         NAME = 'Braph 2.0' % BRAPH2 full name
         VERSION = '2.0' % BRAPH2 version
-        WEB = 'braph.org' % BRAPH2 website
-        STR = 'BRAPH2' % BRAPH2 sort string
+        STR = 'BRAPH2' % BRAPH2 short string
         BUILD = 1 % BRAPH2 build number
         AUTHORS = 'Giovanni Volpe, Emiliano Gomez Ruiz, Anna Canal Garcia, Yu-Wei Chang, Mite Mijalkov, Joana Braga Pereira, Ehsan Kakaei, Eric Westman, et al.'
         COPYRIGHT = ['Copyright 2014-' datestr(now,'yyyy')]
+        WEB = 'braph.org' % BRAPH2 website
+        TWITTER = 'braph2software' % BRAPH2 twitter handle
     end
     properties (Constant) % BRAPH2 extensions
         EXT_ELEMENT = {'*.b2'} % BRAPH2 element extension
         EXT_PIPELINE = {'*.braph2'} % BRAPH2 pipeline extension
-        EXT_WORKSPACE = {'*.b2ws'} % BRAPH2 workspace extension % FIXME: keep? eliminate? change to .mat?
     end
     properties (Constant) % BRAPH2 error codes
+        ADDON = 'AddOn' % addon not installed
         WRONG_INPUT = 'WrongInput' % wrong input value or format in a function
         WRONG_OUTPUT = 'WrongOutput' % wrong output value or format in a function
         BUG_FUNC = 'BugFunc' % bug in a function (wrong behavior)
@@ -65,76 +72,79 @@ classdef BRAPH2
         BUG_IO = 'BugIO' % bug while loading/saving files
         BUG_JSON = 'BugJSON' % bug in JSON encoding/deconding
         BUG_ERR = 'BugErr' % bug in the handling of errors
-        IM_ERR = 'ImporterError. Most likely wrong file format.' % error while importing a file
-        EX_ERR = 'ExporterError.' % error while exporting a file
     end
     properties (Constant) % BRAPH2 GUI constants
         FONTUNITS = 'points'
         FONTSIZE = 12
-        COL_FIG = [1 .9725 .929] % figure background color
-        COL_M = [.20 .50 .80] % standard metadata background color % [.745 .839 .902]
-        COL_P = [.40 .50 .60] % standard parameter background color % [.38 .514 .6]
-        COL_D = [.60 .50 .40] % standard data background color % [.902 .835 .745]
-        COL_R = [.80 .50 .20] % standard result background color % [.62 .545 .439]
+        COL = [.9 .4 .1] % official BRAPH2 color
+        COL_FIG = [1 .9725 .929] % standard figure background color
+        COL_M = [.20 .50 .80] % standard metadata prop background color
+        COL_P = [.40 .50 .60] % standard parameter prop background color
+        COL_D = [.60 .50 .40] % standard data prop background color
+        COL_R = [.80 .50 .20] % standard result prop background color
+        COL_F = [.25 .50 .75] % standard figure prop background color
+        COL_G = [.30 .50 .70] % standard gui prop background color
     end
     methods (Static)
         function credits()
             %CREDITS provides information about the authors of BRAPH2.
             %
-            % See also license.
+            % See also license, braph2msgbox.
             
-            CreateStruct.WindowStyle = 'modal';
-            CreateStruct.Interpreter = 'tex';
-            h = msgbox({'' ...
+            title = ['About ' BRAPH2.STR];
+            
+            message = {'' ...
                 ['{\bf\color{orange}' BRAPH2.STR '}'] ...
                 ['{\color{gray}version ' BRAPH2.VERSION '}'] ...
-                ['{\color{gray}' BRAPH2.WEB '}'] ...
+                ['{\color{gray}build ' int2str(BRAPH2.BUILD) '}'] ...
                 '' ...
                 BRAPH2.AUTHORS ...
                 '' ...
+                ['{\color{gray}' BRAPH2.WEB '}'] ...
+                '' ...
                 ['{\color{gray}' BRAPH2.COPYRIGHT '}'] ...
                 '' ...
-                ''}, ...
-                ['About ' BRAPH2.STR], ...
-                CreateStruct);
-            set_braph2_icon(h)
+                ''};
+            
+            braph2msgbox(title, message)
         end
         function license()
             %LICENSE provides information about the license of BRAPH2.
             %
-            % See also about.
+            % See also about, braph2msgbox.
             
-            CreateStruct.WindowStyle = 'modal';
-            CreateStruct.Interpreter = 'tex';
-            h = msgbox({'' ...
+            title = [BRAPH2.STR ' License'];
+            
+            message = {'' ...
                 ['{\bf\color{orange}' BRAPH2.STR '}'] ...
                 ['{\color{gray}version ' BRAPH2.VERSION '}'] ...
-                ['{\color{gray}' BRAPH2.WEB '}'] ...
+                ['{\color{gray}build ' int2str(BRAPH2.BUILD) '}'] ...
                 '' ...
                 'The license is available in the file {\bfbraph2license.rtf} on the main directory of BRAPH2.' ...
                 'In brief, BRAPH2 is provided as is and is free to use for research.' ...
                 '' ...
+                ['{\color{gray}' BRAPH2.WEB '}'] ...
+                '' ...
                 ['{\color{gray}' BRAPH2.COPYRIGHT '}'] ...
                 '' ...
-                ''}, ...
-                [BRAPH2.STR ' License'], ...
-                CreateStruct);
-            set_braph2_icon(h)
+                ''};
+            
+            braph2msgbox(title, message)
         end
         function web()
             %WEB opens the BRAPH2 website.
             %
             % See also forum, twitter.
             
-            url = 'http://braph.org/'; % FIXME: finalize once web ready
+            url = ['https://' BRAPH2.WEB];
             web(url);
         end
         function forum()
-            %FORUM opens the BRAPH2 forum.
+            %FORUM opens the BRAPH2 forums.
             %
             % See also web, twitter.
             
-            url = 'http://braph.org/forum/'; % FIXME: finalize once web ready
+            url = ['https://' BRAPH2.WEB '/forums/'];
             web(url);
         end
         function twitter()
@@ -142,11 +152,11 @@ classdef BRAPH2
             %
             % See also web, forum.
             
-            url = 'http://twitter.com/braph2software'; % FIXME: finalize once web ready
+            url = ['https://twitter.com/' BRAPH2.TWITTER];
             web(url);
         end
         function menu_about = add_menu_about(f)
-            %ADD_MENU_ABOUT adds the menu about to a figure.
+            %ADD_MENU_ABOUT adds the about menu to a figure.
             %
             % MENU_ABOUT = ADD_MENU_ABOUT(F) adds the menu about to the figure F
             %  and returns its handle MENU_ABOUT.
@@ -156,7 +166,7 @@ classdef BRAPH2
                 'Label', 'BRAPH.org ...', ...
                 'Callback', 'BRAPH2.web()')
             uimenu(menu_about, ...
-                'Label', 'Forum...', ...
+                'Label', 'Forums ...', ...
                 'Callback', 'BRAPH2.forum()')
             uimenu(menu_about, ...
                 'Label', 'Twitter ...', ...
@@ -174,13 +184,12 @@ classdef BRAPH2
             % ADD_TOOL_ABOUT(TOOLBAR) adds the about tools to TOOLBAR.
             %
             % The tool buttons are findable and accessible through their tags:
-            %  tool = findall(gui.toolbar, 'Tag', 'BRAPH2.Web');
-            %  tool = findall(gui.toolbar, 'BRAPH2.Forum');
-            %  tool = findall(gui.toolbar, 'BRAPH2.Twitter');
-            %  tool = findall(gui.toolbar, 'BRAPH2.License');
-            %  tool = findall(gui.toolbar, 'BRAPH2.Credits');
+            %  TOOL = findall(TOOLBAR, 'Tag', 'BRAPH2.Web');
+            %  TOOL = findall(TOOLBAR, 'Tag', 'BRAPH2.Forum');
+            %  TOOL = findall(TOOLBAR, 'Tag', 'BRAPH2.Twitter');
+            %  TOOL = findall(TOOLBAR, 'Tag', 'BRAPH2.License');
+            %  TOOL = findall(TOOLBAR, 'Tag', 'BRAPH2.Credits');
             
-            uipushtool(toolbar, 'Separator', 'on', 'Visible', 'off')
             uipushtool(toolbar, 'Separator', 'on', 'Visible', 'off')
             
             % Website
@@ -195,7 +204,7 @@ classdef BRAPH2
             uipushtool(toolbar, ...
                 'Tag', 'BRAPH2.Forum', ...                
                 'Separator', 'off', ...
-                'TooltipString', 'Link to the BRAPH 2.0 forum', ...
+                'TooltipString', 'Link to the BRAPH 2.0 forums', ...
                 'CData', imresize(imread('icon_forum.png'), [24 24]), ...
                 'ClickedCallback', 'BRAPH2.forum()');
             
@@ -207,7 +216,6 @@ classdef BRAPH2
                 'CData', imresize(imread('icon_twitter.png'), [24 24]), ...
                 'ClickedCallback', 'BRAPH2.twitter()');
             
-            uipushtool(toolbar, 'Separator', 'on', 'Visible', 'off')
             uipushtool(toolbar, 'Separator', 'on', 'Visible', 'off')
             
             % License
