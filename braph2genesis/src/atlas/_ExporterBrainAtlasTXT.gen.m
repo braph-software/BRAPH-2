48bfebf71eb1ae1824230dd8955fe5d5eea03d7b
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2022-04-13 00:09:32 +0200

    Update _ExporterBrainAtlasTXT.gen.m

--- braph2genesis/src/atlas/_ExporterBrainAtlasTXT.gen.m
+++ braph2genesis/src/atlas/_ExporterBrainAtlasTXT.gen.m
@@ -119,8 +119,12 @@ end
 %%% ¡test!
 %%%% ¡name!
 Delete file TBE
+%%%% ¡probability!
+1
 %%%% ¡code!
+warning('off', 'MATLAB:DELETE:FileNotFound')
 delete([fileparts(which('test_braph2')) filesep 'default_txt_file_to_save_brain_atlas_most_likely_to_be_erased.txt'])
+warning('on', 'MATLAB:DELETE:FileNotFound')
 
 %%% ¡test!
 %%%% ¡name!
