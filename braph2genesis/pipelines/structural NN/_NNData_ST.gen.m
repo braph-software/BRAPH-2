b4262f98b5bf249f8a817bbf2abf1f0cdc9f8f4f
Yu-Wei Chang <hejyuwei@gmail.com>
2022-04-14 14:20:10 +0200

    fix the format typo in all nn data

--- braph2genesis/pipelines/structural NN/_NNData_ST.gen.m
+++ braph2genesis/pipelines/structural NN/_NNData_ST.gen.m
@@ -21,7 +21,7 @@ INPUT_TYPE (data, option) is the input type for training or testing the NN.
 'structural_data'
 
 %%% ¡prop!
-GR_NN (result, group) is a group of NN subjects.
+GR_NN (result, item) is a group of NN subjects.
 %%%% ¡settings!
 'NNGroup'
 %%%% ¡default!
