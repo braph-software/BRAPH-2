fc59ac66064866dcdc20181f45d6154a354f17bd
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2022-04-15 09:39:59 +0200

    Update _ETF3.gen.m

--- braph2genesis/src/ds/_ETF3.gen.m
+++ braph2genesis/src/ds/_ETF3.gen.m
@@ -55,7 +55,7 @@ assert(isa(et1.get('IT22'), 'ETC2'))
 assert(isa(et1.get('IT23'), 'ETC3'))
 assert(isa(et1.get('IT33'), 'ETC3'))
 
-et2 = ETF1();
+et2 = ETF2();
 assert(isa(et2.get('IT11'), 'ETC1'))
 assert(isa(et2.get('IT12'), 'ETC2'))
 assert(isa(et2.get('IT13'), 'ETC3'))
@@ -63,7 +63,7 @@ assert(isa(et2.get('IT22'), 'ETC2'))
 assert(isa(et2.get('IT23'), 'ETC3'))
 assert(isa(et2.get('IT33'), 'ETC3'))
 
-et3 = ETF2();
+et3 = ETF3();
 assert(isa(et3.get('IT11'), 'ETC3'))
 assert(isa(et3.get('IT12'), 'ETC3'))
 assert(isa(et3.get('IT13'), 'ETC3'))
