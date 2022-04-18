d1a1c163153ff7a5ec84a9c74b8837fe4263406c
Yu-Wei Chang <hejyuwei@gmail.com>
2022-04-18 20:29:15 +0200

    check NN toolbox before test_ETG

--- braph2genesis/src/ds/_ETG.gen.m
+++ braph2genesis/src/ds/_ETG.gen.m
@@ -29,66 +29,69 @@ Test classification model for JSON encoding and decoding
 %%%% ¡probability!
 .1
 %%%% ¡code!
-% create neural network objects
-size_x = 28;
-size_y = 28;
-num_channel = 1;
-num_neuron = 100;
-num_class = 10;
-layers = [
-    imageInputLayer([size_x size_y num_channel], 'Name', 'input', 'Mean', rand(size_x, size_y, num_channel))
-    fullyConnectedLayer(num_neuron, 'Name', 'fc1', 'Weights', rand(num_neuron, size_x * size_y), 'Bias', rand(num_neuron, 1))
-    fullyConnectedLayer(num_class, 'Name', 'fc2', 'Weights', rand(num_class, num_neuron), 'Bias', rand(num_class, 1))
-    softmaxLayer('Name', 'softmax')
-    classificationLayer('Classes', categorical(1:num_class), 'Name', 'classOutput')
-    ];
-net_series = SeriesNetwork(layers);
-lgraph = layerGraph(layers);
-net_dag = assembleNetwork(lgraph);
-lgraph = removeLayers(lgraph, 'classOutput');
-net_dl = dlnetwork(lgraph); 
+if BRAPH2.installed('NN', 'warning') && BRAPH2.insatlled('ONNXCONVERTER', 'warning')
+    % create neural network objects
+    size_x = 28;
+    size_y = 28;
+    num_channel = 1;
+    num_neuron = 100;
+    num_class = 10;
+    layers = [
+        imageInputLayer([size_x size_y num_channel], 'Name', 'input', 'Mean', rand(size_x, size_y, num_channel))
+        fullyConnectedLayer(num_neuron, 'Name', 'fc1', 'Weights', rand(num_neuron, size_x * size_y), 'Bias', rand(num_neuron, 1))
+        fullyConnectedLayer(num_class, 'Name', 'fc2', 'Weights', rand(num_class, num_neuron), 'Bias', rand(num_class, 1))
+        softmaxLayer('Name', 'softmax')
+        classificationLayer('Classes', categorical(1:num_class), 'Name', 'classOutput')
+        ];
+    net_series = SeriesNetwork(layers);
+    lgraph = layerGraph(layers);
+    net_dag = assembleNetwork(lgraph);
+    lgraph = removeLayers(lgraph, 'classOutput');
+    net_dl = dlnetwork(lgraph);
 
-etg = ETG( ...
-    'MODEL_SERIES', net_series, ...
-    'MODEL_DAG', net_dag, ...
-    'MODEL_DL', net_dl, ...
-    'MODEL_NETWORK', network() ...
-    );
+    etg = ETG( ...
+        'MODEL_SERIES', net_series, ...
+        'MODEL_DAG', net_dag, ...
+        'MODEL_DL', net_dl, ...
+        'MODEL_NETWORK', network() ...
+        );
 
-[json, struct, el_list] = encodeJSON(etg);
-[etg_dec, struct_dec, el_list_dec] = Element.decodeJSON(json);
-
+    [json, struct, el_list] = encodeJSON(etg);
+    [etg_dec, struct_dec, el_list_dec] = Element.decodeJSON(json);
+end
 %%% ¡test!
 %%%% ¡name!
 Test regression model for JSON encoding and decoding
 %%%% ¡probability!
 .1
 %%%% ¡code!
-% create neural network objects
-size_x = 28;
-size_y = 28;
-num_channel = 1;
-num_neuron = 100;
-num_class = 10;
-layers = [
-    imageInputLayer([size_x size_y num_channel], 'Name', 'input', 'Mean', rand(size_x, size_y, num_channel))
-    fullyConnectedLayer(num_neuron, 'Name', 'fc1', 'Weights', rand(num_neuron, size_x * size_y), 'Bias', rand(num_neuron, 1))
-    fullyConnectedLayer(num_class, 'Name', 'fc2', 'Weights', rand(num_class, num_neuron), 'Bias', rand(num_class, 1))
-    softmaxLayer('Name', 'softmax')
-    regressionLayer('Name', 'regressOutput')
-    ];
-net_series = SeriesNetwork(layers);
-lgraph = layerGraph(layers);
-net_dag = assembleNetwork(lgraph);
-lgraph = removeLayers(lgraph, 'regressOutput');
-net_dl = dlnetwork(lgraph); 
+if BRAPH2.installed('NN', 'warning') && BRAPH2.insatlled('ONNXCONVERTER', 'warning')
+    % create neural network objects
+    size_x = 28;
+    size_y = 28;
+    num_channel = 1;
+    num_neuron = 100;
+    num_class = 10;
+    layers = [
+        imageInputLayer([size_x size_y num_channel], 'Name', 'input', 'Mean', rand(size_x, size_y, num_channel))
+        fullyConnectedLayer(num_neuron, 'Name', 'fc1', 'Weights', rand(num_neuron, size_x * size_y), 'Bias', rand(num_neuron, 1))
+        fullyConnectedLayer(num_class, 'Name', 'fc2', 'Weights', rand(num_class, num_neuron), 'Bias', rand(num_class, 1))
+        softmaxLayer('Name', 'softmax')
+        regressionLayer('Name', 'regressOutput')
+        ];
+    net_series = SeriesNetwork(layers);
+    lgraph = layerGraph(layers);
+    net_dag = assembleNetwork(lgraph);
+    lgraph = removeLayers(lgraph, 'regressOutput');
+    net_dl = dlnetwork(lgraph);
 
-etg = ETG( ...
-    'MODEL_SERIES', net_series, ...
-    'MODEL_DAG', net_dag, ...
-    'MODEL_DL', net_dl, ...
-    'MODEL_NETWORK', network() ...
-    );
+    etg = ETG( ...
+        'MODEL_SERIES', net_series, ...
+        'MODEL_DAG', net_dag, ...
+        'MODEL_DL', net_dl, ...
+        'MODEL_NETWORK', network() ...
+        );
 
-[json, struct, el_list] = encodeJSON(etg);
-[etg_dec, struct_dec, el_list_dec] = Element.decodeJSON(json);
+    [json, struct, el_list] = encodeJSON(etg);
+    [etg_dec, struct_dec, el_list_dec] = Element.decodeJSON(json);
+end
