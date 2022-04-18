4cc915b0462e1060461f51c0b4042e78f6ea9028
Yu-Wei Chang <hejyuwei@gmail.com>
2022-04-18 15:45:57 +0200

    remove check_nn_toolboxes from NNBase

--- braph2genesis/src/nn/_NNBase.gen.m
+++ braph2genesis/src/nn/_NNBase.gen.m
@@ -22,45 +22,25 @@ GR (data, item) is a group of NN subjects containing the information for trainin
 'NNGroup'
 
 %%% ¡prop!
-MODEL (result, cell) is a trained neural network.
+MODEL (result, net) is a trained neural network.
+%%%% ¡calculate!
+value = squeezenet;
 %%%% ¡gui!
 pr = PPNNBase_Model('EL', nn, 'PROP', nn.MODEL, varargin{:});
-
-%% ¡methods!
-function installed = check_nn_toolboxes(nn)
-    %CHECK_NN_TOOLBOXES checks whether the deep-learning-required toolboxes are installed.
-    %
-    % INSTALLED = CHECK_NN_TOOLBOXES(NN) checks the installation of the toolboxes:
-    %  "Deep Learning Toolbox" and 
-    %  "Deep learning Toolbox Converter for ONNX Model Format" installation status. 
-    %  If they are not installed, then it throws a warning.
-    %  Typically, this method is only called internally when training
-    %  any subclass of the neural networks.
-    
-    addons = matlab.addons.installedAddons;
-    installed = all(ismember(["Deep Learning Toolbox"; "Deep Learning Toolbox Converter for ONNX Model Format"], addons.Name));
-    if ~installed
-        warning(['Some of the required deep-learning toolboxs are not installed. ' ...
-            'Please, refer to ' ...
-            '<a href="matlab: web(''https://se.mathworks.com/products/deep-learning.html'') ">Deep Learning Toolbox</a> ' ...
-            'and ' ...
-            '<a href="matlab: web(''https://se.mathworks.com/matlabcentral/fileexchange/67296-deep-learning-toolbox-converter-for-onnx-model-format'') ">Deep Learning Toolbox Converter for ONNX Model Format</a>'])
-    end
-end
 
 %% ¡tests!
 
 %%% ¡test!
 %%%% ¡name!
-Net import and export
+Net format verification
 %%%% ¡code!
 net = squeezenet;
 img = rand(net.Layers(1).InputSize);
 pred_from_original_net = predict(net, img);
 
-net_braph = NNBase().to_net(NNBase().from_net(squeezenet), "BCSS", "classification");
+net_braph = NNBase().get('MODEL');
 pred_from_braph = predict(net_braph, img);
 
-assert(max(abs(pred_from_braph - pred_from_original_net)) < 1E-06, ...
+assert(isequal(pred_from_original_net, pred_from_braph), ...
     [BRAPH2.STR ':NNBase:' BRAPH2.BUG_ERR], ...
-    'Prediction is not being calculated correctly for neural networks.')
+    'Prediction is not being calculated correctly for saved neural networks.')
