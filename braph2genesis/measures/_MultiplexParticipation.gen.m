604bbe20154f9d611f0144097683a25da666f018
Anna Canal Garcia <31701548+annacanal@users.noreply.github.com>
2022-04-27 15:52:03 +0200

    typos descriptions

--- braph2genesis/measures/_MultiplexParticipation.gen.m
+++ braph2genesis/measures/_MultiplexParticipation.gen.m
@@ -4,7 +4,7 @@ MultiplexParticipation < Measure (m, multiplex participation) is the graph multi
 %%% ¡description!
 The multiplex participation is the nodal homogeneity of the number of 
 neighbours of a node across the layers.
-It is calcualted as: Pi = L/(L - 1) [1 - sum_{l=1}^{L} (ki(l)/oi)^2]
+It is calculated as: Pi = L/(L - 1) [1 - sum_{l=1}^{L} (ki(l)/oi)^2]
 where L is the number of layers, ki(l) is the degree in the l-th
 layer and oi is the overlapping degree of the node.
 Pi = 1 when the degree is the same in all layers and Pi = 0 when a
