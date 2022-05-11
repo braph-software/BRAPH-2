00b5f070aa58d6fc023c986db3bb965b514d50d0
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2022-05-11 23:04:05 +0200

    Update _Panel.gen.m

--- braph2genesis/src/gui/_Panel.gen.m
+++ braph2genesis/src/gui/_Panel.gen.m
@@ -51,7 +51,7 @@ BKGCOLOR (gui, color) is the panel background color.
 BRAPH2.COL_FIG
 %%%% ¡postprocessing!
 if check_graphics(pn.p, 'uipanel') && ~isequal(get(pn.p, 'Color'), pn.get('BKGCOLOR'))
-    set(pn.p, 'Color', pn.get('BKGCOLOR'))
+    set(pn.p, 'BackgroundColor', pn.get('BKGCOLOR'))
 end
 
 %% ¡methods!
