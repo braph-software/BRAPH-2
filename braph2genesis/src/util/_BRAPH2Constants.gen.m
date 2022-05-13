9648588e4cb63a7013f8535a647c49f75a19ee17
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2022-05-13 06:14:15 +0200

    Update _BRAPH2Constants.gen.m

--- braph2genesis/src/util/_BRAPH2Constants.gen.m
+++ braph2genesis/src/util/_BRAPH2Constants.gen.m
@@ -7,53 +7,51 @@ BRAPH2
 %% ¡props!
 
 %%% ¡prop!
-FONTUNITS (metadata, option) sets the units of the font.
-%%%% ¡settings!
-{'points', 'pixels', 'normalized', 'inches', 'centimeters', 'characters'}
-%%%% ¡default!
-'points'
-
-%%% ¡prop!
-FONTSIZE (metadata, markersize) sets the size of the font.
+FONTSIZE (gui, markersize) sets the size of the font in pixels.
 %%%% ¡default!
 12
 
 %%% ¡prop!
-COL (metadata, color) is the official BRAPH2 color.
+S (gui, markersize) sets the scaling factor of the graphical components.
+%%%% ¡default!
+1
+
+%%% ¡prop!
+COL (gui, color) is the official BRAPH2 color.
 %%%% ¡default!
 [.9 .4 .1]
 
 %%% ¡prop!
-COL_FIG (metadata, color) is the standard figure background color.
+COL_FIG (gui, color) is the standard figure background color.
 %%%% ¡default!
 [1 .9725 .929]
 
 %%% ¡prop!
-COL_M (metadata, color) is the standard metadata prop background color.
+COL_M (gui, color) is the standard metadata prop background color.
 %%%% ¡default!
 [.20 .50 .80]
 
 %%% ¡prop!
-COL_P (metadata, color) is the standard parameter prop background color.
+COL_P (gui, color) is the standard parameter prop background color.
 %%%% ¡default!
 [.40 .50 .60]
 
 %%% ¡prop!
-COL_D (metadata, color) is the standard data prop background color.
+COL_D (gui, color) is the standard data prop background color.
 %%%% ¡default!
 [.60 .50 .40]
 
 %%% ¡prop!
-COL_R (metadata, color) is the standard result prop background color.
+COL_R (gui, color) is the standard result prop background color.
 %%%% ¡default!
 [.80 .50 .20]
 
 %%% ¡prop!
-COL_F (metadata, color) is the standard figure prop background color.
+COL_F (gui, color) is the standard figure prop background color.
 %%%% ¡default!
 [.25 .50 .75]
 
 %%% ¡prop!
-COL_G (metadata, color) is the standard gui prop background color.
+COL_G (gui, color) is the standard gui prop background color.
 %%%% ¡default!
 [.30 .50 .70]
