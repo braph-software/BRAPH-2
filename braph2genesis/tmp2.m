be104c6ac95ad658f4123d56f997c922e8d89941
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2022-05-18 10:41:54 +0200

    Update tmp2.m

--- braph2genesis/tmp2.m
+++ braph2genesis/tmp2.m
@@ -11,12 +11,75 @@ clc
 % test_code = ['test_' el_class]
 % eval(test_code)
 
-f = uifigure('units', 'pixels', 'position', [0 0 300 900], 'AutoResizeChildren', 'off', 'Color', 'r');
-toolbar = uitoolbar(f);
-menubar = uimenu(f);
-p = uipanel(f, 'Units', 'pixels', 'Position', [10 10 280 880-BRAPH2.FONTSIZE*6], 'BackgroundColor', 'k');
+
+f = uifigure('Visible', 'off', 'Units', 'Normalized', 'Position', [0 0 .2 .5]);
 drawnow()
-get(f, 'Position')
+set(f, 'Position', [0 0 .2 1]);
+drawnow()
+pause(1)
+h_title = h(0, 'pixels') - h(f, 'pixels')
+
+set(f, 'Position', [0 0 .2 .5]);
+menubar = uimenu(f);
+drawnow()
+set(f, 'Position', [0 0 .2 1]);
+drawnow()
+pause(1)
+h_menubar = h(0, 'pixels') - h(f, 'pixels') - h_title
+
+set(f, 'Position', [0 0 .2 .5]);
+toolbar = uitoolbar(f);
+drawnow()
+set(f, 'Position', [0 0 .2 1]);
+drawnow()
+pause(1)
+h_toolbar = h(0, 'pixels') - h(f, 'pixels') - h_title - h_menubar
+
+delete(f)
+
+
+% f = uifigure('Visible', 'off', 'Units', 'Normalized', 'Position', [0 0 .2 .8]);
+% menubar = uimenu(f);
+% toolbar = uitoolbar(f);
+% drawnow()
+% set(f, 'Position', [0 0 .2 1]);
+% drawnow()
+% pause(1)
+% h_title = h(f, 'pixels') % h(0, 'pixels') - h(f, 'pixels')
+% 
+% delete(f)
+
+
+
+% f = uifigure('Units', 'Normalized', 'Position', [0 0 .2 .8]);
+% menubar = uimenu(f);
+% toolbar = uitoolbar(f);
+% drawnow()
+% set(f, 'Position', [0 0 .2 1]);
+% drawnow()
+% pause(1)
+% h_title = h(f, 'pixels') % h(0, 'pixels') - h(f, 'pixels')
+
+
+% f = uifigure('Units', 'Normalized', 'Position', [0 0 .2 .8]);
+% pause(1)
+% h_title = h(f, 'pixels') % h(0, 'pixels') - h(f, 'pixels')
+% 
+% menubar = uimenu(f);
+% p = uipanel(f, 'Units', 'normalized', 'Position', [.01 .01 .98 .98], 'BackgroundColor', 'k');
+% pause(1)
+% h_menubar = h(f, 'pixels') % (h(0, 'pixels') - h_title) - h(f, 'pixels')
+% 
+% toolbar = uitoolbar(f);
+% pause(1)
+% h_toolbar = h(f, 'pixels') % (h(0, 'pixels') - h_title - h_menubar) - h(f, 'pixels')
+
+% f = uifigure('units', 'pixels', 'position', [0 0 300 900], 'AutoResizeChildren', 'off', 'Color', 'r');
+% toolbar = uitoolbar(f);
+% menubar = uimenu(f);
+% p = uipanel(f, 'Units', 'pixels', 'Position', [10 10 280 880-BRAPH2.FONTSIZE*6], 'BackgroundColor', 'k');
+% drawnow()
+% get(f, 'Position')
 
 
 % gui = GUI('MENUBAR', true, 'TOOLBAR', true);
