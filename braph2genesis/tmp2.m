f10df15f9925e353fe13b5ffd9e0e19c83193c9e
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2022-05-28 22:43:12 +0200

    Update tmp2.m

--- braph2genesis/tmp2.m
+++ braph2genesis/tmp2.m
@@ -11,23 +11,23 @@ clc
 % test_code = ['test_' el_class]
 % eval(test_code)
 
-br1 = BrainRegion('ID', 'id1', 'LABEL', 'label1', 'NOTES', 'notes1', 'X', 1, 'Y', 1, 'Z', 1);
-br2 = BrainRegion('ID', 'id2', 'LABEL', 'label2', 'NOTES', 'notes2', 'X', 2, 'Y', 2, 'Z', 2);
-br3 = BrainRegion('ID', 'id3', 'LABEL', 'label3', 'NOTES', 'notes3', 'X', 3, 'Y', 3, 'Z', 3);
-br4 = BrainRegion('ID', 'id4', 'LABEL', 'label4', 'NOTES', 'notes4', 'X', 4, 'Y', 4, 'Z', 4);
-br5 = BrainRegion('ID', 'id5', 'LABEL', 'label5', 'NOTES', 'notes5', 'X', 5, 'Y', 5, 'Z', 5);
-br6 = BrainRegion('ID', 'id6', 'LABEL', 'label6', 'NOTES', 'notes6', 'X', 6, 'Y', 6, 'Z', 6);
-
-items = {br1, br2, br3, br4, br5, br6};
-
-idict_1 = IndexedDictionary( ...
-    'id', 'idict', ...
-    'it_class', 'BrainRegion', ...
-    'it_key', IndexedDictionary.getPropDefault(IndexedDictionary.IT_KEY), ...
-    'it_list', items ...
-    );
-ba = BrainAtlas('ID', 'BA1', 'LABEL', 'brain atlas', 'Notes', 'Notes on brain atlas.', 'br_dict', idict_1);
-f = GUIElement('PE', ba, 'CLOSEREQ', false).draw();
+% br1 = BrainRegion('ID', 'id1', 'LABEL', 'label1', 'NOTES', 'notes1', 'X', 1, 'Y', 1, 'Z', 1);
+% br2 = BrainRegion('ID', 'id2', 'LABEL', 'label2', 'NOTES', 'notes2', 'X', 2, 'Y', 2, 'Z', 2);
+% br3 = BrainRegion('ID', 'id3', 'LABEL', 'label3', 'NOTES', 'notes3', 'X', 3, 'Y', 3, 'Z', 3);
+% br4 = BrainRegion('ID', 'id4', 'LABEL', 'label4', 'NOTES', 'notes4', 'X', 4, 'Y', 4, 'Z', 4);
+% br5 = BrainRegion('ID', 'id5', 'LABEL', 'label5', 'NOTES', 'notes5', 'X', 5, 'Y', 5, 'Z', 5);
+% br6 = BrainRegion('ID', 'id6', 'LABEL', 'label6', 'NOTES', 'notes6', 'X', 6, 'Y', 6, 'Z', 6);
+% 
+% items = {br1, br2, br3, br4, br5, br6};
+% 
+% idict_1 = IndexedDictionary( ...
+%     'id', 'idict', ...
+%     'it_class', 'BrainRegion', ...
+%     'it_key', IndexedDictionary.getPropDefault(IndexedDictionary.IT_KEY), ...
+%     'it_list', items ...
+%     );
+% ba = BrainAtlas('ID', 'BA1', 'LABEL', 'brain atlas', 'Notes', 'Notes on brain atlas.', 'br_dict', idict_1);
+% f = GUIElement('PE', ba, 'CLOSEREQ', false).draw();
 
 
 % % % el_class = 'PanelElement';
@@ -40,8 +40,6 @@ f = GUIElement('PE', ba, 'CLOSEREQ', false).draw();
 % % % 
 % % % pf = PFBrainSurface('SURF', ImporterBrainSurfaceNV('FILE', 'human_ICBM152.nv').get('SURF'));
 % % % pe = PanelElement('EL', pf, 'VISIBLE', [10 1 5 21 25]);
-% % % pe.get('VISIBLE');
-% % % pe.get('PR_DICT');
 % % % 
 % % % guis = GUIElement('PE', pe, 'MENUBAR', false, 'TOOLBAR', false, 'CLOSEREQ', false);
 % % % fs = guis.draw();
@@ -70,9 +68,9 @@ f = GUIElement('PE', ba, 'CLOSEREQ', false).draw();
 % test_code = ['test_' el_class]
 % eval(test_code)
 
-% pf = PFBrainSurface('SURF', ImporterBrainSurfaceNV('FILE', 'human_ICBM152.nv').get('SURF'));
-% gui = GUIFig('PF', pf, 'FILE', 'xxx sss', 'CLOSEREQ', false);
-% f = gui.draw('Units', 'normalized', 'Position', [.1 .5 .4 .4]);
+pf = PFBrainSurface('SURF', ImporterBrainSurfaceNV('FILE', 'human_ICBM152.nv').get('SURF'));
+gui = GUIFig('PF', pf, 'FILE', 'xxx sss', 'CLOSEREQ', false);
+f = gui.draw('Units', 'normalized', 'Position', [.1 .5 .4 .4]);
 
 % % % guis = GUIElement('PE', pf, 'CLOSEREQ', false);
 % % % fs = guis.draw();
