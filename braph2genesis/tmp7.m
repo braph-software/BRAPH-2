e96c0060a911cee94de3a215fb1d9c02bd12de92
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2022-06-04 09:31:28 +0200

    Update tmp7.m

--- braph2genesis/tmp7.m
+++ braph2genesis/tmp7.m
@@ -1,7 +1,7 @@
 close all
 delete(findall(0, 'type', 'figure'))
-clear all
-clc
+% clear all
+% clc
 
 % el_class = 'PanelPropIDictTable';
 % delete([fileparts(which('braph2')) '/src/gui/' el_class '.m'])
@@ -11,21 +11,21 @@ clc
 % % test_code = ['test_' el_class]
 % % eval(test_code)
 
-el_class = 'SettingsText';
-delete([fileparts(which('braph2')) '/src/gui/' el_class '.m'])
-create_Element([fileparts(which('braph2genesis')) '/src/gui/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/gui'])
-create_Element([fileparts(which('braph2genesis')) '/src/gui/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/gui'])
-% create_test_Element([fileparts(which('braph2genesis')) '/src/gui/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/gui'])
-% test_code = ['test_' el_class]
-% eval(test_code)
+% el_class = 'SettingsSymbol';
+% delete([fileparts(which('braph2')) '/src/gui/' el_class '.m'])
+% create_Element([fileparts(which('braph2genesis')) '/src/gui/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/gui'])
+% create_Element([fileparts(which('braph2genesis')) '/src/gui/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/gui'])
+% % create_test_Element([fileparts(which('braph2genesis')) '/src/gui/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/gui'])
+% % test_code = ['test_' el_class]
+% % eval(test_code)
 
-el_class = 'PFBrainAtlas';
-delete([fileparts(which('braph2')) '/src/atlas/' el_class '.m'])
-create_Element([fileparts(which('braph2genesis')) '/src/atlas/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/atlas'])
-create_Element([fileparts(which('braph2genesis')) '/src/atlas/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/atlas'])
-% create_test_Element([fileparts(which('braph2genesis')) '/src/atlas/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/atlas'])
-% test_code = ['test_' el_class]
-% eval(test_code)
+% el_class = 'PFBrainAtlas';
+% delete([fileparts(which('braph2')) '/src/atlas/' el_class '.m'])
+% create_Element([fileparts(which('braph2genesis')) '/src/atlas/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/atlas'])
+% create_Element([fileparts(which('braph2genesis')) '/src/atlas/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/atlas'])
+% % create_test_Element([fileparts(which('braph2genesis')) '/src/atlas/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/atlas'])
+% % test_code = ['test_' el_class]
+% % eval(test_code)
 
 % br1 = BrainRegion('ID', 'id1', 'LABEL', 'label1', 'NOTES', 'notes1', 'X', 1, 'Y', 1, 'Z', 1);
 % br2 = BrainRegion('ID', 'id2', 'LABEL', 'label2', 'NOTES', 'notes2', 'X', 2, 'Y', 2, 'Z', 2);
@@ -48,10 +48,15 @@ create_Element([fileparts(which('braph2genesis')) '/src/atlas/_' el_class '.gen.
 % gui0 = GUIFig('PF', pf0, 'FILE', 'xxx sss', 'WAITBAR', true, 'CLOSEREQ', false);
 % f0 = gui0.draw('Units', 'normalized', 'Position', [.1 .1 .4 .8]);
 
+tic 
 ba1 = ImporterBrainAtlasXLS('FILE', [fileparts(which('braph2')) filesep() 'atlases' filesep() 'aal90_atlas.xlsx']).get('BA');
+toc 
 pf1 = PFBrainAtlas(...
     'SURF', ImporterBrainSurfaceNV('FILE', 'human_ICBM152.nv').get('SURF'), ...
     'BA', ba1 ...
     );
+toc
 gui1 = GUIFig('PF', pf1, 'FILE', 'xxx sss', 'WAITBAR', false, 'CLOSEREQ', false);
+toc
 f1 = gui1.draw('Units', 'normalized', 'Position', [.1 .1 .4 .8]);
+toc
