088e16332458e7575ad79db4bef88f2108d83bd9
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2022-06-17 18:01:25 +0200

    Analysis Group initial commit

---
+++ braph2genesis/---pipelines---/- structural -/DONE/_AnalyzeGroup_ST_WU.gen.m
@@ -0,0 +1,93 @@
+%% ¡header!
+AnalyzeGroup_ST_WU < AnalyzeGroup (a, graph analysis with structural data) is a graph analysis using structural data.
+
+%% ¡description!
+This graph analysis uses structural data and analyzes them using weighted undirected graphs,
+binary undirected multigraphs with fixed thresholds,
+or binary undirected multigraphs with fixed densities.
+
+%% ¡props!
+
+%%% ¡prop!
+CORRELATION_RULE (parameter, option) is the correlation type.
+%%%% ¡settings!
+Correlation.CORRELATION_RULE_LIST
+%%%% ¡default!
+Correlation.PEARSON
+
+%%% ¡prop!
+NEGATIVE_WEIGHT_RULE (parameter, option) determines how to deal with negative weights.
+%%%% ¡settings!
+Correlation.NEGATIVE_WEIGHT_RULE_LIST
+%%%% ¡default!
+Correlation.ZERO
+
+%% ¡props_update!
+
+%%% ¡prop!
+GR (data, item) is the subject group, which also defines the subject class SubjectST.
+%%%% ¡default!
+Group('SUB_CLASS', 'SubjectST')
+
+%%% ¡prop!
+G (result, item) is the graph obtained from this analysis.
+%%%% ¡settings!
+'GraphWU'
+%%%% ¡default!
+GraphWU()
+%%%% ¡calculate!
+gr = a.get('GR');
+data_list = cellfun(@(x) x.get('ST'), gr.get('SUB_DICT').getItems, 'UniformOutput', false);
+data = cat(2, data_list{:})'; % correlation is a column based operation
+atlas = BrainAtlas();
+if ~isempty(gr) && ~isa(gr, 'NoValue') && gr.get('SUB_DICT').length > 0
+    atlas = gr.get('SUB_DICT').getItem(1).get('BA');
+end
+
+if any(strcmp(a.get('CORRELATION_RULE'), {Correlation.PEARSON_CV, Correlation.SPEARMAN_CV}))
+    age_list = cellfun(@(x) x.get('age'), gr.get('SUB_DICT').getItems, 'UniformOutput', false);
+    age = cat(2, age_list{:})';
+    sex_list = cellfun(@(x) x.get('sex'), gr.get('SUB_DICT').getItems, 'UniformOutput', false);
+    sex = zeros(size(age));
+    for i=1:length(sex_list)
+        switch lower(sex_list{i})
+            case 'female'
+                sex(i) = 1;
+            case 'male'
+                sex(i) = -1;
+            otherwise
+                sex(i) = 0;
+        end
+    end
+    A = Correlation.getAdjacencyMatrix(data, a.get('CORRELATION_RULE'), a.get('NEGATIVE_WEIGHT_RULE'), [age, sex]);
+else
+    A = Correlation.getAdjacencyMatrix(data, a.get('CORRELATION_RULE'), a.get('NEGATIVE_WEIGHT_RULE'));
+end
+
+g = GraphWU( ...
+    'ID', ['g ' gr.get('ID')], ...
+    'B', A, ...
+    'BRAINATLAS', atlas ...
+    );
+
+value = g;
+
+%% ¡methods!
+function pr = getPPCompareGroup_CPDict(a, varargin) 
+    %GEPPPCOMPAREGROUP_CPDICT returns the comparison plot panel compatible with the analysis.
+    %
+    % PR = GEPPPCOMPAREGROUP_CPDICT(A) returns the comparison plot panel
+    %  that is compatible with the analyze group.
+    %
+    % See also CompareGroup.
+    
+    pr = PPCompareGroup_CPDict_ST_WU(varargin{:});
+end
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+Example
+%%%% ¡code!
+example_ST_WU
--- braph2genesis/tmp10.m
+++ braph2genesis/tmp10.m
@@ -3,31 +3,33 @@ delete(findall(0, 'type', 'figure'))
 clear all
 clc
 
-el_class = 'PanelPropCell';
-delete([fileparts(which('braph2')) '/src/gui/' el_class '.m'])
-create_Element([fileparts(which('braph2genesis')) '/src/gui/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/gui'])
-create_Element([fileparts(which('braph2genesis')) '/src/gui/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/gui'])
-% create_test_Element([fileparts(which('braph2genesis')) '/src/gui/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/gui'])
-% test_code = ['test_' el_class]
-% eval(test_code)
+%% PanelPropCell
 
-et = ETA( ...
-    'PROP_CELL_M', {}, ...
-    'PROP_CELL_P', {
-        [1 1; 1 1] [1 2; 1 2] [1 3; 1 3]
-        [2 1; 2 1] [2 2; 2 2] [2 3; 2 3]
-        [3 1; 3 1] [3 2; 3 2] [3 3; 3 3]
-        }, ...
-    'PROP_CELL_D', {
-        [] [1] [1 2] [1 3]
-        [] [1; 1] [1 2; 1 2] [1 3; 1 3]
-        }, ...
-    'PROP_CELL_F', {}, ...
-    'PROP_CELL_G', {
-        rand(3) rand(3, 2) []
-        rand(2, 3) rand(2) rand(2, 4)
-        [] rand(4, 2) rand(4)
-        } ...
-    );
-gui = GUIElement('PE', et);
-f = gui.draw();
+% el_class = 'PanelPropCell';
+% delete([fileparts(which('braph2')) '/src/gui/' el_class '.m'])
+% create_Element([fileparts(which('braph2genesis')) '/src/gui/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/gui'])
+% create_Element([fileparts(which('braph2genesis')) '/src/gui/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/gui'])
+% % create_test_Element([fileparts(which('braph2genesis')) '/src/gui/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/gui'])
+% % test_code = ['test_' el_class]
+% % eval(test_code)
+
+% et = ETA( ...
+%     'PROP_CELL_M', {}, ...
+%     'PROP_CELL_P', {
+%         [1 1; 1 1] [1 2; 1 2] [1 3; 1 3]
+%         [2 1; 2 1] [2 2; 2 2] [2 3; 2 3]
+%         [3 1; 3 1] [3 2; 3 2] [3 3; 3 3]
+%         }, ...
+%     'PROP_CELL_D', {
+%         [] [1] [1 2] [1 3]
+%         [] [1; 1] [1 2; 1 2] [1 3; 1 3]
+%         }, ...
+%     'PROP_CELL_F', {}, ...
+%     'PROP_CELL_G', {
+%         rand(3) rand(3, 2) []
+%         rand(2, 3) rand(2) rand(2, 4)
+%         [] rand(4, 2) rand(4)
+%         } ...
+%     );
+% gui = GUIElement('PE', et);
+% f = gui.draw();
