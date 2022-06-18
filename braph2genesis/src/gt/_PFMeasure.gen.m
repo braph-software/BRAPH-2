f3ccdcc27f7b8525575fd660c2cff2ece36b6cae
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2022-06-18 16:38:11 +0200

    Create _PFMeasure.gen.m

---
+++ braph2genesis/src/gt/_PFMeasure.gen.m
@@ -0,0 +1,21 @@
+%% ¡header!
+PFMeasure < PanelFig (pf, panel figure measure) is a plot of a measure.
+
+%%% ¡description!
+% % % PFBrainSurface manages the plot of the brain surface choosen by the user. 
+% % % A collection of brain surfaces in NV format can be found in the folder 
+% % % ./braph2/brainsurfs/.
+% % % This class provides the common methods needed to manage the plot of 
+% % % the surface. In particualr, the user can change lighting, material, 
+% % % camlight, shadning, colormap, facecolor, brain color, face color, 
+% % % edge color, and background color. 
+
+%%% ¡seealso!
+PanelFig, Graph
+
+%% ¡props!
+
+%%% ¡prop!
+M (metadata, item) is the measure.
+%%%% ¡settings!
+'Measure'
