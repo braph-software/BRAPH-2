48f5dc6c1c7aaeec6a150aa293431a2c0af0f346
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2022-06-20 09:24:23 +0200

    Update _MultigraphBUD.gen.m

--- braph2genesis/graphs/_MultigraphBUD.gen.m
+++ braph2genesis/graphs/_MultigraphBUD.gen.m
@@ -27,8 +27,6 @@ negativity = Graph.NONNEGATIVE * ones(layernumber);
 
 %%% ¡prop!
 DENSITIES (parameter, rvector) is the vector of densities.
-%%%% ¡gui_!
-% % % pr = PlotPropSmartVector('EL', g, 'PROP', MultigraphBUD.DENSITIES, 'MAX', 100, 'MIN', 0, varargin{:});
 
 %% ¡props_update!
 
@@ -49,12 +47,14 @@ value = A;
 %%%% ¡gui!
 bas = g.get('BAS');
 ba = bas{1};
-brs = ba.get('BR_DICT').getItems();
-rowname = '{';
-for bri = 1:1:length(brs)
-    rowname = [rowname ' ''' brs{bri}.get('ID') ''''];
-end
-rowname = [rowname '}'];
+br_ids = ba.get('BR_DICT').getKeys();
+rowname = ['{' sprintf('''%s'' ', br_ids{:}) '}'];
+% % % brs = ba.get('BR_DICT').getItems();
+% % % rowname = '{';
+% % % for bri = 1:1:length(brs)
+% % %     rowname = [rowname ' ''' brs{bri}.get('ID') ''''];
+% % % end
+% % % rowname = [rowname '}'];
 
 densities = g.get('DENSITIES');
 xsliderlabels = '{';
