08e122551442dbe976dd75dce1e09e8a9f1795db
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2022-06-28 00:58:07 +0200

    Update _PFMeasure.gen.m

--- braph2genesis/src/gt/_PFMeasure.gen.m
+++ braph2genesis/src/gt/_PFMeasure.gen.m
@@ -15,6 +15,12 @@ PanelFig, Measure
 
 %% ¡properties!
 p  % handle for panel
+h_axes % handle for the axes
+h_line % handle for line
+
+toolbar
+tool_axis
+tool_grid
 
 %% ¡props!
 
@@ -23,8 +29,25 @@ M (metadata, item) is the measure.
 %%%% ¡settings!
 'Measure'
 
+%%% ¡prop!
+ST_AXIS (figure, item) determines the axis settings.
+%%%% ¡settings!
+'SettingsAxis'
+%%%% ¡default!
+SettingsAxis('GRID', false, 'AXIS', true)
+%%%% ¡postprocessing!
+if (isempty(varargin) || pf.prop_set('ST_AXIS', varargin)) && check_graphics(pf.h_axes, 'axes')
+    % update state of toggle tool
+    set(pf.tool_grid, 'State', pf.get('ST_AXIS').get('GRID'))
+
+    % update state of toggle tool
+    set(pf.tool_axis, 'State', pf.get('ST_AXIS').get('AXIS'))
+end
+%%%% ¡gui!
+pr = SettingsAxisPP('EL', m, 'PROP', PFMeasure.ST_AXIS, varargin{:});
+
 %% ¡methods!
-function p_out = draw(m, varargin)
+function p_out = draw(pf, varargin)
     %DRAW draws the measure graphical panel.
     %
     % DRAW(PL) draws the measure graphical panel.
@@ -40,7 +63,65 @@ function p_out = draw(m, varargin)
     %
     % see also settings, uipanel.
 
-    m.p = draw@PanelFig(m, varargin{:});
+    pf.p = draw@PanelFig(pf, varargin{:});
+
+    % axes
+    if ~check_graphics(pf.h_axes, 'axes')
+        pf.h_axes = uiaxes( ...
+            'Parent', pf.p, ...
+            'Tag', 'h_axes', ...
+            'Units', 'normalized', ...
+            'OuterPosition', [0 0 1 1] ...
+            );
+% % %         pf.h_axes.Toolbar.Visible = 'off';
+% % %         pf.h_axes.Interactions = [];
+    end
+    
+    % data line
+    if ~check_graphics(pf.h_line, 'line')
+        pf.h_line = plot(pf.h_axes, [1 2], [1 -1]); % % %
+    end    
+    
+    pf.memorize('ST_AXIS').h(pf.h_axes).set('PANEL', pf, 'UITAG', 'h_axes')
+    listener(pf.get('ST_AXIS'), 'PropSet', @cb_st_axis);
+    function cb_st_axis(~, ~) % (src, event)
+        set(pf.tool_axis, 'State', pf.get('ST_AXIS').get('AXIS'))
+        set(pf.tool_grid, 'State', pf.get('ST_AXIS').get('GRID'))
+    end
+
+    % Toolbar
+    if ~check_graphics(pf.toolbar, 'uitoolbar')
+        pf.toolbar = findobj(ancestor(pf.p, 'Figure'), 'Tag', 'ToolBar');
+    end
+    
+    if check_graphics(pf.toolbar, 'uitoolbar') && ~check_graphics(pf.tool_axis, 'uitoggletool') % implies that also the other tools are not defined
+        
+        uipushtool(pf.toolbar, 'Separator', 'on', 'Visible', 'off')
+
+        % Axis
+        pf.tool_axis = uitoggletool(pf.toolbar, ...
+            'Tag', 'tool_axis', ...
+            'State', pf.get('ST_AXIS').get('AXIS'), ...
+            'Tooltip', 'Show axis', ...
+            'CData', imread('icon_axis.png'), ...
+            'OnCallback', {@cb_axis, true}, ...
+            'OffCallback', {@cb_axis, false});
+        
+        % Grid
+        pf.tool_grid = uitoggletool(pf.toolbar, ...
+            'Tag', 'tool_grid', ...
+            'State', pf.get('ST_AXIS').get('GRID'), ...
+            'Tooltip', 'Show grid', ...
+            'CData', imread('icon_grid.png'), ...
+            'OnCallback', {@cb_grid, true}, ...
+            'OffCallback', {@cb_grid, false});
+    end
+    function cb_axis(~, ~, axis) % (src, event)
+        pf.get('ST_AXIS').set('AXIS', axis);
+    end
+    function cb_grid(~, ~, grid) % (src, event)
+        pf.get('ST_AXIS').set('GRID', grid);
+    end
 
 % % % if Measure.is_global(m)
 % % %     pr.set( ...
@@ -124,10 +205,10 @@ function p_out = draw(m, varargin)
     
     % output
     if nargout > 0
-        p_out = m.p;
+        p_out = pf.p;
     end
 end
-function str = tostring(m, varargin)
+function str = tostring(pf, varargin)
     %TOSTRING string with information about the measure.
     %
     % STR = TOSTRING(PF) returns a string with information about the measure.
@@ -139,7 +220,7 @@ function str = tostring(m, varargin)
     %
     % See also disp, tree.
 
-    str = ['Plot ' m.get('ID')];
+    str = ['Plot ' pf.get('M').get('ID')];
     str = tostring(str, varargin{:});
     str = str(2:1:end-1);
 end
