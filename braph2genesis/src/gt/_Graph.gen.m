99168d2f57b4034ba7e2651d6d1da4d0c549732a
egolol <egolol87@gmail.com>
2022-07-19 10:13:34 +0200

    first commit
    PFGraph template

--- braph2genesis/src/gt/_Graph.gen.m
+++ braph2genesis/src/gt/_Graph.gen.m
@@ -84,7 +84,13 @@ PFG (gui, item) contains the panel figure of the graph.
 'PFGraph'
 %%%% ¡postprocessing!
 if ~braph2_testing % to avoid problems with isqual when the element is recursive
-    g.memorize('PFG').set('G', g)
+    if isa(g.getr('PFG'), 'NoValue')
+        if Graph.is_graph(g)
+            g.set('PFG', PFGraph('G', g))
+        else
+            g.memorize('PFG').set('G', g)
+        end
+    end
 end
 %%%% ¡gui!
 pr = PanelPropItem('EL', g, 'PROP', Graph.PFG, ...
