00599317501211619738368cdf4d563f1c97ddd1
egolol <egolol87@gmail.com>
2022-07-19 10:53:51 +0200

    Update _PFGraph.gen.m

--- braph2genesis/src/gt/_PFGraph.gen.m
+++ braph2genesis/src/gt/_PFGraph.gen.m
@@ -35,7 +35,16 @@ if (isempty(varargin) || pf.prop_set('ST_AXIS', varargin)) && check_graphics(pf.
     set(pf.tool_axis, 'State', pf.get('ST_AXIS').get('AXIS'))
 end
 %%%% ¡gui!
-pr = SettingsAxisPP('EL', pf, 'PROP', PFMeasure.ST_AXIS, varargin{:});
+pr = SettingsAxisPP('EL', pf, 'PROP', PFGraph.ST_AXIS, varargin{:});
+
+%%% ¡prop!
+ST_COLORMAP (figure, item) determines the colormap settings.
+%%%% ¡settings!
+'SettingsColormap'
+%%%% ¡default!
+SettingsColormap('STYLE', 'JET', 'BAR', true)
+%%%% ¡gui!
+pr = SettingsColormapPP('EL', pf, 'PROP', PFGraph.ST_COLORMAP, varargin{:});
 
 %% ¡methods!
 function p_out = draw(pf, varargin)
@@ -94,7 +103,14 @@ function p_out = draw(pf, varargin)
     function cb_grid(~, ~, grid) % (src, event)
         pf.get('ST_AXIS').set('GRID', grid);
     end
-    
+
+
+    % colormap listener
+    listener(pf.get('ST_COLORMAP'), 'PropSet', @cb_st_colormap);
+    function cb_st_colormap(~, ~) % (src, event)
+        set(pf.tool_axis, 'State', pf.get('ST_AXIS').get('AXIS'))        
+    end
+
     % output
     if nargout > 0
         p_out = pf.p;
@@ -115,4 +131,217 @@ function str = tostring(pf, varargin)
     str = ['Plot ' pf.get('G').get('ID')];
     str = tostring(str, varargin{:});
     str = str(2:1:end-1);
+end
+function h = plotw(pr, A, varargin)
+    % PLOTW plots a weighted matrix
+    %
+    % H = PLOTW(A) plots the weighted matrix A and returns the handle to
+    %   the plot H.
+    %
+    % H = PLOTW(A,'PropertyName',PropertyValue) sets the property of the
+    %   matrix plot PropertyName to PropertyValue.
+    %   All standard plot properties of surf can be used.
+    %   Additional admissive properties are:
+    %       xlabels   -   1:1:number of matrix elements (default)
+    %       ylabels   -   1:1:number of matrix elements (default)
+    %
+    % See also Graph, plotb, surf.
+
+    N = length(A);
+
+    % x labels
+    xlabels = (1:1:N);
+    for n = 1:2:length(varargin)
+        if strcmpi(varargin{n}, 'xlabels')
+            xlabels = varargin{n + 1};
+        end
+    end
+    if ~iscell(xlabels)
+        xlabels = {xlabels};
+    end
+
+    % y labels
+    ylabels = (1:1:N);
+    for n = 1:2:length(varargin)
+        if strcmpi(varargin{n}, 'ylabels')
+            ylabels = varargin{n + 1};
+        end
+    end
+    if ~iscell(ylabels)
+        ylabels = {ylabels};
+    end
+
+    ht = surf('Parent', pr.h_axes, ...
+        (0:1:N), ...
+        (0:1:N), ...
+        [A, zeros(size(A, 1), 1); zeros(1, size(A, 1) + 1)]);
+    view(pr.h_axes, 2)
+    colorbar(pr.h_axes)
+    shading(pr.h_axes, 'flat')
+    axis(pr.h_axes, 'equal', 'square', 'tight')
+    grid(pr.h_axes, 'off')
+    box(pr.h_axes, 'on')
+    set(pr.h_axes, ...
+        'XAxisLocation', 'top', ...
+        'XTick', (1:1:N) - .5, ...
+        'XTickLabel', {}, ...
+        'YAxisLocation', 'left', ...
+        'YDir', 'Reverse', ...
+        'YTick', (1:1:N) - .5, ...
+        'YTickLabel', ylabels)
+
+    if ~verLessThan('matlab', '8.4.0')
+        set(pr.h_axes, ...
+            'XTickLabelRotation', 90, ...
+            'XTickLabel', xlabels)
+    else
+        t = text('Parent', pr.h_axes, (1:1:N) - .5, zeros(1, N), xlabels);
+        set(t, ...
+            'HorizontalAlignment', 'left', ...
+            'VerticalAlignment', 'middle', ...
+            'Rotation', 90);
+    end
+
+    colormap(pr.h_axes, 'jet')
+
+    % output if needed
+    if nargout > 0
+        h = ht;
+    end
+end
+function h = plotb(pr, A, varargin)
+    % PLOTB plots a binary matrix
+    %
+    % H = PLOTB(A) plots the binarized version of weighted matrix A and
+    %   returns the handle to the plot H.
+    %   The matrix A can be binarized by fixing the threshold
+    %   (default, threshold = 0.5).
+    %
+    % H = PLOTB(A, 'PropertyName', PropertyValue) sets the property of the
+    %   matrix plot PropertyName to PropertyValue.
+    %   All standard plot properties of surf can be used.
+    %   Additional admissive properties are:
+    %       threshold   -   0.5 (default)
+    %       xlabels     -   1:1:number of matrix elements (default)
+    %       ylabels     -   1:1:number of matrix elements (default)
+    %
+    % See also Graph, binarize, plotw, surf.
+
+    N = length(A);
+
+    % threshold
+    threshold = get_from_varargin(0, 'threshold', varargin{:});
+
+    % density
+    density = get_from_varargin([], 'density', varargin{:});
+
+    % x labels
+    xlabels = (1:1:N);
+    for n = 1:2:length(varargin)
+        if strcmpi(varargin{n}, 'xlabels')
+            xlabels = varargin{n + 1};
+        end
+    end
+    if ~iscell(xlabels)
+        xlabels = {xlabels};
+    end
+
+    % y labels
+    ylabels = (1:1:N);
+    for n = 1:2:length(varargin)
+        if strcmpi(varargin{n}, 'ylabels')
+            ylabels = varargin{n + 1};
+        end
+    end
+    if ~iscell(ylabels)
+        ylabels = {ylabels};
+    end
+
+    B = binarize(A, 'threshold', threshold, 'density', density);
+
+    ht = surf('Parent', pr.h_axes, ...
+        (0:1:N), ...
+        (0:1:N), ...
+        [B, zeros(size(B, 1), 1); zeros(1, size(B, 1) + 1)]);
+    view(pr.h_axes, 2)
+    shading(pr.h_axes, 'flat')
+    axis(pr.h_axes, 'equal', 'square', 'tight')
+    grid(pr.h_axes, 'off')
+    colorbar(pr.h_axes)
+    box(pr.h_axes, 'on')
+    set(pr.h_axes, ...
+        'XAxisLocation', 'top',  ...
+        'XTick', (1:1:N) - .5, ...
+        'XTickLabel', {},  ...
+        'YAxisLocation', 'left',  ...
+        'YDir', 'Reverse',  ...
+        'YTick', (1:1:N) - .5, ...
+        'YTickLabel', ylabels)
+
+    if ~verLessThan('matlab',  '8.4.0')
+        set(pr.h_axes, ...
+            'XTickLabelRotation',90, ...
+            'XTickLabel', xlabels)
+    else
+        t = text('Parent', pr.h_axes, (1:1:N) - .5, zeros(1,N), xlabels);
+        set(t, ...
+            'HorizontalAlignment', 'left',  ...
+            'VerticalAlignment', 'middle',  ...
+            'Rotation',90);
+    end
+
+    colormap(pr.h_axes, 'bone')
+
+    % output if needed
+    if nargout > 0
+        h = ht;
+    end
+end
+function h = hist(pr, A, varargin)
+    % HIST plots the histogram and density of a matrix
+    %
+    % H = HIST(A) plots the histogram of a matrix A and the associated density and
+    %   returns the handle to the plot H.
+    %
+    % H = HIST(A,'PropertyName',PropertyValue) sets the property of the histogram
+    %   plot PropertyName to PropertyValue.
+    %   All standard plot properties of surf can be used.
+    %   Additional admissive properties are:
+    %       bins       -   -1:.001:1 (default)
+    %       diagonal   -   'exclude' (default) | 'include'
+    %
+    % See also Graph, histogram.
+
+    [count, bins, density] = histogram(A, varargin{:});
+
+    bins = [bins(1) bins bins(end)];
+    count = [0 count 0];
+    density = [100 density 0];
+
+    hold(pr.h_axes, 'on')
+    cla(pr.h_axes)
+    ht1 = fill(pr.h_axes, bins, count, 'k');
+    ht2 = plot(bins, density, 'b', 'linewidth', 2, 'Parent', pr.h_axes);
+    hold(pr.h_axes, 'off')
+    xlabel(pr.h_axes, 'coefficient values / threshold')
+    ylabel(pr.h_axes, 'coefficient counts / density')
+
+    grid(pr.h_axes, 'off')
+    box(pr.h_axes, 'on')
+    colorbar(pr.h_axes, 'off')
+    axis(pr.h_axes, 'square', 'tight')
+    set(pr.h_axes, ...
+        'XAxisLocation', 'bottom',  ...
+        'XTickLabelMode', 'auto',  ...
+        'XTickMode', 'auto',  ...
+        'YTickLabelMode', 'auto',  ...
+        'YAxisLocation', 'left',  ...
+        'YDir', 'Normal',  ...
+        'YTickMode', 'auto',  ...
+        'YTickLabelMode', 'auto')
+
+    % output if needed
+    if nargout > 0
+        h = [ht1 ht2];
+    end
 end
