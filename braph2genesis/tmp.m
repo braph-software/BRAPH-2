28fc6fb88be8b974ab2a696e3964f6c4175045d5
egolol <egolol87@gmail.com>
2022-08-15 12:44:37 +0200

    speeds up

--- braph2genesis/tmp.m
+++ braph2genesis/tmp.m
@@ -1,13 +1,13 @@
 close all
 clear all %#ok<CLALL>
 
-el_class = 'PFAnalysisEnsemble';
-delete([fileparts(which('braph2')) '/src/gui/' el_class '.m'])
-create_Element([fileparts(which('braph2genesis')) '/src/measures/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/measures'])
-create_Element([fileparts(which('braph2genesis')) '/src/measures/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/measures'])
-create_test_Element([fileparts(which('braph2genesis')) '/src/measures/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/measures'])
-test_code = ['test_' el_class]
-eval(test_code)
+el_class = 'Measure';
+delete([fileparts(which('braph2')) '/src/gt/' el_class '.m'])
+create_Element([fileparts(which('braph2genesis')) '/src/gt/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/gt'])
+create_Element([fileparts(which('braph2genesis')) '/src/gt/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/gt'])
+create_test_Element([fileparts(which('braph2genesis')) '/src/gt/_' el_class '.gen.m'], [fileparts(which('braph2')) '/src/gt'])
+% test_code = ['test_' el_class]
+% eval(test_code)
 
 % el_class = 'PlotElement';
 % delete([fileparts(which('braph2')) '/src/gui/' el_class '.m'])
