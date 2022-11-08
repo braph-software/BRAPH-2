ec5108ed4ed0de934fa1bb816f6cb6aedb9a7b0b
Yu-Wei Chang <hejyuwei@gmail.com>
2022-11-08 14:04:03 +0100

    Update _NNRegressorEvaluator.gen.m

--- braph2genesis/neuralnetworks/_NNRegressorEvaluator.gen.m
+++ braph2genesis/neuralnetworks/_NNRegressorEvaluator.gen.m
@@ -18,8 +18,8 @@ elseif ~any(ismember(nne.get('GR').get('SUB_DICT').getItem(1).get('INPUT_LABEL')
     gr = nne.get('GR');
     net = nn.memorize('MODEL');
 
-    [inputs, num_features] = nn.reconstruct_inputs(gr);
-    [targets, classes, masks] = nn.reconstruct_targets(gr);
+    [inputs, num_features, masks] = nn.reconstruct_inputs(gr);
+    [targets, classes] = nn.reconstruct_targets(gr);
 
     original_loss = double(sqrt(mean((net.predict(inputs) - targets).^2)));
 
