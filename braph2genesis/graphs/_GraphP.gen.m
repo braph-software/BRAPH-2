8e47456336d36de31c06439114ca8f52abdfd9ff
egolol <egolol87@gmail.com>
2022-11-21 11:08:39 +0100

    Update _GraphP.gen.m

--- braph2genesis/graphs/_GraphP.gen.m
+++ braph2genesis/graphs/_GraphP.gen.m
@@ -75,38 +75,3 @@ pr = PanelPropCell('EL', g, 'PROP', GraphP.A, ...
     'ROWNAME', rowname, ...
     'COLUMNNAME', rowname, ...
     varargin{:});
-
-    RANDOMIZE returns a randomized graph and the correlation coefficients
-    
-    RANDOM_G = RANDOMIZE(G) returns the randomized graph
-    RANDOM_G obtained with a randomized correlation
-    matrix via the static function randomize_A.
-    
-    RANDOM_G = RANDOMIZE(G, 'AttemptPerEdge', VALUE, 'NumberOfWeights', VALUE)
-    returns the randomized graph RANDOM_G obtained with a randomized correlation
-    matrix via the static function randomize_A, it passes the
-    attempts per edge and the number of weights specified by the user.
-    
-    See also randomize_A
-
-    get rules
-    number_of_weights = g.get('NUMBEROFWEIGHTS');
-    attempts_per_edge = g.get('ATTEMPTSPEREDGE');
-
-    correction for multigraphs
-    if Graph.is_multigraph(g)
-        tmp_b = g.get('B');
-        tmp_g = GraphWU('B', tmp_b);
-        tmp_A = cell2mat(tmp_g.get('A'));
-        random_B = GraphWU.randomize_A(tmp_A, attempts_per_edge, number_of_weights);
-        if isa(g, 'MultigraphBUD')
-            random_g = MultigraphBUD('B', random_B, 'Densities', g.get('Densities'));
-        else
-            random_g = MultigraphBUT('B', random_B, 'Thresholds', g.get('Thresholds'));
-        end
-    else
-        A = cell2mat(g.get('A'));
-        random_A = GraphWU.randomize_A(A, attempts_per_edge, number_of_weights);
-        random_g = GraphWU('B', random_A);
-    end
-end
