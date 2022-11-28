8658e23624ae3c77afafb463be1cf1c2460fba30
Anna Canal Garcia <31701548+annacanal@users.noreply.github.com>
2022-11-28 10:25:23 +0100

    Update _AnalyzeEnsemble_CON_WD.gen.m

--- braph2genesis/pipelines/connectivity/_AnalyzeEnsemble_CON_WD.gen.m
+++ braph2genesis/pipelines/connectivity/_AnalyzeEnsemble_CON_WD.gen.m
@@ -7,6 +7,26 @@ This graph analysis uses connectivity data and analyzes them using weighted dire
 %% ¡props_update!
 
 %%% ¡prop!
+TEMPLATE (parameter, item) is the analysis template to set the parameters.
+%%%% ¡settings!
+'AnalyzeEnsemble_CON_WD'
+
+%%% ¡prop!
+GRAPH_TEMPLATE (parameter, item) is the graph template to set all graph and measure parameters.
+%%%% ¡settings!
+'GraphWD'
+%%%% ¡postprocessing!
+if ~braph2_testing
+    if isa(a.getr('GRAPH_TEMPLATE'), 'NoValue')
+        if a.get('GR').get('SUB_DICT').length() > 0
+            a.set('GRAPH_TEMPLATE', GraphWD('BAS', a.get('GR').get('SUB_DICT').getItem(1).get('BA')));
+        else
+            a.set('GRAPH_TEMPLATE', GraphWD());
+        end
+    end
+end
+
+%%% ¡prop!
 GR (data, item) is the subject group, which also defines the subject class SubjectCON.
 %%%% ¡default!
 Group('SUB_CLASS', 'SubjectCON')
@@ -14,7 +34,7 @@ Group('SUB_CLASS', 'SubjectCON')
 %%% ¡prop!
 ME_DICT (result, idict) contains the calculated measures of the graph ensemble.
 %%%% ¡gui!
-pr = PPAnalyzeEnsemble_ME_DICT('EL', a, 'PROP', AnalyzeEnsemble_CON_WD.ME_DICT, 'WAITBAR', true, varargin{:});
+% % % pr = PPAnalyzeEnsemble_ME_DICT('EL', a, 'PROP', AnalyzeEnsemble_CON_WD.ME_DICT, 'WAITBAR', true, varargin{:});
 
 %%% ¡prop!
 G_DICT (result, idict) is the graph (GraphWD) ensemble obtained from this analysis.
@@ -25,33 +45,40 @@ IndexedDictionary('IT_CLASS', 'GraphWD')
 %%%% ¡calculate!
 g_dict = IndexedDictionary('IT_CLASS', 'GraphWD');
 gr = a.get('GR');
-atlas = BrainAtlas();
+ba = BrainAtlas();
 if ~isempty(gr) && ~isa(gr, 'NoValue') && gr.get('SUB_DICT').length > 0 
-    atlas = gr.get('SUB_DICT').getItem(1).get('BA');
+    ba = gr.get('SUB_DICT').getItem(1).get('BA');
 end
-gr = a.get('GR');
+
 for i = 1:1:gr.get('SUB_DICT').length()
 	sub = gr.get('SUB_DICT').getItem(i);
     g = GraphWD( ...
         'ID', ['g ' sub.get('ID')], ...
-        'BRAINATLAS', atlas, ...
+        'BAS', ba, ...
         'B', Callback('EL', sub, 'TAG', 'CON') ...
         );
     g_dict.add(g)
+        
+    if isa(a.getr('TEMPLATE'), 'NoValue')
+        g.set('TEMPLATE', a.memorize('GRAPH_TEMPLATE'))
+    else
+        g.set('TEMPLATE', a.get('TEMPLATE').memorize('GRAPH_TEMPLATE'))
+    end
 end
 
 value = g_dict;
-%% ¡methods!
-function pr = getPPCompareEnsemble_CPDict(a, varargin) 
-    %GETPPCOMPAREENSEMBLE_CPDICT returns the comparison ensemble plot panel compatible with the analysis.
-    %
-    % PR = GETPPCOMPAREENSEMBLE_CPDICT(A) returns the comparison ensemble plot panel
-    %  that is compatible with the analyze ensemble.
-    %
-    % See also CompareEnsemble.
-    
-    pr = PPCompareEnsemble_CPDict_WD(varargin{:});
-end
+
+% % % %% ¡methods!
+% % % function pr = getPPCompareEnsemble_CPDict(a, varargin) 
+% % %     %GETPPCOMPAREENSEMBLE_CPDICT returns the comparison ensemble plot panel compatible with the analysis.
+% % %     %
+% % %     % PR = GETPPCOMPAREENSEMBLE_CPDICT(A) returns the comparison ensemble plot panel
+% % %     %  that is compatible with the analyze ensemble.
+% % %     %
+% % %     % See also CompareEnsemble.
+% % %     
+% % %     pr = PPCompareEnsemble_CPDict_WD(varargin{:});
+% % % end
 
 %% ¡tests!
 
