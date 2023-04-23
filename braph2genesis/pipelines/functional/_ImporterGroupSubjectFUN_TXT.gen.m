7c5a842eb205609ce0e7b91a207779d39422d502
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-04-23 10:18:20 +0200

    Update _ImporterGroupSubjectFUN_TXT.gen.m

--- braph2genesis/pipelines/functional/_ImporterGroupSubjectFUN_TXT.gen.m
+++ braph2genesis/pipelines/functional/_ImporterGroupSubjectFUN_TXT.gen.m
@@ -11,12 +11,54 @@ Subject ID (column 1), Subject AGE (column 2), and Subject SEX (column 3).
 The first row contains the headers and each subsequent row the values for each subject.
 
 %%% ¡seealso!
-Element, Importer, ExporterGroupSubjectFUN_TXT
+Group, SubjectFUN, ExporterGroupSubjectFUN_TXT
+
+%% ¡props_update!
+
+%%% ¡prop!
+NAME (constant, string) is the name of the FUN subject group importer from TXT.
+%%%% ¡default!
+'ImporterGroupSubjectFUN_TXT'
+
+%%% ¡prop!
+DESCRIPTION (constant, string) is the description of the FUN subject group importer from TXT.
+%%%% ¡default!
+'ImporterGroupSubjectFUN_TXT imports a group of subjects with connectivity data from a series of TXT file and their covariates (optional) from another TXT file.'
+
+%%% ¡prop!
+TEMPLATE (parameter, item) is the template of the FUN subject group importer from TXT.
+
+%%% ¡prop!
+ID (data, string) is a few-letter code for the FUN subject group importer from TXT.
+%%%% ¡default!
+'ImporterGroupSubjectFUN_TXT ID'
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the FUN subject group importer from TXT.
+%%%% ¡default!
+'ImporterGroupSubjectFUN_TXT label'
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the FUN subject group importer from TXT.
+%%%% ¡default!
+'ImporterGroupSubjectFUN_TXT notes'
 
 %% ¡props!
 
 %%% ¡prop!
 DIRECTORY (data, string) is the directory containing the FUN subject group files from which to load the subject group.
+%%%% ¡default!
+[fileparts(which('SubjectFUN')) filesep 'example data FUN' filesep 'xls' filesep 'FUN_Group_1']
+
+%%% ¡prop!
+GET_DIR (query, item) opens a dialog box to set the directory from where to load the TXT files of the FUN subject group.
+%%%% ¡settings!
+'ImporterGroupSubjectFUN_TXT'
+%%%% ¡calculate!
+directory = uigetdir('Select directory');
+if ischar(directory) && isfolder(directory)
+	im.set('DIRECTORY', directory);
+end
 
 %%% ¡prop!
 BA (data, item) is a brain atlas.
@@ -39,11 +81,12 @@ gr = Group( ...
     );
 
 gr.lock('SUB_CLASS');
+
 directory = im.get('DIRECTORY');
-if ~isfolder(directory) && ~braph2_testing()
-    im.uigetdir()
-    directory = im.get('DIRECTORY');
+if ~isfolder(directory)
+    directory = ImporterGroupSubjectCON_TXT.getPropDefault('DIRECTORY');
 end
+
 if isfolder(directory)
     % sets group props
     wb = braph2waitbar(im.get('WAITBAR'), 0, 'Reading directory ...');
@@ -54,36 +97,37 @@ if isfolder(directory)
         'LABEL', name, ...
         'NOTES', ['Group loaded from ' directory] ...
         );
+    
     try
         % analyzes file
         files = dir(fullfile(directory, '*.txt'));
         
-        % Check if there are covariates to add (age and sex)
-        cov_folder = dir(directory);
-        cov_folder = cov_folder([cov_folder(:).isdir] == 1);
-        cov_folder = cov_folder(~ismember({cov_folder(:).name}, {'.', '..'}));
-        if ~isempty(cov_folder)
-            file_cov = dir(fullfile([directory filesep() cov_folder.name], '*.txt'));
-            raw_covariates = readtable([directory filesep() cov_folder.name filesep() file_cov.name], 'Delimiter', '	');
-            age = raw_covariates{:, 2};
-            sex = raw_covariates{:, 3};
-        else
-            age = ones(length(files), 1);
-            unassigned =  {'unassigned'};
-            sex = unassigned(ones(length(files), 1));
-        end
+% % %         % Check if there are covariates to add (age and sex)
+% % %         cov_folder = dir(directory);
+% % %         cov_folder = cov_folder([cov_folder(:).isdir] == 1);
+% % %         cov_folder = cov_folder(~ismember({cov_folder(:).name}, {'.', '..'}));
+% % %         if ~isempty(cov_folder)
+% % %             file_cov = dir(fullfile([directory filesep() cov_folder.name], '*.txt'));
+% % %             raw_covariates = readtable([directory filesep() cov_folder.name filesep() file_cov.name], 'Delimiter', '	');
+% % %             age = raw_covariates{:, 2};
+% % %             sex = raw_covariates{:, 3};
+% % %         else
+% % %             age = ones(length(files), 1);
+% % %             unassigned =  {'unassigned'};
+% % %             sex = unassigned(ones(length(files), 1));
+% % %         end
         
         braph2waitbar(wb, .15, 'Loading subjecy group ...')
 
-        if length(files) > 0
+        if ~isempty(files)
             % brain atlas
             ba = im.get('BA');
-            br_number = ba.get('BR_DICT').length;
-            subdict = gr.get('SUB_DICT');
+            br_number = ba.get('BR_DICT').get('LENGTH');
+            sub_dict = gr.get('SUB_DICT');
             
             % adds subjects
             for i = 1:1:length(files)
-                braph2waitbar(wb, .30 + .70 * i / length(files), ['Loading subject ' num2str(i) ' of ' num2str(length(files)) ' ...'])
+                braph2waitbar(wb, .25 + .75 * i / length(files), ['Loading subject ' num2str(i) ' of ' num2str(length(files)) ' ...'])
 
                 % read file
                 FUN = table2array(readtable(fullfile(directory, files(i).name), 'Delimiter', '	'));
@@ -97,24 +141,22 @@ if isfolder(directory)
                     for j = 1:1:br_number
                         br_id = ['br' int2str(j)];
                         br = BrainRegion('ID', br_id);
-                        idict.add(br)
+                        idict.get('ADD', br)
                     end
                     ba.set('br_dict', idict);
                 end
-                subdict = gr.get('SUB_DICT');
+                sub_dict = gr.get('SUB_DICT');
                 
                 [~, sub_id] = fileparts(files(i).name);
                 sub = SubjectFUN( ...
                     'ID', sub_id, ...
-                    'BA', ba, ...
-                    'age', age(i), ...
-                    'sex', sex{i} ...
+                    'BA', ba, ... % % % 'age', age(i), ... % % % 'sex', sex{i} ...
                     );
                 
                 sub.set('FUN', FUN);
-                subdict.add(sub);
+                sub_dict.get('ADD', sub);
             end
-            gr.set('sub_dict', subdict);
+            gr.set('sub_dict', sub_dict);
         end
     catch e
         braph2waitbar(wb, 'close')
@@ -123,18 +165,16 @@ if isfolder(directory)
     end
     
     braph2waitbar(wb, 'close')
-elseif ~braph2_testing()
-    error([BRAPH2.STR ':ImporterGroupSubjectFUN_TXT:' BRAPH2.BUG_IO], ...
-        [BRAPH2.STR ':ImporterGroupSubjectFUN_TXT:' BRAPH2.BUG_IO]);
+else
+    error([BRAPH2.STR ':ImporterGroupSubjectFUN_TXT:' BRAPH2.CANCEL_IO], ...
+        [BRAPH2.STR ':ImporterGroupSubjectFUN_TXT:' BRAPH2.CANCEL_IO] '\\n' ...
+        'The prop DIRECTORY must be an existing directory, but it is ''' directory '''.'] ...
+        );
 end
+
 value = gr;
 
-%% ¡methods!
-function uigetdir(im)
-    % UIGETDIR opens a dialog box to set the directory from where to load the TXT files of the FUN subject group.
+%% ¡tests!
 
-    directory = uigetdir('Select directory');
-    if ischar(directory) && isfolder(directory)
-        im.set('DIRECTORY', directory);
-    end
-end
+%%% ¡excluded_props!
+[ImporterGroupSubjectFUN_TXT.GET_DIR]
