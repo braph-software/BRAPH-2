87935a238c274aa540ec9ce92fc58190229b654c
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-04-25 13:08:21 +0200

    Update _ImporterGroupSubjectCON_XLS.gen.m

--- braph2genesis/pipelines/connectivity/_ImporterGroupSubjectCON_XLS.gen.m
+++ braph2genesis/pipelines/connectivity/_ImporterGroupSubjectCON_XLS.gen.m
@@ -186,10 +186,49 @@ mkdir(data_dir);
 % Brain Atlas
 ba = ImporterBrainAtlasXLS('FILE', 'desikan_atlas.xlsx');
 ExporterBrainAtlasXLS('FILE', [data_dir filesep() 'desikan_atlas.xlsx'])
+N = ba.get('LENGTH');
 
 % Group 1
+K1 = 2; % degree (mean node degree is 2K) - group 1
+beta1 = 0.3; % Rewiring probability - group 1
+for i = 1:1:50 % subject number
+    h1 = WattsStrogatz(N, K1, beta1); % create two WS graph
+    % figure(1) % Plot the two graphs to double-check
+    % plot(h1, 'NodeColor',[1 0 0], 'EdgeColor',[0 0 0], 'EdgeAlpha',0.1, 'Layout','circle');
+    % title(['Group 1: Graph with $N = $ ' num2str(N_nodes) ...
+    %     ' nodes, $K = $ ' num2str(K1) ', and $\beta = $ ' num2str(beta1)], ...
+    %     'Interpreter','latex')
+    % axis equal
+
+    A1 = full(adjacency(h1)); A1(1:length(A1)+1:numel(A1)) = 0; % extract the adjacency matrix
+    r = 0 + (0.5 - 0)*rand(size(A1)); diffA = A1 - r; A1(A1 ~= 0) = diffA(A1 ~= 0); % make the adjacency matrix weighted
+    A1 = max(A1, transpose(A1)); % make the adjacency matrix symmetric
+
+    gr1_dir = [data_dir filesep() 'CON_Group_1_XLS'];
+    mkdir(gr1_dir);
+    writetable(array2table(A1), [gr1_dir filesep() 'SubjectCON_' num2str(i) '.xlsx'], 'WriteRowNames', false, 'WriteVariableNames', false)
+end
 
 % Group 2
+K2 = 2; % degree (mean node degree is 2K) - group 2
+beta2 = 0.85; % Rewiring probability - group 2
+for i = 1:1:50
+    h2 = WattsStrogatz(N, K2, beta2);
+    % figure(2)
+    % plot(h2, 'NodeColor',[1 0 0], 'EdgeColor',[0 0 0], 'EdgeAlpha',0.1, 'Layout','circle');
+    % title(['Group 2: Graph with $N = $ ' num2str(N_nodes) ...
+    %     ' nodes, $K = $ ' num2str(K2) ', and $\beta = $ ' num2str(beta2)], ...
+    %     'Interpreter','latex')
+    % axis equal
+
+    A2 = full(adjacency(h2)); A2(1:length(A2)+1:numel(A2)) = 0;
+    r = 0 + (0.5 - 0)*rand(size(A2)); diffA = A2 - r; A2(A2 ~= 0) = diffA(A2 ~= 0);
+    A2 = max(A2, transpose(A2));
+
+    gr2_dir = [data_dir filesep() 'CON_Group_2_XLS'];
+    mkdir(gr2_dir);
+    writetable(array2table(A2), [gr2_dir filesep() 'SubjectCON_' num2str(i) '.xlsx'], 'WriteRowNames', false, 'WriteVariableNames', false)
+end
 
 %%% ¡test!
 %%%% ¡name!
