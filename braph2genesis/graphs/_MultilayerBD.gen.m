d9f6b543f875677a90c0074c7f2063fdaf7f3a71
bzufiriaki <blanca.zufiria.gerboles@ki.se>
2023-04-26 10:41:19 +0200

    add MultilayerBD and change Distance measure

---
+++ braph2genesis/graphs/_MultilayerBD.gen.m
@@ -0,0 +1,248 @@
+%% ¡header!
+MultilayerBD < Graph (g, multilayer binary directed graph) is a multilayer binary directed graph.
+
+%%% ¡description!
+In a multilayer binary directed (BD) graph, layers have different number 
+ of nodes with within-layer directed edgeseither 0 (absence of connection) 
+ or 1 (existence of connection).
+There are connections between layers connecting the corresponding nodes.
+
+%% ¡props_update!
+
+%%% ¡prop!
+
+NAME (constant, string) is the name of the multilayer weighted undirected graph.
+%%%% ¡default!
+'MultilayerBD'
+
+%%% ¡prop!
+DESCRIPTION (constant, string) is the description of the multilayer weighted undirected graph.
+%%%% ¡default!
+'In a multilayer binary directed (BD) graph, layers have different number of nodes with within-layer directed edgeseither 0 (absence of connection) or 1 (existence of connection). There are connections between layers connecting the corresponding nodes.'
+
+%%% ¡prop!
+TEMPLATE (parameter, item) is the template of the multilayer binary directed graph.
+%%% ¡_prop!
+% % % TEMPLATE (parameter, item) is the graph template to set the graph and measure parameters.
+% % % %%%% ¡_settings!
+% % % 'MultilayerBD'
+
+%%% ¡prop!
+ID (data, string) is a few-letter code for the multilayer binary directed graph.
+%%%% ¡default!
+'MultilayerBD ID'
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the multilayer binary directed graph.
+%%%% ¡default!
+'MultilayerBD label'
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the multilayer binary directed graph.
+%%%% ¡default!
+'MultilayerBD notes'
+
+%%% ¡prop!
+GRAPH_TYPE (constant, scalar) returns the graph type __Graph.MULTILAYER__.
+%%%% ¡default!
+Graph.MULTILAYER
+
+%%% ¡prop!
+CONNECTIVITY_TYPE (query, smatrix) returns the connectivity type __Graph.BINARY__ * ones(layernumber).
+%%%% ¡calculate!
+if isempty(varargin)
+    layernumber = 1;
+else
+    layernumber = varargin{1};
+end
+value = Graph.BINARY * ones(layernumber);
+
+%%% ¡prop!
+DIRECTIONALITY_TYPE (query, smatrix) returns the directionality type __Graph.DIRECTED__ * ones(layernumber).
+%%%% ¡calculate!
+if isempty(varargin)
+    layernumber = 1;
+else
+    layernumber = varargin{1};
+end
+value = Graph.DIRECTED * ones(layernumber);
+
+%%% ¡prop!
+SELFCONNECTIVITY_TYPE (query, smatrix) returns the self-connectivity type __Graph.NONSELFCONNECTED__ on the diagonal and __Graph.SELFCONNECTED__ off diagonal.
+%%%% ¡calculate!
+if isempty(varargin)
+    layernumber = 1;
+else
+    layernumber = varargin{1};
+end
+value = Graph.SELFCONNECTED * ones(layernumber);
+value(1:layernumber+1:end) = Graph.NONSELFCONNECTED;                
+
+%%% ¡prop!
+NEGATIVITY_TYPE (query, smatrix) returns the negativity type __Graph.NONNEGATIVE__ * ones(layernumber).
+%%%% ¡calculate!
+if isempty(varargin)
+    layernumber = 1;
+else
+    layernumber = varargin{1};
+end
+value = Graph.NONNEGATIVE * ones(layernumber);
+
+%%% ¡prop!
+A (result, cell) is the cell containing the multilayer weighted adjacency matrices of the multilayer weighted undirected graph.
+%%%% ¡calculate!
+B = g.get('B'); %#ok<PROPLC>
+L = length(B); %#ok<PROPLC> % number of layers
+for i = 1:1:L
+    M = dediagonalize(B{i}); % removes self-connections by removing diagonal from adjacency matrix, equivalent to dediagonalize(M, 'DediagonalizeRule', 0)
+    M = semipositivize(M, 'SemipositivizeRule', g.get('SEMIPOSITIVIZE_RULE')); % removes negative weights
+    M = binarize(M, varargin{:}); % enforces binary adjacency matrix, equivalent to binarize(M, 'threshold', 0, 'bins', [-1:.001:1])
+    B(i, i) = {M};
+end
+A = B;
+value = A;
+
+%%%% ¡gui!
+pr = PanelPropCell('EL', g, 'PROP', MultilayerBD.A, ...
+    'TABLE_HEIGHT', s(40), ...
+    'XYSLIDERLOCK', true, ... 
+    'XSLIDERSHOW', false, ...
+    'YSLIDERSHOW', true, ...
+    'YSLIDERLABELS', g.getCallback('ALAYERLABELS'), ...
+    'YSLIDERWIDTH', s(5), ...
+    'ROWNAME', g.getCallback('ANODELABELS'), ...
+    'COLUMNNAME', g.getCallback('ANODELABELS'), ...
+    varargin{:});
+
+%%% ¡prop!
+PARTITIONS (result, rvector) returns the number of layers in the partitions of the graph.
+%%%% ¡calculate!
+value = ones(1, g.get('LAYERNUMBER'));
+
+%%% ¡prop!
+ALAYERLABELS (query, stringlist) returns the layer labels to be used by the slider.
+%%%% ¡calculate!
+alayerlabels = g.get('LAYERLABELS');
+if isempty(alayerlabels) && ~isa(g.getr('A'), 'NoValue') % ensures that it's not unecessarily calculated
+    alayerlabels = cellfun(@num2str, num2cell([1:1:g.get('LAYERNUMBER')]), 'uniformoutput', false);
+end
+value = alayerlabels;
+
+%%% ¡prop!
+COMPATIBLE_MEASURES (constant, stringlist) is the list of compatible measures.
+%%%% ¡default!
+getCompatibleMeasures('MultilayerBD')
+
+%% ¡props!
+
+%%% ¡prop!
+B (data, cell) is the input cell containing the multiplex adjacency matrices.
+%%%% ¡default!
+{[] []; [] []}
+%%%% ¡gui!
+pr = PanelPropCell('EL', g, 'PROP', MultilayerBD.B, ...
+    'TABLE_HEIGHT', s(40), ...
+    'XSLIDERSHOW', true, ...
+    'XSLIDERLABELS', g.get('LAYERLABELS'), ...
+    'XSLIDERHEIGHT', s(3.5), ...
+    'YSLIDERSHOW', false, ...
+    'ROWNAME', g.getCallback('ANODELABELS'), ...
+    'COLUMNNAME', g.getCallback('ANODELABELS'), ...
+    varargin{:});
+
+
+%%% ¡prop!
+SEMIPOSITIVIZE_RULE (parameter, option) determines how to remove the negative edges.
+%%%% ¡settings!
+{'zero', 'absolute'}
+
+%% ¡tests!
+
+%%% ¡excluded_props!
+[MultilayerBD.PFGA MultilayerBD.PFGH]
+
+%%% ¡test!
+%%%% ¡name!
+Constructor - Full
+%%%% ¡probability!
+.01
+%%%% ¡code!
+B1 = rand(randi(10));
+B2 = rand(randi(10));
+B3 = rand(randi(10));
+B12 = rand(size(B1,1),size(B2,2));
+B13 = rand(size(B1,1),size(B3,2));
+B23 = rand(size(B2,1),size(B3,2));
+B = {
+    B1                           B12                            B13
+    B12'                         B2                             B23
+    B13'                         B23'                           B3
+    };
+g = MultilayerBD('B', B);
+g.get('A_CHECK')
+A1 = binarize(semipositivize(dediagonalize(B1)));
+A2 = binarize(semipositivize(dediagonalize(B2)));
+A3 = binarize(semipositivize(dediagonalize(B3)));
+B{1,1} = A1;
+B{2,2} = A2;
+B{3,3} = A3;
+A = B
+assert(isequal(g.get('A'), A), ...
+    [BRAPH2.STR ':MultilayerBD:' BRAPH2.FAIL_TEST], ...
+    'MultilayerBD is not constructing well.')
+
+% %%% ¡test!
+% %%%% ¡name!
+% Semipositivize Rule
+% %%%% ¡probability!
+% .01
+% %%%% ¡code!
+
+
+%% ¡_props!
+
+%%% ¡_prop!
+ATTEMPTSPEREDGE (parameter, scalar) is the attempts to rewire each edge.
+%%%% ¡_default!
+5
+
+%% ¡_methods!
+function random_g = randomize(g)
+    % RANDOMIZE returns a randomized graph
+    %
+    % RANDOMIZED_G = RANDOMIZE(G) returns the randomized
+    % graph RANDOM_G obtained with a randomized correlation
+    % matrix via the static function randomize_A while preserving
+    % degree distributions. The randomization it is done layer by
+    % layer and then integrated in the 2-D supra-adjacency matrix
+    % cell array.
+    %
+    % RANDOMIZED_G = RANDOMIZE(G, 'AttemptsPerEdge', VALUE)
+    % returns the randomized graph RANDOM_G obtained with a
+    % randomized correlation matrix via the static function
+    % randomize_A while preserving  degree distributions.
+    % The multilayer is randomized layer by layer where randomized
+    % adjacency matrix of each layer are then integrated in the
+    % 2-D supra-adjacency matrix cell array.
+    %
+    % See also GraphBD
+
+    % get rules
+    attempts_per_edge = g.get('ATTEMPTSPEREDGE');
+
+    if nargin<2
+        attempts_per_edge = 5;
+    end
+
+    % get A
+    A = g.get('A');
+    L = g.layernumber();
+    random_multi_A = cell(1, L);
+
+    for li = 1:1:L
+        Aii = A{li, li};
+        random_A = GraphBD.randomize_A(Aii, attempts_per_edge);
+        random_multi_A(li) = {random_A};
+    end
+    random_g = MultilayerBD('B', random_multi_A);
+end
