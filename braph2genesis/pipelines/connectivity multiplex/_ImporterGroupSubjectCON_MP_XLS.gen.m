4a4b259ecbbc6c8504b8069d5af5e85b7e607de3
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-04-28 18:18:51 +0200

    Update _ImporterGroupSubjectCON_MP_XLS.gen.m

--- braph2genesis/pipelines/connectivity multiplex/_ImporterGroupSubjectCON_MP_XLS.gen.m
+++ braph2genesis/pipelines/connectivity multiplex/_ImporterGroupSubjectCON_MP_XLS.gen.m
@@ -2,14 +2,18 @@
 ImporterGroupSubjectCON_MP_XLS < Importer (im, importer of CON MP subject group from XLS/XLSX) imports a group of subjects with connectivity multiplex data from a series of XLS/XLSX file.
 
 %%% ¡description!
-ImporterGroupSubjectCON_MP_XLS imports a group of subjects with connectivity multiplex data from a series of XLS/XLSX files and their covariates (optional) from another XLS/XLSX file.
-All these files must be in the same subject folder; also, no other files should be in the folder.
-Inside each subject folder, there should be a file for each layer containing 
-a table with each row corresponding to a time serie and each column to a brain region.
-The XLS/XLSX file containing the covariates must be in the same group directory 
-and consists of the following columns:
-Subject ID (column 1), Subject AGE (column 2), and Subject SEX (column 3).
-The first row contains the headers and each subsequent row the values for each subject.
+ImporterGroupSubjectCON_XLS imports a group of subjects with connectivity 
+ multiplex data from a series of XLS/XLSX files contained in a folder named 
+ "GROUP_ID". All these files must be in the same folder; also, no other 
+ files should be in the folder. Each file contains a table of values 
+ corresponding to the adjacency matrix of a layer. Files should be labeled 
+ with the layer number indicated as, e.g., "SUBJECT_ID.1.xlsx" and 
+ "SUBJECT_ID.2.xlsx".
+The variables of interest are from another XLS/XLSX file named "GROUP_ID_void.xlsx" 
+ (if exisitng) consisting of the following columns: 
+ Subject ID (column 1), covariates (subsequent columns). 
+ The 1st row contains the headers, the 2nd row a string with the categorical
+ variables of interest, and each subsequent row the values for each subject.
 
 %%% ¡seealso!
 Group, SubjectCON_MP, ExporterGroupSubjectCON_MP_XLS
@@ -88,7 +92,6 @@ directory = im.get('DIRECTORY');
 if isfolder(directory)
     wb = braph2waitbar(im.get('WAITBAR'), 0, 'Reading directory ...');
 
-    % sets group props
     [~, name] = fileparts(directory);
     gr.set( ...
         'ID', name, ...
@@ -97,71 +100,101 @@ if isfolder(directory)
     );
 
     try
-        subject_folders = dir(directory);
-        subject_folders = subject_folders([subject_folders(:).isdir] == 1);
-        subject_folders = subject_folders(~ismember({subject_folders(:).name}, {'.', '..'}));
+        braph2waitbar(wb, .15, 'Loading subject group ...')
 
-        if ~isempty(subject_folders)
-            braph2waitbar(wb, .15, 'Loading subject group ...')
-
-% % %             % Check if there are covariates to add (age and sex)
-% % %             file_cov_XLSX = dir(fullfile(directory, '*.xlsx'));
-% % %             file_cov_XLS = dir(fullfile(directory, '*.xls'));
-% % %             file_cov = [file_cov_XLSX; file_cov_XLS];
-% % %             if isfile(fullfile(directory, file_cov.name))
-% % %                 [~, ~, raw_covariates] = xlsread(fullfile(directory, file_cov.name));
-% % %                 age = raw_covariates(2:end, 2);
-% % %                 sex = raw_covariates(2:end, 3);
-% % %             else
-% % %                 age = {[0]};
-% % %                 age = age(ones(length(subject_folders), 1));
-% % %                 unassigned =  {'unassigned'};
-% % %                 sex = unassigned(ones(1, length(subject_folders)));
-% % %             end
-
-            % get all layers per subject folder
-            for i = 1:1:length(subject_folders)
-                braph2waitbar(wb, .25 + .75 * i / length(subject_folders), ['Loading subject ' num2str(i) ' of ' num2str(length(subject_folders)) ' ...'])
-
-                subjects_paths = [directory filesep() subject_folders(i).name];
-                % analyzes file
-                files_XLSX = dir(fullfile(subjects_paths, '*.xlsx'));
-                files_XLS = dir(fullfile(subjects_paths, '*.xls'));
-                files = [files_XLSX; files_XLS];
-
-                CON_MP = cell(1, length(files));
-                % get all layers per subject folder
-                for j = 1:1:length(files)
-                    CON_MP(j) = {xlsread(fullfile(subjects_paths, files(j).name))};
+        files = [dir(fullfile(directory, '*.xlsx')); dir(fullfile(directory, '*.xls'))];
+        
+        if ~isempty(files)
+            % brain atlas
+            ba = im.get('BA');
+            if ba.get('BR_DICT').get('LENGTH') == 0
+                % adds the number of regions of the first file to the brain atlas
+                br_number = size(xlsread(fullfile(directory, files(1).name)), 1);
+                br_dict = ba.memorize('BR_DICT');
+                for j = 1:1:br_number
+                    br_dict.get('ADD', BrainRegion('ID', ['br' int2str(j)]))
                 end
+            end
+            
+            % determines the numebr of layers and the subjects
+            L = 0;
+            sub_ids = {};
+            for i = 1:1:length(files)
+                [~, sub_id_layer_no] = fileparts(files(i).name);
+                splits = regexp(sub_id_layer_no, '(.+)\\.(\\d+)', 'tokens');
+                sub_ids = [sub_ids, splits{1}{1}];
+                L = max(L, int32(str2double(splits{1}{2})));
+            end
+            sub_ids = unique(sub_ids);
+            
+            % adds subjects
+            sub_dict = gr.memorize('SUB_DICT');
+            for i = 1:1:length(sub_ids)
+                braph2waitbar(wb, .15 + .85 * i / length(files), ['Loading subject ' num2str(i) ' of ' num2str(length(files)) ' ...'])
 
-                % brain atlas
-                ba = im.get('BA');
-                br_number = size(CON_MP{1}, 2);
-                if ba.get('BR_DICT').get('LENGTH') ~= br_number
-                    ba = BrainAtlas();
-                    idict = ba.get('BR_DICT');
-                    for j = 1:1:br_number
-                        br_id = ['br' int2str(j)];
-                        br = BrainRegion('ID', br_id);
-                        idict.get('ADD', br)
+                % read files
+                CON_MP = {};
+                for l = 1:1:L
+                    filename = fullfile(directory, [sub_ids{i} '.' int2str() '.xls']);
+                    if isfile(filename)
+                        CON = xlsread(filename);
+                    else
+                        CON = xlsread([filename 'x']);
                     end
-                    ba.set('BR_DICT', idict);
+                    if size(CON, 1) ~= ba.get('BR_DICT').get('LENGTH') || size(CON, 2) ~= ba.get('BR_DICT').get('LENGTH')
+                        error( ...
+                            [BRAPH2.STR ':' class(im) ':' BRAPH2.ERR_IO], ...
+                            [BRAPH2.STR ':' class(im) ':' BRAPH2.ERR_IO '\\n' ...
+                            'The file ' sub_id ' should contain a matrix ' int2str(ba.get('BR_DICT').get('LENGTH')) 'x' int2str(ba.get('BR_DICT').get('LENGTH')) ', ' ...
+                            'while it is ' int2str(size(CON, 1)) 'x' int2str(size(CON, 2)) '.'] ...
+                            )
+                    end
+                    CON_MP = [CON_MP, CON];
                 end
-
-                sub_dict = gr.get('SUB_DICT');           
-                sub = SubjectCON_MP( ...
-                    'ID', subject_folders(i).name, ...
+                
+                sub = SubjectCON( ...
+                    'ID', sub_id, ...
                     'BA', ba, ...
-                    'L', length(files), ...
-                    'CON_MP', CON_MP ... % % % 'age', age{i}, ... % % % 'sex', sex{i} ...
-                    );
-
+                    'L', L, ...
+                    'CON_MP', CON_MP ...
+                );
                 sub_dict.get('ADD', sub);
             end
-            gr.set('SUB_DICT', sub_dict);
+            
+            % variables of interest
+            vois = [];
+            if isfile([directory '_vois.xls'])
+                [~, ~, vois] = xlsread([directory '_vois.xls']);
+            elseif isfile([directory '_vois.xlsx'])
+                [~, ~, vois] = xlsread([directory '_vois.xlsx']);
+            end
+            if ~isempty(vois)
+                for i = 3:1:size(vois, 1)
+                    sub_id = vois{i, 1};
+                    sub = sub_dict.get('IT', sub_id);
+                    for v = 2:1:size(vois, 2)
+                        voi_id = vois{1, v};
+                        if isnumeric(vois{2, v}) % VOINumeric
+                            sub.memorize('VOI_DICT').get('ADD', ...
+                                VOINumeric( ...
+                                    'ID', voi_id, ...
+                                    'V', vois{i, v} ...
+                                    ) ...
+                                );
+                        elseif ischar(vois{2, v}) % VOICategoric
+                            sub.memorize('VOI_DICT').get('ADD', ...
+                                VOICategoric( ...
+                                    'ID', voi_id, ...
+                                    'CATEGORIES', str2cell(vois{2, v}), ...
+                                    'V', find(strcmp(vois{i, v}, str2cell(vois{2, v}))) ...
+                                    ) ...
+                                );
+                        end                        
+                    end
+                end
+            end
         end
-        catch e
+    catch e
             braph2waitbar(wb, 'close')
 
             rethrow(e)
@@ -169,8 +202,8 @@ if isfolder(directory)
     
     braph2waitbar(wb, 'close')
 else
-    error([BRAPH2.STR ':ImporterGroupSubjectCON_MP_XLS:' BRAPH2.CANCEL_IO], ...
-        [BRAPH2.STR ':ImporterGroupSubjectCON_MP_XLS:' BRAPH2.CANCEL_IO '\\n' ...
+    error([BRAPH2.STR ':ImporterGroupSubjectCON_MP_XLS:' BRAPH2.ERR_IO], ...
+        [BRAPH2.STR ':ImporterGroupSubjectCON_MP_XLS:' BRAPH2.ERR_IO '\\n' ...
         'The prop DIRECTORY must be an existing directory, but it is ''' directory '''.'] ...
         );
 end
@@ -264,8 +297,8 @@ for i = 1:1:50 % subject number
     % figure(2)
     % imshow(A_full2)
 
-    writetable(array2table(A_full1), [gr1_dir filesep() sub_id '_L1.xlsx'], 'WriteVariableNames', false)
-    writetable(array2table(A_full2), [gr1_dir filesep() sub_id '_L2.xlsx'], 'WriteVariableNames', false)
+    writetable(array2table(A_full1), [gr1_dir filesep() sub_id '.1.xlsx'], 'WriteVariableNames', false)
+    writetable(array2table(A_full2), [gr1_dir filesep() sub_id '.2.xlsx'], 'WriteVariableNames', false)
     
     % variables of interest
     vois1 = [vois1; {sub_id, randi(90), sex_options(randi(2))}];
@@ -284,7 +317,7 @@ indices1 = 1:1:45;
 indices2 = 46:1:90;
 indices = {indices1; indices2};
 
-gr2_name = 'CON_Group_2_XLS';
+gr2_name = 'CON_MP_Group_2_XLS';
 gr2_dir = [data_dir filesep() gr2_name];
 mkdir(gr2_dir);
 vois2 = [
@@ -334,13 +367,13 @@ for i = 51:1:100
     % figure(2)
     % imshow(A_full2)
 
-    writetable(array2table(A_full1), [gr2_dir filesep() sub_id num2str(i) '_L1.xlsx'], 'WriteVariableNames', false)
-    writetable(array2table(A_full2), [gr2_dir filesep() sub_id num2str(i) '_L2.xlsx'], 'WriteVariableNames', false)
+    writetable(array2table(A_full1), [gr2_dir filesep() sub_id num2str(i) '.1.xlsx'], 'WriteVariableNames', false)
+    writetable(array2table(A_full2), [gr2_dir filesep() sub_id num2str(i) '.2.xlsx'], 'WriteVariableNames', false)
     
     % variables of interest
     vois2 = [vois2; {sub_id, randi(90), sex_options(randi(2))}];
 end
-% writetable(table(vois2), [data_dir filesep() gr2_name '_vois.xlsx'], 'WriteVariableNames', false)
+writetable(table(vois2), [data_dir filesep() gr2_name '_vois.xlsx'], 'WriteVariableNames', false)
 
 % reset RNG
 rng(rng_settings_)
