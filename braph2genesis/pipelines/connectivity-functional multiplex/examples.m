7c1137d1703b4a598802b3ed063b1e047488ee48
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-04-29 19:27:54 +0200

    Update examples.m

--- braph2genesis/pipelines/connectivity-functional multiplex/examples.m
+++ braph2genesis/pipelines/connectivity-functional multiplex/examples.m
@@ -22,6 +22,8 @@ rng_settings_ = rng(); rng('default')
 
 sex_options = {'Female' 'Male'};
 
+T = 200; % Length of the time series
+
 % Group 1 directories
 gr1_name = 'CON_FUN_MP_Group_1_XLS';
 gr1_dir = [data_dir filesep() gr1_name];
@@ -91,6 +93,95 @@ for i = 51:1:100
     vois2 = [vois2; {sub_id, randi(90), sex_options(randi(2))}];
 end
 writetable(table(vois2), [data_dir filesep() gr2_name '.vois.xlsx'], 'WriteVariableNames', false)
+
+% Group 1 - FUN - 5 modules of 18 nodes each
+% initialize values for the WS model
+K1 = [3 4 5 6 7];
+beta1 = [0.02 0.1 0.3 0.5 0.8];
+% initialize the indices where the matrices will be placed
+indices1 = 1:1:18;
+indices2 = 19:1:36;
+indices3 = 37:1:54;
+indices4 = 55:1:72;
+indices5 = 73:1:90;
+indices = {indices1; indices2; indices3; indices4; indices5};
+for i = 1:1:50 % subject number
+    sub_id = ['SubjectCON_FUN_MP_' num2str(i)];
+
+    % randomize the parameters
+    K_temp = K1(randperm(length(K1)));
+    beta_temp = beta1(randperm(length(beta1)));
+
+    % initialize matrix for the subject
+    A_full = zeros(N);
+
+    % loop over each module
+    for i_mod = 1:1:5
+        A_full(indices{i_mod}, indices{i_mod}) = full(adjacency(WattsStrogatz(18, K_temp(i_mod), beta_temp(i_mod))));
+    end
+    A_full(1:length(A_full)+1:numel(A_full)) = 1;
+
+    % this is needed to make the matrices positive definite
+    A_full = A_full * transpose(A_full);
+    % figure(1)
+    % imshow(A_full)
+
+    % This matrix will be covariance matrices for the two groups
+    % Specify the mean
+    mu_gr1 = ones(1, length(A_full));
+    % calculate time series
+    R1 = mvnrnd(mu_gr1, A_full, T);
+    % Normalize the time series
+    mean_R1 = mean(R1);
+    std_R1 = std(R1);
+    R1 = (R1 - mean(R1)) ./ std(R1);
+    
+    writetable(array2table(R1), [gr1_dir '.FUN' filesep() sub_id '.FUN.xlsx'], 'WriteVariableNames', false)
+end
+
+% Group 2 - FUN - 2 modules of 45 nodes each
+% initialize values for the WS model
+K2 = [3 7];
+beta2 = [0.02 0.85];
+% initialize the indices where the matrices will be placed
+indices1 = 1:1:45;
+indices2 = 46:1:90;
+indices = {indices1; indices2};
+for i = 51:1:100
+    sub_id = ['SubjectCON_FUN_MP_' num2str(i)];
+
+    % randomize the parameters
+    K_temp = K2(randperm(length(K2)));
+    beta_temp = beta2(randperm(length(beta2)));
+
+    % initialize matrix for the subject
+    A_full = zeros(N);
+
+    % loop over each module
+    for i_mod = 1:1:2
+    A_full(indices{i_mod},indices{i_mod}) = full(adjacency(WattsStrogatz(45, K_temp(i_mod), beta_temp(i_mod))));
+    end
+    A_full(1:length(A_full)+1:numel(A_full)) = 1;
+
+    % this is needed to make the matrices positive definite
+    A_full = A_full * transpose(A_full);
+    % figure(2)
+    % imshow(A_full)
+
+    % This matrix will be covariance matrices for the two groups
+    % Specify the mean
+    mu_gr2 = ones(1, length(A_full));
+
+    % calculate time series
+    R2 = mvnrnd(mu_gr2, A_full, T);
+
+    % Normalize the time series
+    mean_R2 = mean(R2);
+    std_R2 = std(R2);
+    R2 = (R2 - mean(R2)) ./ std(R2);
+
+    writetable(array2table(R2), [gr2_dir '.FUN' filesep() sub_id '.FUN.xlsx'], 'WriteVariableNames', false)
+end
 
 % reset RNG
 rng(rng_settings_)
