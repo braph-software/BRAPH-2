e29a491c885a6b84f446b0515f4f73bc1323c588
egolol <egolol87@gmail.com>
2023-06-01 16:35:46 +0200

    measures

---
+++ braph2genesis/measures/_Clustering.gen.m
@@ -0,0 +1,286 @@
+%% ¡header!
+Clustering < Triangles (m, clustering) is the graph clustering.
+
+%%% ¡description!
+The clustering coefficient of a node is the fraction of triangles present 
+around a node. The clustering coefficient is calculated as the ratio between 
+the number of triangles present around a node and the maximum number of 
+triangles that could possibly be formed around that node.
+
+%% ¡props_update!
+
+%%% ¡prop!
+NAME (constant, string) is the name of the clustering.
+%%%% ¡default!
+'Clustering'
+
+%%% ¡prop!
+DESCRIPTION (constant, string) is the description of the clustering.
+%%%% ¡default!
+'The clustering coefficient of a node is the fraction of triangles present around a node. The clustering coefficient is calculated as the ratio between the number of triangles present around a node and the maximum number of triangles that could possibly be formed around that node.'
+
+%%% ¡prop!
+TEMPLATE (parameter, item) is the template of the clustering.
+
+%%% ¡prop!
+ID (data, string) is a few-letter code of the clustering.
+%%%% ¡default!
+'Clustering ID'
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the clustering.
+%%%% ¡default!
+'Clustering label'
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the clustering.
+%%%% ¡default!
+'Clustering notes'
+
+%%% ¡prop!
+SHAPE (constant, scalar) is the measure shape __Measure.NODAL__.
+%%%% ¡default!
+Measure.NODAL
+
+%%% ¡prop!
+SCOPE (constant, scalar) is the measure scope __Measure.UNILAYER__.
+%%%% ¡default!
+Measure.UNILAYER
+
+%%% ¡prop!
+PARAMETRICITY (constant, scalar) is the parametricity of the measure __Measure.NONPARAMETRIC__.
+%%%% ¡default!
+Measure.NONPARAMETRIC
+
+%%% ¡prop!
+COMPATIBLE_GRAPHS (constant, classlist) is the list of compatible graphs.
+%%%% ¡default!
+{'GraphWU' 'GraphWD' 'GraphBU' 'GraphBD' 'MultigraphBUD' 'MultigraphBUT' 'MultiplexWU' 'MultiplexWD' 'MultiplexBU' 'MultiplexBUD' 'MultiplexBUT' 'MultiplexBD'}
+
+%%% ¡prop!
+M (result, cell) is the clustering.
+%%%% ¡calculate!
+g = m.get('G');  % graph from measure class
+A = g.get('A');  % cell with adjacency matrix (for graph) or 2D-cell array (for multigraph, multiplex, etc.) 
+L = g.get('LAYERNUMBER');
+
+triangles = calculateValue@Triangles(m, prop);
+
+clustering = cell(L, 1);
+directionality_type =   g.get('DIRECTIONALITY_TYPE', L);
+            
+parfor li = 1:1:L
+    Aii = A{li, li};
+    if directionality_type == Graph.UNDIRECTED              
+        degree = Degree('G', g).get('M');
+        clustering_layer = 2 * triangles{li} ./ (degree{li} .* (degree{li} - 1));
+
+    else %% directed graphs    
+        in_degree = InDegree('G', g).get('M');
+        out_degree = OutDegree('G', g).get('M');
+        directed_triangles_rule = m.get('RULE');
+        switch lower(directed_triangles_rule)
+            case {'all'}  % all rule
+                clustering_layer = triangles{li} ./ ((out_degree{li} + in_degree{li}) .* (out_degree{li} + in_degree{li} - 1) - 2 * diag(Aii^2));
+            case {'middleman'}  % middleman rule
+                clustering_layer = triangles{li} ./ ((out_degree{li} .* in_degree{li}) - diag(Aii^2));
+            case {'in'}  % in rule
+                clustering_layer = triangles{li} ./ (in_degree{li} .* (in_degree{li} - 1));
+            case {'out'}  % out rule
+                clustering_layer = triangles{li} ./ (out_degree{li} .* (out_degree{li} - 1));
+            otherwise  % {'cycle'}  % cycle rule
+                clustering_layer = triangles{li} ./ ((out_degree{li} .* in_degree{li}) - diag(Aii^2));
+        end
+    end
+    clustering_layer(isnan(clustering_layer)) = 0;  % Should return zeros, not NaN
+    clustering(li) = {clustering_layer};
+end
+
+value = clustering;
+
+
+%% ¡tests!
+
+%%% ¡excluded_props!
+[Clustering.PFM]
+
+%%% ¡test!
+%%%% ¡name!
+GraphBU
+%%%% ¡code!
+B_BU = [
+    0 1 1 1; 
+    1 0 1 0; 
+    1 1 0 1; 
+    1 0 1 0
+    ];
+clustering_BU = {[2/3 1 2/3 1]'};
+g = GraphBU('B', B_BU);
+m_outside_g = Clustering('G', g);
+assert(isequal(m_outside_g.get('M'), clustering_BU), ...
+    [BRAPH2.STR ':Clustering:' BRAPH2.FAIL_TEST], ...
+    [class(m_outside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+m_inside_g = g.get('MEASURE', 'Clustering');
+assert(isequal(m_inside_g.get('M'), clustering_BU), ...
+    [BRAPH2.STR ':Clustering:' BRAPH2.FAIL_TEST], ...
+    [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+
+%%% ¡test!
+%%%% ¡name!
+GraphBD
+%%%% ¡code!
+B_BD = [
+    0 0 1; 
+    1 0 0; 
+    0 1 0 
+    ];
+clustering_BD_out = {[0 0 0]'};  % out rule
+
+g = GraphBD('B', B_BD);
+m_outside_g = Clustering('G', g, 'RULE', 'out');
+assert(isequal(m_outside_g.get('M'), clustering_BD_out), ...
+    [BRAPH2.STR ':Clustering:' BRAPH2.FAIL_TEST], ...
+    [class(m_outside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+m_inside_g = g.get('MEASURE', 'Clustering');
+m_inside_g.set('RULE', 'out');
+assert(isequal(m_inside_g.get('M'), clustering_BD_out), ...
+    [BRAPH2.STR ':Clustering:' BRAPH2.FAIL_TEST], ...
+    [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+%%% ¡test!
+%%%% ¡name!
+MultigraphBUT
+%%%% ¡code!
+B = [
+    0 1 1 1; 
+    1 0 1 0; 
+    1 1 0 1; 
+    1 0 1 0
+    ];
+
+thresholds = [0 1];
+
+known_clustering = {
+    [2/3 1 2/3 1]'
+    [0   0 0   0]'
+    };
+
+g = MultigraphBUT('B', B, 'THRESHOLDS', thresholds);
+
+m_outside_g = Clustering('G', g);
+assert(isequal(m_outside_g.get('M'), known_clustering), ...
+    [BRAPH2.STR ':Clustering:' BRAPH2.FAIL_TEST], ...
+    [class(m_outside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+m_inside_g = g.get('MEASURE', 'Clustering');
+assert(isequal(m_inside_g.get('M'), known_clustering), ...
+    [BRAPH2.STR ':Clustering:' BRAPH2.FAIL_TEST], ...
+    [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+%%% ¡test!
+%%%% ¡name!
+MultiplexBU
+%%%% ¡code!
+B11 = [
+      0 1 1 1;
+      1 0 1 0;
+      1 1 0 1;
+      1 0 1 0
+      ];
+B22 = [
+      0 1 1 1;
+      1 0 1 0;
+      1 1 0 1;
+      1 0 1 0
+      ];
+B  = {B11 B22};
+
+known_clustering = {
+                 [2/3 1 2/3 1]'
+                 [2/3 1 2/3 1]'
+                 };      
+
+g = MultiplexBU('B', B);
+m_outside_g = Clustering('G', g);
+assert(isequal(m_outside_g.get('M'), known_clustering), ...
+    [BRAPH2.STR ':Clustering:' BRAPH2.FAIL_TEST], ...
+    [class(m_outside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+m_inside_g = g.get('MEASURE', 'Clustering');
+assert(isequal(m_inside_g.get('M'), known_clustering), ...
+    [BRAPH2.STR ':Clustering:' BRAPH2.FAIL_TEST], ...
+    [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+%%% ¡test!
+%%%% ¡name!
+MultiplexBUT
+%%%% ¡code!
+B11 = [
+      0 1 1 1;
+      1 0 1 0;
+      1 1 0 1;
+      1 0 1 0
+      ];
+B22 = [
+      0 1 1 1;
+      1 0 1 0;
+      1 1 0 1;
+      1 0 1 0
+      ];
+B  = {B11 B22};
+
+thresholds = [0 1];
+known_clustering = {
+                 [2/3 1 2/3 1]'
+                 [2/3 1 2/3 1]'
+                 [0 0 0 0]'
+                 [0 0 0 0]'
+                 };      
+
+g = MultiplexBUT('B', B, 'THRESHOLDS', thresholds);
+m_outside_g = Clustering('G', g);
+assert(isequal(m_outside_g.get('M'), known_clustering), ...
+    [BRAPH2.STR ':Clustering:' BRAPH2.FAIL_TEST], ...
+    [class(m_outside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+m_inside_g = g.get('MEASURE', 'Clustering');
+assert(isequal(m_inside_g.get('M'), known_clustering), ...
+    [BRAPH2.STR ':Clustering:' BRAPH2.FAIL_TEST], ...
+    [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+%%% ¡test!
+%%%% ¡name!
+MultiplexBD
+%%%% ¡code!
+B11 = [
+      0 0 1; 
+      1 0 0; 
+      0 1 0 
+      ];
+B22 = [
+      0 0 1; 
+      1 0 0; 
+      0 1 0 
+      ];
+B = {B11 B22};
+
+% cycle rule - default
+known_clustering = {
+                 [1 1 1]'
+                 [1 1 1]'
+                 }; 
+
+g = MultiplexBD('B', B);
+m_outside_g = Clustering('G', g, 'RULE', 'cycle');
+assert(isequal(m_outside_g.get('M'), known_clustering), ...
+    [BRAPH2.STR ':Clustering:' BRAPH2.FAIL_TEST], ...
+    [class(m_outside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+m_inside_g = g.get('MEASURE', 'Clustering');
+m_inside_g.set('RULE', 'cycle');
+assert(isequal(m_inside_g.get('M'), known_clustering), ...
+    [BRAPH2.STR ':Clustering:' BRAPH2.FAIL_TEST], ...
+    [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
---
+++ braph2genesis/measures/_ClusteringAv.gen.m
@@ -0,0 +1,253 @@
+%% ¡header!
+ClusteringAv < Clustering (m, clusteringav) is the graph clusteringav.
+
+%%% ¡description!
+The average clustering of a graph is the average of the clustering 
+coefficients of all nodes. Connection weights are ignored in calculations.
+
+%% ¡props_update!
+
+%%% ¡prop!
+NAME (constant, string) is the name of the clusteringav.
+%%%% ¡default!
+'ClusteringAv'
+
+%%% ¡prop!
+DESCRIPTION (constant, string) is the description of the clusteringav.
+%%%% ¡default!
+'The average clustering of a graph is the average of the clustering coefficients of all nodes. Connection weights are ignored in calculations.'
+
+%%% ¡prop!
+TEMPLATE (parameter, item) is the template of the clusteringav.
+
+%%% ¡prop!
+ID (data, string) is a few-letter code of the clusteringav.
+%%%% ¡default!
+'ClusteringAv ID'
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the clusteringav.
+%%%% ¡default!
+'ClusteringAv label'
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the clusteringav.
+%%%% ¡default!
+'ClusteringAv notes'
+
+%%% ¡prop!
+SHAPE (constant, scalar) is the measure shape __Measure.GLOBAL__.
+%%%% ¡default!
+Measure.GLOBAL
+
+%%% ¡prop!
+SCOPE (constant, scalar) is the measure scope __Measure.UNILAYER__.
+%%%% ¡default!
+Measure.UNILAYER
+
+%%% ¡prop!
+PARAMETRICITY (constant, scalar) is the parametricity of the measure __Measure.NONPARAMETRIC__.
+%%%% ¡default!
+Measure.NONPARAMETRIC
+
+%%% ¡prop!
+COMPATIBLE_GRAPHS (constant, classlist) is the list of compatible graphs.
+%%%% ¡default!
+{'GraphWU' 'GraphWD' 'GraphBU' 'GraphBD' 'MultigraphBUD' 'MultigraphBUT' 'MultiplexWU' 'MultiplexWD' 'MultiplexBU' 'MultiplexBUD' 'MultiplexBUT' 'MultiplexBD'}
+
+%%% ¡prop!
+M (result, cell) is the clusteringav.
+%%%% ¡calculate!
+g = m.get('G'); % graph from measure class
+clustering  = calculateValue@Clustering(m, prop);
+layerNumber = g.get('LAYERNUMBER');
+clustering_av = cell(layerNumber, 1);
+parfor li = 1:1:length(clustering)
+    clustering_av(li) = {mean(clustering{li})};
+end
+
+%% ¡tests!
+
+%%% ¡excluded_props!
+[ClusteringAv.PFM]
+
+%%% ¡test!
+%%%% ¡name!
+GraphBU
+%%%% ¡code!
+B_BU = [
+    0 1 1 1; 
+    1 0 1 0; 
+    1 1 0 1; 
+    1 0 1 0
+    ];
+clustering_BU = {[2/3 1 2/3 1]'};
+g = GraphBU('B', B_BU);
+m_outside_g = ClusteringAv('G', g);
+assert(isequal(m_outside_g.get('M'), {mean(clustering_BU)}), ...
+    [BRAPH2.STR ':ClusteringAv:' BRAPH2.FAIL_TEST], ...
+    [class(m_outside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+m_inside_g = g.get('MEASURE', 'ClusteringAv');
+assert(isequal(m_inside_g.get('M'), {mean(clustering_BU)}), ...
+    [BRAPH2.STR ':ClusteringAv:' BRAPH2.FAIL_TEST], ...
+    [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+
+%%% ¡test!
+%%%% ¡name!
+GraphBD
+%%%% ¡code!
+B_BD = [
+    0 0 1; 
+    1 0 0; 
+    0 1 0 
+    ];
+clustering_BD_out = {[0 0 0]'};  % out rule
+
+g = GraphBD('B', B_BD);
+m_outside_g = ClusteringAv('G', g, 'RULE', 'out');
+assert(isequal(m_outside_g.get('M'), {mean(clustering_BD_out)}), ...
+    [BRAPH2.STR ':ClusteringAv:' BRAPH2.FAIL_TEST], ...
+    [class(m_outside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+m_inside_g = g.get('MEASURE', 'ClusteringAv');
+m_inside_g.set('RULE', 'out');
+assert(isequal(m_inside_g.get('M'), {mean(clustering_BD_out)}), ...
+    [BRAPH2.STR ':ClusteringAv:' BRAPH2.FAIL_TEST], ...
+    [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+%%% ¡test!
+%%%% ¡name!
+MultigraphBUT
+%%%% ¡code!
+B = [
+    0 1 1 1; 
+    1 0 1 0; 
+    1 1 0 1; 
+    1 0 1 0
+    ];
+
+thresholds = [0 1];
+
+known_clustering = {
+     mean([2/3 1 2/3 1])
+    0
+    };
+
+g = MultigraphBUT('B', B, 'THRESHOLDS', thresholds);
+
+m_outside_g = ClusteringAv('G', g);
+assert(isequal(m_outside_g.get('M'), known_clustering), ...
+    [BRAPH2.STR ':ClusteringAv:' BRAPH2.FAIL_TEST], ...
+    [class(m_outside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+m_inside_g = g.get('MEASURE', 'ClusteringAv');
+assert(isequal(m_inside_g.get('M'), known_clustering), ...
+    [BRAPH2.STR ':ClusteringAv:' BRAPH2.FAIL_TEST], ...
+    [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+%%% ¡test!
+%%%% ¡name!
+MultiplexBU
+%%%% ¡code!
+B11 = [
+      0 1 1 1;
+      1 0 1 0;
+      1 1 0 1;
+      1 0 1 0
+      ];
+B22 = [
+      0 1 1 1;
+      1 0 1 0;
+      1 1 0 1;
+      1 0 1 0
+      ];
+B  = {B11 B22};
+
+known_clustering = {
+                 mean([2/3 1 2/3 1])
+                 mean([2/3 1 2/3 1])
+                 };      
+
+g = MultiplexBU('B', B);
+m_outside_g = ClusteringAv('G', g);
+assert(isequal(m_outside_g.get('M'), known_clustering), ...
+    [BRAPH2.STR ':ClusteringAv:' BRAPH2.FAIL_TEST], ...
+    [class(m_outside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+m_inside_g = g.get('MEASURE', 'ClusteringAv');
+assert(isequal(m_inside_g.get('M'), known_clustering), ...
+    [BRAPH2.STR ':ClusteringAv:' BRAPH2.FAIL_TEST], ...
+    [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+%%% ¡test!
+%%%% ¡name!
+MultiplexBUT
+%%%% ¡code!
+B11 = [
+      0 1 1 1;
+      1 0 1 0;
+      1 1 0 1;
+      1 0 1 0
+      ];
+B22 = [
+      0 1 1 1;
+      1 0 1 0;
+      1 1 0 1;
+      1 0 1 0
+      ];
+B  = {B11 B22};
+
+thresholds = [0 1];
+known_clustering = {
+                 mean([2/3 1 2/3 1])
+                 mean([2/3 1 2/3 1])
+                 0 
+                 0
+                 };      
+
+g = MultiplexBUT('B', B, 'THRESHOLDS', thresholds);
+m_outside_g = ClusteringAv('G', g);
+assert(isequal(m_outside_g.get('M'), known_clustering), ...
+    [BRAPH2.STR ':ClusteringAv:' BRAPH2.FAIL_TEST], ...
+    [class(m_outside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+m_inside_g = g.get('MEASURE', 'ClusteringAv');
+assert(isequal(m_inside_g.get('M'), known_clustering), ...
+    [BRAPH2.STR ':ClusteringAv:' BRAPH2.FAIL_TEST], ...
+    [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+%%% ¡test!
+%%%% ¡name!
+MultiplexBD
+%%%% ¡code!
+B11 = [
+      0 0 1; 
+      1 0 0; 
+      0 1 0 
+      ];
+B22 = [
+      0 0 1; 
+      1 0 0; 
+      0 1 0 
+      ];
+B = {B11 B22};
+
+% cycle rule - default
+known_clustering = {
+                 mean([1 1 1])
+                 mean([1 1 1])
+                 }; 
+
+g = MultiplexBD('B', B);
+m_outside_g = ClusteringAv('G', g, 'RULE', 'cycle');
+assert(isequal(m_outside_g.get('M'), known_clustering), ...
+    [BRAPH2.STR ':ClusteringAv:' BRAPH2.FAIL_TEST], ...
+    [class(m_outside_g) ' is not being calculated correctly for ' class(g) '.'])
+
+m_inside_g = g.get('MEASURE', 'ClusteringAv');
+m_inside_g.set('RULE', 'cycle');
+assert(isequal(m_inside_g.get('M'), known_clustering), ...
+    [BRAPH2.STR ':ClusteringAv:' BRAPH2.FAIL_TEST], ...
+    [class(m_inside_g) ' is not being calculated correctly for ' class(g) '.'])
