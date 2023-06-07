f6cdec4f8f5fef194da9015556c66861e1022290
JW.S <jiaweisun0512@gmail.com>
2023-06-07 22:27:10 +0200

    Update _MultilayerBU.gen.m

--- braph2genesis/graphs/_MultilayerBU.gen.m
+++ braph2genesis/graphs/_MultilayerBU.gen.m
@@ -2,7 +2,7 @@
 MultilayerBU < Graph (g, multilayer binary undirected graph) is a multilayer binary undirected graph.
 
 %%% ¡description!
-In a multilayer binary undirected (BU) graph, layers have same or different number 
+In a multilayer binary undirected (BU) graph, layers could have different number
  of nodes with within-layer undirected edges either 0 (absence of connection) 
  or 1 (existence of connection).
 The connectivity matrices are symmetric.
@@ -19,7 +19,7 @@ NAME (constant, string) is the name of the multilayer binary undirected graph.
 %%% ¡prop!
 DESCRIPTION (constant, string) is the description of the multilayer binary undirected graph.
 %%%% ¡default!
-'In a multilayer binary undirected (BU) graph, layers have same or different nodes with within-layer undirected edges either 0 (absence of connection) or 1 (existence of connection). The connectivity matrices are symmetric. There are connections between layers connecting the corresponding nodes.'
+'In a multilayer binary undirected (BU) graph, layers could have different number of nodes with within-layer undirected edges either 0 (absence of connection)  or 1 (existence of connection).The connectivity matrices are symmetric.There are connections between layers connecting the corresponding nodes.'
 
 %%% ¡prop!
 TEMPLATE (parameter, item) is the template of the multilayer binary undirected graph.
@@ -176,9 +176,9 @@ Constructor - Full
 B1 = rand(randi(10));
 B2 = rand(randi(10));
 B3 = rand(randi(10));
-B12 = rand(size(B1,1),size(B2,2));
-B13 = rand(size(B1,1),size(B3,2));
-B23 = rand(size(B2,1),size(B3,2));
+B12 = rand(size(B1, 1),size(B2, 2));
+B13 = rand(size(B1, 1),size(B3, 2));
+B23 = rand(size(B2, 1),size(B3, 2));
 B = {
     B1                           B12                            B13
     B12'                         B2                             B23
@@ -189,9 +189,9 @@ g.get('A_CHECK')
 A1 = symmetrize(binarize(semipositivize(dediagonalize(B1))));
 A2 = symmetrize(binarize(semipositivize(dediagonalize(B2))));
 A3 = symmetrize(binarize(semipositivize(dediagonalize(B3))));
-B{1,1} = A1;
-B{2,2} = A2;
-B{3,3} = A3;
+B{1, 1} = A1;
+B{2, 2} = A2;
+B{3, 3} = A3;
 A = B
 assert(isequal(g.get('A'), A), ...
     [BRAPH2.STR ':MultilayerBU:' BRAPH2.FAIL_TEST], ...
@@ -217,9 +217,9 @@ g0 = MultilayerBU('B', B); % 'SEMIPOSITIVIZE_RULE', 'zero', 'STANDARDIZE_RULE', 
 A1 = dediagonalize(max(B1, B1') > 0);
 A2 = dediagonalize(max(B2, B2') > 0);
 A3 = dediagonalize(max(B3, B3') > 0);
-A0{1,1} = A1;
-A0{2,2} = A2;
-A0{3,3} = A3;
+A0{1, 1} = A1;
+A0{2, 2} = A2;
+A0{3, 3} = A3;
 
 assert(isequal(g0.get('A'), A0), ...
     [BRAPH2.STR ':MultilayerBU:' BRAPH2.FAIL_TEST], ...
@@ -229,9 +229,9 @@ g_max = MultilayerBU('B', B, 'SEMIPOSITIVIZE_RULE', 'max'); % 'STANDARDIZE_RULE'
 A1 = dediagonalize(max(B1, B1') > 0);
 A2 = dediagonalize(max(B2, B2') > 0);
 A3 = dediagonalize(max(B3, B3') > 0);
-A_max{1,1} = A1;
-A_max{2,2} = A2;
-A_max{3,3} = A3;
+A_max{1, 1} = A1;
+A_max{2, 2} = A2;
+A_max{3, 3} = A3;
 assert(isequal(g_max.get('A'), A_max), ...
     [BRAPH2.STR ':MultilayerBU:' BRAPH2.FAIL_TEST], ...
     'MultilayerBU is not constructing well.')
@@ -240,9 +240,9 @@ g_sum = MultilayerBU('B', B, 'SYMMETRIZE_RULE', 'sum'); % 'SEMIPOSITIVIZE_RULE',
 A1 = dediagonalize(B1+ B1' > 0);
 A2 = dediagonalize(B2 + B2' > 0);
 A3 = dediagonalize(B3 + B3' > 0);
-A_sum{1,1} = A1;
-A_sum{2,2} = A2;
-A_sum{3,3} = A3;
+A_sum{1, 1} = A1;
+A_sum{2, 2} = A2;
+A_sum{3, 3} = A3;
 assert(isequal(g_sum.get('A'), A_sum), ...
     [BRAPH2.STR ':MultilayerBU:' BRAPH2.FAIL_TEST], ...
     'MultilayerBU is not constructing well.')
@@ -251,9 +251,9 @@ g_average = MultilayerBU('B', B, 'SYMMETRIZE_RULE', 'average'); % 'SEMIPOSITIVIZ
 A1 = dediagonalize((B1+ B1' ) / 2 > 0);
 A2 = dediagonalize((B2 + B2') / 2 > 0);
 A3 = dediagonalize((B3 + B3') / 2 > 0);
-A_sum{1,1} = A1;
-A_sum{2,2} = A2;
-A_sum{3,3} = A3;
+A_sum{1, 1} = A1;
+A_sum{2, 2} = A2;
+A_sum{3, 3} = A3;
 assert(isequal(g_average.get('A'), A_average), ...
     [BRAPH2.STR ':MultilayerBU:' BRAPH2.FAIL_TEST], ...
     'MultilayerBU is not constructing well.')
@@ -262,9 +262,9 @@ g_min = MultilayerBU('B', B, 'SYMMETRIZE_RULE', 'min'); % 'SEMIPOSITIVIZE_RULE',
 A1 = dediagonalize(min(B1, B1') > 0);
 A2 = dediagonalize(min(B2, B2') > 0);
 A3 = dediagonalize(min(B3, B3') > 0);
-A_min{1,1} = A1;
-A_min{2,2} = A2;
-A_min{3,3} = A3;
+A_min{1, 1} = A1;
+A_min{2, 2} = A2;
+A_min{3, 3} = A3;
 assert(isequal(g_min.get('A'), A_min), ...
     [BRAPH2.STR ':MultilayerBU:' BRAPH2.FAIL_TEST], ...
     'MultilayerBU is not constructing well.')
@@ -273,9 +273,9 @@ g_zero = MultilayerBU('B', B, 'SEMIPOSITIVIZE_RULE', 'zero'); % 'SYMMETRIZE_RULE
 A1 = dediagonalize(max(B1, B1') > 0);
 A2 = dediagonalize(max(B2, B2') > 0);
 A3 = dediagonalize(max(B3, B3') > 0);
-A_zero{1,1} = A1;
-A_zero{2,2} = A2;
-A_zero{3,3} = A3;
+A_zero{1, 1} = A1;
+A_zero{2, 2} = A2;
+A_zero{3, 3} = A3;
 assert(isequal(g_zero.get('A'), A_zero), ...
     [BRAPH2.STR ':MultilayerBU:' BRAPH2.FAIL_TEST], ...
     'MultilayerBU is not constructing well.')
@@ -284,9 +284,9 @@ g_zero_max = MultilayerBU('B', B, 'SEMIPOSITIVIZE_RULE', 'zero', 'SYMMETRIZE_RUL
 A1 = dediagonalize(max(B1, B1') > 0);
 A2 = dediagonalize(max(B2, B2') > 0);
 A3 = dediagonalize(max(B3, B3') > 0);
-A_zero_max{1,1} = A1;
-A_zero_max{2,2} = A2;
-A_zero_max{3,3} = A3;
+A_zero_max{1, 1} = A1;
+A_zero_max{2, 2} = A2;
+A_zero_max{3, 3} = A3;
 assert(isequal(g_zero_max.get('A'), A_zero_max), ...
     [BRAPH2.STR ':MultilayerBU:' BRAPH2.FAIL_TEST], ...
     'MultilayerBU is not constructing well.')
@@ -295,9 +295,9 @@ g_zero_sum = MultilayerBU('B', B, 'SEMIPOSITIVIZE_RULE', 'zero', 'SYMMETRIZE_RUL
 A1 = dediagonalize(B1 + B1' > 0);
 A2 = dediagonalize(B2 + B2' > 0);
 A3 = dediagonalize(B3 + B3' > 0);
-A_zero_sum{1,1} = A1;
-A_zero_sum{2,2} = A2;
-A_zero_sum{3,3} = A3;
+A_zero_sum{1, 1} = A1;
+A_zero_sum{2, 2} = A2;
+A_zero_sum{3, 3} = A3;
 assert(isequal(g_zero_sum.get('A'), A_zero_sum), ...
     [BRAPH2.STR ':MultilayerBU:' BRAPH2.FAIL_TEST], ...
     'MultilayerBU is not constructing well.')
@@ -306,9 +306,9 @@ g_zero_average = MultilayerBU('B', B, 'SEMIPOSITIVIZE_RULE', 'zero', 'SYMMETRIZE
 A1 = dediagonalize((B1 + B1') / 2 > 0);
 A2 = dediagonalize((B2 + B2') / 2 > 0);
 A3 = dediagonalize((B3 + B3') / 2 > 0);
-A_zero_average{1,1} = A1;
-A_zero_average{2,2} = A2;
-A_zero_average{3,3} = A3;
+A_zero_average{1, 1} = A1;
+A_zero_average{2, 2} = A2;
+A_zero_average{3, 3} = A3;
 assert(isequal(g_zero_average.get('A'), A_zero_average), ...
     [BRAPH2.STR ':MultilayerBU:' BRAPH2.FAIL_TEST], ...
     'MultilayerBU is not constructing well.')
@@ -317,9 +317,9 @@ g_zero_min = MultilayerBU('B', B, 'SEMIPOSITIVIZE_RULE', 'zero', 'SYMMETRIZE_RUL
 A1 = dediagonalize(min(B1, B1') > 0);
 A2 = dediagonalize(min(B2, B2') > 0);
 A3 = dediagonalize(min(B3, B3') > 0);
-A_zero_min{1,1} = A1;
-A_zero_min{2,2} = A2;
-A_zero_min{3,3} = A3;
+A_zero_min{1, 1} = A1;
+A_zero_min{2, 2} = A2;
+A_zero_min{3, 3} = A3;
 assert(isequal(g_zero_min.get('A'), A_zero_min), ...
     [BRAPH2.STR ':MultilayerBU:' BRAPH2.FAIL_TEST], ...
     'MultilayerBU is not constructing well.')
@@ -328,9 +328,9 @@ g_absolute = MultilayerBU('B', B, 'SEMIPOSITIVIZE_RULE', 'absolute'); % 'SYMMETR
 A1 = dediagonalize(max(B1, B1') ~= 0);
 A2 = dediagonalize(max(B2, B2') ~= 0);
 A3 = dediagonalize(max(B3, B3') ~= 0);
-A_absolute{1,1} = A1;
-A_absolute{2,2} = A2;
-A_absolute{3,3} = A3;
+A_absolute{1, 1} = A1;
+A_absolute{2, 2} = A2;
+A_absolute{3, 3} = A3;
 assert(isequal(g_absolute.get('A'), A_absolute), ...
     [BRAPH2.STR ':MultilayerBU:' BRAPH2.FAIL_TEST], ...
     'MultilayerBU is not constructing well.')
@@ -339,9 +339,9 @@ g_absolute_max = MultilayerBU('B', B, 'SEMIPOSITIVIZE_RULE', 'absolute', 'SYMMET
 A1 = dediagonalize(max(B1, B1') ~= 0);
 A2 = dediagonalize(max(B2, B2') ~= 0);
 A3 = dediagonalize(max(B3, B3') ~= 0);
-A_absolute_max{1,1} = A1;
-A_absolute_max{2,2} = A2;
-A_absolute_max{3,3} = A3;
+A_absolute_max{1, 1} = A1;
+A_absolute_max{2, 2} = A2;
+A_absolute_max{3, 3} = A3;
 assert(isequal(g_absolute_max.get('A'), A_absolute_max), ...
     [BRAPH2.STR ':MultilayerBU:' BRAPH2.FAIL_TEST], ...
     'MultilayerBU is not constructing well.')
@@ -350,9 +350,9 @@ g_absolute_sum = MultilayerBU('B', B, 'SEMIPOSITIVIZE_RULE', 'absolute', 'SYMMET
 A1 = dediagonalize(B1 + B1' ~= 0);
 A2 = dediagonalize(B2 + B2' ~= 0);
 A3 = dediagonalize(B3 + B3' ~= 0);
-A_absolute_max{1,1} = A1;
-A_absolute_max{2,2} = A2;
-A_absolute_max{3,3} = A3;
+A_absolute_max{1, 1} = A1;
+A_absolute_max{2, 2} = A2;
+A_absolute_max{3, 3} = A3;
 assert(isequal(g_absolute_sum.get('A'), A_absolute_max), ...
     [BRAPH2.STR ':MultilayerBU:' BRAPH2.FAIL_TEST], ...
     'MultilayerBU is not constructing well.')
@@ -361,9 +361,9 @@ g_absolute_average = MultilayerBU('B', B, 'SEMIPOSITIVIZE_RULE', 'absolute', 'SY
 A1 = dediagonalize((B1 + B1') / 2~= 0);
 A2 = dediagonalize((B2 + B2') / 2~= 0);
 A3 = dediagonalize((B3 + B3') / 2~= 0);
-A_absolute_average{1,1} = A1;
-A_absolute_average{2,2} = A2;
-A_absolute_average{3,3} = A3;
+A_absolute_average{1, 1} = A1;
+A_absolute_average{2, 2} = A2;
+A_absolute_average{3, 3} = A3;
 assert(isequal(g_absolute_average.get('A'), A_absolute_average), ...
     [BRAPH2.STR ':MultilayerBU:' BRAPH2.FAIL_TEST], ...
     'MultilayerBU is not constructing well.')
@@ -372,9 +372,9 @@ g_absolute_min = MultilayerBU('B', B, 'SEMIPOSITIVIZE_RULE', 'absolute', 'SYMMET
 A1 = dediagonalize(min(B1, B1') ~= 0);
 A2 = dediagonalize(min(B2, B2') ~= 0);
 A3 = dediagonalize(min(B3, B3') ~= 0);
-A_absolute_min{1,1} = A1;
-A_absolute_min{2,2} = A2;
-A_absolute_min{3,3} = A3;
+A_absolute_min{1, 1} = A1;
+A_absolute_min{2, 2} = A2;
+A_absolute_min{3, 3} = A3;
 assert(isequal(g_absolute_min.get('A'), A_absolute_min), ...
     [BRAPH2.STR ':MultilayerBU:' BRAPH2.FAIL_TEST], ...
     'MultilayerBU is not constructing well.')
