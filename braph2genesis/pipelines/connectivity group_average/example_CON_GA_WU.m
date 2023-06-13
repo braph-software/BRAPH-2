b73e36a74aa9f76a2014bb7551703f019bcf6e46
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-06-13 14:23:07 +0200

    Update example_CON_GA_WU.m

--- braph2genesis/pipelines/connectivity group_average/example_CON_GA_WU.m
+++ braph2genesis/pipelines/connectivity group_average/example_CON_GA_WU.m
@@ -5,7 +5,7 @@ clear variables %#ok<*NASGU>
 
 %% Load BrainAtlas
 im_ba = ImporterBrainAtlasXLS( ...
-    'FILE', [fileparts(which('SubjectCON')) filesep 'Example data CON' filesep 'atlas.xlsx'], ...
+    'FILE', [fileparts(which('SubjectCON')) filesep 'Example data CON XLS' filesep 'atlas.xlsx'], ...
     'WAITBAR', true ...
     );
 
@@ -13,7 +13,7 @@ ba = im_ba.get('BA');
 
 %% Load Groups of SubjectCON
 im_gr1 = ImporterGroupSubjectCON_XLS( ...
-    'DIRECTORY', [fileparts(which('SubjectCON')) filesep 'Example data CON' filesep 'xls' filesep 'GroupName1'], ...
+    'DIRECTORY', [fileparts(which('SubjectCON')) filesep 'Example data CON XLS' filesep 'ST_Group_1.xlsx'], ...
     'BA', ba, ...
     'WAITBAR', true ...
     );
@@ -21,7 +21,7 @@ im_gr1 = ImporterGroupSubjectCON_XLS( ...
 gr1 = im_gr1.get('GR');
 
 im_gr2 = ImporterGroupSubjectCON_XLS( ...
-    'DIRECTORY', [fileparts(which('SubjectCON')) filesep 'Example data CON' filesep 'xls' filesep 'GroupName2'], ...
+    'DIRECTORY', [fileparts(which('SubjectCON')) filesep 'Example data CON XLS' filesep 'ST_Group_2.xlsx'], ...
     'BA', ba, ...
     'WAITBAR', true ...
     );
@@ -39,13 +39,13 @@ a_WU2 = AnalyzeGroup_CON_GA_WU( ...
 
 % measure calculation
 g_WU1 = a_WU1.get('G');
-strength_WU1 = g_WU1.get('MEASURE', 'Strength').get('M');
-strength_av_WU1 = g_WU1.get('MEASURE', 'StrengthAv').get('M');
+% % % strength_WU1 = g_WU1.get('MEASURE', 'Strength').get('M');
+% % % strength_av_WU1 = g_WU1.get('MEASURE', 'StrengthAv').get('M');
 distance_WU1 = g_WU1.get('MEASURE', 'Distance').get('M');
 
 g_WU2 = a_WU2.get('G');
-strength_WU2 = g_WU2.get('MEASURE', 'Strength').get('M');
-strength_av_WU2 = g_WU2.get('MEASURE', 'StrengthAv').get('M');
+% % % strength_WU2 = g_WU2.get('MEASURE', 'Strength').get('M');
+% % % strength_av_WU2 = g_WU2.get('MEASURE', 'StrengthAv').get('M');
 distance_WU2 = g_WU2.get('MEASURE', 'Distance').get('M');
 
 % comparison
@@ -58,14 +58,20 @@ c_WU = CompareGroup( ...
     'MEMORIZE', true ...
     );
 
-strength_WU_diff = c_WU.get('COMPARISON', 'Strength').get('DIFF');
-strength_WU_p1 = c_WU.get('COMPARISON', 'Strength').get('P1');
-strength_WU_p2 = c_WU.get('COMPARISON', 'Strength').get('P2');
-strength_WU_cil = c_WU.get('COMPARISON', 'Strength').get('CIL');
-strength_WU_ciu = c_WU.get('COMPARISON', 'Strength').get('CIU');
+% % % strength_WU_diff = c_WU.get('COMPARISON', 'Strength').get('DIFF');
+% % % strength_WU_p1 = c_WU.get('COMPARISON', 'Strength').get('P1');
+% % % strength_WU_p2 = c_WU.get('COMPARISON', 'Strength').get('P2');
+% % % strength_WU_cil = c_WU.get('COMPARISON', 'Strength').get('CIL');
+% % % strength_WU_ciu = c_WU.get('COMPARISON', 'Strength').get('CIU');
 
-strength_av_WU_diff = c_WU.get('COMPARISON', 'StrengthAv').get('DIFF');
-strength_av_WU_p1 = c_WU.get('COMPARISON', 'StrengthAv').get('P1');
-strength_av_WU_p2 = c_WU.get('COMPARISON', 'StrengthAv').get('P2');
-strength_av_WU_cil = c_WU.get('COMPARISON', 'StrengthAv').get('CIL');
-strength_av_WU_ciu = c_WU.get('COMPARISON', 'StrengthAv').get('CIU');
+% % % strength_av_WU_diff = c_WU.get('COMPARISON', 'StrengthAv').get('DIFF');
+% % % strength_av_WU_p1 = c_WU.get('COMPARISON', 'StrengthAv').get('P1');
+% % % strength_av_WU_p2 = c_WU.get('COMPARISON', 'StrengthAv').get('P2');
+% % % strength_av_WU_cil = c_WU.get('COMPARISON', 'StrengthAv').get('CIL');
+% % % strength_av_WU_ciu = c_WU.get('COMPARISON', 'StrengthAv').get('CIU');
+
+distance_WU_diff = c_WU.get('COMPARISON', 'Distance').get('DIFF');
+distance_WU_p1 = c_WU.get('COMPARISON', 'Distance').get('P1');
+distance_WU_p2 = c_WU.get('COMPARISON', 'Distance').get('P2');
+distance_WU_cil = c_WU.get('COMPARISON', 'Distance').get('CIL');
+distance_WU_ciu = c_WU.get('COMPARISON', 'Distance').get('CIU');
