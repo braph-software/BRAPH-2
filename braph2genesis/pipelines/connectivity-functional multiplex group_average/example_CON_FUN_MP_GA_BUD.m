825d966b80172de61589e636f98ad7c71cd4eccc
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-06-13 14:57:47 +0200

    u

--- braph2genesis/pipelines/connectivity-functional multiplex group_average/example_CON_FUN_MP_GA_BUD.m
+++ braph2genesis/pipelines/connectivity-functional multiplex group_average/example_CON_FUN_MP_GA_BUD.m
@@ -46,21 +46,21 @@ im_gr2 = ImporterGroupSubjectFUN_XLS( ...
 gr2_FUN = im_gr2.get('GR');
 
 %% Combine Groups of SubjectCON with Groups of SubjectFUN
-co_gr1 = CombineGroups_CON_FUN( ...
-    'GR1', gr1_CON, ...
-    'GR2', gr1_FUN, ...
+co_gr1 = CombineGroups_CON_FUN_MP( ...
+    'GR_CON', gr1_CON, ...
+    'GR_FUN', gr1_FUN, ...
     'WAITBAR', true ...
     );
 
-gr1 = co_gr1.get('GR');
+gr1 = co_gr1.get('GR_CON_FUN_MP');
 
-co_gr2 = CombineGroups_CON_FUN( ...
-    'GR1', gr2_CON, ...
-    'GR2', gr2_FUN, ...
+co_gr2 = CombineGroups_CON_FUN_MP( ...
+    'GR_CON', gr2_CON, ...
+    'GR_FUN', gr2_FUN, ...
     'WAITBAR', true ...
     );
 
-gr2 = co_gr2.get('GR');
+gr2 = co_gr2.get('GR_CON_FUN_MP');
 
 %% Analysis CON FUN MP GA BUD
 densities = 5:5:15;
@@ -77,14 +77,16 @@ a_BUD2 = AnalyzeGroup_CON_FUN_MP_GA_BUD( ...
 
 % measure calculation
 g_BUD1 = a_BUD1.get('G');
-mpc_BUD1 = g_BUD1.get('MEASURE', 'MultiplexParticipation').get('M');
-mpc_av_BUD1 = g_BUD1.get('MEASURE', 'MultiplexParticipationAv').get('M');
-edgeov_BUD1 = g_BUD1.get('MEASURE', 'EdgeOverlap').get('M');
+degree_WU1 = g_WU1.get('MEASURE', 'Degree').get('M');
+% % % mpc_BUD1 = g_BUD1.get('MEASURE', 'MultiplexParticipation').get('M');
+% % % mpc_av_BUD1 = g_BUD1.get('MEASURE', 'MultiplexParticipationAv').get('M');
+% % % edgeov_BUD1 = g_BUD1.get('MEASURE', 'EdgeOverlap').get('M');
 
 g_BUD2 = a_BUD2.get('G');
-mpc_BUD2 = g_BUD2.get('MEASURE', 'MultiplexParticipation').get('M');
-mpc_av_BUD2 = g_BUD2.get('MEASURE', 'MultiplexParticipationAv').get('M');
-edgeov_BUD2 = g_BUD2.get('MEASURE', 'EdgeOverlap').get('M');
+degree_WU2 = g_WU2.get('MEASURE', 'Degree').get('M');
+% % % mpc_BUD2 = g_BUD2.get('MEASURE', 'MultiplexParticipation').get('M');
+% % % mpc_av_BUD2 = g_BUD2.get('MEASURE', 'MultiplexParticipationAv').get('M');
+% % % edgeov_BUD2 = g_BUD2.get('MEASURE', 'EdgeOverlap').get('M');
 
 % comparison
 c_BUD = CompareGroup( ...
@@ -96,14 +98,26 @@ c_BUD = CompareGroup( ...
     'MEMORIZE', true ...
     );
 
-mpc_BUD_diff = c_BUD.get('COMPARISON', 'MultiplexParticipation').get('DIFF');
-mpc_BUD_p1 = c_BUD.get('COMPARISON', 'MultiplexParticipation').get('P1');
-mpc_BUD_p2 = c_BUD.get('COMPARISON', 'MultiplexParticipation').get('P2');
-mpc_BUD_cil = c_BUD.get('COMPARISON', 'MultiplexParticipation').get('CIL');
-mpc_BUD_ciu = c_BUD.get('COMPARISON', 'MultiplexParticipation').get('CIU');
+degree_WU_diff = c_WU.get('COMPARISON', 'Degree').get('DIFF');
+degree_WU_p1 = c_WU.get('COMPARISON', 'Degree').get('P1');
+degree_WU_p2 = c_WU.get('COMPARISON', 'Degree').get('P2');
+degree_WU_cil = c_WU.get('COMPARISON', 'Degree').get('CIL');
+degree_WU_ciu = c_WU.get('COMPARISON', 'Degree').get('CIU');
 
-mpc_av_BUD_diff = c_BUD.get('COMPARISON', 'MultiplexParticipationAv').get('DIFF');
-mpc_av_BUD_p1 = c_BUD.get('COMPARISON', 'MultiplexParticipationAv').get('P1');
-mpc_av_BUD_p2 = c_BUD.get('COMPARISON', 'MultiplexParticipationAv').get('P2');
-mpc_av_BUD_cil = c_BUD.get('COMPARISON', 'MultiplexParticipationAv').get('CIL');
-mpc_av_BUD_ciu = c_BUD.get('COMPARISON', 'MultiplexParticipationAv').get('CIU');
+% % % mpc_BUD_diff = c_BUD.get('COMPARISON', 'MultiplexParticipation').get('DIFF');
+% % % mpc_BUD_p1 = c_BUD.get('COMPARISON', 'MultiplexParticipation').get('P1');
+% % % mpc_BUD_p2 = c_BUD.get('COMPARISON', 'MultiplexParticipation').get('P2');
+% % % mpc_BUD_cil = c_BUD.get('COMPARISON', 'MultiplexParticipation').get('CIL');
+% % % mpc_BUD_ciu = c_BUD.get('COMPARISON', 'MultiplexParticipation').get('CIU');
+
+% % % mpc_av_BUD_diff = c_BUD.get('COMPARISON', 'MultiplexParticipationAv').get('DIFF');
+% % % mpc_av_BUD_p1 = c_BUD.get('COMPARISON', 'MultiplexParticipationAv').get('P1');
+% % % mpc_av_BUD_p2 = c_BUD.get('COMPARISON', 'MultiplexParticipationAv').get('P2');
+% % % mpc_av_BUD_cil = c_BUD.get('COMPARISON', 'MultiplexParticipationAv').get('CIL');
+% % % mpc_av_BUD_ciu = c_BUD.get('COMPARISON', 'MultiplexParticipationAv').get('CIU');
+
+% % % edgeov_WU_diff = c_WU.get('COMPARISON', 'EdgeOverlap').get('DIFF');
+% % % edgeov_WU_p1 = c_WU.get('COMPARISON', 'EdgeOverlap').get('P1');
+% % % edgeov_WU_p2 = c_WU.get('COMPARISON', 'EdgeOverlap').get('P2');
+% % % edgeov_WU_cil = c_WU.get('COMPARISON', 'EdgeOverlap').get('CIL');
+% % % edgeov_WU_ciu = c_WU.get('COMPARISON', 'EdgeOverlap').get('CIU');
--- braph2genesis/pipelines/connectivity-functional multiplex group_average/example_CON_FUN_MP_GA_WU.m
+++ braph2genesis/pipelines/connectivity-functional multiplex group_average/example_CON_FUN_MP_GA_WU.m
@@ -46,21 +46,21 @@ im_gr2 = ImporterGroupSubjectFUN_XLS( ...
 gr2_FUN = im_gr2.get('GR');
 
 %% Combine Groups of SubjectCON with Groups of SubjectFUN
-co_gr1 = CombineGroups_CON_FUN( ...
-    'GR1', gr1_CON, ...
-    'GR2', gr1_FUN, ...
+co_gr1 = CombineGroups_CON_FUN_MP( ...
+    'GR_CON', gr1_CON, ...
+    'GR_FUN', gr1_FUN, ...
     'WAITBAR', true ...
     );
 
-gr1 = co_gr1.get('GR');
+gr1 = co_gr1.get('GR_CON_FUN_MP');
 
-co_gr2 = CombineGroups_CON_FUN( ...
-    'GR1', gr2_CON, ...
-    'GR2', gr2_FUN, ...
+co_gr2 = CombineGroups_CON_FUN_MP( ...
+    'GR_CON', gr2_CON, ...
+    'GR_FUN', gr2_FUN, ...
     'WAITBAR', true ...
     );
 
-gr2 = co_gr2.get('GR');
+gr2 = co_gr2.get('GR_CON_FUN_MP');
 
 %% Analysis CON FUN MP GA WU
 a_WU1 = AnalyzeGroup_CON_FUN_MP_GA_WU( ...
@@ -73,14 +73,16 @@ a_WU2 = AnalyzeGroup_CON_FUN_MP_GA_WU( ...
 
 % measure calculation
 g_WU1 = a_WU1.get('G');
-wmpc_WU1 = g_WU1.get('MEASURE', 'WeightedMultiplexParticipation').get('M');
-wmpc_av_WU1 = g_WU1.get('MEASURE', 'WeightedMultiplexParticipationAv').get('M');
-wedgeov_WU1 = g_WU1.get('MEASURE', 'WeightedEdgeOverlap').get('M');
+degree_WU1 = g_WU1.get('MEASURE', 'Degree').get('M');
+% % % wmpc_WU1 = g_WU1.get('MEASURE', 'WeightedMultiplexParticipation').get('M');
+% % % wmpc_av_WU1 = g_WU1.get('MEASURE', 'WeightedMultiplexParticipationAv').get('M');
+% % % wedgeov_WU1 = g_WU1.get('MEASURE', 'WeightedEdgeOverlap').get('M');
 
 g_WU2 = a_WU2.get('G');
-wmpc_WU2 = g_WU2.get('MEASURE', 'WeightedMultiplexParticipation').get('M');
-wmpc_av_WU2 = g_WU2.get('MEASURE', 'WeightedMultiplexParticipationAv').get('M');
-wedgeov_WU2 = g_WU2.get('MEASURE', 'WeightedEdgeOverlap').get('M');
+degree_WU2 = g_WU2.get('MEASURE', 'Degree').get('M');
+% % % wmpc_WU2 = g_WU2.get('MEASURE', 'WeightedMultiplexParticipation').get('M');
+% % % wmpc_av_WU2 = g_WU2.get('MEASURE', 'WeightedMultiplexParticipationAv').get('M');
+% % % wedgeov_WU2 = g_WU2.get('MEASURE', 'WeightedEdgeOverlap').get('M');
 
 % comparison
 c_WU = CompareGroup( ...
@@ -92,14 +94,26 @@ c_WU = CompareGroup( ...
     'MEMORIZE', true ...
     );
 
-wmpc_WU_diff = c_WU.get('COMPARISON', 'WeightedMultiplexParticipation').get('DIFF');
-wmpc_WU_p1 = c_WU.get('COMPARISON', 'WeightedMultiplexParticipation').get('P1');
-wmpc_WU_p2 = c_WU.get('COMPARISON', 'WeightedMultiplexParticipation').get('P2');
-wmpc_WU_cil = c_WU.get('COMPARISON', 'WeightedMultiplexParticipation').get('CIL');
-wmpc_WU_ciu = c_WU.get('COMPARISON', 'WeightedMultiplexParticipation').get('CIU');
+degree_WU_diff = c_WU.get('COMPARISON', 'Degree').get('DIFF');
+degree_WU_p1 = c_WU.get('COMPARISON', 'Degree').get('P1');
+degree_WU_p2 = c_WU.get('COMPARISON', 'Degree').get('P2');
+degree_WU_cil = c_WU.get('COMPARISON', 'Degree').get('CIL');
+degree_WU_ciu = c_WU.get('COMPARISON', 'Degree').get('CIU');
 
-wmpc_av_WU_diff = c_WU.get('COMPARISON', 'WeightedMultiplexParticipationAv').get('DIFF');
-wmpc_av_WU_p1 = c_WU.get('COMPARISON', 'WeightedMultiplexParticipationAv').get('P1');
-wmpc_av_WU_p2 = c_WU.get('COMPARISON', 'WeightedMultiplexParticipationAv').get('P2');
-wmpc_av_WU_cil = c_WU.get('COMPARISON', 'WeightedMultiplexParticipationAv').get('CIL');
-wmpc_av_WU_ciu = c_WU.get('COMPARISON', 'WeightedMultiplexParticipationAv').get('CIU');
+% % % wmpc_WU_diff = c_WU.get('COMPARISON', 'WeightedMultiplexParticipation').get('DIFF');
+% % % wmpc_WU_p1 = c_WU.get('COMPARISON', 'WeightedMultiplexParticipation').get('P1');
+% % % wmpc_WU_p2 = c_WU.get('COMPARISON', 'WeightedMultiplexParticipation').get('P2');
+% % % wmpc_WU_cil = c_WU.get('COMPARISON', 'WeightedMultiplexParticipation').get('CIL');
+% % % wmpc_WU_ciu = c_WU.get('COMPARISON', 'WeightedMultiplexParticipation').get('CIU');
+
+% % % wmpc_av_WU_diff = c_WU.get('COMPARISON', 'WeightedMultiplexParticipationAv').get('DIFF');
+% % % wmpc_av_WU_p1 = c_WU.get('COMPARISON', 'WeightedMultiplexParticipationAv').get('P1');
+% % % wmpc_av_WU_p2 = c_WU.get('COMPARISON', 'WeightedMultiplexParticipationAv').get('P2');
+% % % wmpc_av_WU_cil = c_WU.get('COMPARISON', 'WeightedMultiplexParticipationAv').get('CIL');
+% % % wmpc_av_WU_ciu = c_WU.get('COMPARISON', 'WeightedMultiplexParticipationAv').get('CIU');
+
+% % % wedgeov_WU_diff = c_WU.get('COMPARISON', 'WeightedEdgeOverlap').get('DIFF');
+% % % wedgeov_WU_p1 = c_WU.get('COMPARISON', 'WeightedEdgeOverlap').get('P1');
+% % % wedgeov_WU_p2 = c_WU.get('COMPARISON', 'WeightedEdgeOverlap').get('P2');
+% % % wedgeov_WU_cil = c_WU.get('COMPARISON', 'WeightedEdgeOverlap').get('CIL');
+% % % wedgeov_WU_ciu = c_WU.get('COMPARISON', 'WeightedEdgeOverlap').get('CIU');
