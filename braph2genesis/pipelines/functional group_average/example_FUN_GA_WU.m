7593cb55b112ff7c89920076ed059b2d2101e079
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-06-13 12:06:06 +0200

    Update example_FUN_GA_WU.m

--- braph2genesis/pipelines/functional group_average/example_FUN_GA_WU.m
+++ braph2genesis/pipelines/functional group_average/example_FUN_GA_WU.m
@@ -5,7 +5,7 @@ clear variables %#ok<*NASGU>
 
 %% Load BrainAtlas
 im_ba = ImporterBrainAtlasXLS( ...
-    'FILE', [fileparts(which('SubjectFUN')) filesep 'Example data FUN' filesep 'atlas.xlsx'], ...
+    'FILE', [fileparts(which('SubjectFUN')) filesep 'Example data FUN XLS' filesep 'atlas.xlsx'], ...
     'WAITBAR', true ...
     );
 
@@ -13,7 +13,7 @@ ba = im_ba.get('BA');
 
 %% Load Groups of SubjectFUN
 im_gr1 = ImporterGroupSubjectFUN_XLS( ...
-    'DIRECTORY', [fileparts(which('SubjectFUN')) filesep 'Example data FUN' filesep 'xls' filesep 'GroupName1'], ...
+    'DIRECTORY', [fileparts(which('SubjectFUN')) filesep 'Example data FUN XLS' filesep 'FUN_Group_1_XLS'], ...
     'BA', ba, ...
     'WAITBAR', true ...
     );
@@ -21,7 +21,7 @@ im_gr1 = ImporterGroupSubjectFUN_XLS( ...
 gr1 = im_gr1.get('GR');
 
 im_gr2 = ImporterGroupSubjectFUN_XLS( ...
-    'DIRECTORY', [fileparts(which('SubjectFUN')) filesep 'Example data FUN' filesep 'xls' filesep 'GroupName2'], ...
+    'DIRECTORY', [fileparts(which('SubjectFUN')) filesep 'Example data FUN XLS' filesep 'FUN_Group_2_XLS'], ...
     'BA', ba, ...
     'WAITBAR', true ...
     );
@@ -39,12 +39,14 @@ a_WU2 = AnalyzeGroup_FUN_GA_WU( ...
 
 % measure calculation
 g_WU1 = a_WU1.get('G');
-strength_WU1 = g_WU1.get('MEASURE', 'Strength').get('M');
-modularity_WU1 = g_WU1.get('MEASURE', 'Modularity').get('M');
+degree_WU1 = g_WU1.get('MEASURE', 'Degree').get('M');
+% % % strength_WU1 = g_WU1.get('MEASURE', 'Strength').get('M');
+% % % modularity_WU1 = g_WU1.get('MEASURE', 'Modularity').get('M');
 
 g_WU2 = a_WU2.get('G');
-strength_WU2 = g_WU2.get('MEASURE', 'Strength').get('M');
-modularity_WU2 = g_WU2.get('MEASURE', 'Modularity').get('M');
+degree_WU2 = g_WU2.get('MEASURE', 'Degree').get('M');
+% % % strength_WU2 = g_WU2.get('MEASURE', 'Strength').get('M');
+% % % modularity_WU2 = g_WU2.get('MEASURE', 'Modularity').get('M');
 
 % comparison
 c_WU = CompareGroup( ...
@@ -56,13 +58,19 @@ c_WU = CompareGroup( ...
     'MEMORIZE', true ...
     );
 
-strength_WU_diff = c_WU.get('COMPARISON', 'Strength').get('DIFF');
-strength_WU_p1 = c_WU.get('COMPARISON', 'Strength').get('P1');
-strength_WU_p2 = c_WU.get('COMPARISON', 'Strength').get('P2');
-strength_WU_cil = c_WU.get('COMPARISON', 'Strength').get('CIL');
-strength_WU_ciu = c_WU.get('COMPARISON', 'Strength').get('CIU');
+degree_WU_diff = c_WU.get('COMPARISON', 'Degree').get('DIFF');
+degree_WU_p1 = c_WU.get('COMPARISON', 'Degree').get('P1');
+degree_WU_p2 = c_WU.get('COMPARISON', 'Degree').get('P2');
+degree_WU_cil = c_WU.get('COMPARISON', 'Degree').get('CIL');
+degree_WU_ciu = c_WU.get('COMPARISON', 'Degree').get('CIU');
 
-modularity_WU_diff = c_WU.get('COMPARISON', 'Modularity').get('DIFF');
-modularity_WU_p1 = c_WU.get('COMPARISON', 'Modularity').get('P1');
-modularity_WU_p2 = c_WU.get('COMPARISON', 'Modularity').get('P2');
-modularity_WU_ciu = c_WU.get('COMPARISON', 'Modularity').get('CIU');
+% % % strength_WU_diff = c_WU.get('COMPARISON', 'Strength').get('DIFF');
+% % % strength_WU_p1 = c_WU.get('COMPARISON', 'Strength').get('P1');
+% % % strength_WU_p2 = c_WU.get('COMPARISON', 'Strength').get('P2');
+% % % strength_WU_cil = c_WU.get('COMPARISON', 'Strength').get('CIL');
+% % % strength_WU_ciu = c_WU.get('COMPARISON', 'Strength').get('CIU');
+
+% % % modularity_WU_diff = c_WU.get('COMPARISON', 'Modularity').get('DIFF');
+% % % modularity_WU_p1 = c_WU.get('COMPARISON', 'Modularity').get('P1');
+% % % modularity_WU_p2 = c_WU.get('COMPARISON', 'Modularity').get('P2');
+% % % modularity_WU_ciu = c_WU.get('COMPARISON', 'Modularity').get('CIU');
