8b1f4bb450adbb3a19fe69ebbfcbc58a5392af32
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-06-19 13:35:19 +0200

    u

---
+++ braph2genesis/pipelines/connectivity/_AnalyzeEnsemble_CON_BUT.gen.m
@@ -0,0 +1,110 @@
+%% ¡header!
+AnalyzeEnsemble_CON_BUT < AnalyzeEnsemble (a, graph analysis with connectivity data of fixed threshold) is an ensemble-based graph analysis using connectivity data of fixed threshold.
+
+%% ¡description!
+This ensemble-based graph analysis (AnalyzeEnsemble_CON_BUT) analyzes 
+connectivity data using binary undirected multigraphs with fixed thresholds.
+
+%%% ¡seealso!
+Subject_CON, MultigraphBUT
+
+%% ¡props_update!
+
+%%% ¡prop!
+NAME (constant, string) is the name of the ensenmble-based graph analysis using connectivity data of fixed threshold.
+%%%% ¡default!
+'AnalyzeEnsemble_CON_BUT'
+
+%%% ¡prop!
+DESCRIPTION (constant, string) is the description of the ensenmble-based graph analysis using connectivity data of fixed threshold.
+%%%% ¡default!
+'This ensemble-based graph analysis (AnalyzeEnsemble_CON_BUT) analyzes connectivity data using binary undirected multigraphs with fixed thresholds.'
+
+%%% ¡prop!
+TEMPLATE (parameter, item) is the template of the ensenmble-based graph analysis using connectivity data of fixed threshold.
+
+%%% ¡prop!
+ID (data, string) is a few-letter code for the ensenmble-based graph analysis using connectivity data of fixed threshold.
+%%%% ¡default!
+'AnalyzeEnsemble_CON_BUT ID'
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the ensenmble-based graph analysis using connectivity data of fixed threshold.
+%%%% ¡default!
+'AnalyzeEnsemble_CON_BUT label'
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the ensenmble-based graph analysis using connectivity data of fixed threshold.
+%%%% ¡default!
+'AnalyzeEnsemble_CON_BUT notes'
+
+
+of fixed threshold
+
+%%% ¡prop!
+GR (data, item) is the subject group, which also defines the subject class SubjectCON.
+%%%% ¡default!
+Group('SUB_CLASS', 'SubjectCON')
+
+%%% ¡prop!
+ME_DICT (result, idict) contains the calculated measures of the graph ensemble.
+% % % %%%% ¡gui!
+% % % pr = PPAnalyzeEnsemble_ME_DICT('EL', a, 'PROP', AnalyzeEnsemble_CON_BUT.ME_DICT, 'WAITBAR', true, varargin{:});
+
+%%% ¡prop!
+G_DICT (result, idict) is the graph (MultigraphBUT) ensemble obtained from this analysis.
+%%%% ¡settings!
+'MultigraphBUT'
+%%%% ¡default!
+IndexedDictionary('IT_CLASS', 'MultigraphBUT')
+% % % %%%% ¡calculate!
+% % % g_dict = IndexedDictionary('IT_CLASS', 'MultigraphBUT');
+% % % node_labels = '';
+% % % gr = a.get('GR');
+% % % thresholds = a.get('THRESHOLDS');
+% % % 
+% % % ba = BrainAtlas();
+% % % if ~isempty(gr) && ~isa(gr, 'NoValue') && gr.get('SUB_DICT').length > 0 
+% % %     ba = gr.get('SUB_DICT').getItem(1).get('BA');
+% % % end
+% % % 
+% % % for i = 1:1:gr.get('SUB_DICT').length()
+% % % 	sub = gr.get('SUB_DICT').getItem(i);
+% % %     g = MultigraphBUT( ...
+% % %         'ID', ['g ' sub.get('ID')], ...
+% % %         'B', Callback('EL', sub, 'TAG', 'CON'), ...
+% % %         'LAYERTICKS', thresholds, ...  
+% % %         'BAS', ba ...
+% % %         );
+% % %     g_dict.add(g)
+% % %     
+% % %     if isa(a.getr('TEMPLATE'), 'NoValue')
+% % %         g.set('TEMPLATE', a.memorize('GRAPH_TEMPLATE'))
+% % %     else
+% % %         g.set('TEMPLATE', a.get('TEMPLATE').memorize('GRAPH_TEMPLATE'))        
+% % %     end    
+% % % end
+% % % 
+% % % value = g_dict;
+
+%% ¡props!
+
+%%% ¡prop!
+THRESHOLDS (parameter, rvector) is the vector of thresholds.
+%%%% ¡default!
+[-1:.5:1]
+%%%% ¡gui!
+pr = PanelPropRVectorSmart('EL', a, 'PROP', AnalyzeEnsemble_CON_BUT.THRESHOLDS, ...
+    'MIN', -1, 'MAX', 1, ...
+    'DEFAULT', AnalyzeEnsemble_CON_BUT.getPropDefault('THRESHOLDS'), ...
+    varargin{:});
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+Example
+%%%% ¡probability!
+.01
+%%%% ¡code!
+% % % example_CON_BUT
--- braph2genesis/pipelines/connectivity/_AnalyzeEnsemble_CON_WU.gen.m
+++ braph2genesis/pipelines/connectivity/_AnalyzeEnsemble_CON_WU.gen.m
@@ -1,32 +1,41 @@
 %% ¡header!
-AnalyzeEnsemble_CON_WU < AnalyzeEnsemble (a, graph analysis with connectivity data) is a graph analysis using connectivity data.
+AnalyzeEnsemble_CON_WU < AnalyzeEnsemble (a, graph analysis with connectivity data) is a ensenmble-based graph analysis using connectivity data.
 
-%% ¡description!
-This graph analysis uses connectivity data and analyzes them using weighted undirected graphs,
-binary undirected multigraphs with fixed thresholds,
-or binary undirected multigraphs with fixed densities.
+%%% ¡description!
+This ensemble-based graph analysis (AnalyzeEnsemble_CON_WU) analyzes connectivity data using weighted undirected graphs.
+
+%%% ¡seealso!
+SubjectCON, GraphWU
 
 %% ¡props_update!
 
 %%% ¡prop!
-TEMPLATE (parameter, item) is the analysis template to set the parameters.
-%%%% ¡settings!
+NAME (constant, string) is the name of the ensenmble-based graph analysis using connectivity data.
+%%%% ¡default!
 'AnalyzeEnsemble_CON_WU'
 
 %%% ¡prop!
-GRAPH_TEMPLATE (parameter, item) is the graph template to set all graph and measure parameters.
-%%%% ¡settings!
-'GraphWU'
-%%%% ¡postprocessing!
-if ~braph2_testing
-    if isa(a.getr('GRAPH_TEMPLATE'), 'NoValue')
-        if a.get('GR').get('SUB_DICT').length() > 0
-            a.set('GRAPH_TEMPLATE', GraphWU('BAS', a.get('GR').get('SUB_DICT').getItem(1).get('BA')));
-        else
-            a.set('GRAPH_TEMPLATE', GraphWU());
-        end
-    end
-end
+DESCRIPTION (constant, string) is the description of the ensenmble-based graph analysis using connectivity data.
+%%%% ¡default!
+'This ensemble-based graph analysis (AnalyzeEnsemble_CON_WU) analyzes connectivity data using weighted undirected graphs.'
+
+%%% ¡prop!
+TEMPLATE (parameter, item) is the template of the ensenmble-based graph analysis using connectivity data.
+
+%%% ¡prop!
+ID (data, string) is a few-letter code for the ensenmble-based graph analysis using connectivity data.
+%%%% ¡default!
+'AnalyzeEnsemble_CON_WU ID'
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the ensenmble-based graph analysis using connectivity data.
+%%%% ¡default!
+'AnalyzeEnsemble_CON_WU label'
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the ensenmble-based graph analysis using connectivity data.
+%%%% ¡default!
+'AnalyzeEnsemble_CON_WU notes'
 
 %%% ¡prop!
 GR (data, item) is the subject group, which also defines the subject class SubjectCON.
@@ -35,58 +44,60 @@ Group('SUB_CLASS', 'SubjectCON')
 
 %%% ¡prop!
 ME_DICT (result, idict) contains the calculated measures of the graph ensemble.
-%%%% ¡gui_!
-% % % pr = PPAnalyzeEnsemble_ME_DICT('EL', a, 'PROP', AnalyzeEnsemble_CON_WU.ME_DICT, 'WAITBAR', true, varargin{:});
+% % % %%%% ¡gui!
+% % % % % % pr = PPAnalyzeEnsemble_ME_DICT('EL', a, 'PROP', AnalyzeEnsemble_CON_WU.ME_DICT, 'WAITBAR', true, varargin{:});
 
 %%% ¡prop!
 G_DICT (result, idict) is the graph (GraphWU) ensemble obtained from this analysis.
 %%%% ¡settings!
 'GraphWU'
-%%%% ¡default!
-IndexedDictionary('IT_CLASS', 'GraphWU')
-%%%% ¡calculate!
-g_dict = IndexedDictionary('IT_CLASS', 'GraphWU');
-gr = a.get('GR');
-
-ba = BrainAtlas();
-if ~isempty(gr) && ~isa(gr, 'NoValue') && gr.get('SUB_DICT').length > 0 
-    ba = gr.get('SUB_DICT').getItem(1).get('BA');
-end
-
-gr = a.get('GR');
-for i = 1:1:gr.get('SUB_DICT').length()
-	sub = gr.get('SUB_DICT').getItem(i);
-    g = GraphWU( ...
-        'ID', ['g ' sub.get('ID')], ...
-        'BAS', ba, ...
-        'B', Callback('EL', sub, 'TAG', 'CON') ...
-        );
-    g_dict.add(g)
-    
-    if isa(a.getr('TEMPLATE'), 'NoValue')
-        g.set('TEMPLATE', a.memorize('GRAPH_TEMPLATE'))
-    else
-        g.set('TEMPLATE', a.get('TEMPLATE').memorize('GRAPH_TEMPLATE'))
-    end
-end
-
-value = g_dict;
-% % % %% ¡methods!
-% % % function pr = getPPCompareEnsemble_CPDict(a, varargin) 
-% % %     %GETPPCOMPAREENSEMBLE_CPDICT returns the comparison ensemble plot panel compatible with the analysis.
-% % %     %
-% % %     % PR = GETPPCOMPAREENSEMBLE_CPDICT(A) returns the comparison ensemble plot panel
-% % %     %  that is compatible with the analyze ensemble.
-% % %     %
-% % %     % See also CompareEnsemble.
-% % %     
-% % %     pr = PPCompareEnsemble_CPDict_WU(varargin{:});
+% % % %%%% ¡default!
+% % % IndexedDictionary('IT_CLASS', 'GraphWU')
+% % % %%%% ¡calculate!
+% % % g_dict = IndexedDictionary('IT_CLASS', 'GraphWU');
+% % % gr = a.get('GR');
+% % % 
+% % % ba = BrainAtlas();
+% % % if ~isempty(gr) && ~isa(gr, 'NoValue') && gr.get('SUB_DICT').length > 0 
+% % %     ba = gr.get('SUB_DICT').getItem(1).get('BA');
 % % % end
+% % % 
+% % % gr = a.get('GR');
+% % % for i = 1:1:gr.get('SUB_DICT').length()
+% % % 	sub = gr.get('SUB_DICT').getItem(i);
+% % %     g = GraphWU( ...
+% % %         'ID', ['g ' sub.get('ID')], ...
+% % %         'BAS', ba, ...
+% % %         'B', Callback('EL', sub, 'TAG', 'CON') ...
+% % %         );
+% % %     g_dict.add(g)
+% % %     
+% % %     if isa(a.getr('TEMPLATE'), 'NoValue')
+% % %         g.set('TEMPLATE', a.memorize('GRAPH_TEMPLATE'))
+% % %     else
+% % %         g.set('TEMPLATE', a.get('TEMPLATE').memorize('GRAPH_TEMPLATE'))
+% % %     end
+% % % end
+% % % 
+% % % value = g_dict;
+% % % % % % %% ¡methods!
+% % % % % % function pr = getPPCompareEnsemble_CPDict(a, varargin) 
+% % % % % %     %GETPPCOMPAREENSEMBLE_CPDICT returns the comparison ensemble plot panel compatible with the analysis.
+% % % % % %     %
+% % % % % %     % PR = GETPPCOMPAREENSEMBLE_CPDICT(A) returns the comparison ensemble plot panel
+% % % % % %     %  that is compatible with the analyze ensemble.
+% % % % % %     %
+% % % % % %     % See also CompareEnsemble.
+% % % % % %     
+% % % % % %     pr = PPCompareEnsemble_CPDict_WU(varargin{:});
+% % % % % % end
 
 %% ¡tests!
 
 %%% ¡test!
 %%%% ¡name!
 Example
+%%%% ¡probability!
+.01
 %%%% ¡code!
-example_CON_WU
+% % % example_CON_WU
