677015f9d65686683e8d0d4164a3b2e8618ab64c
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-06-21 22:34:33 +0200

    Update temp.m

--- temp.m
+++ temp.m
@@ -1305,19 +1305,19 @@ close all; delete(findall(0, 'type', 'figure')); clear all
 %     eval(['test_' el_class])
 % end
 
-el_class_list = {'AnalyzeEnsemble_CON_FUN_MP_WU'} % {'AnalyzeEnsemble_CON_FUN_MP_WU' 'AnalyzeEnsemble_CON_FUN_MP_BUT' 'AnalyzeEnsemble_CON_FUN_MP_BUD'}
-for i = 1:1:length(el_class_list)
-    el_class = el_class_list{i};
-    el_path = '/pipelines/connectivity-functional multiplex';
-    delete([fileparts(which('braph2')) el_path filesep() el_class '.m'])
-    create_Element([fileparts(which('braph2genesis')) el_path filesep() '_' el_class '.gen.m'], [fileparts(which('braph2')) el_path])
-    create_Element([fileparts(which('braph2genesis')) el_path filesep() '_' el_class '.gen.m'], [fileparts(which('braph2')) el_path])
-    create_layout([fileparts(which('braph2genesis')) el_path filesep() '_' el_class '.gen.m'], [fileparts(which('braph2')) el_path])
-    create_test_Element([fileparts(which('braph2genesis')) el_path filesep() '_' el_class '.gen.m'], [fileparts(which('braph2')) el_path])
-    eval(['test_' el_class])
-end
+% el_class_list = {'AnalyzeEnsemble_CON_FUN_MP_BUD'} % {'AnalyzeEnsemble_CON_FUN_MP_WU' 'AnalyzeEnsemble_CON_FUN_MP_BUT' 'AnalyzeEnsemble_CON_FUN_MP_BUD'}
+% for i = 1:1:length(el_class_list)
+%     el_class = el_class_list{i};
+%     el_path = '/pipelines/connectivity-functional multiplex';
+%     delete([fileparts(which('braph2')) el_path filesep() el_class '.m'])
+%     create_Element([fileparts(which('braph2genesis')) el_path filesep() '_' el_class '.gen.m'], [fileparts(which('braph2')) el_path])
+%     create_Element([fileparts(which('braph2genesis')) el_path filesep() '_' el_class '.gen.m'], [fileparts(which('braph2')) el_path])
+%     create_layout([fileparts(which('braph2genesis')) el_path filesep() '_' el_class '.gen.m'], [fileparts(which('braph2')) el_path])
+%     create_test_Element([fileparts(which('braph2genesis')) el_path filesep() '_' el_class '.gen.m'], [fileparts(which('braph2')) el_path])
+%     eval(['test_' el_class])
+% end
 
-% el_class_list = {'AnalyzeEnsemble_CON_MP_WU'} % {'AnalyzeEnsemble_CON_MP_WU' 'AnalyzeEnsemble_CON_MP_BUT' 'AnalyzeEnsemble_CON_MP_BUT'}
+% el_class_list = {'AnalyzeEnsemble_CON_MP_BUD'} % {'AnalyzeEnsemble_CON_MP_WU' 'AnalyzeEnsemble_CON_MP_BUT' 'AnalyzeEnsemble_CON_MP_BUT'}
 % for i = 1:1:length(el_class_list)
 %     el_class = el_class_list{i};
 %     el_path = '/pipelines/connectivity multiplex';
@@ -1341,7 +1341,7 @@ end
 %     eval(['test_' el_class])
 % end
 
-% el_class_list = {'AnalyzeEnsemble_FUN_WU'} % {'AnalyzeEnsemble_FUN_WU' 'AnalyzeEnsemble_FUN_BUT' 'AnalyzeEnsemble_FUN_BUD'}
+% el_class_list = {'AnalyzeEnsemble_FUN_BUD'} % {'AnalyzeEnsemble_FUN_WU' 'AnalyzeEnsemble_FUN_BUT' 'AnalyzeEnsemble_FUN_BUD'}
 % for i = 1:1:length(el_class_list)
 %     el_class = el_class_list{i};
 %     el_path = '/pipelines/functional';
@@ -1353,7 +1353,7 @@ end
 %     eval(['test_' el_class])
 % end
 
-% el_class_list = {'AnalyzeEnsemble_FUN_WU'} % {'AnalyzeEnsemble_FUN_MP_WU' 'AnalyzeEnsemble_FUN_MP_BUT' 'AnalyzeEnsemble_FUN_MP_BUD'}
+% el_class_list = {'AnalyzeEnsemble_FUN_MP_BUD'} % {'AnalyzeEnsemble_FUN_MP_WU' 'AnalyzeEnsemble_FUN_MP_BUT' 'AnalyzeEnsemble_FUN_MP_BUD'}
 % for i = 1:1:length(el_class_list)
 %     el_class = el_class_list{i};
 %     el_path = '/pipelines/functional multiplex';
@@ -1365,14 +1365,14 @@ end
 %     eval(['test_' el_class])
 % end
 
-% el_class_list = {'AnalyzeEnsemble_FUN_OMP_WU'}
-% for i = 1:1:length(el_class_list)
-%     el_class = el_class_list{i};
-%     el_path = '/pipelines/functional ordered_multiplex';
-%     delete([fileparts(which('braph2')) el_path filesep() el_class '.m'])
-%     create_Element([fileparts(which('braph2genesis')) el_path filesep() '_' el_class '.gen.m'], [fileparts(which('braph2')) el_path])
-%     create_Element([fileparts(which('braph2genesis')) el_path filesep() '_' el_class '.gen.m'], [fileparts(which('braph2')) el_path])
-%     create_layout([fileparts(which('braph2genesis')) el_path filesep() '_' el_class '.gen.m'], [fileparts(which('braph2')) el_path])
-%     create_test_Element([fileparts(which('braph2genesis')) el_path filesep() '_' el_class '.gen.m'], [fileparts(which('braph2')) el_path])
-%     eval(['test_' el_class])
-% end
+el_class_list = {'AnalyzeEnsemble_FUN_OMP_WU'}
+for i = 1:1:length(el_class_list)
+    el_class = el_class_list{i};
+    el_path = '/pipelines/functional ordered_multiplex';
+    delete([fileparts(which('braph2')) el_path filesep() el_class '.m'])
+    create_Element([fileparts(which('braph2genesis')) el_path filesep() '_' el_class '.gen.m'], [fileparts(which('braph2')) el_path])
+    create_Element([fileparts(which('braph2genesis')) el_path filesep() '_' el_class '.gen.m'], [fileparts(which('braph2')) el_path])
+    create_layout([fileparts(which('braph2genesis')) el_path filesep() '_' el_class '.gen.m'], [fileparts(which('braph2')) el_path])
+    create_test_Element([fileparts(which('braph2genesis')) el_path filesep() '_' el_class '.gen.m'], [fileparts(which('braph2')) el_path])
+    eval(['test_' el_class])
+end
