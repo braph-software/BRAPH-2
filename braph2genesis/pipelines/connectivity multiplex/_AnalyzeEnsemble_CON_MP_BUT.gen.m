730ce646d323cc6161a32e68c84b1e5cb8c7378a
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-06-24 13:41:00 +0200

    u

--- braph2genesis/pipelines/connectivity multiplex/_AnalyzeEnsemble_CON_MP_BUT.gen.m
+++ braph2genesis/pipelines/connectivity multiplex/_AnalyzeEnsemble_CON_MP_BUT.gen.m
@@ -54,38 +54,34 @@ GRAPH_TEMPLATE (parameter, item) is the graph template to set all graph and meas
 G_DICT (result, idict) is the graph (MultiplexBUT) ensemble obtained from this analysis.
 %%%% ¡settings!
 'MultiplexBUT'
-%%%% ¡_default!
-% % % IndexedDictionary('IT_CLASS', 'MultiplexBUT')
-%%%% ¡_calculate!
-% % % g_dict = IndexedDictionary('IT_CLASS', 'MultiplexBUT');
-% % % gr = a.get('GR');
+%%%% ¡calculate!
+g_dict = IndexedDictionary('IT_CLASS', 'MultiplexBUT');
+gr = a.get('GR');
 % % % node_labels = '';
-% % % 
+
 % % % ba = BrainAtlas();
 % % % if ~isempty(gr) && ~isa(gr, 'NoValue') && gr.get('SUB_DICT').length > 0 
 % % %     ba = gr.get('SUB_DICT').getItem(1).get('BA');
 % % % end
-% % % 
-% % % thresholds = a.get('THRESHOLDS'); % this is a vector
-% % % for i = 1:1:gr.get('SUB_DICT').length()
-% % % 	sub = gr.get('SUB_DICT').getItem(i);
-% % %     
-% % %     g = MultiplexBUT( ...
-% % %         'ID', ['g ' sub.get('ID')], ...
-% % %         'B', Callback('EL', sub, 'TAG', 'CON_MP'), ...
-% % %         'THRESHOLDS', thresholds, ...
-% % %         'BAS', ba ...
-% % %         );
-% % %     g_dict.add(g)
-% % %     
-% % %     if isa(a.getr('TEMPLATE'), 'NoValue')
-% % %         g.set('TEMPLATE', a.memorize('GRAPH_TEMPLATE'))
-% % %     else
-% % %         g.set('TEMPLATE', a.get('TEMPLATE').memorize('GRAPH_TEMPLATE'))        
-% % %     end    
-% % % end
-% % % 
-% % % value = g_dict;
+
+thresholds = a.get('THRESHOLDS'); % this is a vector
+for i = 1:1:gr.get('SUB_DICT').get('LENGTH')
+    sub = gr.get('SUB_DICT').get('IT', i);
+    g = MultiplexBUT( ...
+        'ID', ['g ' sub.get('ID')], ... % % % 'BAS', ba ...
+        'B', sub.getCallback('CON_MP'), ...
+        'THRESHOLDS', thresholds ...
+        );
+    g_dict.add(g)
+end
+
+if ~isa(a.get('GRAPH_TEMPLATE'), 'NoValue')
+    for i = 1:1:g_dict.get('LENGTH')
+        g_dict.get('IT', i).set('TEMPLATE', a.get('GRAPH_TEMPLATE'))
+    end
+end
+
+value = g_dict;
 
 %%% ¡prop!
 ME_DICT (result, idict) contains the calculated measures of the graph ensemble.
@@ -117,3 +113,99 @@ if ~isfile([fileparts(which('SubjectCON_MP')) filesep 'Example data CON_MP XLS' 
 end
 
 example_CON_MP_BUT
+
+%%% ¡test!
+%%%% ¡name!
+GUI - Analysis
+%%%% ¡probability!
+.01
+%%%% ¡parallel!
+false
+%%%% ¡code!
+im_ba = ImporterBrainAtlasXLS('FILE', 'aal90_atlas.xlsx');
+ba = im_ba.get('BA');
+
+gr = Group('SUB_CLASS', 'SubjectCON_MP', 'SUB_DICT', IndexedDictionary('IT_CLASS', 'SubjectCON_MP'));
+for i = 1:1:10
+    sub = SubjectCON_MP( ...
+        'ID', ['SUB CON_MP ' int2str(i)], ...
+        'LABEL', ['Subejct CON_MP ' int2str(i)], ...
+        'NOTES', ['Notes on subject CON_MP ' int2str(i)], ...
+        'BA', ba, ...
+        'L', 3, ...
+        'LAYERLABELS', {'L1' 'L2' 'L3'}, ...
+        'CON_MP', {rand(ba.get('BR_DICT').get('LENGTH')), rand(ba.get('BR_DICT').get('LENGTH')), rand(ba.get('BR_DICT').get('LENGTH'))} ...
+        );
+    sub.memorize('VOI_DICT').get('ADD', VOINumeric('ID', 'Age', 'V', 100 * rand()))
+    sub.memorize('VOI_DICT').get('ADD', VOICategoric('ID', 'Sex', 'CATEGORIES', {'Female', 'Male'}, 'V', randi(2, 1)))
+    gr.get('SUB_DICT').get('ADD', sub)
+end
+
+a = AnalyzeEnsemble_CON_MP_BUT('GR', gr);
+
+gui = GUIElement('PE', a, 'CLOSEREQ', false);
+gui.get('DRAW')
+gui.get('SHOW')
+
+gui.get('CLOSE')
+
+%%% ¡test!
+%%%% ¡name!
+GUI - Comparison
+%%%% ¡probability!
+.01
+%%%% ¡parallel!
+false
+%%%% ¡code!
+im_ba = ImporterBrainAtlasXLS('FILE', 'aal90_atlas.xlsx');
+ba = im_ba.get('BA');
+
+gr1 = Group('SUB_CLASS', 'SubjectCON_MP', 'SUB_DICT', IndexedDictionary('IT_CLASS', 'SubjectCON_MP'));
+for i = 1:1:10
+    sub = SubjectCON_MP( ...
+        'ID', ['SUB CON_MP ' int2str(i)], ...
+        'LABEL', ['Subejct CON_MP ' int2str(i)], ...
+        'NOTES', ['Notes on subject CON_MP ' int2str(i)], ...
+        'BA', ba, ...
+        'L', 3, ...
+        'LAYERLABELS', {'L1' 'L2' 'L3'}, ...
+        'CON_MP', {rand(ba.get('BR_DICT').get('LENGTH')), rand(ba.get('BR_DICT').get('LENGTH')), rand(ba.get('BR_DICT').get('LENGTH'))} ...
+        );
+    sub.memorize('VOI_DICT').get('ADD', VOINumeric('ID', 'Age', 'V', 100 * rand()))
+    sub.memorize('VOI_DICT').get('ADD', VOICategoric('ID', 'Sex', 'CATEGORIES', {'Female', 'Male'}, 'V', randi(2, 1)))
+    gr1.get('SUB_DICT').get('ADD', sub)
+end
+
+gr2 = Group('SUB_CLASS', 'SubjectCON_MP', 'SUB_DICT', IndexedDictionary('IT_CLASS', 'SubjectCON_MP'));
+for i = 1:1:10
+    sub = SubjectCON_MP( ...
+        'ID', ['SUB CON_MP ' int2str(i)], ...
+        'LABEL', ['Subejct CON_MP ' int2str(i)], ...
+        'NOTES', ['Notes on subject CON_MP ' int2str(i)], ...
+        'BA', ba, ...
+        'L', 3, ...
+        'LAYERLABELS', {'L1' 'L2' 'L3'}, ...
+        'CON_MP', {rand(ba.get('BR_DICT').get('LENGTH')), rand(ba.get('BR_DICT').get('LENGTH')), rand(ba.get('BR_DICT').get('LENGTH'))} ...
+        );
+    sub.memorize('VOI_DICT').get('ADD', VOINumeric('ID', 'Age', 'V', 100 * rand()))
+    sub.memorize('VOI_DICT').get('ADD', VOICategoric('ID', 'Sex', 'CATEGORIES', {'Female', 'Male'}, 'V', randi(2, 1)))
+    gr2.get('SUB_DICT').get('ADD', sub)
+end
+
+a1 = AnalyzeEnsemble_CON_MP_BUT('GR', gr1);
+a2 = AnalyzeEnsemble_CON_MP_BUT('GR', gr2, 'TEMPLATE', a1);
+
+c = CompareEnsemble( ...
+    'P', 10, ...
+    'A1', a1, ...
+    'A2', a2, ...
+    'WAITBAR', true, ...
+    'VERBOSE', false, ...
+    'MEMORIZE', true ...
+    );
+
+gui = GUIElement('PE', c, 'CLOSEREQ', false);
+gui.get('DRAW')
+gui.get('SHOW')
+
+gui.get('CLOSE')
