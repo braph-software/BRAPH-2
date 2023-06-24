c02084da292e86268ae67d12c37b8b5070be378c
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-06-24 12:18:03 +0200

    Update _ComparisonEnsemble.gen.m

--- braph2genesis/src/analysis/_ComparisonEnsemble.gen.m
+++ braph2genesis/src/analysis/_ComparisonEnsemble.gen.m
@@ -60,11 +60,9 @@ C (data, item) is the ensemble-based comparison.
 
 %%% ¡prop!
 DIFF (result, cell) is the ensemble comparison value.
-%%%% ¡_calculate!
-% % % if isempty(cp.diff)
-% % %     [cp.diff, cp.p1, cp.p2, cp.ci_lower, cp.ci_upper] = calculate_results(cp);
-% % % end
-% % % value = cp.diff;
+%%%% ¡calculate!
+results = cp.memorize('CALCULATE_RESULTS');
+value = results{1}; % diff
 %%%% ¡_gui!
 % % % g = cp.get('C').get('A1').get('GRAPH_TEMPLATE');
 % % % measure = cp.get('MEASURE');
@@ -174,11 +172,9 @@ DIFF (result, cell) is the ensemble comparison value.
 
 %%% ¡prop!
 P1 (result, cell) is the one-tailed p-value.
-%%%% ¡_calculate!
-% % % if isempty(cp.p1)
-% % %     [cp.diff, cp.p1, cp.p2, cp.ci_lower, cp.ci_upper] = calculate_results(cp);
-% % % end
-% % % value = cp.p1;
+%%%% ¡calculate!
+results = cp.memorize('CALCULATE_RESULTS');
+value = results{2}; % p1
 %%%% ¡_gui!
 % % % g = cp.get('C').get('A1').get('GRAPH_TEMPLATE');
 % % % measure = cp.get('MEASURE');
@@ -288,11 +284,9 @@ P1 (result, cell) is the one-tailed p-value.
 
 %%% ¡prop!
 P2 (result, cell) is the two-tailed p-value.
-%%%% ¡_calculate!
-% % % if isempty(cp.p2)
-% % %     [cp.diff, cp.p1, cp.p2, cp.ci_lower, cp.ci_upper] = calculate_results(cp);
-% % % end
-% % % value = cp.p2;
+%%%% ¡calculate!
+results = cp.memorize('CALCULATE_RESULTS');
+value = results{3}; % p2
 %%%% ¡_gui!
 % % % g = cp.get('C').get('A1').get('GRAPH_TEMPLATE');
 % % % measure = cp.get('MEASURE');
@@ -402,11 +396,9 @@ P2 (result, cell) is the two-tailed p-value.
 
 %%% ¡prop!
 CIL (result, cell) is the lower value of the 95%% confidence interval.
-%%%% ¡_calculate!
-% % % if isempty(cp.ci_lower)
-% % %     [cp.diff, cp.p1, cp.p2, cp.ci_lower, cp.ci_upper] = calculate_results(cp);
-% % % end
-% % % value = cp.ci_lower;
+%%%% ¡calculate!
+results = cp.memorize('CALCULATE_RESULTS');
+value = results{4}; % ci_lower
 %%%% ¡_gui!
 % % % g = cp.get('C').get('A1').get('GRAPH_TEMPLATE');
 % % % measure = cp.get('MEASURE');
@@ -516,11 +508,9 @@ CIL (result, cell) is the lower value of the 95%% confidence interval.
 
 %%% ¡prop!
 CIU (result, cell) is the upper value of the 95%% confidence interval.
-%%%% ¡_calculate!
-% % % if isempty(cp.ci_upper)
-% % %     [cp.diff, cp.p1, cp.p2, cp.ci_lower, cp.ci_upper] = calculate_results(cp);
-% % % end
-% % % value = cp.ci_upper;
+%%%% ¡calculate!
+results = cp.memorize('CALCULATE_RESULTS');
+value = results{5}; % ci_upper
 %%%% ¡_gui!
 % % % g = cp.get('C').get('A1').get('GRAPH_TEMPLATE');
 % % % measure = cp.get('MEASURE');
@@ -733,121 +723,105 @@ PFBG (gui, item) contains the panel figure of the brain graph.
 % % %     'GUICLASS', 'GUIFig', ...
 % % %     varargin{:});
 
-%% ¡_properties!
-% % % diff
-% % % p1
-% % % p2
-% % % ci_lower
-% % % ci_upper
+%%% ¡prop!
+CALCULATE_RESULTS (evanescent, cell) calculates the comparison results {diff, p1, p2, ci_lower, ci_upper}.
+%%%% ¡calculate!
+% {DIFF, P1, P2, CIL, CIU} = cp.get('CALCULATE_RESULTS') calcultes the 
+%  one-talied p-value P1, the two-tailed p-value P2, the lower bound of the 
+%  confidence interval CIL, and the the upper bound of the confidence interval.
+%  Typically, this methos is only called internally.
 
-%% ¡_methods!
-% % % function [diff, p1, p2, ci_lower, ci_upper] = calculate_results(cp)
-% % %     %CALCULATE_RESULTS calculates the comparison results.
-% % %     %
-% % %     % [P1, P2, CIL, CIU] = CALCULATE_RESULTS(CP) calcultes the one-talied
-% % %     %  p-value P1, the two-tailed p-value P2, the lower bound of the confidence
-% % %     %  interval CIL, and the the upper bound of the confidence interval.
-% % %     %  Typically, this methos is only called internally.
-% % % 
-% % %     measure_class = cp.get('MEASURE');
-% % %     if strcmpi(cp.get('MEASURE'), 'Measure')
-% % %         diff = {};
-% % %         p1 = {};
-% % %         p2 = {};
-% % %         ci_lower = {};
-% % %         ci_upper = {};
-% % %         return
-% % %     end
-% % %         
-% % %     % get parameters from example measure
-% % %     core_measure = cp.get('MEASURE_TEMPLATE');
-% % %     j = 1;
-% % %     varargin = {};
-% % %     if Measure.getPropNumber() ~= core_measure.getPropNumber()
-% % %         for i = Measure.getPropNumber() + 1:core_measure.getPropNumber()
-% % %             if ~isa(core_measure.getr(i), 'NoValue')
-% % %                 varargin{j} = core_measure.getPropTag(i);
-% % %                 varargin{j + 1} = Callback('EL', core_measure, 'PROP', i); % % % core_measure.getr(i);
-% % %             end
-% % %             j = j + 2;
-% % %         end
-% % %         varargin = varargin(~cellfun('isempty', varargin));
-% % %     end
-% % % 
-% % %     c = cp.get('C');
-% % % 
-% % %     wb = braph2waitbar(c.get('WAITBAR'), 0, ['Comparing ensemble ' cp.get('MEASURE') '...']);
-% % % 
-% % %     % Pre-calculate and save measures of all subjects
-% % %     ms1 = cellfun(@(x) x.getMeasure(measure_class, varargin{:}).memorize('M'), c.get('A1').memorize('G_DICT').getItems, 'UniformOutput', false);
-% % %     ms2 = cellfun(@(x) x.getMeasure(measure_class, varargin{:}).memorize('M'), c.get('A2').memorize('G_DICT').getItems, 'UniformOutput', false);
-% % %     
-% % %     % Measure for groups 1 and 2, and their difference
-% % %     m1 = c.get('A1').getMeasureEnsemble(measure_class).memorize('M');
-% % %     m2 = c.get('A2').getMeasureEnsemble(measure_class).memorize('M');
-% % %     diff = cellfun(@(x, y) y - x, m1, m2, 'UniformOutput', false);
-% % % 
-% % %     % Permutations
-% % %     P = c.get('P'); %#ok<*PROPLC>
-% % % 
-% % %     m1_perms = cell(1, P);
-% % %     m2_perms = cell(1, P);
-% % %     diff_perms = cell(1, P);
-% % % 
-% % %     start = tic;
-% % %     seeds = c.memorize('PERM_SEEDS');
-% % %     for p = 1:20:P
-% % %         parfor q = p:min(p+20, P)
-% % %             % [a1_perm, a2_perm] = c.getPerm(i, memorize);
-% % %             % 
-% % %             % m1_perms{1, i} = a1_perm.getMeasureEnsemble(measure_class).memorize('M');
-% % %             % m2_perms{1, i} = a2_perm.getMeasureEnsemble(measure_class).memorize('M');
-% % %             % diff_perms{1, i} = cellfun(@(x, y) y - x, m1_perms{1, i}, m2_perms{1, i}, 'UniformOutput', false);
-% % % 
-% % %             rng(seeds(q), 'twister')
-% % % 
-% % %             [ms1_perm, ms2_perm] = permutation(ms1, ms2, c.get('LONGITUDINAL'));
-% % % 
-% % %             ms1_av = cell(size(ms1_perm{1}));
-% % %             ms2_av = cell(size(ms1_perm{1}));
-% % %             for i = 1:1:size(ms1_perm{1}, 1)
-% % %                 for j = 1:1:size(ms1_perm{1}, 2)
-% % % 
-% % %                     ms1_perm_ij = cellfun(@(x) x{i, j}, ms1_perm, 'UniformOutput', false);
-% % %                     ms1_av{i, j} = mean(cat(ndims(ms1_perm_ij{1}) + 1, ms1_perm_ij{:}), ndims(ms1_perm_ij{1}) + 1);
-% % % 
-% % %                     ms2_perm_ij = cellfun(@(x) x{i, j}, ms2_perm, 'UniformOutput', false);
-% % %                     ms2_av{i, j} = mean(cat(ndims(ms2_perm_ij{1}) + 1, ms2_perm_ij{:}), ndims(ms2_perm_ij{1}) + 1);
-% % %                 end
-% % %             end
-% % %             m1_perms{1, q} = ms1_av;
-% % %             m2_perms{1, q} = ms2_av;
-% % %             diff_perms{1, q} = cellfun(@(x, y) y - x, m1_perms{1, q}, m2_perms{1, q}, 'UniformOutput', false);
-% % %         end
-% % % 
-% % %         braph2waitbar(wb, p / P, ['Comparing group ' cp.get('MEASURE') '. Permutation ' num2str(p) ' of ' num2str(P) ' - ' int2str(toc(start)) '.' int2str(mod(toc(start), 1) * 10) 's ...'])
-% % %         if c.get('VERBOSE')
-% % %             disp(['** PERMUTATION TEST - sampling #' int2str(p) '/' int2str(P) ' - ' int2str(toc(start)) '.' int2str(mod(toc(start), 1) * 10) 's'])
-% % %         end
-% % %         if c.get('INTERRUPTIBLE')
-% % %             pause(c.get('INTERRUPTIBLE'))
-% % %         end
-% % %     end
-% % % 
-% % %     braph2waitbar(wb, 'close')
-% % % 
-% % %     % Statistical analysis
-% % %     p1 = cell(size(diff));
-% % %     p2 = cell(size(diff));
-% % %     ci_lower = cell(size(diff));
-% % %     ci_upper = cell(size(diff));
-% % %     for i = 1:1:size(diff, 1)
-% % %         for j = 1:1:size(diff, 2)
-% % %             p1(i, j) = pvalue1(diff(i, j), cellfun(@(x) x{i, j}, diff_perms, 'UniformOutput', false));
-% % %             p2(i, j) = pvalue2(diff(i, j), cellfun(@(x) x{i, j}, diff_perms, 'UniformOutput', false));
-% % %             qtl = quantiles(cellfun(@(x) x{i, j}, diff_perms, 'UniformOutput', false), 40);
-% % %             ci_lower(i, j) = {cellfun(@(x) x(2), qtl)};
-% % %             ci_upper(i, j) = {cellfun(@(x) x(40), qtl)};
-% % %         end
-% % %     end
-% % % end
+measure_class = cp.get('MEASURE');
+if strcmpi(cp.get('MEASURE'), 'Measure')
+    diff = {};
+    p1 = {};
+    p2 = {};
+    ci_lower = {};
+    ci_upper = {};
+    value = {diff, p1, p2, ci_lower, ci_upper};
+    return
+end
+
+c = cp.get('C');
+
+wb = braph2waitbar(c.get('WAITBAR'), 0, ['Comparing ensemble ' cp.get('MEASURE') '...']);
+
+% Pre-calculate and save measures of all subjects
+ms1 = cellfun(@(x) x.get('MEASURE', measure_class, varargin{:}).memorize('M'), c.get('A1').memorize('G_DICT').get('IT_LIST'), 'UniformOutput', false);
+ms2 = cellfun(@(x) x.get('MEASURE', measure_class, varargin{:}).memorize('M'), c.get('A2').memorize('G_DICT').get('IT_LIST'), 'UniformOutput', false);
+
+% Measure for groups 1 and 2, and their difference
+m1 = c.get('A1').get('MEASUREENSEMBLE', measure_class).memorize('M');
+m2 = c.get('A2').get('MEASUREENSEMBLE', measure_class).memorize('M');
+diff = cellfun(@(x, y) y - x, m1, m2, 'UniformOutput', false);
+
+% Permutations
+P = c.get('P'); %#ok<*PROPLC>
+
+m1_perms = cell(1, P);
+m2_perms = cell(1, P);
+diff_perms = cell(1, P);
+
+start = tic;
+seeds = c.memorize('PERM_SEEDS');
+for p = 1:20:P
+    parfor q = p:min(p+20, P)
+        % [a1_perm, a2_perm] = c.getPerm(i, memorize);
+        % 
+        % m1_perms{1, i} = a1_perm.getMeasureEnsemble(measure_class).memorize('M');
+        % m2_perms{1, i} = a2_perm.getMeasureEnsemble(measure_class).memorize('M');
+        % diff_perms{1, i} = cellfun(@(x, y) y - x, m1_perms{1, i}, m2_perms{1, i}, 'UniformOutput', false);
+
+        rng(seeds(q), 'twister')
+
+        [ms1_perm, ms2_perm] = permutation(ms1, ms2, c.get('LONGITUDINAL'));
+
+        ms1_av = cell(size(ms1_perm{1}));
+        ms2_av = cell(size(ms1_perm{1}));
+        for i = 1:1:size(ms1_perm{1}, 1)
+            for j = 1:1:size(ms1_perm{1}, 2)
+
+                ms1_perm_ij = cellfun(@(x) x{i, j}, ms1_perm, 'UniformOutput', false);
+                ms1_av{i, j} = mean(cat(ndims(ms1_perm_ij{1}) + 1, ms1_perm_ij{:}), ndims(ms1_perm_ij{1}) + 1);
+
+                ms2_perm_ij = cellfun(@(x) x{i, j}, ms2_perm, 'UniformOutput', false);
+                ms2_av{i, j} = mean(cat(ndims(ms2_perm_ij{1}) + 1, ms2_perm_ij{:}), ndims(ms2_perm_ij{1}) + 1);
+            end
+        end
+        m1_perms{1, q} = ms1_av;
+        m2_perms{1, q} = ms2_av;
+        diff_perms{1, q} = cellfun(@(x, y) y - x, m1_perms{1, q}, m2_perms{1, q}, 'UniformOutput', false);
+    end
+
+    braph2waitbar(wb, p / P, ['Comparing group ' cp.get('MEASURE') '. Permutation ' num2str(p) ' of ' num2str(P) ' - ' int2str(toc(start)) '.' int2str(mod(toc(start), 1) * 10) 's ...'])
+    if c.get('VERBOSE')
+        disp(['** PERMUTATION TEST - sampling #' int2str(p) '/' int2str(P) ' - ' int2str(toc(start)) '.' int2str(mod(toc(start), 1) * 10) 's'])
+    end
+    if c.get('INTERRUPTIBLE')
+        pause(c.get('INTERRUPTIBLE'))
+    end
+end
+
+braph2waitbar(wb, 'close')
+
+% Statistical analysis
+p1 = cell(size(diff));
+p2 = cell(size(diff));
+ci_lower = cell(size(diff));
+ci_upper = cell(size(diff));
+for i = 1:1:size(diff, 1)
+    for j = 1:1:size(diff, 2)
+        p1(i, j) = pvalue1(diff(i, j), cellfun(@(x) x{i, j}, diff_perms, 'UniformOutput', false));
+        p2(i, j) = pvalue2(diff(i, j), cellfun(@(x) x{i, j}, diff_perms, 'UniformOutput', false));
+        qtl = quantiles(cellfun(@(x) x{i, j}, diff_perms, 'UniformOutput', false), 40);
+        ci_lower(i, j) = {cellfun(@(x) x(2), qtl)};
+        ci_upper(i, j) = {cellfun(@(x) x(40), qtl)};
+	end
+end
+
+value = {diff, p1, p2, ci_lower, ci_upper};
+
+%% ¡tests!
+
+%%% ¡excluded_props!
+[ComparisonEnsemble.CALCULATE_RESULTS]
