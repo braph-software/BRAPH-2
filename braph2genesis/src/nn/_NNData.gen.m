f37bb916650bd8a214273382a09791e4e26662c3
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-07-19 15:44:14 +0200

    Create _NNData.gen.m

---
+++ braph2genesis/src/nn/_NNData.gen.m
@@ -0,0 +1,29 @@
+%% ¡header!
+NNData < ConcreteElement (d, neural network data) ...
+
+...
+
+%% ¡props_update!
+...
+    
+%% ¡props!
+
+%%% ¡prop!
+DP_CLASS (parameter, class) is the class of the data points
+%%%% ¡settings!
+'NNDataPoint'
+
+%%% ¡prop!
+DP_DICT (data, idict) is an indexed dictionary containing the data points.
+%%%% ¡settings!
+'NNDataPoint'
+
+%%% ¡prop!
+INPUTS (result, cell) is a collection of the inputs from all data points.
+%%%% ¡calculate!
+...
+
+%%% ¡prop!
+TARGETS (result, cell) is a collection of the targets from all data points.
+%%%% ¡calculate!
+...
