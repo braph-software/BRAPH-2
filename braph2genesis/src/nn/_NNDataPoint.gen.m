adc4ede853d9cc2f257315ec1c9f5b7e04fcac57
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-07-19 15:44:12 +0200

    Create _NNDataPoint.gen.m

---
+++ braph2genesis/src/nn/_NNDataPoint.gen.m
@@ -0,0 +1,15 @@
+%% ¡header!
+NNDataPoint < ConcreteElement (dp, neural network data point) ...
+
+...
+
+%% ¡props_update!
+...
+    
+%% ¡props!
+
+%%% ¡prop!
+INPUT (result, cell) is the input value for this data point.
+    
+%%% ¡prop!
+TARGET (result, cell) is the target value for this data point.
