5c40d75745cafcaba47263240e4d11b64b29ff72
Yu-Wei Chang <hejyuwei@gmail.com>
2023-08-15 09:03:43 +0200

    NN classification CON (#111)
    * u
    
    * Update _NNBase.gen.m
    
    * Update _NNBase.gen.m
    
    * Update _NNBase.gen.m
    
    * Update _NNBase.gen.m
    
    * Update _NNBase.gen.m
    
    * u
    
    * u
    
    * u
    
    * u
    
    * u
    
    * Update _NNClassifierMLP.gen.m
    
    * Update _NNEvaluator_CLA.gen.m
    
    * Update _NNEvaluator.gen.m
    
    * Update _NNEvaluator.gen.m
    
    * u
    
    * u
    
    * Update _NNEvaluator_REG.gen.m
    
    * Update _NNEvaluator_CLA.gen.m
    
    * u
    
    * u
    
    * u
    
    * u
    
    * u
    
    * u
    
    * u
    
    * u
    
    * Update pipeline_connectivity_regression.braph2
    
    * Update pipeline_connectivity_regression.braph2
    
    * Update _NNDatasetSplit.gen.m
    
    * Update pipeline_connectivity_regression.braph2
    
    * Update pipeline_connectivity_regression.braph2
    
    * u
    
    * Update _NNClassifier_Evaluator.gen.m
    
    * u
    
    * u
    
    * add panel for property net
    
    * u
    
    * Update _PanelPropNet.gen.m
    
    * Update _PanelPropNet.gen.m
    
    * u
    
    * Update Element.m
    
    * Update _PanelPropNet.gen.m
    
    * Update _PanelPropNet.gen.m
    
    * Update _PanelPropNet.gen.m
    
    ---------
    
    Co-authored-by: Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>

---
+++ braph2genesis/neuralnetworks/_NNClassifierMLP.gen.m
@@ -0,0 +1,222 @@
+%% ¡header!
+NNClassifierMLP < NNBase (nn, multi-layer perceptron classifier) comprises a multi-layer perceptron classifier model and a given dataset.
+
+%%% ¡description!
+A neural network multi-layer perceptron classifier (NNClassifierMLP) comprises a multi-layer perceptron classifier model and a given dataset.
+NNClassifierMLP trains the multi-layer perceptron classifier with a formatted inputs ("CB", channel and batch) derived from the given dataset.
+
+%%% ¡seealso!
+NNDataPoint_CON_CLA, NNClassifier_Evaluator
+
+%% ¡props_update!
+
+%%% ¡prop!
+NAME (constant, string) is the name of the neural network multi-layer perceptron classifier.
+%%%% ¡default!
+'NNClassifierMLP'
+
+%%% ¡prop!
+DESCRIPTION (constant, string) is the description of the neural network multi-layer perceptron classifier.
+%%%% ¡default!
+'A neural network multi-layer perceptron classifier (NNClassifierMLP) comprises a multi-layer perceptron classifier model and a given dataset. NNClassifierMLP trains the multi-layer perceptron classifier with a formatted inputs ("CB", channel and batch) derived from the given dataset.'
+
+%%% ¡prop!
+TEMPLATE (parameter, item) is the template of the neural network multi-layer perceptron classifier.
+%%%% ¡settings!
+'NNClassifierMLP'
+
+%%% ¡prop!
+ID (data, string) is a few-letter code for the neural network multi-layer perceptron classifier.
+%%%% ¡default!
+'NNClassifierMLP ID'
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the neural network multi-layer perceptron classifier.
+%%%% ¡default!
+'NNClassifierMLP label'
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the neural network multi-layer perceptron classifier.
+%%%% ¡default!
+'NNClassifierMLP notes'
+
+%%% ¡prop!
+INPUTS (query, cell) constructs the data in the CB (channel-batch) format.
+%%%% ¡calculate!
+% inputs = nn.get('inputs', D) returns a cell array with the
+%  inputs for all data points in dataset D.
+if isempty(varargin)
+    value = {};
+    return
+end
+d = varargin{1};
+inputs = d.get('INPUTS');
+if isempty(inputs)
+    value = {};
+else
+    nn_inputs = [];
+    for i = 1:1:length(inputs)
+        input = cell2mat(inputs{i});
+        nn_inputs = [nn_inputs; input(:)'];
+    end
+    value = {nn_inputs};
+end
+
+%%% ¡prop!
+MODEL (result, net) is a trained neural network model.
+%%%% ¡calculate!
+inputs = cell2mat(nn.get('INPUTS', nn.get('D')));
+targets = nn.get('TARGET_IDS', nn.get('D'));
+if isempty(inputs) || isempty(targets)
+    value = network();
+else
+    number_features = size(inputs, 2);
+    number_targets = size(targets, 2);
+    targets = categorical(targets); 
+    number_classes = numel(categories(targets));
+    
+    layers = nn.get('LAYERS');
+    nn_architecture = [featureInputLayer(number_features, 'Name', 'Input')];
+    for i = 1:1:length(layers)
+        nn_architecture = [nn_architecture
+            fullyConnectedLayer(layers(i), 'Name', ['Dense_' num2str(i)])
+            batchNormalizationLayer('Name', ['BatchNormalization_' num2str(i)])
+            dropoutLayer('Name', ['Dropout_' num2str(i)])
+            ];
+    end
+    nn_architecture = [nn_architecture
+        reluLayer('Name', 'Relu_output')
+        fullyConnectedLayer(number_classes, 'Name', 'Dense_output')
+        softmaxLayer
+        classificationLayer('Name', 'Output')
+        ];
+
+    % specify trianing options
+    options = trainingOptions(nn.get('SOLVER'), ...
+        'MiniBatchSize', nn.get('BATCH'), ...
+        'MaxEpochs', nn.get('EPOCHS'), ...
+        'Shuffle', nn.get('SHUFFLE'), ...
+        'Plots', nn.get('PLOT_TRAINING'), ...
+        'Verbose', nn.get('VERBOSE'));
+
+    % train the neural network
+    value = trainNetwork(inputs, targets, nn_architecture, options);
+end
+
+%% ¡props!
+
+%%% ¡prop!
+TARGET_IDS (query, stringlist) constructs the target IDs which represent the class of each data point.
+%%%% ¡calculate!
+% targets = nn.get('TARGET_IDS', D) returns a cell array with the
+%  targets for all data points in dataset D.
+if isempty(varargin)
+    value = {''};
+    return
+end
+d = varargin{1};
+targets = d.get('TARGETS');
+if isempty(targets)
+    value = {''};
+else
+    nn_targets = [];
+    for i = 1:1:length(targets)
+        target = targets{i};
+        nn_targets = [nn_targets; target];
+    end
+    value = nn_targets;
+end
+
+
+%%% ¡prop!
+LAYERS (data, rvector) defines the number of layers and their neurons.
+%%%% ¡default!
+[32 32]
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+train the classifier with example data
+%%%% ¡code!
+
+% ensure the example data is generated
+if ~isfile([fileparts(which('NNDataPoint_CON_CLA')) filesep 'Example data NN CLA CON XLS' filesep 'atlas.xlsx'])
+    test_NNDataPoint_CON_CLA % create example files
+end
+
+% Load BrainAtlas
+im_ba = ImporterBrainAtlasXLS( ...
+    'FILE', [fileparts(which('NNDataPoint_CON_CLA')) filesep 'Example data NN CLA CON XLS' filesep 'atlas.xlsx'], ...
+    'WAITBAR', true ...
+    );
+
+ba = im_ba.get('BA');
+
+% Load Groups of SubjectCON
+im_gr1 = ImporterGroupSubjectCON_XLS( ...
+    'DIRECTORY', [fileparts(which('NNDataPoint_CON_CLA')) filesep 'Example data NN CLA CON XLS' filesep 'CON_Group_1_XLS'], ...
+    'BA', ba, ...
+    'WAITBAR', true ...
+    );
+
+gr1 = im_gr1.get('GR');
+
+im_gr2 = ImporterGroupSubjectCON_XLS( ...
+    'DIRECTORY', [fileparts(which('NNDataPoint_CON_CLA')) filesep 'Example data NN CLA CON XLS' filesep 'CON_Group_2_XLS'], ...
+    'BA', ba, ...
+    'WAITBAR', true ...
+    );
+
+gr2 = im_gr2.get('GR');
+
+% create item lists of NNDataPoint_CON_CLA
+[~, group_folder_name] = fileparts(im_gr1.get('DIRECTORY'));
+it_list1 = cellfun(@(x) NNDataPoint_CON_CLA( ...
+    'ID', x.get('ID'), ...
+    'SUB', x, ...
+    'TARGET_IDS', {group_folder_name}), ...
+    gr1.get('SUB_DICT').get('IT_LIST'), ...
+    'UniformOutput', false);
+
+[~, group_folder_name] = fileparts(im_gr2.get('DIRECTORY'));
+it_list2 = cellfun(@(x) NNDataPoint_CON_CLA( ...
+    'ID', x.get('ID'), ...
+    'SUB', x, ...
+    'TARGET_IDS', {group_folder_name}), ...
+    gr2.get('SUB_DICT').get('IT_LIST'), ...
+    'UniformOutput', false);
+
+% create NNDataPoint_CON_CLA DICT items
+dp_list1 = IndexedDictionary(...
+        'IT_CLASS', 'NNDataPoint_CON_CLA', ...
+        'IT_LIST', it_list1 ...
+        );
+
+dp_list2 = IndexedDictionary(...
+        'IT_CLASS', 'NNDataPoint_CON_CLA', ...
+        'IT_LIST', it_list2 ...
+        );
+
+% create a NNDataset containing the NNDataPoint_CON_CLA DICT
+d1 = NNDataset( ...
+    'DP_CLASS', 'NNDataPoint_CON_CLA', ...
+    'DP_DICT', dp_list1 ...
+    );
+
+d2 = NNDataset( ...
+    'DP_CLASS', 'NNDataPoint_CON_CLA', ...
+    'DP_DICT', dp_list2 ...
+    );
+
+% combine the two datasets
+d = NNDatasetCombine('D_LIST', {d1, d2}).get('D');
+
+nn = NNClassifierMLP('D', d, 'LAYERS', [10 10 10]);
+trained_model = nn.get('MODEL');
+
+% Check whether the number of fully-connected layer matches (excluding Dense_output layer)
+assert(length(nn.get('LAYERS')) == sum(contains({trained_model.Layers.Name}, 'Dense')) - 1, ...
+    [BRAPH2.STR ':NNClassifierMLP:' BRAPH2.FAIL_TEST], ...
+    'NNClassifierMLP does not construct the layers correctly. The number of the inputs should be the same as the length of dense layers the property.' ...
+    )
---
+++ braph2genesis/neuralnetworks/_NNClassifier_Evaluator.gen.m
@@ -0,0 +1,226 @@
+%% ¡header!
+NNClassifier_Evaluator < NNEvaluator (nne, neural network evaluator for classification) evaluates the performance of a neural network classifier with a given dataset.
+
+%%% ¡description!
+A neural network evaluator for classifier (NNClassifier_Evaluator) evaluates the performance of a neural network classifier with a given dataset.
+NNClassifier_Evaluator evaluates the performance of the trained classifier with a given dataset in terms of various classification metrics (e.g., confusion matrix, area under the receiver operating characteristic curve).
+
+%%% ¡seealso!
+NNDataPoint_CON_CLA, NNClassifierMLP
+
+%% ¡props_update!
+
+%%% ¡prop!
+NAME (constant, string) is the name of the neural network evaluator for the classification task.
+%%%% ¡default!
+'NNClassifier_Evaluator'
+
+%%% ¡prop!
+DESCRIPTION (constant, string) is the description of the neural network evaluator for the classification task.
+%%%% ¡default!
+'A neural network evaluator for classifier (NNClassifier_Evaluator) evaluates the performance of a neural network classifier with a given dataset. NNClassifier_Evaluator evaluates the performance of the trained classifier with a given dataset in terms of various classification metrics (e.g., confusion matrix, area under the receiver operating characteristic curve).'
+
+%%% ¡prop!
+TEMPLATE (parameter, item) is the template of the neural network evaluator for the classification task.
+%%%% ¡settings!
+'NNClassifier_Evaluator'
+
+%%% ¡prop!
+ID (data, string) is a few-letter code for the neural network evaluator for the classification task.
+%%%% ¡default!
+'NNClassifier_Evaluator ID'
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the neural network evaluator for the classification task.
+%%%% ¡default!
+'NNClassifier_Evaluator label'
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the neural network evaluator for the classification task.
+%%%% ¡default!
+'NNClassifier_Evaluator notes'
+    
+%%% ¡prop!
+NN (data, item) contains a trained neural network classifier.
+%%%% ¡settings!
+'NNClassifierMLP'
+
+%% ¡props!
+
+%%% ¡prop!
+GROUND_TRUTH (query, stringlist) returns the matrix of ground truth derived from the targets.
+%%%% ¡calculate!
+targets = nne.get('D').get('TARGETS');
+if isempty(targets)
+    value = {''};
+else
+    for i = 1:length(targets)
+        value(i, :) = targets{i};
+    end
+end
+
+%%% ¡prop!
+AUC (result, rvector) provides the value of the area under the receiver operating characteristic curve.
+%%%% ¡calculate!
+predictions = cell2mat(nne.get('NN').get('PREDICT', nne.get('D')));
+if isempty(predictions)
+    value = [];
+else
+    class_names = nne.get('NN').get('MODEL').Layers(end).Classes;
+    ground_truth = categorical(nne.get('GROUND_TRUTH'));
+    rocNet = rocmetrics(ground_truth, predictions, class_names);
+    value = rocNet.AUC;
+end
+
+%%% ¡prop!
+AVG_AUC (result, scalar) provides the metric of the average AUC value.
+%%%% ¡calculate!
+auc = nne.get('AUC');
+if isempty(auc)
+    value = 0;
+else
+    value = double(mean(auc));
+end
+
+%%% ¡prop!
+C_MATRIX (result, matrix) provides the confusion matrix.
+%%%% ¡calculate!
+predictions = cell2mat(nne.get('NN').get('PREDICT', nne.get('D')));
+predictions = bsxfun(@eq, predictions, max(predictions, [], 2));
+if isempty(predictions)
+    value = [];
+else
+    classNames = nne.get('NN').get('MODEL').Layers(end).Classes;
+    for i = 1:length(predictions)
+        classified(i) = classNames(predictions(i, :));
+    end
+    ground_truth = categorical(nne.get('GROUND_TRUTH'));
+    value = confusionmat(classified, ground_truth);
+end
+
+%%% ¡prop!
+PERMUTATION_FEATURE_IMPORTANCE (result, cell) assess the significance of each feature by randomly shuffling its values and measuring how much the performance of the model decreases.
+%%%% ¡calculate!
+value = {};
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+evaluate a classifier with the example data
+%%%% ¡code!
+
+% ensure the example data is generated
+if ~isfile([fileparts(which('NNDataPoint_CON_CLA')) filesep 'Example data NN CLA CON XLS' filesep 'atlas.xlsx'])
+    test_NNDataPoint_CON_CLA % create example files
+end
+
+% Load BrainAtlas
+im_ba = ImporterBrainAtlasXLS( ...
+    'FILE', [fileparts(which('NNDataPoint_CON_CLA')) filesep 'Example data NN CLA CON XLS' filesep 'atlas.xlsx'], ...
+    'WAITBAR', true ...
+    );
+
+ba = im_ba.get('BA');
+
+% Load Groups of SubjectCON
+im_gr1 = ImporterGroupSubjectCON_XLS( ...
+    'DIRECTORY', [fileparts(which('NNDataPoint_CON_CLA')) filesep 'Example data NN CLA CON XLS' filesep 'CON_Group_1_XLS'], ...
+    'BA', ba, ...
+    'WAITBAR', true ...
+    );
+
+gr1 = im_gr1.get('GR');
+
+im_gr2 = ImporterGroupSubjectCON_XLS( ...
+    'DIRECTORY', [fileparts(which('NNDataPoint_CON_CLA')) filesep 'Example data NN CLA CON XLS' filesep 'CON_Group_2_XLS'], ...
+    'BA', ba, ...
+    'WAITBAR', true ...
+    );
+
+gr2 = im_gr2.get('GR');
+
+% create item lists of NNDataPoint_CON_CLA
+[~, group_folder_name] = fileparts(im_gr1.get('DIRECTORY'));
+it_list1 = cellfun(@(x) NNDataPoint_CON_CLA( ...
+    'ID', x.get('ID'), ...
+    'SUB', x, ...
+    'TARGET_IDS', {group_folder_name}), ...
+    gr1.get('SUB_DICT').get('IT_LIST'), ...
+    'UniformOutput', false);
+
+[~, group_folder_name] = fileparts(im_gr2.get('DIRECTORY'));
+it_list2 = cellfun(@(x) NNDataPoint_CON_CLA( ...
+    'ID', x.get('ID'), ...
+    'SUB', x, ...
+    'TARGET_IDS', {group_folder_name}), ...
+    gr2.get('SUB_DICT').get('IT_LIST'), ...
+    'UniformOutput', false);
+
+% create NNDataPoint_CON_CLA DICT items
+dp_list1 = IndexedDictionary(...
+        'IT_CLASS', 'NNDataPoint_CON_CLA', ...
+        'IT_LIST', it_list1 ...
+        );
+
+dp_list2 = IndexedDictionary(...
+        'IT_CLASS', 'NNDataPoint_CON_CLA', ...
+        'IT_LIST', it_list2 ...
+        );
+
+% create a NNDataset containing the NNDataPoint_CON_CLA DICT
+d1 = NNDataset( ...
+    'DP_CLASS', 'NNDataPoint_CON_CLA', ...
+    'DP_DICT', dp_list1 ...
+    );
+
+d2 = NNDataset( ...
+    'DP_CLASS', 'NNDataPoint_CON_CLA', ...
+    'DP_DICT', dp_list2 ...
+    );
+
+% combine the two datasets
+d = NNDatasetCombine('D_LIST', {d1, d2}).get('D');
+
+nn = NNClassifierMLP('D', d, 'LAYERS', [10 10 10]);
+nn.get('TRAIN');
+nne = NNClassifier_Evaluator('NN', nn, 'D', d);
+
+% Check whether the ground truth are derived as expected
+ground_truth = nne.get('GROUND_TRUTH');
+targets = d.get('TARGETS');
+
+for i = 1:size(ground_truth, 1)
+    check(i) = isequal(targets{i}, ground_truth(i, :));
+end
+assert(all(check), ...
+    [BRAPH2.STR ':NNEvaluator_CLA:' BRAPH2.FAIL_TEST], ...
+    'NNEvaluator_CLA does not calculate the ground truth correctly.' ...
+    )
+
+% Check whether the number of predictions are the same as the input datapoints
+predictions = cell2mat(nne.get('NN').get('PREDICT', nne.get('D')));
+num_predictions = length(predictions);
+num_datapoints = nne.get('D').get('DP_DICT').get('LENGTH');
+assert(isequal(num_predictions, num_datapoints), ...
+    [BRAPH2.STR ':NNEvaluator_CLA:' BRAPH2.FAIL_TEST], ...
+    'NNEvaluator_CLA does not calculate the predictions correctly.' ...
+    )
+
+% Check whether the dimension of the confusion matrix matches with the number of classes
+c_matrix = nne.get('C_MATRIX');
+dim_c = size(c_matrix, 1);
+num_classes = numel(nne.get('NN').get('MODEL').Layers(end).Classes);
+assert(isequal(dim_c, num_classes), ...
+    [BRAPH2.STR ':NNEvaluator_CLA:' BRAPH2.FAIL_TEST], ...
+    'NNEvaluator_CLA does not calculate the confusion matrix correctly.' ...
+    )
+
+% Check whether the dimension of the AUC matches with the number of classes
+auc = nne.get('AUC');
+dim_auc = length(auc);
+num_classes = numel(nne.get('NN').get('MODEL').Layers(end).Classes);
+assert(isequal(dim_auc, num_classes), ...
+    [BRAPH2.STR ':NNEvaluator_CLA:' BRAPH2.FAIL_TEST], ...
+    'NNEvaluator_CLA does not calculate the confusion matrix correctly.' ...
+    )
