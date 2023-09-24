e416227e6e9f28198ffec7bd161eed627e1428bc
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-09-24 08:51:24 +0200

    Update example_FUN_MP_GA_WU.m

--- braph2genesis/pipelines/functional multiplex group_average/example_FUN_MP_GA_WU.m
+++ braph2genesis/pipelines/functional multiplex group_average/example_FUN_MP_GA_WU.m
@@ -75,4 +75,4 @@ weo_av_WU_diff = c_WU.get('COMPARISON', 'WeightedEdgeOvlp').get('DIFF');
 weo_av_WU_p1 = c_WU.get('COMPARISON', 'WeightedEdgeOvlp').get('P1');
 weo_av_WU_p2 = c_WU.get('COMPARISON', 'WeightedEdgeOvlp').get('P2');
 weo_av_WU_cil = c_WU.get('COMPARISON', 'WeightedEdgeOvlp').get('CIL');
-weo_av_WU_ciu = c_WU.get('COMPARISON', 'WeightedEdgeOverlap').get('CIU');
+weo_av_WU_ciu = c_WU.get('COMPARISON', 'WeightedEdgeOvlp').get('CIU');
