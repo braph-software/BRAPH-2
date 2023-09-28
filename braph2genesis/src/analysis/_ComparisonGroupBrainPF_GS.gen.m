f3fc0e400fda1f11577ecc0765ac4da9b3591f7f
bzufiriaki <112950493+bzufiriaki@users.noreply.github.com>
2023-09-28 16:16:40 +0200

    Bz measure brain pf bz (#197)
    * ComparisonGroupBrainPF
    
    * ComparisonGroupBrainPF
    
    * solve prop error
    
    * ECLASSS like this, ending with % % %
    
    * solve props problems
    
    * solve test removing figures
    
    * solve test removing figures
    
    * Update _ComparisonGroup.gen.m
    
    * Update _ComparisonGroupBrainPF_GS.gen.m
    
    ---------
    
    Co-authored-by: Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>

---
+++ braph2genesis/src/analysis/_ComparisonGroupBrainPF_GS.gen.m
@@ -0,0 +1,124 @@
+%% ¡header!
+ComparisonGroupBrainPF_GS < ComparisonGroupBrainPF (pf, panel global superglobal group comparison on brain surface figure) is the base element to plot a global superglobal group comparison on brain surface figure.
+
+%%% ¡description!
+ComparisonGroupBrainPF_GS manages the  % % % .
+
+%%% ¡seealso!
+ComparisonGroup
+
+%% ¡layout!
+
+%%% ¡prop!
+%%%% ¡id!
+ComparisonGroupBrainPF_GS.ID
+%%%% ¡title!
+Brain Atlas Figure ID
+
+%%% ¡prop!
+%%%% ¡id!
+ComparisonGroupBrainPF_GS.LABEL
+%%%% ¡title!
+Brain Atlas Figure NAME
+
+%%% ¡prop!
+%%%% ¡id!
+ComparisonGroupBrainPF_GS.WAITBAR
+%%%% ¡title!
+WAITBAR ON/OFF
+
+%%% ¡prop!
+%%%% ¡id!
+ComparisonGroupBrainPF_GS.NOTES
+%%%% ¡title!
+Brain Atlas NOTES
+
+%%% ¡prop!
+%%%% ¡id!
+ComparisonGroupBrainPF_GS.BKGCOLOR
+%%%% ¡title!
+BACKGROUND COLOR
+
+%%% ¡prop!
+%%%% ¡id!
+ComparisonGroupBrainPF_GS.ST_POSITION
+%%%% ¡title!
+PANEL POSITION
+
+%%% ¡prop!
+%%%% ¡id!
+ComparisonGroupBrainPF_GS.ST_AXIS
+%%%% ¡title!
+AXIS
+
+%% ¡props_update!
+
+%%% ¡prop!
+ELCLASS (constant, string) is the class of the % % % .
+%%%% ¡default!
+'ComparisonGroupBrainPF_GS'
+
+%%% ¡prop!
+NAME (constant, string) is the name of the panel figure global superglobal group comparison on brain surface figure.
+%%%% ¡default!
+'ComparisonGroupBrainPF_GS'
+
+%%% ¡prop!
+DESCRIPTION (constant, string) is the description of the panel figure global superglobal group comparison on brain surface figure.
+%%%% ¡default!
+'ComparisonGroupBrainPF_GS manages the basic functionalities to plot of a global superglobal group comparison on brain surface figure.'
+
+%%% ¡prop!
+TEMPLATE (parameter, item) is the template of the panel figure global superglobal group comparison on brain surface figure.
+%%%% ¡settings!
+'ComparisonGroupBrainPF_GS'
+
+%%% ¡prop!
+ID (data, string) is a few-letter code for the panel figure global superglobal group comparison on brain surface figure.
+%%%% ¡default!
+'ComparisonGroupBrainPF_GS ID'
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the panel figure global superglobal group comparison on brain surface figure.
+%%%% ¡default!
+'ComparisonGroupBrainPF_GS label'
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the panel figure global superglobal group comparison on brain surface figure.
+%%%% ¡default!
+'ComparisonGroupBrainPF_GS notes'
+
+%%% ¡prop!
+SETUP (query, empty) calculates the group comparison on brain surface figure value and stores it.
+%%%% ¡calculate!
+%%%__WARN_TBI__
+value = [];
+
+%% ¡props!
+
+%%% ¡prop!
+NODES (figure, rvector) are the node numbers of the global group comparison figure on brain surface.
+%%%% ¡_gui!
+% % % bas = pf.get('M').get('G').get('BAS');
+% % % ba = bas{1};
+% % % 
+% % % pr = PP_BrainRegion('EL', pf, 'PROP', PFMeasureNU.BR1_ID, ...
+% % %     'BA', ba, ...
+% % %     varargin{:});
+
+%% ¡tests!
+
+%%% ¡excluded_props!
+[ComparisonGroupBrainPF_GS.PARENT ComparisonGroupBrainPF_GS.H ComparisonGroupBrainPF_GS.ST_POSITION ComparisonGroupBrainPF_GS.ST_AXIS ComparisonGroupBrainPF_GS.ST_SURFACE ComparisonGroupBrainPF_GS.ST_AMBIENT ComparisonGroupBrainPF_GS.CP] 
+
+%%% ¡warning_off!
+true
+
+%%% ¡test!
+%%%% ¡name!
+Remove Figures
+%%%% ¡code!
+warning('off', [BRAPH2.STR ':ComparisonGroupBrainPF_GS'])
+assert(length(findall(0, 'type', 'figure')) == 5)
+delete(findall(0, 'type', 'figure'))
+warning('on', [BRAPH2.STR ':ComparisonGroupBrainPF_GS'])
