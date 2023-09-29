10ad97694fb360d5daf3876060e0bc3d73fa7ce5
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-09-29 21:07:48 +0200

    u

---
+++ braph2genesis/pipelines/connectivity multiplex/_ExporterGroupSubjectCON_MP_XLS.gen.m
@@ -0,0 +1,331 @@
+%% ¡header!
+ExporterGroupSubjectCON_MP_XLS < Exporter (ex, exporter of CON MP subject group in XLSX) exports a group of subjects with connectivity multiplex data to a series of XLSX file.
+
+%%% ¡description!
+ExporterGroupSubjectCON_MP_XLS exports a group of subjects with connectivity 
+ multiplex data to a series of tab-separated XLSX files contained in a folder 
+ named "GROUP_ID". All these files are saved in the same folder. Each file 
+ contains a table of values corresponding to the adjacency matrix. Files 
+ should be labeled with the layer number indicated as, e.g., 
+ "SUBJECT_ID.1.xlsx" and "SUBJECT_ID.2.xlsx".
+The variables of interest (if existing) are saved in another tab-separated 
+ XLSX file named "GROUP_ID.vois.xlsx" consisting of the following columns: 
+ Subject ID (column 1), covariates (subsequent columns). 
+ The 1st row contains the headers, the 2nd row a string with the categorical
+ variables of interest, and each subsequent row the values for each subject.
+
+%%% ¡seealso!
+Group, SunbjectCON_MP, ImporterGroupSubjectCON_MP_XLS
+
+%% ¡props_update!
+
+%%% ¡prop!
+ELCLASS (constant, string) is the class of the % % % .
+%%%% ¡default!
+'ExporterGroupSubjectCON_MP_XLS'
+
+%%% ¡prop!
+NAME (constant, string) is the name of the CON MP subject group exporter in XLSX.
+%%%% ¡default!
+'ExporterGroupSubjectCON_MP_XLS'
+
+%%% ¡prop!
+DESCRIPTION (constant, string) is the description of the CON MP subject group exporter in XLSX.
+%%%% ¡default!
+'ExporterGroupSubjectCON_MP_XLS exports a group of subjects with connectivity multiplex data to a series of XLSX file and their covariates (if existing).'
+
+%%% ¡prop!
+TEMPLATE (parameter, item) is the template of the CON MP subject group exporter in XLSX.
+%%%% ¡settings!
+'ExporterGroupSubjectCON_MP_XLS'
+
+%%% ¡prop!
+ID (data, string) is a few-letter code for the CON MP subject group exporter in XLSX.
+%%%% ¡default!
+'ExporterGroupSubjectCON_MP_XLS ID'
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the CON MP subject group exporter in XLSX.
+%%%% ¡default!
+'ExporterGroupSubjectCON_MP_XLS label'
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the CON MP subject group exporter in XLSX.
+%%%% ¡default!
+'ExporterGroupSubjectCON_MP_XLS notes'
+
+%% ¡props!
+
+%%% ¡prop!
+GR (data, item) is a group of subjects with connectivity multiplex data.
+%%%% ¡settings!
+'Group'
+%%%% ¡check_prop!
+check = any(strcmp(value.get(Group.SUB_CLASS_TAG), subclasses('SubjectCON_MP', [], [], true))); 
+%%%% ¡default!
+Group('SUB_CLASS', 'SubjectCON_MP', 'SUB_DICT', IndexedDictionary('IT_CLASS', 'SubjectCON_MP'))
+
+%%% ¡prop!
+DIRECTORY (data, string) is the directory name where to save the group of subjects with connectivity multiplex data.
+%%%% ¡default!
+[fileparts(which('test_braph2')) filesep 'default_group_subjects_CON_MP_most_likely_to_be_erased']
+
+%%% ¡prop!
+PUT_DIR (query, item) opens a dialog box to set the directory where to save the group of subjects with connectivity data.
+%%%% ¡settings!
+'ExporterGroupSubjectCON_MP_XLS'
+%%%% ¡calculate!
+directory = uigetdir('Select directory');
+if ischar(directory) && isfolder(directory)
+    ex.set('DIRECTORY', directory);
+end
+value = ex;
+
+%%% ¡prop!
+SAVE (result, empty) saves the group of subjects with connectivity multiplex data in XLSX files in the selected directory.
+%%%% ¡calculate!
+directory = ex.get('DIRECTORY');
+
+if isfolder(directory)
+    wb = braph2waitbar(ex.get('WAITBAR'), 0, 'Retrieving path ...');
+
+    gr = ex.get('GR');
+
+    gr_directory = [directory filesep() gr.get('ID')];
+    if ~exist(gr_directory, 'dir')
+        mkdir(gr_directory)
+    end
+    
+	braph2waitbar(wb, .15, 'Organizing info ...')
+
+    sub_dict = gr.get('SUB_DICT');
+    sub_number = sub_dict.get('LENGTH');
+
+    for i = 1:1:sub_number
+        braph2waitbar(wb, .15 + .85 * i / sub_number, ['Saving subject ' num2str(i) ' of ' num2str(sub_number) ' ...'])
+
+        L = sub_dict.get('IT', 1).get('L');
+        
+        sub = sub_dict.get('IT', i);
+        sub_id = sub.get('ID');
+        sub_CON_MP = sub.get('CON_MP');
+                
+        for l = 1:1:L
+            tab = table(sub_CON_MP{l});
+            
+            sub_file = [gr_directory filesep() sub_id '.' int2str(l) '.xlsx'];
+            
+            % save file
+            writetable(tab, sub_file, 'WriteVariableNames', false);
+        end
+    end
+    
+    % variables of interest
+    voi_ids = {};
+    for i = 1:1:sub_number
+        sub = sub_dict.get('IT', i);
+        voi_ids = unique([voi_ids, sub.get('VOI_DICT').get('KEYS')]);
+    end
+    if ~isempty(voi_ids)
+        vois = cell(2 + sub_number, 1 + length(voi_ids));
+        vois{1, 1} = 'Subject ID';
+        vois(1, 2:end) = voi_ids;
+        for i = 1:1:sub_number
+            sub = sub_dict.get('IT', i);
+            vois{2 + i, 1} = sub.get('ID');
+            
+            voi_dict = sub.get('VOI_DICT');
+            for v = 1:1:voi_dict.get('LENGTH')
+                voi = voi_dict.get('IT', v);
+                voi_id = voi.get('ID');
+                if isa(voi, 'VOINumeric') % Numeric
+                    vois{2 + i, 1 + find(strcmp(voi_id, voi_ids))} = voi.get('V');
+                elseif isa(voi, 'VOICategoric') % Categoric
+                    categories = voi.get('CATEGORIES');
+                    vois{2, 1 + find(strcmp(voi_id, voi_ids))} = cell2str(categories);
+                    vois{2 + i, 1 + find(strcmp(voi_id, voi_ids))} = categories{voi.get('V')};
+                end
+            end
+        end
+        writetable(table(vois), [gr_directory '.vois.xlsx'], 'WriteVariableNames', false)
+    end
+    
+    braph2waitbar(wb, 'close')
+end
+
+% sets value to empty
+value = [];
+
+%% ¡tests!
+
+%%% ¡excluded_props!
+[ExporterGroupSubjectCON_MP_XLS.PUT_DIR]
+
+%%% ¡test!
+%%%% ¡name!
+Delete directory TBE
+%%%% ¡probability!
+1
+%%%% ¡code!
+warning('off', 'MATLAB:DELETE:FileNotFound')
+dir_to_be_erased = ExporterGroupSubjectCON_MP_XLS.getPropDefault('DIRECTORY');
+if isfolder(dir_to_be_erased)
+    rmdir(dir_to_be_erased, 's')
+end
+warning('on', 'MATLAB:DELETE:FileNotFound')
+
+
+%%% ¡test!
+%%%% ¡name!
+Export and import
+%%%% ¡probability!
+.01
+%%%% ¡code!
+br1 = BrainRegion( ...
+    'ID', 'ISF', ...
+    'LABEL', 'superiorfrontal', ...
+    'NOTES', 'notes1', ...
+    'X', -12.6, ...
+    'Y', 22.9, ...
+    'Z', 42.4 ...
+    );
+br2 = BrainRegion( ...
+    'ID', 'lFP', ...
+    'LABEL', 'frontalpole', ...
+    'NOTES', 'notes2', ...
+    'X', -8.6, ...
+    'Y', 61.7, ...
+    'Z', -8.7 ...
+    );
+br3 = BrainRegion( ...
+    'ID', 'lRMF', ...
+    'LABEL', 'rostralmiddlefrontal', ...
+    'NOTES', 'notes3', ...
+    'X', -31.3, ...
+    'Y', 41.2, ...
+    'Z', 16.5 ...
+    );
+br4 = BrainRegion( ...
+    'ID', 'lCMF', ...
+    'LABEL', 'caudalmiddlefrontal', ...
+    'NOTES', 'notes4', ...
+    'X', -34.6, ...
+    'Y', 10.2, ...
+    'Z', 42.8 ...
+    );
+br5 = BrainRegion( ...
+    'ID', 'lPOB', ...
+    'LABEL', 'parsorbitalis', ...
+    'NOTES', 'notes5', ...
+    'X', -41, ...
+    'Y', 38.8, ...
+    'Z', -11.1 ...
+    );
+
+ba = BrainAtlas( ...
+    'ID', 'TestToSaveCoolID', ...
+    'LABEL', 'Brain Atlas', ...
+    'NOTES', 'Brain atlas notes', ...
+    'BR_DICT', IndexedDictionary('IT_CLASS', 'BrainRegion', 'IT_LIST', {br1, br2, br3, br4, br5}) ...
+    );
+
+sub1 = SubjectCON_MP( ...
+    'ID', 'SUB CON_MP 1', ...
+    'LABEL', 'Subejct CON_MP 1', ...
+    'NOTES', 'Notes on subject CON_MP 1', ...
+    'BA', ba, ...
+    'L', 2, ...
+    'CON_MP', {rand(ba.get('BR_DICT').get('LENGTH'), ba.get('BR_DICT').get('LENGTH')), rand(ba.get('BR_DICT').get('LENGTH'), ba.get('BR_DICT').get('LENGTH'))} ...
+    );
+sub1.memorize('VOI_DICT').get('ADD', VOINumeric('ID', 'Age', 'V', 75))
+sub1.memorize('VOI_DICT').get('ADD', VOICategoric('ID', 'Sex', 'CATEGORIES', {'Female', 'Male'}, 'V', find(strcmp('Female', {'Female', 'Male'}))))
+
+sub2 = SubjectCON_MP( ...
+    'ID', 'SUB CON_MP 2', ...
+    'LABEL', 'Subejct CON_MP 2', ...
+    'NOTES', 'Notes on subject CON_MP 2', ...
+    'BA', ba, ...
+    'L', 2, ...
+    'CON_MP', {rand(ba.get('BR_DICT').get('LENGTH'), ba.get('BR_DICT').get('LENGTH')), rand(ba.get('BR_DICT').get('LENGTH'), ba.get('BR_DICT').get('LENGTH'))} ...
+    );
+sub2.memorize('VOI_DICT').get('ADD', VOINumeric('ID', 'Age', 'V', 70))
+sub2.memorize('VOI_DICT').get('ADD', VOICategoric('ID', 'Sex', 'CATEGORIES', {'Female', 'Male'}, 'V', find(strcmp('Male', {'Female', 'Male'}))))
+
+sub3 = SubjectCON_MP( ...
+    'ID', 'SUB CON_MP 3', ...
+    'LABEL', 'Subejct CON_MP 3', ...
+    'NOTES', 'Notes on subject CON_MP 3', ...
+    'BA', ba, ...
+    'L', 2, ...
+    'CON_MP', {rand(ba.get('BR_DICT').get('LENGTH'), ba.get('BR_DICT').get('LENGTH')), rand(ba.get('BR_DICT').get('LENGTH'), ba.get('BR_DICT').get('LENGTH'))} ...
+    );
+sub3.memorize('VOI_DICT').get('ADD', VOINumeric('ID', 'Age', 'V', 50))
+sub3.memorize('VOI_DICT').get('ADD', VOICategoric('ID', 'Sex', 'CATEGORIES', {'Female', 'Male'}, 'V', find(strcmp('Female', {'Female', 'Male'}))))
+
+gr = Group( ...
+    'ID', 'GR CON_MP', ...
+    'LABEL', 'Group label', ...
+    'NOTES', 'Group notes', ...
+    'SUB_CLASS', 'SubjectCON_MP', ...
+    'SUB_DICT', IndexedDictionary('IT_CLASS', 'SubjectCON_MP', 'IT_LIST', {sub1, sub2, sub3}) ...
+    );
+
+directory = [fileparts(which('test_braph2')) filesep 'trial_group_subjects_CON_MP_to_be_erased'];
+if ~exist(directory, 'dir')
+    mkdir(directory)
+end
+
+ex = ExporterGroupSubjectCON_MP_XLS( ...
+    'DIRECTORY', directory, ...
+    'GR', gr ...
+    );
+ex.get('SAVE');
+
+% import with same brain atlas
+im1 = ImporterGroupSubjectCON_MP_XLS( ...
+    'DIRECTORY', [directory filesep() gr.get(Group.ID)], ...
+    'BA', ba ...
+    );
+gr_loaded1 = im1.get('GR');
+
+assert(gr.get('SUB_DICT').get('LENGTH') == gr_loaded1.get('SUB_DICT').get('LENGTH'), ...
+	[BRAPH2.STR ':ExporterGroupSubjectCON_MP_XLS:' BRAPH2.FAIL_TEST], ...
+    'Problems saving or loading a group.')
+for i = 1:1:max(gr.get('SUB_DICT').get('LENGTH'), gr_loaded1.get('SUB_DICT').get('LENGTH'))
+    sub = gr.get('SUB_DICT').get('IT', i);
+    sub_loaded = gr_loaded1.get('SUB_DICT').get('IT', i);    
+    assert( ...
+        isequal(sub.get('ID'), sub_loaded.get('ID')) & ...
+        isequal(sub.get('BA'), sub_loaded.get('BA')) & ...
+        isequal(sub.get('VOI_DICT').get('IT', 'Age').get('V'), sub_loaded.get('VOI_DICT').get('IT', 'Age').get('V')) & ... 
+        isequal(sub.get('VOI_DICT').get('IT', 'Sex').get('V'), sub_loaded.get('VOI_DICT').get('IT', 'Sex').get('V')) & ...        
+        isequal(sub.get('L'), sub_loaded.get('L')) & ...
+        isequal(sub.get('CON_MP'), sub_loaded.get('CON_MP')), ...
+        [BRAPH2.STR ':ExporterGroupSubjectCON_MP_XLS:' BRAPH2.FAIL_TEST], ...
+        'Problems saving or loading a group.')    
+end
+
+% import with new brain atlas
+im2 = ImporterGroupSubjectCON_MP_XLS( ...
+    'DIRECTORY', [directory filesep() gr.get(Group.ID)] ...
+    );
+gr_loaded2 = im2.get('GR');
+
+assert(gr.get('SUB_DICT').get('LENGTH') == gr_loaded2.get('SUB_DICT').get('LENGTH'), ...
+	[BRAPH2.STR ':ExporterGroupSubjectCON_MP_XLS:' BRAPH2.FAIL_TEST], ...
+    'Problems saving or loading a group.')
+for i = 1:1:max(gr.get('SUB_DICT').get('LENGTH'), gr_loaded2.get('SUB_DICT').get('LENGTH'))
+    sub = gr.get('SUB_DICT').get('IT', i);
+    sub_loaded = gr_loaded2.get('SUB_DICT').get('IT', i);
+    assert( ...
+        isequal(sub.get('ID'), sub_loaded.get('ID')) & ...
+        ~isequal(sub.get('BA').get('ID'), sub_loaded.get('BA').get('ID')) & ...
+        isequal(sub.get('VOI_DICT').get('IT', 'Age').get('V'), sub_loaded.get('VOI_DICT').get('IT', 'Age').get('V')) & ... 
+        isequal(sub.get('VOI_DICT').get('IT', 'Sex').get('V'), sub_loaded.get('VOI_DICT').get('IT', 'Sex').get('V')) & ...
+        isequal(sub.get('L'), sub_loaded.get('L')) & ...
+        isequal(sub.get('CON_MP'), sub_loaded.get('CON_MP')), ...
+        [BRAPH2.STR ':ExporterGroupSubjectCON_MP_XLS:' BRAPH2.FAIL_TEST], ...
+        'Problems saving or loading a group.')    
+end
+
+rmdir(directory, 's')
---
+++ braph2genesis/pipelines/connectivity multiplex/_SubjectCON_MP.gen.m
@@ -0,0 +1,263 @@
+%% ¡header!
+SubjectCON_MP < Subject (sub, subject with connectivity multiplex data) is a subject with connectivity multiplex data.
+
+%%% ¡description!
+Subject with L connectivity matrices (e.g. obtained from DTI).
+
+%%% ¡seealso!
+ImporterGroupSubjectCON_MP_TXT, ExporterGroupSubjectCON_MP_TXT, ImporterGroupSubjectCON_MP_XLS, ExporterGroupSubjectCON_MP_XLS
+
+%% ¡gui!
+
+%%% ¡menu_import!
+if isa(el, 'Group')
+    uimenu(menu_import, ...
+        'Tag', 'MENU.Import.TXT', ...
+        'Label', 'Import TXT ...', ...
+        'Callback', {@cb_importer_TXT});
+end
+function cb_importer_TXT(~, ~)
+    try
+        im = ImporterGroupSubjectCON_MP_TXT( ...
+            'ID', 'Import Group of SubjectCON_MPs from TXT', ...
+            'WAITBAR', true ...
+            ).get('GET_DIR');
+        if ~isa(im.getr('DIRECTORY'), 'NoValue')
+            gr = im.get('GR');
+            gui = GUIElement('PE', gr);
+            gui.get('DRAW')
+            gui.get('SHOW')
+        end
+    catch e
+        %TODO implement and use braph2msgbox instead of warndlg
+        warndlg(['Please, select a valid input Group of SubjectCON_MPs in TXT format. ' newline() ...
+            newline() ...
+            'Error message:' newline() ...
+            newline() ...
+            e.message newline()], 'Warning');
+    end
+end
+
+if isa(el, 'Group')
+    uimenu(menu_import, ...
+        'Tag', 'MENU.Import.XLS', ...
+        'Label', 'Import XLS ...', ...
+        'Callback', {@cb_importer_XLS});
+end
+function cb_importer_XLS(~, ~)
+    try
+        im = ImporterGroupSubjectCON_MP_XLS( ...
+            'ID', 'Import Group of SubjectCON_MPs from XLS', ...
+            'WAITBAR', true ...
+            ).get('GET_DIR');
+        if ~isa(im.getr('DIRECTORY'), 'NoValue')
+            gr = im.get('GR');
+            gui = GUIElement('PE', gr);
+            gui.get('DRAW')
+            gui.get('SHOW')
+        end
+    catch e
+        %TODO implement and use braph2msgbox instead of warndlg
+        warndlg(['Please, select a valid input Group of SubjectCON_MPs in XLS format. ' newline() ...
+            newline() ...
+            'Error message:' newline() ...
+            newline() ...
+            e.message newline()], 'Warning');
+    end
+end
+
+%%% ¡menu_export!
+if isa(el, 'Group')
+    uimenu(menu_export, ...
+        'Tag', 'MENU.Export.TXT', ...
+        'Label', 'Export TXT ...', ...
+        'Callback', {@cb_exporter_TXT});
+end
+function cb_exporter_TXT(~, ~)
+    ex = ExporterGroupSubjectCON_MP_TXT( ...
+        'ID', 'Export Brain Group of SubjectCON_MPs to TXT', ...
+        'GR', el, ...
+        'WAITBAR', true ...
+        ).get('PUT_DIR');
+    if ~isa(ex.get('DIRECTORY'), 'NoValue')
+        ex.get('SAVE');
+    end
+end
+
+if isa(el, 'Group')
+    uimenu(menu_export, ...
+        'Tag', 'MENU.Export.XLS', ...
+        'Label', 'Export XLS ...', ...
+        'Callback', {@cb_exporter_XLS});
+end
+function cb_exporter_XLS(~, ~)
+    ex = ExporterGroupSubjectCON_MP_XLS( ...
+        'ID', 'Export Brain Group of SubjectCON_MPs to XLS', ...
+        'GR', el, ...
+        'WAITBAR', true ...
+        ).get('PUT_DIR');
+    if ~isa(ex.get('DIRECTORY'), 'NoValue')
+        ex.get('SAVE');
+    end
+end
+
+%% ¡layout!
+
+%%% ¡prop!
+%%%% ¡id!
+SubjectCON_MP.ID
+%%%% ¡title!
+Subject ID
+
+%%% ¡prop!
+%%%% ¡id!
+SubjectCON_MP.LABEL
+%%%% ¡title!
+Subject LABEL
+
+%%% ¡prop!
+%%%% ¡id!
+SubjectCON_MP.BA
+%%%% ¡title!
+Brain Atlas
+
+%%% ¡prop!
+%%%% ¡id!
+SubjectCON_MP.VOI_DICT
+%%%% ¡title!
+Variables of Interest
+
+%%% ¡prop!
+%%%% ¡id!
+SubjectCON_MP.L
+%%%% ¡title!
+Layer Number
+
+%%% ¡prop!
+%%%% ¡id!
+SubjectCON_MP.LAYERLABELS
+%%%% ¡title!
+Layer Labels
+
+%%% ¡prop!
+%%%% ¡id!
+SubjectCON_MP.CON_MP
+%%%% ¡title!
+Connectivity DATA LAYERS
+
+%%% ¡prop!
+%%%% ¡id!
+SubjectCON_MP.NOTES
+%%%% ¡title!
+Subject NOTES
+
+%% ¡props_update!
+
+%%% ¡prop!
+ELCLASS (constant, string) is the class of the % % % .
+%%%% ¡default!
+'SubjectCON_MP'
+
+%%% ¡prop!
+NAME (constant, string) is the name of the subject.
+%%%% ¡default!
+'SubjectCON_MP'
+
+%%% ¡prop!
+DESCRIPTION (constant, string) is the description of the subject.
+%%%% ¡default!
+'Subject with L connectivity matrices (e.g. obtained from DTI).'
+
+%%% ¡prop!
+TEMPLATE (parameter, item) is the template of the subject.
+%%% ¡settings!
+'SubjectCON_MP'
+
+%%% ¡prop!
+ID (data, string) is a few-letter code for the subject.
+%%%% ¡default!
+'SubjectCON_MP ID'
+
+%%% ¡prop!
+LABEL (metadata, string) is an extended label of the subject.
+%%%% ¡default!
+'SubjectCON_MP label'
+
+%%% ¡prop!
+NOTES (metadata, string) are some specific notes about the subject.
+%%%% ¡default!
+'SubjectCON_MP notes'
+
+%% ¡props!
+
+%%% ¡prop!
+BA (data, item) is a brain atlas.
+%%%% ¡settings!
+'BrainAtlas'
+
+%%% ¡prop!
+L (data, scalar) is the number of layers of subject data.
+%%%% ¡default!
+2
+
+%%% ¡prop!
+LAYERLABELS (metadata, stringlist) are the layer labels provided by the user.
+
+%%% ¡prop!
+ALAYERLABELS (query, stringlist) returns the processed layer labels.
+%%%% ¡calculate!
+value = sub.get('LAYERLABELS');
+
+%%% ¡prop!
+CON_MP (data, cell) is a cell containing L matrices corresponding connectivity matrices of each layer.
+%%%% ¡check_value!
+br_number = sub.get('BA').get('BR_DICT').get('LENGTH');
+num_layers = sub.get('L');
+check = (iscell(value) && isequal(length(value), num_layers)  && isequal( cellfun(@(v) size(v, 1), value), ones(1, num_layers) * br_number)  && isequal( cellfun(@(v) size(v, 2), value), ones(1, num_layers) * br_number)) || (isempty(value) && br_number == 0);
+if check
+    msg = 'All ok!';
+else   
+    msg = ['CON_MP must be a cell with L square matrices with the dimensiton equal to the number of brain regions (' int2str(br_number) ').'];
+end
+%%%% ¡gui!
+pr = PanelPropCell('EL', sub, 'PROP', SubjectCON_MP.CON_MP, ...
+    'TABLE_HEIGHT', s(40), ...
+    'XSLIDERSHOW', true, ...
+    'XSLIDERLABELS', sub.getCallback('ALAYERLABELS'), ...
+    'YSLIDERSHOW', false, ...
+    'ROWNAME', sub.get('BA').get('BR_DICT').getCallback('KEYS'), ...
+    'COLUMNNAME', sub.get('BA').get('BR_DICT').getCallback('KEYS'), ...
+    varargin{:});
+
+%% ¡tests!
+
+%%% ¡test!
+%%%% ¡name!
+GUI
+%%%% ¡probability!
+.01
+%%%% ¡code!
+im_ba = ImporterBrainAtlasXLS('FILE', 'aal90_atlas.xlsx');
+ba = im_ba.get('BA');
+
+gr = Group('SUB_CLASS', 'SubjectCON_MP', 'SUB_DICT', IndexedDictionary('IT_CLASS', 'SubjectCON_MP'));
+for i = 1:1:10
+    sub = SubjectCON_MP( ...
+        'ID', ['SUB CON_MP ' int2str(i)], ...
+        'LABEL', ['Subejct CON_MP ' int2str(i)], ...
+        'NOTES', ['Notes on subject CON_MP ' int2str(i)], ...
+        'BA', ba, ...
+        'L', 3, ...
+        'LAYERLABELS', {'L1' 'L2' 'L3'}, ...
+        'CON_MP', {rand(ba.get('BR_DICT').get('LENGTH')), rand(ba.get('BR_DICT').get('LENGTH')), rand(ba.get('BR_DICT').get('LENGTH'))} ...
+        );
+    sub.memorize('VOI_DICT').get('ADD', VOINumeric('ID', 'Age', 'V', 100 * rand()))
+    sub.memorize('VOI_DICT').get('ADD', VOICategoric('ID', 'Sex', 'CATEGORIES', {'Female', 'Male'}, 'V', randi(2, 1)))
+    gr.get('SUB_DICT').get('ADD', sub)
+end
+
+gui = GUIElement('PE', gr, 'CLOSEREQ', false);
+gui.get('DRAW')
+gui.get('SHOW')
+
+gui.get('CLOSE')
