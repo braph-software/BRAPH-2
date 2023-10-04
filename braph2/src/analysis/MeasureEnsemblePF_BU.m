ab806b91ed7cbf4043bcc9e8dc10fe2e8a9925e7
Anna Canal Garcia <31701548+annacanal@users.noreply.github.com>
2023-10-04 14:59:41 +0200

    Updates to global plots multiplex measures (#1585, AC)

--- braph2/src/analysis/MeasureEnsemblePF_BU.m
+++ braph2/src/analysis/MeasureEnsemblePF_BU.m
@@ -729,9 +729,12 @@ classdef MeasureEnsemblePF_BU < MeasureEnsemblePF
 			
 			switch prop
 				case 27 % MeasureEnsemblePF_BU.SETUP
-					me = pf.get('ME');
-					g = me.get('A').get('GRAPH_TEMPLATE');
-					
+                    me = pf.get('ME');
+                    if me.get('A').get('G_DICT').get('LENGTH')
+                        g = me.get('A').get('G_DICT').get('IT', 1);
+                    else
+                        g = me.get('A').get('GRAPH_TEMPLATE');
+                    end
 					x = g.get('ALAYERTICKS');
 					
 					nodes = pf.get('NODES');
--- braph2genesis/src/analysis/_MeasureEnsemblePF_NU.gen.m
+++ braph2genesis/src/analysis/_MeasureEnsemblePF_NU.gen.m
@@ -122,7 +122,11 @@ NOTES (metadata, string) are some specific notes about the panel figure nodal un
 SETUP (query, empty) calculates the group comparison figure value and stores it.
 %%%% ¡calculate!
 me = pf.get('ME');
-g = me.get('A').get('GRAPH_TEMPLATE');
+if me.get('A').get('G_DICT').get('LENGTH')
+    g = me.get('A').get('G_DICT').get('IT', 1);
+else
+    g = me.get('A').get('GRAPH_TEMPLATE');
+end
 
 x = g.get('ALAYERTICKS');
 
