cadf398261d34888f5a1890bc5ea555a6829af79
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-10-18 06:59:33 +0200

    Update _NoValue.gen.m

--- braph2genesis/src/ds/_NoValue.gen.m
+++ braph2genesis/src/ds/_NoValue.gen.m
@@ -6,12 +6,12 @@ Sealed=true
 
 %%% ¡description!
 NoValue is an element without values used to represent a value that has
-not been set (for properties of categories METADATA, PARAMETER, DATA, FIGURE or GUI) 
-or calculated (for properties of category RESULT, QUERY, EVANESCENT), 
-while it should not be used for properties of category CONSTANT.
-For computational efficiency, it is best to use only one instance of NV 
-using Element.getNoValue(), instead of creating new instances using the 
-constructor NoValue(). 
+ not been set (for properties of categories METADATA, PARAMETER, DATA, FIGURE or GUI) 
+ or calculated (for properties of category RESULT, QUERY, EVANESCENT), 
+ while it should not be used for properties of category CONSTANT.
+For computational efficiency, it is best to use only one instance of NoValue 
+ using Element.getNoValue(), instead of creating new instances using the 
+ constructor NoValue(). 
 No element can be a subclass of NoValue.
 
 %%% ¡seealso!
