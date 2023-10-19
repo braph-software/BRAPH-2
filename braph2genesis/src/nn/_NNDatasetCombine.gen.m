fa335ea9ccaf7a3c537a49bb1a8df5275834ae19
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-10-19 17:57:08 +0200

    Update _NNDatasetCombine.gen.m

--- braph2genesis/src/nn/_NNDatasetCombine.gen.m
+++ braph2genesis/src/nn/_NNDatasetCombine.gen.m
@@ -1,5 +1,5 @@
 %% ¡header!
-NNDatasetCombine < ConcreteElement (dco, splitter of a neural network data) combines neural network datasets.
+NNDatasetCombine < ConcreteElement (dco, combiner of a neural network data) combines neural network datasets.
 
 %%% ¡description!
 A dataset combiner (NNDatasetCombine) takes a list of neural network datasets and combines them into a single dataset. 
@@ -12,37 +12,37 @@ NNDataset, NNDatasetSplit
 %% ¡props_update!
 
 %%% ¡prop!
-ELCLASS (constant, string) is the class of the combination of neural networks datasets.
+ELCLASS (constant, string) is the class of the combiner of neural networks datasets.
 %%%% ¡default!
 'NNDatasetCombine'
 
 %%% ¡prop!
-NAME (constant, string) is the name of the combination of neural networks datasets.
+NAME (constant, string) is the name of the combiner of neural networks datasets.
 %%%% ¡default!
 'NNDatasetCombine'
 
 %%% ¡prop!
-DESCRIPTION (constant, string) is the description of the combination of neural networks datasets.
+DESCRIPTION (constant, string) is the description of the combiner of neural networks datasets.
 %%%% ¡default!
 'A dataset combiner (NNDatasetCombine) takes a list of neural network datasets and combines them into a single dataset. The resulting combined dataset contains all the unique datapoints from the input datasets, and any overlapping datapoints are excluded to ensure data consistency.'
 
 %%% ¡prop!
-TEMPLATE (parameter, item) is the template of the combination of neural networks datasets.
+TEMPLATE (parameter, item) is the template of the combiner of neural networks datasets.
 %%%% ¡settings!
 'NNDatasetCombine'
 
 %%% ¡prop!
-ID (data, string) is a few-letter code of the combination of neural networks datasets.
+ID (data, string) is a few-letter code of the combiner of neural networks datasets.
 %%%% ¡default!
 'NNDatasetCombine ID'
 
 %%% ¡prop!
-LABEL (metadata, string) is an extended label of the combination of neural networks datasets.
+LABEL (metadata, string) is an extended label of the combiner of neural networks datasets.
 %%%% ¡default!
 'NNDatasetCombine label'
 
 %%% ¡prop!
-NOTES (metadata, string) are some specific notes of the combination of neural networks datasets.
+NOTES (metadata, string) are some specific notes of the combiner of neural networks datasets.
 %%%% ¡default!
 'NNDatasetCombine notes'
     
