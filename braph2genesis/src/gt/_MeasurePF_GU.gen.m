db74e4d52461284c73e04d36b54cae49ee15cef1
Yu-Wei Chang <hejyuwei@gmail.com>
2024-08-31 21:46:17 +0200

    Update _MeasurePF_GU.gen.m

--- braph2genesis/src/gt/_MeasurePF_GU.gen.m
+++ braph2genesis/src/gt/_MeasurePF_GU.gen.m
@@ -198,6 +198,6 @@ true
 Remove Figures
 %%%% ¡code!
 warning('off', [BRAPH2.STR ':MeasurePF_GU'])
-assert(length(findall(0, 'type', 'figure')) == 1)
+assert(length(findall(0, 'type', 'figure')) == 5)
 delete(findall(0, 'type', 'figure'))
 warning('on', [BRAPH2.STR ':MeasurePF_GU'])
