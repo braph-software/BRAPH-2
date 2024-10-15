e3fe70349674cddae49c5ac9178c99a441fb947a
Yu-Wei Chang <hejyuwei@gmail.com>
2024-10-15 19:52:31 +0300

    Fix for warnings  (#1747)

--- braph2genesis/measures/_PathLength.gen.m
+++ braph2genesis/measures/_PathLength.gen.m
@@ -129,22 +129,23 @@ path_length_rule = m.get('RULE');
 distance = Distance('G', g).get('M');
 
 parfor li = 1:1:L
-    path_length_layer = zeros(N(1), 1);
+    node_number_layer = N(li);
+    path_length_layer = zeros(node_number_layer, 1);
     distance_layer = distance{li};
 
     switch lower(path_length_rule)
         case {'subgraphs'}
-            for u = 1:1:N
+            for u = 1:1:node_number_layer
                 Du = distance_layer(:, u);
                 path_length_layer(u) = mean(Du(Du~=Inf & Du~=0));
             end
         case {'mean'}
-            for u = 1:1:N
+            for u = 1:1:node_number_layer
                 Du = distance_layer(:, u);
                 path_length_layer(u) = mean(Du(Du~=0));
             end
         otherwise  % 'harmonic' 'default'
-            for u = 1:1:N
+            for u = 1:1:node_number_layer
                 Du = distance_layer(:, u);
                 path_length_layer(u) = harmmean(Du(Du~=0));
             end
